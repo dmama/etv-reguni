1a064b0665009d6740a484a786848c0effa556b3
Manuel Siggen <manuel.siggen@vd.ch>
2010-02-25 11:46:54 +0000

    Corrigé le test.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/3.5@38989 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web-it/src/test/java/ch/vd/uniregctb/webservice/batch/BatchClientTest.java
+++ unireg/web-it/src/test/java/ch/vd/uniregctb/webservice/batch/BatchClientTest.java
@@ -45,10 +45,11 @@ public class BatchClientTest extends WebitTest {
 
 		final List<String> list = client.getBatchNames();
 		assertNotNull(list);
-		assertEquals(37, list.size());
+		assertEquals(38, list.size());
 		int i = 0;
 		assertEquals("IT-BatchClientJob", list.get(i++));
 		assertEquals("IT-InContainerTestingJob", list.get(i++));
+		assertEquals("DumpAssujettissementsJob", list.get(i++));
 		assertEquals("DatabaseIndexerJob", list.get(i++));
 		assertEquals("OptimizeIndexJob", list.get(i++));
 		assertEquals("CheckCoherenceIndexerJob", list.get(i++));
