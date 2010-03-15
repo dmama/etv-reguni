5e01694ebbef3384494ade26a9719c6472c29469
Manuel Siggen <manuel.siggen@vd.ch>
2010-03-15 07:42:37 +0000

    Configuré cette version pour l'intégration.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/4.2@39707 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/scripts/build_and_deploy_integration.sh
+++ unireg/scripts/build_and_deploy_integration.sh
@@ -32,9 +32,9 @@ echo "Version: $sversion / $lversion"
 
 user=dsi_unireg@ssv0309v
 upDir=/ccv/data/dsi_unireg/uploads
-configDir=/ccv/data/dsi_unireg/cat_uniregI/app/unireg-is/config
-deployDir=/ccv/data/dsi_unireg/cat_uniregI/webapps/fiscalite#int-unireg-is
-workDir=/ccv/data/dsi_unireg/cat_uniregI/work/unireg-is
+configDir=/ccv/data/dsi_unireg/cat_uniregI/app/unireg/config
+deployDir=/ccv/data/dsi_unireg/cat_uniregI/webapps/fiscalite#int-unireg
+workDir=/ccv/data/dsi_unireg/cat_uniregI/work/unireg
 env=integration
 
 relFileOrig=uniregctb-release.zip
