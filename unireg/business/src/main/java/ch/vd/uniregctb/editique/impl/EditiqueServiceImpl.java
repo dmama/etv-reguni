1b23d04e8f9097d5bafb7179b115b843d45bc1d5
Jacob Deker <jacob.deker@vd.ch>
2010-03-17 10:22:21 +0000

    Création d'une instance séparée de JmsTemplate à chaque demande d'impression (et réception de document imprimé)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/3.5.3@39812 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/editique/impl/EditiqueServiceImpl.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/editique/impl/EditiqueServiceImpl.java
@@ -7,6 +7,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import javax.jms.BytesMessage;
+import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Message;
 
@@ -74,11 +75,12 @@ public final class EditiqueServiceImpl implements EditiqueService {
 
 	private FoldersService foldersService;
 
-	/** Queue d'envoi vers éditique */
-	private JmsTemplate editiqueOutput;
+	// ConnectionFactory pour les envois/réceptions JMS (éditique)
+	private ConnectionFactory jmsConnectionFactory;
 
-	/** Queue de réception de éditique */
-	private JmsTemplate editiqueInput;
+	private String queueEditiqueOutput;
+
+	private String queueEditiqueInput;
 
 	/** Temps d'attente (en secondes) du retour du document PDF / PCL lors d'une impression locale. */
 	private int receiveTimeout = 120;
@@ -189,9 +191,9 @@ public final class EditiqueServiceImpl implements EditiqueService {
 		}
 
 		try {
-			RequestSendMessageCreator  messageCreator = new RequestSendMessageCreator(xml, nomDocument, typeDocument, typeImpression, typeFormat, archive);
-
-			editiqueOutput.send(messageCreator);
+			final RequestSendMessageCreator messageCreator = new RequestSendMessageCreator(xml, nomDocument, typeDocument, typeImpression, typeFormat, archive);
+			final JmsTemplate output = new JmsTemplate(jmsConnectionFactory);
+			output.send(queueEditiqueOutput, messageCreator);
 			jmsMessageID = messageCreator.getMessage().getJMSMessageID();
 			if (LOGGER.isTraceEnabled()) {
 				LOGGER.trace("Message ID JMS :" + jmsMessageID + "--");
@@ -212,17 +214,17 @@ public final class EditiqueServiceImpl implements EditiqueService {
 	 */
 	public EditiqueResultat getDocument(String correlationID, boolean appliqueDelai) throws JMSException {
 
-		long timeout = (appliqueDelai ? receiveTimeout * 1000 : JmsTemplate.RECEIVE_TIMEOUT_NO_WAIT);
-		editiqueInput.setReceiveTimeout(timeout);
+		final JmsTemplate input = new JmsTemplate(jmsConnectionFactory);
+		final long timeout = (appliqueDelai ? receiveTimeout * 1000 : JmsTemplate.RECEIVE_TIMEOUT_NO_WAIT);
+		input.setReceiveTimeout(timeout);
 
 		// On n'extrait de la queue que le message demandé
-		Message message = editiqueInput.receiveSelected(DI_ID + " = '" + correlationID + "'");
+		final Message message = input.receiveSelected(queueEditiqueInput, DI_ID + " = '" + correlationID + "'");
 		if (message == null) {
 			return null;
 		}
 
-		EditiqueResultat resultat = createResultfromMessage(message);
-		return resultat;
+		return createResultfromMessage(message);
 	}
 
 	/**
@@ -589,24 +591,21 @@ public final class EditiqueServiceImpl implements EditiqueService {
 		}
 	}*/
 
-	public void setEditiqueOutput(JmsTemplate template) {
-		this.editiqueOutput = template;
-	}
-
-	public void setEditiqueInput(JmsTemplate template) {
-		this.editiqueInput = template;
-	}
-
-	public JmsTemplate getEditiqueOutput() {
-		return editiqueOutput;
-	}
-
-	public JmsTemplate getEditiqueInput() {
-		return editiqueInput;
-	}
 
 	public int getReceiveTimeout() {
 		return receiveTimeout;
+	}
+
+	public void setJmsConnectionFactory(ConnectionFactory jmsConnectionFactory) {
+		this.jmsConnectionFactory = jmsConnectionFactory;
+	}
+
+	public void setQueueEditiqueOutput(String queueEditiqueOutput) {
+		this.queueEditiqueOutput = queueEditiqueOutput;
+	}
+
+	public void setQueueEditiqueInput(String queueEditiqueInput) {
+		this.queueEditiqueInput = queueEditiqueInput;
 	}
 
 	public void setReceiveTimeout(int recieveTimeout) {
