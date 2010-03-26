e58a983a5a64d62cf303376e33a9f4bf1f1c75fa
Manuel Siggen <manuel.siggen@vd.ch>
2010-03-26 14:28:44 +0000

    Corrigé la calcul des périodes globales qui n'affichait rien pour les ranges où il n'y avait aucune donnée (en gros, les trous n'étaient pas visibles).
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@40337 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/java/ch/vd/uniregctb/tiers/TiersTimelineController.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/tiers/TiersTimelineController.java
@@ -1,13 +1,12 @@
 package ch.vd.uniregctb.tiers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import ch.vd.uniregctb.adresse.AdresseException;
-import ch.vd.uniregctb.adresse.TypeAdresseFiscale;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.apache.log4j.Logger;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -21,7 +20,9 @@ import ch.vd.registre.base.date.DateRange;
 import ch.vd.registre.base.date.DateRangeHelper;
 import ch.vd.registre.base.date.RegDate;
 import ch.vd.uniregctb.adresse.AdresseEnvoi;
+import ch.vd.uniregctb.adresse.AdresseException;
 import ch.vd.uniregctb.adresse.AdresseService;
+import ch.vd.uniregctb.adresse.TypeAdresseFiscale;
 import ch.vd.uniregctb.metier.assujettissement.Assujettissement;
 import ch.vd.uniregctb.metier.assujettissement.AssujettissementException;
 import ch.vd.uniregctb.metier.assujettissement.PeriodeImposition;
@@ -100,6 +101,62 @@ public class TiersTimelineController extends AbstractTiersController {
 	}
 
 	/**
+	 * A partir d'une liste de ranges (qui peuvent se recouvrir), détermine la liste triées par ordre croissant des dates de début et de fin
+	 * des ranges. Chaque date n'apparaît qu'une fois dans la liste.
+	 * <p>
+	 * <b>Note:</b> Si au moins un des ranges possède une date de début nulle, la première date de la liste est nulle. De même, si au moins
+	 * un des ranges possède une date de fin nulle, la dernière date de la liste est nulle.
+	 *
+	 * <pre>
+	 * Ranges d'entrées:
+	 * +----------------------+                           +----------------+
+	 * ¦x                    y¦                           ¦u              v¦
+	 * +----------------------+                           +----------------+
+	 *                      +----------------------+              +----------------+
+	 *                      ¦y                    z¦              ¦r              s¦
+	 *                      +----------------------+              +----------------+
+	 * .
+	 * Dates de sortie: x, y, y+1, z+1, u, r, v+1, s+1
+	 * </pre>
+	 */
+	public static List<RegDate> extractBoundaries(Collection<? extends DateRange> ranges) {
+
+		boolean dateDebutNull = false;
+		boolean dateFinNull = false;
+
+		// Détermination de l'ensemble des dates
+		final SortedSet<RegDate> dates = new TreeSet<RegDate>();
+		for (DateRange r : ranges) {
+			final RegDate dateDebut = r.getDateDebut();
+			if (dateDebut == null) {
+				dateDebutNull = true;
+			}
+			else {
+				dates.add(dateDebut);
+			}
+			RegDate dateFin = r.getDateFin();
+			if (dateFin == null) {
+				dateFinNull = true;
+			}
+			else {
+				dates.add(dateFin.getOneDayAfter());
+			}
+		}
+
+		// Transforme l'ensemble en liste + traitement des valeurs nulles
+		List<RegDate> list = new ArrayList<RegDate>(dates);
+		if (dateDebutNull) {
+			list.add(0, null);
+		}
+		if (dateFinNull) {
+			list.add(null);
+		}
+
+		return list;
+	}
+
+
+	/**
 	 * Remplit les structures de données nécessaire à l'affichage de l'historique du tiers
 	 */
 	private void fillTimeline(TiersTimelineView bean) {
@@ -164,7 +221,16 @@ public class TiersTimelineController extends AbstractTiersController {
 		ranges.addAll(assujettissements);
 		ranges.addAll(periodesImposition);
 		ranges.addAll(forsGestion);
-		final List<DateRange> periodes = DateRangeHelper.projectRanges(ranges);
+		
+		final List<DateRange> periodes = new ArrayList<DateRange>();
+		final List<RegDate> boundaries = extractBoundaries(ranges);
+		RegDate previous = null;
+		for (RegDate current : boundaries) {
+			if (previous != null) {
+				periodes.add(new DateRangeHelper.Range(previous, (current == null ? null : current.getOneDayBefore())));
+			}
+			previous = current;
+		}
 
 		final Table table = bean.getTable();
 		table.setPeriodes(periodes);
