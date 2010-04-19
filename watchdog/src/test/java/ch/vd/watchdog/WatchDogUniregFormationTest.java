4ccffc44e52220ed70972b4c5d5fd5128385cc8a
Manuel Siggen <manuel.siggen@vd.ch>
2010-04-19 09:20:13 +0000

    Renommé le test 'validation-IS' en 'formation'.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@41363 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- watchdog/src/test/java/ch/vd/watchdog/WatchDogUniregFormationTest.java
+++ watchdog/src/test/java/ch/vd/watchdog/WatchDogUniregFormationTest.java
@@ -2,21 +2,20 @@ package ch.vd.watchdog;
 
 import java.net.URL;
 
-import org.apache.log4j.Logger;
-
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.apache.log4j.Logger;
 
 /**
  * Teste que les différents déploiements de l'application Unireg dans les différents environnements (intégration, validation, formation,
  * préproduction) sont bien accessibles.
  */
-public class WatchDogUniregValidationTest extends WatchDogTest {
+public class WatchDogUniregFormationTest extends WatchDogTest {
 
-	private static final Logger LOGGER = Logger.getLogger(WatchDogUniregValidationTest.class);
+	private static final Logger LOGGER = Logger.getLogger(WatchDogUniregFormationTest.class);
 
-	public void testValidation() throws Exception {
-		LOGGER.info("Vérification de Unireg en validation...");
-		HtmlPage page = (HtmlPage) webClient.getPage(new URL("https://validation.portail.etat-de-vaud.ch/fiscalite/val-unireg/"));
+	public void testFormation() throws Exception {
+		LOGGER.info("Vérification de Unireg en formation...");
+		HtmlPage page = (HtmlPage) webClient.getPage(new URL("https://validation.portail.etat-de-vaud.ch/fiscalite/form-unireg/"));
 		assertNotNull(page);
 		String titre = page.getTitleText();
 		assertTrue(titre.equalsIgnoreCase("Recherche des tiers") || titre.equalsIgnoreCase("Sélection de l'OID de travail"));
