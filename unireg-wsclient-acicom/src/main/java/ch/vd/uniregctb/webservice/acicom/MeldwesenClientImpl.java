2a955b6c52b2e688b634731faf2db95707998dd9
Baba Issa Ngom <baba-issa.ngom@vd.ch>
2010-04-27 08:45:05 +0000

    [UNIREG-1938] Projet permettant de mettre a disposition le ws de visualtion des messages Acicom
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/4.3@41748 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

---
+++ unireg-wsclient-acicom/src/main/java/ch/vd/uniregctb/webservice/acicom/MeldwesenClientImpl.java
@@ -0,0 +1,110 @@
+package ch.vd.uniregctb.webservice.acicom;
+
+import javax.wsdl.Service;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.WebServiceException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.util.ResourceUtils;
+
+import ch.vd.dfin.acicom.web.services.meldewesen.impl.AciComException_Exception;
+import ch.vd.dfin.acicom.web.services.meldewesen.impl.ContenuMessage;
+import ch.vd.dfin.acicom.web.services.meldewesen.impl.DocumentNotFoundException_Exception;
+import ch.vd.dfin.acicom.web.services.meldewesen.impl.MeldewesenConsultationServiceImpl;
+import ch.vd.dfin.acicom.web.services.meldewesen.impl.MeldewesenConsultationServiceImplPortType;
+import ch.vd.dfin.acicom.web.services.meldewesen.impl.RecupererContenuMessage;
+
+
+public class MeldwesenClientImpl implements MeldewesenClient {
+
+	private static final Logger LOGGER = Logger.getLogger(MeldwesenClientImpl.class);
+
+	private MeldewesenConsultationServiceImplPortType service;
+
+	private String serviceUrl;
+
+	private String username;
+
+	private String password;
+
+	public void setServiceUrl(String serviceUrl) {
+		this.serviceUrl = serviceUrl;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public ContenuMessage recupererMessage(RecupererContenuMessage infosMessage) throws AciComException_Exception, DocumentNotFoundException_Exception {
+		initService();
+
+		ContenuMessage contenuMessage;
+		final String businessId = infosMessage.getMessageId();
+		try {
+
+			contenuMessage = this.service.recupererContenuMessage(businessId);
+
+		}
+		catch (WebServiceException e) {
+			throw new AciComException_Exception(e.getMessage(), e);
+		}
+
+
+		if (contenuMessage != null) {
+			final String extension = contenuMessage.getExtension();
+			LOGGER.info("Type de document : " + extension);
+
+			try {
+				FileOutputStream fos = new FileOutputStream("messages."+extension);
+				fos.write(contenuMessage.getContent());
+				fos.close();
+			}
+			catch (IOException e) {
+				throw new AciComException_Exception(e.getMessage(), e);
+			}
+			return contenuMessage;
+		}
+		else {
+
+			throw new DocumentNotFoundException_Exception("Le message ayant le business ID"+businessId+"n'a pas été trouvé chez ACICOM");
+
+		}
+
+
+	}
+
+	public void initService() {
+		if (this.service == null) {
+			URL wsdlUrl;
+			try {
+				wsdlUrl = ResourceUtils.getURL("classpath:serviceMeldewesen-v1.1.wsdl");
+			}
+			catch (FileNotFoundException e) {
+				throw new RuntimeException(e);
+			}
+			MeldewesenConsultationServiceImpl MeldewesenService = new MeldewesenConsultationServiceImpl(wsdlUrl);
+			this.service = MeldewesenService.getMeldewesenConsultationServiceImplPort();
+			Map<String, Object> context = ((BindingProvider) service).getRequestContext();
+			if (StringUtils.isNotBlank(this.username)) {
+				context.put(BindingProvider.USERNAME_PROPERTY, this.username);
+				context.put(BindingProvider.PASSWORD_PROPERTY, this.password);
+			}
+			context.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, this.serviceUrl);
+		}
+	}
+}
+
