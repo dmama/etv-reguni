e916a588c50057f46d57d9ddf3e7344f389ad803
Jacob Deker <jacob.deker@vd.ch>
2010-04-28 13:09:25 +0000

    [UNIREG-2340] Ajouté l'interdiction d'annuler un tiers pour lequel il existe encore des DI/LR non-annulées couvrant une période postérieure à la date d'annulation souhaitée.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/4.3@41863 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/activation/ActivationServiceImpl.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/activation/ActivationServiceImpl.java
@@ -1,10 +1,12 @@
 package ch.vd.uniregctb.activation;
 
+import java.util.Date;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Set;
 
 import ch.vd.registre.base.date.RegDate;
-import ch.vd.uniregctb.common.AuthenticationHelper;
+import ch.vd.uniregctb.declaration.Declaration;
 import ch.vd.uniregctb.tiers.AnnuleEtRemplace;
 import ch.vd.uniregctb.tiers.Contribuable;
 import ch.vd.uniregctb.tiers.DebiteurPrestationImposable;
@@ -19,6 +21,7 @@ import ch.vd.uniregctb.tiers.TacheDAO;
 import ch.vd.uniregctb.tiers.Tiers;
 import ch.vd.uniregctb.tiers.TiersService;
 import ch.vd.uniregctb.type.MotifFor;
+import ch.vd.uniregctb.type.TypeEtatTache;
 import ch.vd.uniregctb.type.TypeRapportEntreTiers;
 
 public class ActivationServiceImpl implements ActivationService {
@@ -40,31 +43,51 @@ public class ActivationServiceImpl implements ActivationService {
 	 * @param tiers
 	 * @param dateAnnulation
 	 */
-	public void annuleTiers(Tiers tiers, RegDate dateAnnulation) {
-		tiers.setAnnulationDate(dateAnnulation.asJavaDate());
-		if (AuthenticationHelper.getAuthentication() != null) {
-			tiers.setAnnulationUser(AuthenticationHelper.getCurrentPrincipal());
-		}
-		else {
-			tiers.setAnnulationUser("INCONNU");
-		}
-		if (tiers instanceof Contribuable) {
-			Contribuable contribuable = (Contribuable) tiers;
-			tiersService.closeAllForsFiscaux(contribuable, dateAnnulation, MotifFor.ANNULATION);
-			List<Tache> taches = tacheDAO.find(contribuable.getNumero().longValue());
-			for (Tache tache : taches) {
-				tache.setAnnulationDate(dateAnnulation.asJavaDate());
-				if (AuthenticationHelper.getAuthentication() != null) {
-					tache.setAnnulationUser(AuthenticationHelper.getCurrentPrincipal());
+	public void annuleTiers(Tiers tiers, RegDate dateAnnulation) throws ActivationServiceException {
+
+		// [UNIREG-2340] On ne doit pas pouvoir annuler un tiers s'il possède des déclarations non-annulées qui couvrent des périodes postérieures à l'annulation
+		final RegDate seuilPourDeclarationBloquante = dateAnnulation.getOneDayAfter();
+		final List<Declaration> declarations = tiers.getDeclarationsSorted();
+		if (declarations != null && declarations.size() > 0) {
+
+			// puisqu'elles sont triées, il suffit de trouver la dernière déclaration non annulée et de la tester
+			Declaration nonAnnulee = null;
+			final ListIterator<Declaration> iterator = declarations.listIterator(declarations.size());
+			while (iterator.hasPrevious()) {
+				final Declaration candidate = iterator.previous();
+				if (!candidate.isAnnule()) {
+					nonAnnulee = candidate;
+					break;
 				}
-				else {
-					tache.setAnnulationUser("INCONNU");
+			}
+
+			// si toutes sont annulées, pas de souci...
+			if (nonAnnulee != null) {
+				final RegDate dateFin = nonAnnulee.getDateFin();
+				if (seuilPourDeclarationBloquante.isBeforeOrEqual(dateFin)) {
+					// problème...
+					throw new ActivationServiceException("Il est interdit d'annuler un tiers pour lequel il existe encore des déclarations couvrant une période postérieure à la date d'annulation souhaitée.");
 				}
 			}
 		}
+
+		final Date dateOperation = dateAnnulation.asJavaDate();
+		tiers.annulerPourDate(dateOperation);
+
+		if (tiers instanceof Contribuable) {
+			final Contribuable contribuable = (Contribuable) tiers;
+			tiersService.closeAllForsFiscaux(contribuable, dateAnnulation, MotifFor.ANNULATION);
+			final List<Tache> taches = tacheDAO.find(contribuable.getNumero());
+			for (Tache tache : taches) {
+				if (tache.getEtat() != TypeEtatTache.TRAITE && tache.getAnnulationDate() == null) {
+					tache.annulerPourDate(dateOperation);
+				}
+			}
+		}
+
 		if (tiers instanceof DebiteurPrestationImposable) {
-			DebiteurPrestationImposable debiteur = (DebiteurPrestationImposable) tiers;
-			ForDebiteurPrestationImposable forDebiteurPrestationImposable =debiteur.getForDebiteurPrestationImposableAt(dateAnnulation);
+			final DebiteurPrestationImposable debiteur = (DebiteurPrestationImposable) tiers;
+			final ForDebiteurPrestationImposable forDebiteurPrestationImposable =debiteur.getForDebiteurPrestationImposableAt(dateAnnulation);
 			tiersService.closeForDebiteurPrestationImposable(debiteur, forDebiteurPrestationImposable, dateAnnulation);
 		}
 	}
@@ -75,10 +98,10 @@ public class ActivationServiceImpl implements ActivationService {
 	 * @param tiersRemplacant
 	 * @param dateRemplacement
 	 */
-	public void remplaceTiers(Tiers tiersRemplace, Tiers tiersRemplacant, RegDate dateRemplacement) {
+	public void remplaceTiers(Tiers tiersRemplace, Tiers tiersRemplacant, RegDate dateRemplacement) throws ActivationServiceException {
 		annuleTiers(tiersRemplace, dateRemplacement);
 
-		AnnuleEtRemplace annuleEtRemplace = new AnnuleEtRemplace(dateRemplacement, null, tiersRemplace, tiersRemplacant);
+		final AnnuleEtRemplace annuleEtRemplace = new AnnuleEtRemplace(dateRemplacement, null, tiersRemplace, tiersRemplacant);
 		tiersService.addRapport(annuleEtRemplace, tiersRemplace, tiersRemplacant);
 	}
 
@@ -89,38 +112,39 @@ public class ActivationServiceImpl implements ActivationService {
 	 * @param dateReactivation
 	 */
 	public void reactiveTiers(Tiers tiers, RegDate dateReactivation) {
-		tiers.setAnnulationDate(null);
-		tiers.setAnnulationUser(null);
-		Set<ForFiscal> forsFiscaux = tiers.getForsFiscaux();
+		tiers.setAnnule(false);
+
+		final Set<ForFiscal> forsFiscaux = tiers.getForsFiscaux();
 		for (ForFiscal forFiscal : forsFiscaux) {
 			if (forFiscal.getDateFin() != null) {
 				if (tiers instanceof DebiteurPrestationImposable) {
-					DebiteurPrestationImposable debiteur = (DebiteurPrestationImposable) tiers;
+					final DebiteurPrestationImposable debiteur = (DebiteurPrestationImposable) tiers;
 					tiersService.openForDebiteurPrestationImposable(debiteur, dateReactivation, forFiscal.getNumeroOfsAutoriteFiscale(), forFiscal.getTypeAutoriteFiscale());
 				}
 				if (tiers instanceof Contribuable) {
-					Contribuable contribuable = (Contribuable) tiers;
+					final Contribuable contribuable = (Contribuable) tiers;
 					if (forFiscal instanceof ForFiscalAutreElementImposable) {
-						ForFiscalAutreElementImposable forFiscalAutreElementImposable = (ForFiscalAutreElementImposable) forFiscal;
+						final ForFiscalAutreElementImposable forFiscalAutreElementImposable = (ForFiscalAutreElementImposable) forFiscal;
 						tiersService.openForFiscalAutreElementImposable(contribuable, forFiscalAutreElementImposable.getGenreImpot(), dateReactivation, forFiscalAutreElementImposable.getMotifRattachement(), forFiscalAutreElementImposable.getNumeroOfsAutoriteFiscale(), forFiscalAutreElementImposable.getTypeAutoriteFiscale(), MotifFor.REACTIVATION);
 					}
 					if (forFiscal instanceof ForFiscalAutreImpot) {
 						tiersService.openForFiscalAutreImpot(contribuable, forFiscal.getGenreImpot(), dateReactivation, forFiscal.getNumeroOfsAutoriteFiscale(), forFiscal.getTypeAutoriteFiscale());
 					}
 					if (forFiscal instanceof ForFiscalPrincipal) {
-						ForFiscalPrincipal forFiscalPrincipal = (ForFiscalPrincipal) forFiscal;
+						final ForFiscalPrincipal forFiscalPrincipal = (ForFiscalPrincipal) forFiscal;
 						tiersService.openForFiscalPrincipal(contribuable, dateReactivation, forFiscalPrincipal.getMotifRattachement(), forFiscalPrincipal.getNumeroOfsAutoriteFiscale(), forFiscalPrincipal.getTypeAutoriteFiscale(), forFiscalPrincipal.getModeImposition(), MotifFor.REACTIVATION, true);
 					}
 					if (forFiscal instanceof ForFiscalSecondaire) {
-						ForFiscalSecondaire forFiscalSecondaire = (ForFiscalSecondaire) forFiscal;
+						final ForFiscalSecondaire forFiscalSecondaire = (ForFiscalSecondaire) forFiscal;
 						tiersService.openForFiscalSecondaire(contribuable, forFiscalSecondaire.getGenreImpot(), dateReactivation, null, forFiscalSecondaire.getMotifRattachement(), forFiscalSecondaire.getNumeroOfsAutoriteFiscale(), forFiscalSecondaire.getTypeAutoriteFiscale(), MotifFor.REACTIVATION, null);
 					}
 				}
 			}
 		}
-		AnnuleEtRemplace annuleEtRemplace = (AnnuleEtRemplace) tiers.getRapportObjetValidAt(dateReactivation, TypeRapportEntreTiers.ANNULE_ET_REMPLACE);
+
+		final AnnuleEtRemplace annuleEtRemplace = (AnnuleEtRemplace) tiers.getRapportObjetValidAt(dateReactivation, TypeRapportEntreTiers.ANNULE_ET_REMPLACE);
 		if (annuleEtRemplace != null) {
-			annuleEtRemplace.setDateFin(dateReactivation.addDays(-1));
+			annuleEtRemplace.setDateFin(dateReactivation.getOneDayBefore());
 		}
 	}
 
--- unireg/business/src/test/java/ch/vd/uniregctb/activation/ActivationServiceTest.java
+++ unireg/business/src/test/java/ch/vd/uniregctb/activation/ActivationServiceTest.java
@@ -1,23 +1,33 @@
 package ch.vd.uniregctb.activation;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNull;
+import java.util.List;
 
+import junit.framework.Assert;
 import org.junit.Test;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
 
 import ch.vd.registre.base.date.RegDate;
 import ch.vd.uniregctb.common.BusinessTest;
+import ch.vd.uniregctb.declaration.Declaration;
+import ch.vd.uniregctb.declaration.ModeleDocument;
+import ch.vd.uniregctb.declaration.PeriodeFiscale;
+import ch.vd.uniregctb.interfaces.model.mock.MockCollectiviteAdministrative;
+import ch.vd.uniregctb.interfaces.model.mock.MockCommune;
 import ch.vd.uniregctb.tiers.AnnuleEtRemplace;
 import ch.vd.uniregctb.tiers.ForFiscalPrincipal;
+import ch.vd.uniregctb.tiers.PersonnePhysique;
 import ch.vd.uniregctb.tiers.Tache;
 import ch.vd.uniregctb.tiers.TacheDAO;
 import ch.vd.uniregctb.tiers.Tiers;
 import ch.vd.uniregctb.tiers.TiersService;
 import ch.vd.uniregctb.type.MotifFor;
+import ch.vd.uniregctb.type.Sexe;
+import ch.vd.uniregctb.type.TypeContribuable;
+import ch.vd.uniregctb.type.TypeDocument;
 import ch.vd.uniregctb.type.TypeRapportEntreTiers;
 
-public class ActivationServiceTest extends BusinessTest{
+public class ActivationServiceTest extends BusinessTest {
 
 	private final static String DB_UNIT_DATA_FILE = "classpath:ch/vd/uniregctb/activation/ActivationServiceTest.xml";
 
@@ -39,11 +49,11 @@ public class ActivationServiceTest extends BusinessTest{
 		Tiers tiers = tiersService.getTiers(12600003);
 		RegDate dateAnnulation = RegDate.get(2010, 1, 1);
 		activationService.annuleTiers(tiers, dateAnnulation);
-		assertEquals(dateAnnulation, RegDate.get(tiers.getAnnulationDate()));
+		Assert.assertEquals(dateAnnulation, RegDate.get(tiers.getAnnulationDate()));
 		ForFiscalPrincipal forFiscalPrincipal = tiers.getForFiscalPrincipalAt(dateAnnulation);
-		assertEquals(MotifFor.ANNULATION, forFiscalPrincipal.getMotifFermeture());
+		Assert.assertEquals(MotifFor.ANNULATION, forFiscalPrincipal.getMotifFermeture());
 		Tache tache = tacheDAO.get(Long.valueOf(1));
-		assertEquals(dateAnnulation, RegDate.get(tache.getAnnulationDate()));
+		Assert.assertEquals(dateAnnulation, RegDate.get(tache.getAnnulationDate()));
 	}
 
 	@Test
@@ -53,15 +63,15 @@ public class ActivationServiceTest extends BusinessTest{
 		Tiers tiersRemplacant = tiersService.getTiers(12600009);
 		RegDate dateRemplacement = RegDate.get(2010, 1, 1);
 		activationService.remplaceTiers(tiersRemplace, tiersRemplacant, dateRemplacement);
-		assertEquals(dateRemplacement, RegDate.get(tiersRemplace.getAnnulationDate()));
+		Assert.assertEquals(dateRemplacement, RegDate.get(tiersRemplace.getAnnulationDate()));
 		ForFiscalPrincipal forFiscalPrincipal = tiersRemplace.getForFiscalPrincipalAt(dateRemplacement);
-		assertEquals(MotifFor.ANNULATION, forFiscalPrincipal.getMotifFermeture());
+		Assert.assertEquals(MotifFor.ANNULATION, forFiscalPrincipal.getMotifFermeture());
 		AnnuleEtRemplace annuleEtRemplace = (AnnuleEtRemplace) tiersRemplacant.getRapportObjetValidAt(dateRemplacement, TypeRapportEntreTiers.ANNULE_ET_REMPLACE);
-		assertNotNull(annuleEtRemplace);
+		Assert.assertNotNull(annuleEtRemplace);
 		annuleEtRemplace = (AnnuleEtRemplace) tiersRemplace.getRapportSujetValidAt(dateRemplacement, TypeRapportEntreTiers.ANNULE_ET_REMPLACE);
-		assertNotNull(annuleEtRemplace);
+		Assert.assertNotNull(annuleEtRemplace);
 		Tache tache = tacheDAO.get(Long.valueOf(1));
-		assertEquals(dateRemplacement, RegDate.get(tache.getAnnulationDate()));
+		Assert.assertEquals(dateRemplacement, RegDate.get(tache.getAnnulationDate()));
 	}
 
 	@Test
@@ -70,10 +80,122 @@ public class ActivationServiceTest extends BusinessTest{
 		Tiers tiersAnnule = tiersService.getTiers(12600004);
 		RegDate dateReactivation = RegDate.get(2010, 1, 1);
 		activationService.reactiveTiers(tiersAnnule, dateReactivation);
-		assertNull(tiersAnnule.getAnnulationDate());
-		assertNull(tiersAnnule.getAnnulationUser());
+		Assert.assertNull(tiersAnnule.getAnnulationDate());
+		Assert.assertNull(tiersAnnule.getAnnulationUser());
 		ForFiscalPrincipal forFiscalPrincipal = tiersAnnule.getForFiscalPrincipalAt(dateReactivation);
-		assertEquals(MotifFor.REACTIVATION, forFiscalPrincipal.getMotifOuverture());
+		Assert.assertEquals(MotifFor.REACTIVATION, forFiscalPrincipal.getMotifOuverture());
 	}
 
+	@Test
+	public void testAnnulationInterditePourCauseDeDeclarationUlterieure() throws Exception {
+
+		// mise en place
+		final long ppId = (Long) doInNewTransactionAndSession(new TransactionCallback() {
+			public Long doInTransaction(TransactionStatus status) {
+				final PersonnePhysique pp = addNonHabitant("Achille", "Talon", date(1948, 1, 26), Sexe.MASCULIN);
+				final PeriodeFiscale pf2007 = addPeriodeFiscale(2007);
+				final PeriodeFiscale pf2008 = addPeriodeFiscale(2008);
+				final ModeleDocument modele2007 = addModeleDocument(TypeDocument.DECLARATION_IMPOT_VAUDTAX, pf2007);
+				final ModeleDocument modele2008 = addModeleDocument(TypeDocument.DECLARATION_IMPOT_VAUDTAX, pf2008);
+				addCollAdm(MockCollectiviteAdministrative.CEDI);
+				addDeclarationImpot(pp, pf2007, date(2007, 1, 1), date(2007, 12, 31), TypeContribuable.VAUDOIS_ORDINAIRE, modele2007);
+				addDeclarationImpot(pp, pf2008, date(2008, 1, 1), date(2008, 12, 31), TypeContribuable.VAUDOIS_ORDINAIRE, modele2008);
+				addForPrincipal(pp, date(2007, 1, 1), MotifFor.ARRIVEE_HS, MockCommune.Lausanne);
+				return pp.getNumero();
+			}
+		});
+
+		// on ne doit pas pouvoir l'annuler avant le 31.12.2008
+		doInNewTransactionAndSession(new TransactionCallback() {
+			public Object doInTransaction(TransactionStatus status) {
+				final PersonnePhysique pp = (PersonnePhysique) tiersService.getTiers(ppId);
+				try {
+					activationService.annuleTiers(pp, date(2008, 12, 30));
+					Assert.fail("Il devrait être interdit d'annuler le tiers alors qu'il a encore des DI ouvertes");
+				}
+				catch (ActivationServiceException e) {
+					Assert.assertEquals("Il est interdit d'annuler un tiers pour lequel il existe encore des déclarations couvrant une période postérieure à la date d'annulation souhaitée.", e.getMessage());
+				}
+				return null;
+			}
+		});
+
+		// mais on doit pouvoir l'annuler après le 31.12.2008 (y compris le jour même)
+		doInNewTransactionAndSession(new TransactionCallback() {
+			public Object doInTransaction(TransactionStatus status) {
+				final PersonnePhysique pp = (PersonnePhysique) tiersService.getTiers(ppId);
+				try {
+					activationService.annuleTiers(pp, date(2008, 12, 31));
+				}
+				catch (ActivationServiceException e) {
+					throw new RuntimeException(e);
+				}
+
+				final ForFiscalPrincipal ff = pp.getForFiscalPrincipalAt(date(2008, 12, 31));
+				Assert.assertNotNull(ff);
+				Assert.assertEquals(date(2008, 12, 31), ff.getDateFin());
+				Assert.assertEquals(MotifFor.ANNULATION, ff.getMotifFermeture());
+				return null;
+			}
+		});
+	}
+
+	@Test
+	public void testAnnulationAutoriseeAvecDeclarationUlterieureAnnulee() throws Exception {
+
+		// mise en place
+		final long ppId = (Long) doInNewTransactionAndSession(new TransactionCallback() {
+			public Long doInTransaction(TransactionStatus status) {
+				final PersonnePhysique pp = addNonHabitant("Achille", "Talon", date(1948, 1, 26), Sexe.MASCULIN);
+				final PeriodeFiscale pf2007 = addPeriodeFiscale(2007);
+				final PeriodeFiscale pf2008 = addPeriodeFiscale(2008);
+				final ModeleDocument modele2007 = addModeleDocument(TypeDocument.DECLARATION_IMPOT_VAUDTAX, pf2007);
+				final ModeleDocument modele2008 = addModeleDocument(TypeDocument.DECLARATION_IMPOT_VAUDTAX, pf2008);
+				addCollAdm(MockCollectiviteAdministrative.CEDI);
+				addDeclarationImpot(pp, pf2007, date(2007, 1, 1), date(2007, 12, 31), TypeContribuable.VAUDOIS_ORDINAIRE, modele2007);
+				addDeclarationImpot(pp, pf2008, date(2008, 1, 1), date(2008, 12, 31), TypeContribuable.VAUDOIS_ORDINAIRE, modele2008);
+				addForPrincipal(pp, date(2007, 1, 1), MotifFor.ARRIVEE_HS, MockCommune.Lausanne);
+				return pp.getNumero();
+			}
+		});
+
+		// on ne doit pas pouvoir l'annuler avant le 31.12.2008
+		doInNewTransactionAndSession(new TransactionCallback() {
+			public Object doInTransaction(TransactionStatus status) {
+				final PersonnePhysique pp = (PersonnePhysique) tiersService.getTiers(ppId);
+				try {
+					activationService.annuleTiers(pp, date(2008, 12, 30));
+					Assert.fail("Il devrait être interdit d'annuler le tiers alors qu'il a encore des DI ouvertes");
+				}
+				catch (ActivationServiceException e) {
+					Assert.assertEquals("Il est interdit d'annuler un tiers pour lequel il existe encore des déclarations couvrant une période postérieure à la date d'annulation souhaitée.", e.getMessage());
+				}
+				return null;
+			}
+		});
+
+		// mais on doit pouvoir l'annuler si la DI 2008 est annulée
+		doInNewTransactionAndSession(new TransactionCallback() {
+			public Object doInTransaction(TransactionStatus status) {
+				final PersonnePhysique pp = (PersonnePhysique) tiersService.getTiers(ppId);
+				final List<Declaration> declarations2008 = pp.getDeclarationForPeriode(2008);
+				Assert.assertNotNull(declarations2008);
+				Assert.assertEquals(1, declarations2008.size());
+				declarations2008.get(0).setAnnule(true);
+
+				try {
+					activationService.annuleTiers(pp, date(2008, 12, 30));
+				}
+				catch (ActivationServiceException e) {
+					throw new RuntimeException(e);
+				}
+
+				final ForFiscalPrincipal ff = pp.getForFiscalPrincipalAt(date(2008, 12, 30));
+				Assert.assertNotNull(ff);
+				Assert.assertEquals(date(2008, 12, 30), ff.getDateFin());
+				Assert.assertEquals(MotifFor.ANNULATION, ff.getMotifFermeture());
+				return null;
+			}
+		});
+	}
 }
