cbbb02208ac860b89e46ae0d7054406a96a58236
Manuel Siggen <manuel.siggen@vd.ch>
2010-05-03 11:18:22 +0000

    Supprimé ces répertoires plus utilisés.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@42138 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- warsync/lib/install2localrepo.bat
+++
@@ -1,2 +0,0 @@
-
-mvn install:install-file -Dfile=jnotify-0.91.jar -DgroupId=net.contentobjects.jnotify -DartifactId=jnotify -Dversion=0.91 -Dpackaging=jar
--- warsync/run.cmd
+++
@@ -1,3 +0,0 @@
-
-set CP=lib/jnotify-0.91.jar;lib/log4j-1.2.14.jar;target/warsync-1.0-SNAPSHOT.jar
-java -Djava.library.path=lib -cp %CP% ch.vd.warsync.WarSyncMain
