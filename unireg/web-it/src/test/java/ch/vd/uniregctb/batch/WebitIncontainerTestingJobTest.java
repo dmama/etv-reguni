024dc290fbbfd5f823c334a4a68852a6e485e679
Manuel Siggen <manuel.siggen@vd.ch>
2010-06-03 09:32:08 +0000

    Refactoré le test 'WebitIncontainerTestingJobTest' pour qu'il utilise le batch runner pour démarrer un batch, plutôt que de simuler un clic sur l'interface web.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@43661 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web-it/src/test/java/ch/vd/uniregctb/batch/WebitIncontainerTestingJobTest.java
+++ unireg/web-it/src/test/java/ch/vd/uniregctb/batch/WebitIncontainerTestingJobTest.java
@@ -1,50 +1,46 @@
 package ch.vd.uniregctb.batch;
 
-import com.gargoylesoftware.htmlunit.html.HtmlButtonInput;
-import com.gargoylesoftware.htmlunit.html.HtmlDivision;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import ch.vd.uniregctb.common.WebitTest;
+import ch.vd.uniregctb.ubr.BatchRunnerClient;
+import ch.vd.uniregctb.webservices.batch.JobDefinition;
+import ch.vd.uniregctb.webservices.batch.JobStatut;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+/**
+ * Ce test permet de vérifier que l'exécution du test IcEvtCivilNaissanceTest est le même entre le context des tests unitaires et le context sous Tomcat.
+ */
 public class WebitIncontainerTestingJobTest extends WebitTest {
+
+	private static final Logger LOGGER = Logger.getLogger(WebitIncontainerTestingJobTest.class);
+
+	private static BatchRunnerClient client;
 
 	@Override
 	public void onSetUp() throws Exception {
 		super.onSetUp();
-		webClient.setJavaScriptEnabled(true);
+		if (client == null) {
+			LOGGER.info("Connecting to: " + batchUrl + " with user = " + username);
+
+			client = new BatchRunnerClient(batchUrl, username, password);
+		}
 	}
 
-	@Test(timeout = 30000) // avec le temps d'initialisation d'Unireg (c'est un des premiers tests runnés), on a vu des exécutions qui prenaient 18 secondes.
+	@Test(timeout = 60000)
+	// avec le temps d'initialisation d'Unireg (c'est un des premiers tests runnés), on a vu des exécutions qui prenaient 18 secondes.
 	public void testRunBatch() throws Exception {
 
 		final String jobName = "IT-InContainerTestingJob";
-		// Démarre le job IT-InContainerTestingJob
-		{
-			final HtmlPage page = getHtmlPage("/admin/batch.do");
-			final HtmlButtonInput button = (HtmlButtonInput) page.getHtmlElementById("start" + jobName);
-			assertNotNull(button);
-			button.click();
-		}
 
-		// Le démarrage du batch devrait être immédiat, mais on attend 2 secondes par mesure de précaution
-		Thread.sleep(2000);
+		// Démarre le job et attend qu'il soit terminé
+		client.runBatch(jobName, null);
 
-		// Attente de la fin de l'exécution
-		while (getStatus(jobName).equals("En cours")) {
-			Thread.sleep(100);
-		}
-
-		final String status = getStatus(jobName);
-		assertEquals("OK", status);
-	}
-
-	private String getStatus(String jobName) throws Exception {
-		final HtmlPage page = getHtmlPage("/admin/batch.do");
-		final HtmlDivision divStatus = (HtmlDivision) page.getHtmlElementById(jobName + "-status");
-		return divStatus.asText();
+		final JobDefinition definition = client.getBatchDefinition(jobName);
+		assertNotNull(definition);
+		assertEquals(JobStatut.JOB_OK, definition.getStatut());
 	}
 }
