b89c440cb3e75fe4413bb815a728365a75d3d438
Jacob Deker <jacob.deker@vd.ch>
2010-06-15 05:58:58 +0000

    Ajouté scripts shell utilisés dans la génération des scripts SQL pour les PROD-TRAVAUX
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@44101 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

---
+++ unireg/scripts/prod-travaux/deconnect-evts-civils.sh
@@ -0,0 +1,22 @@
+#! /bin/bash -
+# génère les scripts SQL qui permettent de déconnecter les événements civils reçus sur un doublon de ILF1
+
+if [ $# -ne 2 ]; then
+	echo "Syntaxe : $(basename "$0") <no-jira> <no-ind-doublon>" >&2
+	exit 1
+fi
+
+JIRA=$1
+IND=$2
+
+if [[ ! "$JIRA" =~ ^[0-9]+$ ]]; then
+	echo "Le numéro de cas Jira devrait être un nombre (trouvé : '$JIRA')" >&2
+	exit 1
+elif [[ ! "$IND" =~ ^[0-9]+$ ]]; then
+	echo "Le numéro d'individu derait être un nombre (trouvé : '$IND')" >&2
+	exit 1
+fi
+
+echo "-- Dé-connexion des événements civils sur l'ancien habitant (doublon ILF1)"
+echo "UPDATE EVENEMENT_CIVIL_REGROUPE SET LOG_MDATE=CURRENT_DATE, LOG_MUSER='SQL-$JIRA', HAB_PRINCIPAL=NULL WHERE NO_INDIVIDU_PRINCIPAL=$IND;"
+echo "UPDATE EVENEMENT_CIVIL_REGROUPE SET LOG_MDATE=CURRENT_DATE, LOG_MUSER='SQL-$JIRA', HAB_CONJOINT=NULL WHERE NO_INDIVIDU_CONJOINT=$IND;"
