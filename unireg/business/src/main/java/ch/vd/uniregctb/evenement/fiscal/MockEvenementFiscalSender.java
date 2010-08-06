520f8cbb49ab54e084bc27d617683d8891d81f1b
Jacob Deker <jacob.deker@vd.ch>
2010-08-06 05:39:07 +0000

    Etoffé un peu le MockEvenementFiscalSender afin de pouvoir débugguer plus facilement le cas où des événements sont passés alors qu'ils n'étaient pas attendus
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@46338 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/evenement/fiscal/MockEvenementFiscalSender.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/evenement/fiscal/MockEvenementFiscalSender.java
@@ -1,10 +1,15 @@
 package ch.vd.uniregctb.evenement.fiscal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ch.vd.uniregctb.evenement.EvenementFiscal;
 
 public class MockEvenementFiscalSender implements EvenementFiscalSender {
 
-	public  int count;
+	public int count;
+
+	private final List<String> trace = new ArrayList<String>();
 
 	public MockEvenementFiscalSender() {
 		this.count = 0;
@@ -12,5 +17,13 @@ public class MockEvenementFiscalSender implements EvenementFiscalSender {
 
 	public void sendEvent(EvenementFiscal evenement) throws EvenementFiscalException {
 		count++;
+		trace.add(evenement.toString());
+	}
+
+	/**
+	 * Permet, en debug, de visualiser les événements qui sont passés
+	 */
+	public List<String> getTrace() {
+		return trace;
 	}
 }
