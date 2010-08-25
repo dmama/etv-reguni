f6370f4d5a57a234dbd16e375a640e1945ea24fe
Jacob Deker <jacob.deker@vd.ch>
2010-08-25 14:50:41 +0000

    Implémenté passage par un fichier temporaire sur disque lors de la génération de la liste des contribuables pour le rapport d'exécution du batch des listes nominatives (une suite de réallocations successives du StringBuilder a fait exploser la mémoire en prod)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@47169 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/rapport/PdfListesNominativesRapport.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/rapport/PdfListesNominativesRapport.java
@@ -8,7 +8,14 @@ import ch.vd.uniregctb.listes.listesnominatives.TypeAdresse;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.pdf.PdfWriter;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.io.Writer;
 import java.util.Date;
 import java.util.List;
 
@@ -92,56 +99,83 @@ public class PdfListesNominativesRapport extends PdfRapport{
         final int size = list.size();
         if (size > 0) {
 
-	        final StringBuilder b = new StringBuilder(100 * list.size());
-	        b.append("NUMERO_CTB").append(COMMA).append("NOM_1").append(COMMA).append("NOM_2");
-	        if (results.getTypeAdressesIncluses() == TypeAdresse.FORMATTEE) {
-	            b.append(COMMA).append("ADRESSE_1").append(COMMA).append("ADRESSE_2").append(COMMA).append("ADRESSE_3");
-				b.append(COMMA).append("ADRESSE_4").append(COMMA).append("ADRESSE_5").append(COMMA).append("ADRESSE_6");
+	        File tempFile = null;
+	        try {
+				tempFile = File.createTempFile("ur-listnom-", null);
+		        tempFile.deleteOnExit();
+
+				final Writer writer = new BufferedWriter(new FileWriter(tempFile), 1024 * 1024);
+				writer.append("NUMERO_CTB").append(COMMA).append("NOM_1").append(COMMA).append("NOM_2");
+				if (results.getTypeAdressesIncluses() == TypeAdresse.FORMATTEE) {
+					writer.append(COMMA).append("ADRESSE_1").append(COMMA).append("ADRESSE_2").append(COMMA).append("ADRESSE_3");
+					writer.append(COMMA).append("ADRESSE_4").append(COMMA).append("ADRESSE_5").append(COMMA).append("ADRESSE_6");
+				}
+				else if (results.getTypeAdressesIncluses() == TypeAdresse.STRUCTUREE_RF) {
+					writer.append(COMMA).append("RUE").append(COMMA).append("NPA");
+					writer.append(COMMA).append("LOCALITE").append(COMMA).append("PAYS");
+				}
+				writer.append("\n");
+
+				final String message = String.format("Génération du fichier %s", filename);
+				status.setMessage(message, 0);
+
+				final GentilIterator<ListesNominativesResults.InfoTiers> iter = new GentilIterator<ListesNominativesResults.InfoTiers>(list);
+				while (iter.hasNext()) {
+					if (iter.isAtNewPercent()) {
+						status.setMessage(message, iter.getPercent());
+					}
+
+					final ListesNominativesResults.InfoTiers ligne = iter.next();
+					writer.append(Long.toString(ligne.numeroTiers)).append(COMMA);
+					writer.append(escapeChars(ligne.nomPrenom1)).append(COMMA);
+					writer.append(escapeChars(ligne.nomPrenom2));
+
+					if (results.getTypeAdressesIncluses() == TypeAdresse.FORMATTEE) {
+						Assert.isTrue(ligne instanceof ListesNominativesResults.InfoTiersAvecAdresseFormattee);
+						final ListesNominativesResults.InfoTiersAvecAdresseFormattee ligneAvecAdresse = (ListesNominativesResults.InfoTiersAvecAdresseFormattee) ligne;
+						final String[] adresse = ligneAvecAdresse.adresse;
+						for (int indexLigne = 0; indexLigne < adresse.length; ++indexLigne) {
+							writer.append(COMMA);
+							writer.append(escapeChars(adresse[indexLigne]));
+						}
+					}
+					else if (results.getTypeAdressesIncluses() == TypeAdresse.STRUCTUREE_RF) {
+						Assert.isTrue(ligne instanceof ListesNominativesResults.InfoTiersAvecAdresseStructureeRF);
+						final ListesNominativesResults.InfoTiersAvecAdresseStructureeRF ligneAvecAdresse = (ListesNominativesResults.InfoTiersAvecAdresseStructureeRF) ligne;
+						writer.append(COMMA);
+						writer.append(escapeChars(ligneAvecAdresse.rue)).append(COMMA);
+						writer.append(escapeChars(ligneAvecAdresse.npa)).append(COMMA);
+						writer.append(escapeChars(ligneAvecAdresse.localite)).append(COMMA);
+						writer.append(escapeChars(ligneAvecAdresse.pays));
+					}
+
+					if (!iter.isLast()) {
+						writer.append("\n");
+					}
+				}
+
+		        writer.close();
+
+		        // le fichier a été écrit -> on le relit maintenant dans la chaîne de caractères à renvoyer
+		        final BufferedInputStream in = new BufferedInputStream(new FileInputStream(tempFile));
+		        final long fileLength = tempFile.length();
+		        if (fileLength > Integer.MAX_VALUE) {
+			        throw new RuntimeException("Fichier de sortie beaucoup trop gros !");
+		        }
+		        final byte[] bytes = new byte[(int) fileLength];
+		        in.read(bytes);
+		        in.close();
+
+		        contenu = new String(bytes);
 	        }
-		    else if (results.getTypeAdressesIncluses() == TypeAdresse.STRUCTUREE_RF) {
-		        b.append(COMMA).append("RUE").append(COMMA).append("NPA");
-		        b.append(COMMA).append("LOCALITE").append(COMMA).append("PAYS");
+	        catch (IOException e) {
+		        throw new RuntimeException(e);
 	        }
-	        b.append("\n");
-
-            final String message = String.format("Génération du fichier %s", filename);
-            status.setMessage(message, 0);
-
-            final GentilIterator<ListesNominativesResults.InfoTiers> iter = new GentilIterator<ListesNominativesResults.InfoTiers>(list);
-            while (iter.hasNext()) {
-	            if (iter.isAtNewPercent()) {
-	                status.setMessage(message, iter.getPercent());
-	            }
-
-	            final ListesNominativesResults.InfoTiers ligne = iter.next();
-	            b.append(ligne.numeroTiers).append(COMMA);
-		        b.append(escapeChars(ligne.nomPrenom1)).append(COMMA);
-		        b.append(escapeChars(ligne.nomPrenom2));
-
-	            if (results.getTypeAdressesIncluses() == TypeAdresse.FORMATTEE) {
-	                Assert.isTrue(ligne instanceof ListesNominativesResults.InfoTiersAvecAdresseFormattee);
-	                final ListesNominativesResults.InfoTiersAvecAdresseFormattee ligneAvecAdresse = (ListesNominativesResults.InfoTiersAvecAdresseFormattee) ligne;
-	                final String[] adresse = ligneAvecAdresse.adresse;
-	                for (int indexLigne = 0; indexLigne < adresse.length; ++indexLigne) {
-	                    b.append(COMMA);
-		                b.append(escapeChars(adresse[indexLigne]));
-	                }
-	            }
-		        else if (results.getTypeAdressesIncluses() == TypeAdresse.STRUCTUREE_RF) {
-		            Assert.isTrue(ligne instanceof ListesNominativesResults.InfoTiersAvecAdresseStructureeRF);
-		            final ListesNominativesResults.InfoTiersAvecAdresseStructureeRF ligneAvecAdresse = (ListesNominativesResults.InfoTiersAvecAdresseStructureeRF) ligne;
-		            b.append(COMMA);
-		            b.append(escapeChars(ligneAvecAdresse.rue)).append(COMMA);
-		            b.append(escapeChars(ligneAvecAdresse.npa)).append(COMMA);
-		            b.append(escapeChars(ligneAvecAdresse.localite)).append(COMMA);
-		            b.append(escapeChars(ligneAvecAdresse.pays));
-	            }
-
-	            if (!iter.isLast()) {
-	                b.append("\n");
-	            }
-            }
-            contenu = b.toString();
+	        finally {
+		        if (tempFile != null) {
+			        tempFile.delete();
+		        }
+	        }
         }
         return contenu;
     }
