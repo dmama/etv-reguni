5235b67d8bbd20a25c1858a89617317a8e15d809
Manuel Siggen <manuel.siggen@vd.ch>
2010-09-21 13:30:05 +0000

    Ajouté un watchdog sur le service BVRPlus de SIPF
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@48313 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

---
+++ watchdog-sipf/src/test/java/ch.vd.watchdog/WatchDogBvrPlusIntegrationTest.java
@@ -0,0 +1,31 @@
+package ch.vd.watchdog;
+
+import java.math.BigInteger;
+
+import junit.framework.TestCase;
+
+import ch.vd.service.sipf.wsdl.sipfbvrplus_v1.BvrDemande;
+import ch.vd.service.sipf.wsdl.sipfbvrplus_v1.BvrReponse;
+import ch.vd.uniregctb.webservice.sipf.BVRPlusClientImpl;
+
+/**
+ * Teste que le service BVRPlus de SIPF est disponible et fonctionne.
+ */
+public class WatchDogBvrPlusIntegrationTest extends TestCase {
+
+	public void testIntegration() throws Exception {
+
+		final BVRPlusClientImpl service = new BVRPlusClientImpl();
+		service.setServiceUrl("http://ssv0213v.etat-de-vaud.ch:64904/fiscalite/int-sipf/bvrplus/SipfBVRPlusImpl");
+
+		BvrDemande demande = new BvrDemande();
+		demande.setNdc("0");
+		demande.setAnneeTaxation(BigInteger.valueOf(2008));
+		demande.setTypeDebiteurIS("REGULIER");
+
+		// on essaie avec le débiteur 0 qui n'existe pas pour ne pas générer de nouveau numéro de BVR, la seule chose qui nous intéresse, c'est de recevoir une réponse
+		final BvrReponse reponse = service.getBVRDemande(demande);
+		assertNotNull(reponse);
+		assertTrue(reponse.getMessage().contains("CONTRIB_ABSENT"));
+	}
+}
