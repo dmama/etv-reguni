66880a138b0324d80b0f1c91c0740ee2f07a255d
Manuel Siggen <manuel.siggen@vd.ch>
2010-09-21 14:08:27 +0000

    Désactivé le test parce que le service BVRPlus en intégration est down pour une durée indéterminée.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/4.5@48318 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business-it/src/test/java/ch/vd/uniregctb/webservice/sipf/BVRPlusServiceTest.java
+++ unireg/business-it/src/test/java/ch/vd/uniregctb/webservice/sipf/BVRPlusServiceTest.java
@@ -1,9 +1,11 @@
 package ch.vd.uniregctb.webservice.sipf;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.math.BigInteger;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import ch.vd.service.sipf.wsdl.sipfbvrplus_v1.BvrDemande;
@@ -13,18 +15,21 @@ public class BVRPlusServiceTest {
 
 	//private static final Logger LOGGER = Logger.getLogger(BVRPlusServiceTest.class);
 
+	@Ignore(value = "le service est down pour une durée indéterminée")
 	@Test
 	public void testGetBVRDemande() throws Exception {
 		final BVRPlusClientImpl service = new BVRPlusClientImpl();
 		service.setServiceUrl("http://ssv0213v.etat-de-vaud.ch:64904/fiscalite/int-sipf/bvrplus/SipfBVRPlusImpl");
 
 		BvrDemande demande = new BvrDemande();
-		demande.setNdc("289179");
+		demande.setNdc("0");
 		demande.setAnneeTaxation(BigInteger.valueOf(2008));
 		demande.setTypeDebiteurIS("REGULIER");
 
-		final BvrReponse resultat = service.getBVRDemande(demande);
-		assertNotNull(resultat);
+		// on essaie avec le débiteur 0 qui n'existe pas pour ne pas générer de nouveau numéro de BVR, la seule chose qui nous intéresse, c'est de recevoir une réponse
+		final BvrReponse reponse = service.getBVRDemande(demande);
+		assertNotNull(reponse);
+		assertTrue(reponse.getMessage().contains("CONTRIB_ABSENT"));
 	}
 
 }
