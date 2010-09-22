d2b379a44590af58b3126cf4b911d011f83584bb
Manuel Siggen <manuel.siggen@vd.ch>
2010-09-22 08:45:31 +0000

    Corrigé une erreur dans le script.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@48338 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/scripts/stats/ws_access_stats.sh
+++ unireg/scripts/stats/ws_access_stats.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Ce script extrait les statistiques d'accès par user technique <-> user business 
-grep "tiers2.read" $* | awk '{ print $5, $8 }' | sed -E "s/\[(.*)\].*'(.*)'.*/\1 \1/" | sort | uniq -c | sort -nr
+grep "tiers2.read" $* | awk '{ print $5, $8 }' | sed -E "s/\[(.*)\].*'(.*)'.*/\1 \2/" | sort | uniq -c | sort -nr
