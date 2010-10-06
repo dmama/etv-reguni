92afdd1e104cc57f1f9ae9059fcfde197264a334
Manuel Siggen <manuel.siggen@vd.ch>
2010-10-06 12:28:07 +0000

    Implémenté la possibilité de stocker les images des graphiques générés en local + réorganisé les paramètres de la ligne de commande.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@48851 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/tools/src/main/java/ch/vd/uniregctb/webservices/tiers2/perfs/WsAccessAnalyzer.java
+++ unireg/tools/src/main/java/ch/vd/uniregctb/webservices/tiers2/perfs/WsAccessAnalyzer.java
@@ -1,9 +1,15 @@
 package ch.vd.uniregctb.webservices.tiers2.perfs;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,8 +20,17 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.util.FileCopyUtils;
 
 import ch.vd.registre.base.utils.Assert;
 
@@ -64,7 +79,8 @@ public class WsAccessAnalyzer {
 				{new TimeRange(0, 0), new TimeRange(1, 4), new TimeRange(5, 9), new TimeRange(10, 19), new TimeRange(20, 29), new TimeRange(30, 39), new TimeRange(40, 49), new TimeRange(50, 74),
 						new TimeRange(75, 99), new TimeRange(100, 124), new TimeRange(125, 149), new TimeRange(150, 199), new TimeRange(200, 249), new TimeRange(250, 299), new TimeRange(300, 349),
 						new TimeRange(350, 399), new TimeRange(400, 459), new TimeRange(450, 499), new TimeRange(500, 749), new TimeRange(720, 999), new TimeRange(1000, 1499),
-						new TimeRange(1500, 1999), new TimeRange(2000, 4999), new TimeRange(5000, 9999), new TimeRange(10000, 49999), new TimeRange(50000, Long.MAX_VALUE)};
+						new TimeRange(1500, 1999), new TimeRange(2000, 4999), new TimeRange(5000, 9999), new TimeRange(10000, 29999), new TimeRange(30000, 59999),
+						new TimeRange(60000, Long.MAX_VALUE)};
 
 		private final TimeRange range;
 		private long count;
@@ -98,28 +114,68 @@ public class WsAccessAnalyzer {
 
 	private Map<String, List<ResponseTimeRange>> results = new HashMap<String, List<ResponseTimeRange>>();
 
-	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.out.println("Usage: ./ws-analyzer.sh [OPTION] ws-access.log0 ws-access.log1 ws-access.log2 ...");
-			System.out.println();
-			System.out.println("  --html    output results as graphics within an html page");
+	public static void main(String[] args) throws Exception {
+
+		final CommandLine commandLine = parseCommandLine(args);
+		if (commandLine == null) {
 			return;
 		}
 
-		boolean printHtml = args[0].equals("--html");
-		if (printHtml) {
-			args = (String[]) ArrayUtils.remove(args, 0);
+		final String htmlFile = commandLine.getOptionValue("html");
+		final boolean localImages = commandLine.hasOption("localImages");
+		final String[] files = commandLine.getArgs();
+		final String proxy = commandLine.getOptionValue("proxy");
+
+		if (StringUtils.isNotBlank(proxy)) {
+			final String[] p = proxy.split(":");
+			System.setProperty("http.proxyHost", p[0]);
+			System.setProperty("http.proxyPort", p[1]);
 		}
 
-		WsAccessAnalyzer analyzer = new WsAccessAnalyzer();
-		analyzer.analyze(args);
+		final WsAccessAnalyzer analyzer = new WsAccessAnalyzer();
+		analyzer.analyze(files);
 
-		if (printHtml) {
-			analyzer.printHtml();
+		if (StringUtils.isNotBlank(htmlFile)) {
+			analyzer.printHtml(htmlFile, localImages);
 		}
 		else {
 			analyzer.print();
 		}
+
+	}
+
+	@SuppressWarnings({"static-access", "AccessStaticViaInstance"})
+	private static CommandLine parseCommandLine(String[] args) {
+
+		final CommandLine line;
+		try {
+			CommandLineParser parser = new GnuParser();
+			Option help = new Option("help", "display this help");
+			Option html = OptionBuilder.withArgName("file").hasArg().withDescription("output results in a html file").create("html");
+			Option localImages = new Option("localImages", "store images in local folder (with -html)");
+			Option proxy = OptionBuilder.withArgName("host:port").hasArg().withDescription("use HTTP proxy on given port").create("proxy");
+
+			Options options = new Options();
+			options.addOption(help);
+			options.addOption(html);
+			options.addOption(localImages);
+			options.addOption(proxy);
+
+			// parse the command line arguments
+			line = parser.parse(options, args);
+
+			if (line.hasOption("help") || line.getArgs().length == 0) {
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp("WsAccessAnalyzer [options] FILE...", "Options:", options, null);
+				return null;
+			}
+		}
+		catch (ParseException exp) {
+			System.err.println("Erreur de parsing.  Raison: " + exp.getMessage());
+			return null;
+		}
+
+		return line;
 
 	}
 
@@ -146,7 +202,11 @@ public class WsAccessAnalyzer {
 		}
 	}
 
-	private void printHtml() {
+	private void printHtml(String htmlFile, boolean localImages) throws IOException {
+
+		if (!htmlFile.toLowerCase().endsWith(".html") && !htmlFile.toLowerCase().endsWith(".htm")) {
+			htmlFile += ".html";
+		}
 
 		final List<String> methods = new ArrayList<String>(results.keySet());
 		Collections.sort(methods);
@@ -163,7 +223,7 @@ public class WsAccessAnalyzer {
 
 		for (String method : methods) {
 			final List<ResponseTimeRange> time = results.get(method);
-			content += "    " + buildGoogleChart(method, time) + "<br/><br/><br/>\n";
+			content += "    " + buildChart(method, time, htmlFile, localImages) + "\n";
 		}
 
 		content += "    <br/>\n" +
@@ -171,14 +231,56 @@ public class WsAccessAnalyzer {
 				"  </body>\n" +
 				"</html>";
 
-		System.out.println(content);
+		final FileWriter writer = new FileWriter(htmlFile);
+		writer.write(content);
+		writer.close();
+	}
+
+	private String buildChart(String method, List<ResponseTimeRange> time, String htmlFile, boolean localImages) throws IOException {
+
+		final String url = buildGoogleChartUrl(method, time);
+		
+		if (localImages) {
+			final String dirname = FilenameUtils.removeExtension(htmlFile);
+
+			// on crée un sous-répertoire du même nom (sans l'extension) du fichier html
+			final File dir = new File(dirname);
+			if (!dir.exists() || !dir.isDirectory()) {
+				if (!dir.mkdirs()) {
+					throw new RuntimeException("Unable to create directoy [" + dirname + "]");
+				}
+			}
+
+			// on récupère l'image générée par Google et on la stocke dans le sous-répertoire
+			final String imagename = dirname + "/" + method + ".png";
+			InputStream is = null;
+			OutputStream os = null;
+			try {
+				URL u = new URL(url);
+				is = u.openStream();
+				os = new FileOutputStream(imagename, false);
+				FileCopyUtils.copy(is, os);
+			}
+			finally {
+				if (is != null) {
+					is.close();
+				}
+				if (os != null) {
+					os.close();
+				}
+			}
+			return "<img src=\"" + imagename + "\" width=\"1000\" height=\"200\" alt=\"" + method + "\"/><br/><br/><br/>";
+		}
+		else {
+			return "<img src=\"" + url + "\" width=\"1000\" height=\"200\" alt=\"" + method + "\"/><br/><br/><br/>";
+		}
 	}
 
 	/**
 	 * Voir http://code.google.com/apis/chart/docs/chart_wizard.html
 	 */
 	@SuppressWarnings({"JavaDoc"})
-	private String buildGoogleChart(String method, List<ResponseTimeRange> time) {
+	private String buildGoogleChartUrl(String method, List<ResponseTimeRange> time) {
 
 //		final String labels = "|0|1|2|3|4|5";
 		StringBuilder labels = new StringBuilder();
@@ -209,9 +311,9 @@ public class WsAccessAnalyzer {
 //		final String valuesRange = "0,200";
 		final String valuesRange = "0," + max;
 
-		return new StringBuilder().append("<img src=\"http://chart.apis.google.com/chart?chxl=0:").append(labels).append("&chxr=1,").append(valuesRange)
+		return new StringBuilder().append("http://chart.apis.google.com/chart?chxl=0:").append(labels).append("&chxr=1,").append(valuesRange)
 				.append("&chxs=0,676767,8,0,l,676767&chxt=x,y&chbh=23,5&chs=1000x200&cht=bvg&chco=76A4FB&chds=").append(valuesRange).append("&chd=t:").append(values).append("&chg=20,50&chtt=")
-				.append(method).append("\" width=\"1000\" height=\"200\" alt=\"").append(method).append("\" />").toString();
+				.append(method).toString();
 	}
 
 	private void analyze(String[] args) {
