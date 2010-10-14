508f7134215b26d67a0f4465f3ff391787197b5f
Jacob Deker <jacob.deker@vd.ch>
2010-10-14 07:46:15 +0000

    Version 4.5.3a (MePO 14.10.2010)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/4.5@49231 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/test/java/ch/vd/uniregctb/embedded/UniregTomcatRunner.java
+++ unireg/web/src/test/java/ch/vd/uniregctb/embedded/UniregTomcatRunner.java
@@ -41,7 +41,7 @@ public class UniregTomcatRunner {
 		// Propriétés normallement configurées par Maven et qu'on spécifie à la main parce qu'on pointe vers le répertoire sources (et non pas target).
 
 		// BEGIN_LVERSION
-		System.setProperty("pom.version", "4.5.3");
+		System.setProperty("pom.version", "4.5.3a");
 		// END_LVERSION
 
 		System.setProperty("pom.name", "UniregCTB - Web");
