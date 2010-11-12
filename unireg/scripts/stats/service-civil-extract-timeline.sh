2780cf303e61832c073ae61b545579b4e7e2c887
Jacob Deker <jacob.deker@vd.ch>
2010-11-12 09:47:13 +0000

    Blindé le script pour accepter les fichiers d'entrée sur la ligne de commande ou l'entrée standard indistinctement et pour supporter le cas où seul l'un des deux services getIndividu/getIndividus est utilisé
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@50175 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/scripts/stats/service-civil-extract-timeline.sh
+++ unireg/scripts/stats/service-civil-extract-timeline.sh
@@ -1,6 +1,13 @@
 #! /bin/bash -
-# prend un fichier de log applicatif unireg (unireg-web.log ou unireg-ws.log) sur l'entrée standard
+# prend un fichier de log applicatif unireg (unireg-web.log ou unireg-ws.log) sur l'entrée standard (ou la ligne de commande)
 # et sort des lignes pour les temps d'accès aux service civil (getIndividu et getIndividus) - moyenne des cinq dernières minutes
 
 echo "TIMESTAMP;getIndividu;getIndividus"
-sed -e '/Statistiques des caches et services/,/ - getIndividus/ !D' | grep "\(INFO\|Individu\)" | awk ' /^\[unireg\]/ { print $3 " " $4; } /getIndividu/ { print $4; } ' | sed -e 's/[^0-9: .-]//g' -e '/:/ N' -e 's/\n/ /' -e '/:/ N' -e 's/\n/ /' -e 's/ /x/' -e 's/ /;/g' -e 's/x/ /'
+sed -e '/Statistiques des caches et services/,/ - getIndividus/ !D' -- "$@" | grep "\(INFO\|Individu\)" | awk ' /INFO/ { print $3 " " $4; } / - getIndividu/ { print $2 " " $4; }' | sed -e '/:/ N;/:/ N' -e 's/\n/;/g' -e 's/[^a-zA-Z0-9:;. -]//g' | while IFS=";" read TS INFO1 INFO2; do
+
+	IND=$(echo -e "$INFO1\n$INFO2" | grep "^getIndividu\b")
+	INDS=$(echo -e "$INFO1\n$INFO2" | grep "^getIndividus\b")
+	IFS=";" read IND INDS < <(echo "$IND;$INDS" | sed -e 's/[^0-9;]//g')
+	echo "$TS;$IND;$INDS"
+
+done
