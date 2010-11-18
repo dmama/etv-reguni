dc9c16d417838b8bedfac4ea156b9595b386eb70
Manuel Siggen <manuel.siggen@vd.ch>
2010-11-18 12:31:17 +0000

    [UNIREG-2920] Corrigé l'indexation pour les collectivités administratives où le numéro de CA était indexé à la place du numéro de tiers.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@50476 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/base/sql/4.8.0/unireg-schema_4.7.0_4.8.0_upgrade.sql
+++ unireg/base/sql/4.8.0/unireg-schema_4.7.0_4.8.0_upgrade.sql
@@ -5,3 +5,8 @@ INSERT INTO VERSION_DB (VERSION_NB, SCRIPT_ID) VALUES ('4.8.0', '4.7.0_4.8.0_upg
 -- Suppression de la colonne NUMERO_PM qui dupliquait les données de la colonne NUMERO. 
 --
 ALTER TABLE TIERS DROP COLUMN NUMERO_PM;
+
+--
+-- [UNIREG-2920] Réindexation des collectivités administratives
+--
+update TIERS set INDEX_DIRTY = 1 where TIERS_TYPE = 'CollectiviteAdministrative';
