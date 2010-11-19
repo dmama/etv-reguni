46e6b1194d01175f42a95df9ad099642de488515
Jacob Deker <jacob.deker@vd.ch>
2010-11-19 06:14:09 +0000

    Mis en couleur les temps de réponse
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@50495 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/scripts/stats/ws-access-extract-timeout.sh
+++ unireg/scripts/stats/ws-access-extract-timeout.sh
@@ -1,4 +1,4 @@
 #! /bin/bash -
 # Extrait d'un fichier type ws-access.log les lignes qui correspondent à des temps de réponse supérieurs à la minute
 
-grep "(\([6-9][0-9]\{4\}\|[1-9][0-9]\{5,\}\) ms)" "$@"
+grep --color=auto "(\([6-9][0-9]\{4\}\|[1-9][0-9]\{5,\}\) ms)" "$@"
