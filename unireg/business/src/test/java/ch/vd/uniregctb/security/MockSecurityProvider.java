9a9f20926baf7c888c8793b5c13fda77efcd5453
Jacob Deker <jacob.deker@vd.ch>
2010-11-24 14:42:00 +0000

    [UNIREG-3081] Le validator des adresses (partie IHM) doit tenir compte du type de tiers avant de laisser passer une modification d'adresse (selon la matrice des droits)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@50679 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

---
+++ unireg/business/src/test/java/ch/vd/uniregctb/security/MockSecurityProvider.java
@@ -0,0 +1,48 @@
+package ch.vd.uniregctb.security;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import ch.vd.uniregctb.common.ObjectNotFoundException;
+import ch.vd.uniregctb.type.Niveau;
+
+/**
+ * Mock du service de SecurityProvider (qui ne s'occupe pour le moment que des droits ifosec)
+ */
+public class MockSecurityProvider implements SecurityProviderInterface {
+
+	private final Set<Role> roles;
+
+	public MockSecurityProvider(Collection<Role> roles) {
+		this.roles = new HashSet<Role>(roles);
+	}
+
+	public MockSecurityProvider(Role... roles) {
+		this(Arrays.asList(roles));
+	}
+
+	public boolean isGranted(Role role, String visaOperateur, int codeCollectivite) {
+		return roles.contains(role);
+	}
+
+	public Niveau getDroitAcces(String visaOperateur, long tiersId) throws ObjectNotFoundException {
+		return Niveau.ECRITURE;
+	}
+
+	public List<Niveau> getDroitAcces(String visa, List<Long> ids) {
+		final List<Niveau> res = new ArrayList<Niveau>(ids.size());
+		for (Long id : ids) {
+			if (id != null) {
+				res.add(Niveau.ECRITURE);
+			}
+			else {
+				res.add(null);
+			}
+		}
+		return res;
+	}
+}
--- unireg/web/src/main/java/ch/vd/uniregctb/tiers/validator/TiersAdresseValidator.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/tiers/validator/TiersAdresseValidator.java
@@ -1,21 +1,32 @@
 package ch.vd.uniregctb.tiers.validator;
 
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import ch.vd.registre.base.date.DateRangeHelper;
 import ch.vd.registre.base.date.RegDate;
 import ch.vd.uniregctb.adresse.AdresseException;
 import ch.vd.uniregctb.adresse.AdresseGenerique;
 import ch.vd.uniregctb.adresse.AdresseService;
 import ch.vd.uniregctb.adresse.AdressesFiscales;
+import ch.vd.uniregctb.common.ActionException;
 import ch.vd.uniregctb.interfaces.model.Localite;
 import ch.vd.uniregctb.interfaces.service.ServiceInfrastructureService;
+import ch.vd.uniregctb.metier.assujettissement.Assujettissement;
+import ch.vd.uniregctb.metier.assujettissement.AssujettissementException;
+import ch.vd.uniregctb.metier.assujettissement.HorsCanton;
+import ch.vd.uniregctb.metier.assujettissement.HorsSuisse;
+import ch.vd.uniregctb.metier.assujettissement.SourcierPur;
 import ch.vd.uniregctb.security.Role;
 import ch.vd.uniregctb.security.SecurityProvider;
+import ch.vd.uniregctb.tiers.Contribuable;
 import ch.vd.uniregctb.tiers.DebiteurPrestationImposable;
+import ch.vd.uniregctb.tiers.EnsembleTiersCouple;
 import ch.vd.uniregctb.tiers.MenageCommun;
 import ch.vd.uniregctb.tiers.PersonnePhysique;
 import ch.vd.uniregctb.tiers.Tiers;
@@ -151,7 +162,7 @@ public class TiersAdresseValidator implements Validator {
 						}
 						else if (SecurityProvider.isGranted(Role.ADR_PP_C) && !tiersService.isDecede(pp)) {
 							// on peut modifier l'adresse courrier d'un non décédé uniquement si ont possède le rôle correspondant
-							isAllowed = true;
+							isAllowed = isTiersModifiable(tiers);
 						}
 					}
 					else if (tiers instanceof MenageCommun) {
@@ -170,23 +181,28 @@ public class TiersAdresseValidator implements Validator {
 						}
 						else {
 							if(SecurityProvider.isGranted(Role.ADR_PP_C)) {
-								isAllowed = true;
+								isAllowed = isTiersModifiable(tiers);
 							}
 						}
 					}
 					break;
 				case DOMICILE :
-					if(SecurityProvider.isGranted(Role.ADR_PP_D))
-						isAllowed = true;
+					if (SecurityProvider.isGranted(Role.ADR_PP_D)) {
+						isAllowed = isTiersModifiable(tiers);
+					}
 					break;
 				case POURSUITE :
-					if(SecurityProvider.isGranted(Role.ADR_P))
+					if (SecurityProvider.isGranted(Role.ADR_P)) {
 						isAllowed = true;
+					}
 					break;
 				case REPRESENTATION :
-					if(SecurityProvider.isGranted(Role.ADR_PP_B))
-						isAllowed = true;
+					if (SecurityProvider.isGranted(Role.ADR_PP_B)) {
+						isAllowed = isTiersModifiable(tiers);
+					}
 					break;
+				default:
+					throw new IllegalArgumentException("Valeur non supportée : " + usage);
 			}
 		}
 		else if(tiers.getNatureTiers().equals(Tiers.NATURE_DPI)) {
@@ -194,28 +210,31 @@ public class TiersAdresseValidator implements Validator {
 				isAllowed = SecurityProvider.isGranted(Role.ADR_P);
 			}
 			else {
-				isAllowed = SecurityProvider.isGranted(Role.CREATE_DPI);
+				isAllowed = isTiersModifiable(tiers);
 			}
 		}
 		else {
 			//PM
 			switch (usage) {
-				case COURRIER :
-					if(SecurityProvider.isGranted(Role.ADR_PM_C))
+				case COURRIER:
+					if (SecurityProvider.isGranted(Role.ADR_PM_C)) {
+						isAllowed = isTiersModifiable(tiers);
+					}
+					break;
+				case DOMICILE:
+					if (SecurityProvider.isGranted(Role.ADR_PM_D))
+						isAllowed = isTiersModifiable(tiers);
+					break;
+				case POURSUITE:
+					if (SecurityProvider.isGranted(Role.ADR_P))
 						isAllowed = true;
 					break;
-				case DOMICILE :
-					if(SecurityProvider.isGranted(Role.ADR_PM_D))
-						isAllowed = true;
+				case REPRESENTATION:
+					if (SecurityProvider.isGranted(Role.ADR_PM_B))
+						isAllowed = isTiersModifiable(tiers);
 					break;
-				case POURSUITE :
-					if(SecurityProvider.isGranted(Role.ADR_P))
-						isAllowed = true;
-					break;
-				case REPRESENTATION :
-					if(SecurityProvider.isGranted(Role.ADR_PM_B))
-						isAllowed = true;
-					break;
+				default:
+					throw new IllegalArgumentException("Valeur non supportée : " + usage);
 			}
 		}
 
@@ -227,5 +246,82 @@ public class TiersAdresseValidator implements Validator {
 				errors.reject("error.tiers.interdit");
 			}
 		}
+	}
+
+	private boolean isTiersModifiable(Tiers tiers) {
+
+		try {
+			if (tiers instanceof DebiteurPrestationImposable) {
+				return SecurityProvider.isGranted(Role.CREATE_DPI);
+			}
+			else if (tiers instanceof PersonnePhysique || tiers instanceof MenageCommun) {
+				final Contribuable ctb = (Contribuable) tiers;
+				final boolean isHabitant = isHabitant(ctb);
+				final Contribuable assujettissable = getContribuableAssujettissable(ctb);
+				final List<Assujettissement> assujettissement = Assujettissement.determine(assujettissable);
+				final Assujettissement assujettissementCourant = assujettissement != null ? DateRangeHelper.rangeAt(assujettissement, RegDate.get()) : null;
+				if (assujettissementCourant == null) {
+					// non-assujetti
+					return SecurityProvider.isGranted(isHabitant ? Role.MODIF_HAB_DEBPUR : Role.MODIF_NONHAB_DEBPUR);
+				}
+				else if (assujettissementCourant instanceof HorsCanton || assujettissementCourant instanceof HorsSuisse) {
+					return SecurityProvider.isGranted(Role.MODIF_HC_HS);
+				}
+				else if (assujettissementCourant instanceof SourcierPur) {
+					return SecurityProvider.isGranted(Role.MODIF_VD_SOURC);
+				}
+				else {
+					return SecurityProvider.isGranted(Role.MODIF_VD_ORD);
+				}
+			}
+			else {
+				// pas de rôle particulier pour avoir le droit de modifier les PM en général
+				return true;
+			}
+		}
+		catch (AssujettissementException e) {
+			throw new ActionException(e.getMessage());
+		}
+	}
+
+	/**
+	 * N'a de sens que sur des personnes physiques ou des ménages communs
+	 * @param ctb le contribuable sur lequel on s'interroge
+	 * @return <code>true</code> si le contribuble est habitant, <code>false</code> sinon
+	 */
+	private boolean isHabitant(Contribuable ctb) {
+		final boolean isHabitant;
+		if (ctb instanceof PersonnePhysique || ctb instanceof MenageCommun) {
+			if (ctb instanceof PersonnePhysique) {
+				isHabitant = ((PersonnePhysique) ctb).isHabitantVD();
+			}
+			else {
+				final EnsembleTiersCouple couple = tiersService.getEnsembleTiersCouple((MenageCommun) ctb, null);
+				final PersonnePhysique principal = couple.getPrincipal();
+				final PersonnePhysique conjoint = couple.getConjoint();
+				isHabitant = (principal != null && principal.isHabitantVD()) || (conjoint != null && conjoint.isHabitantVD());
+			}
+		}
+		else {
+			isHabitant = false;
+		}
+		return isHabitant;
+	}
+
+	private Contribuable getContribuableAssujettissable(Contribuable ctb) {
+		final Contribuable assujettissable;
+		if (ctb instanceof PersonnePhysique) {
+			final EnsembleTiersCouple couple = tiersService.getEnsembleTiersCouple((PersonnePhysique) ctb, null);
+			if (couple != null && couple.getMenage() != null) {
+				assujettissable = couple.getMenage();
+			}
+			else {
+				assujettissable = ctb;
+			}
+		}
+		else {
+			assujettissable = ctb;
+		}
+		return assujettissable;
 	}
 }
