ebbe67ce4b3b140b4f4a20448a83b28ba4998f6a
Baba Issa Ngom <baba-issa.ngom@vd.ch>
2010-11-25 08:23:13 +0000

    UNIREG-3060] Ajout du test unitaire sur les adresses civiles d'un contribuable hors canton appartenant à un couple dont le conjoint est encore vaudois
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@50713 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/test/java/ch/vd/uniregctb/tiers/manager/TiersVisuManagerTest.java
+++ unireg/web/src/test/java/ch/vd/uniregctb/tiers/manager/TiersVisuManagerTest.java
@@ -5,16 +5,29 @@ import static junit.framework.Assert.assertNotNull;
 
 import java.util.List;
 
+import junit.framework.Assert;
 import org.junit.Test;
+import org.springframework.transaction.TransactionStatus;
 
+import ch.vd.common.model.EnumTypeAdresse;
 import ch.vd.registre.base.date.RegDate;
 import ch.vd.uniregctb.common.WebParamPagination;
 import ch.vd.uniregctb.common.WebTest;
+import ch.vd.uniregctb.interfaces.model.Individu;
+import ch.vd.uniregctb.interfaces.model.Pays;
+import ch.vd.uniregctb.interfaces.model.mock.MockIndividu;
+import ch.vd.uniregctb.interfaces.model.mock.MockLocalite;
+import ch.vd.uniregctb.interfaces.model.mock.MockPays;
+import ch.vd.uniregctb.interfaces.model.mock.MockRue;
 import ch.vd.uniregctb.interfaces.service.mock.MockServiceCivil;
+import ch.vd.uniregctb.tiers.MenageCommun;
 import ch.vd.uniregctb.tiers.PersonnePhysique;
+import ch.vd.uniregctb.tiers.RapportEntreTiers;
 import ch.vd.uniregctb.tiers.Tiers;
+import ch.vd.uniregctb.tiers.Tutelle;
 import ch.vd.uniregctb.tiers.view.AdresseView;
 import ch.vd.uniregctb.tiers.view.TiersVisuView;
+import ch.vd.uniregctb.type.TypeAdresseCivil;
 
 public class TiersVisuManagerTest extends WebTest {
 
@@ -82,6 +95,72 @@ public class TiersVisuManagerTest extends WebTest {
 		 * 2 * poursuite (1 défaut)
 		 */
 		assertEquals(6, adresses.size());
+
+	}
+
+
+	@Test
+	public void testGetAdressesCivilesPrincipalHC() throws Exception {
+	    serviceCivil.setUp(new MockServiceCivil() {
+			@Override
+			protected void init() {
+
+
+				MockIndividu zotan = addIndividu(185386, RegDate.get(1974, 3, 22), "Zotan", "Mitev", true);
+				MockIndividu marie = addIndividu(185387, RegDate.get(1974, 3, 22), "Marie-Claude", "Wolf", false);
+				addAdresse(marie, TypeAdresseCivil.COURRIER, MockRue.Lausanne.AvenueDeBeaulieu, null, RegDate.get(2009, 9, 1), null);
+				addAdresse(marie, TypeAdresseCivil.PRINCIPALE, MockRue.Lausanne.AvenueDeBeaulieu, null, RegDate.get(2009, 9, 1), null);
+				addAdresse(zotan, TypeAdresseCivil.COURRIER, null, null,null,null,null, MockPays.PaysInconnu, RegDate.get(2009, 12, 18), null);
+				addAdresse(zotan, TypeAdresseCivil.PRINCIPALE, null, null,null,null,null, MockPays.PaysInconnu, RegDate.get(2009, 12, 18), null);
+			   	marieIndividus(zotan, marie, date(2004, 5, 1));
+
+			}
+		});
+
+
+
+
+		final class Numeros {
+			long numeroContribuablePrincipal;
+			long numeroContribuableConjoint;
+			long numeroContribuableMenage;
+		}
+		final Numeros numeros = new Numeros();
+		doInNewTransaction(new TxCallback() {
+			@Override
+			public Object execute(TransactionStatus status) throws Exception {
+				PersonnePhysique habitantZotan=	addHabitant(185386);
+				PersonnePhysique habitantMarie=	addHabitant(185387);
+				// Crée le ménage
+				MenageCommun menage = new MenageCommun();
+				RapportEntreTiers rapport = tiersService.addTiersToCouple(menage, habitantZotan, date(2004, 5, 1), null);
+				menage = (MenageCommun) tiersDAO.get(rapport.getObjetId());
+				numeros.numeroContribuableMenage = menage.getNumero();
+				numeros.numeroContribuablePrincipal = habitantZotan.getNumero();
+
+				rapport = tiersService.addTiersToCouple(menage, habitantMarie, date(2004, 5, 1), null);
+
+				numeros.numeroContribuableConjoint = habitantMarie.getNumero();
+
+				return null;
+			}
+		});
+
+		WebParamPagination webParamPagination = new WebParamPagination(1, 10, "logCreationDate", true);
+		TiersVisuView view = tiersVisuManager.getView(numeros.numeroContribuableMenage, true, true, true, false, webParamPagination);
+		List<AdresseView> adressesMenage = view.getHistoriqueAdresses();
+		List<AdresseView> adressesZotan = view.getHistoriqueAdressesCiviles();
+		List<AdresseView> adressesMarie = view.getHistoriqueAdressesCivilesConjoint();
+
+		/*
+		 * 2 * courrier
+		 * 2 * representation (1 fiscale + 1 défaut)
+		 * 2 * poursuite (1 défaut)
+		 */
+		assertEquals(2, adressesZotan.size());
+		assertEquals(date(2009,12,18), RegDate.get(adressesZotan.get(0).getDateDebut()));
+		assertEquals("",adressesZotan.get(0).getLocalite());
+
 
 	}
 
