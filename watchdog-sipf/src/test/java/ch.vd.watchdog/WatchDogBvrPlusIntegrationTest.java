252a60333fd236de071059a85d92b5c344c4a4b6
Manuel Siggen <manuel.siggen@vd.ch>
2011-01-26 13:05:39 +0000

    Intégré la nouvelle version du web-service BVRPlus qui contient maintenant une méthode 'ping'.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@53316 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- watchdog-sipf/src/test/java/ch.vd.watchdog/WatchDogBvrPlusIntegrationTest.java
+++ watchdog-sipf/src/test/java/ch.vd.watchdog/WatchDogBvrPlusIntegrationTest.java
@@ -17,15 +17,6 @@ public class WatchDogBvrPlusIntegrationTest extends TestCase {
 
 		final BVRPlusClientImpl service = new BVRPlusClientImpl();
 		service.setServiceUrl("http://ssv0213v.etat-de-vaud.ch:50110/fiscalite/int-sipf/bvrplus/SipfBVRPlusImpl");
-
-		BvrDemande demande = new BvrDemande();
-		demande.setNdc("0");
-		demande.setAnneeTaxation(BigInteger.valueOf(2008));
-		demande.setTypeDebiteurIS("REGULIER");
-
-		// on essaie avec le débiteur 0 qui n'existe pas pour ne pas générer de nouveau numéro de BVR, la seule chose qui nous intéresse, c'est de recevoir une réponse
-		final BvrReponse reponse = service.getBVRDemande(demande);
-		assertNotNull(reponse);
-		assertTrue(reponse.getMessage().contains("CONTRIB_ABSENT"));
+		service.ping();
 	}
 }
