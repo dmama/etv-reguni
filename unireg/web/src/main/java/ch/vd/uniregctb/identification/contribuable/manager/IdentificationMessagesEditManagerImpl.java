6962b881c23009202bee3fc7e04774c3bd22fcbe
Baba Issa Ngom <baba-issa.ngom@vd.ch>
2011-02-07 16:18:38 +0000

    [UNIREG-3125] Suppression de la resolution du nom du canton à partir du code de  l'emeteur ID.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@53835 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/java/ch/vd/uniregctb/identification/contribuable/manager/IdentificationMessagesEditManagerImpl.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/identification/contribuable/manager/IdentificationMessagesEditManagerImpl.java
@@ -113,7 +113,7 @@ public class IdentificationMessagesEditManagerImpl implements IdentificationMess
 		demandeIdentificationView.setEtatMessage(identificationContribuable.getEtat());
 		if(identificationContribuable.getDemande() != null) {
 			demandeIdentificationView.setDateMessage(identificationContribuable.getDemande().getDate());
-			demandeIdentificationView.setEmetteurId(identCtbService.getNomCantonFromEmetteurId(identificationContribuable.getDemande().getEmetteurId()));
+			demandeIdentificationView.setEmetteurId(identificationContribuable.getDemande().getEmetteurId());
 			demandeIdentificationView.setPeriodeFiscale(Integer.valueOf(identificationContribuable.getDemande().getPeriodeFiscale()));
 			demandeIdentificationView.setTypeMessage(identificationContribuable.getDemande().getTypeMessage());
 			demandeIdentificationView.setBusinessId(identificationContribuable.getHeader().getBusinessId());
