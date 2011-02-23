d88b6890381785fc18b0b3587cf936b41e7c4148
Jacob Deker <jacob.deker@vd.ch>
2011-02-23 11:07:13 +0000

    Correction du message renvoyé en cas d'employeur inconnu et nettoyage des imports
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@54706 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/evenement/iam/EvenementIAMServiceImpl.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/evenement/iam/EvenementIAMServiceImpl.java
@@ -2,28 +2,9 @@ package ch.vd.uniregctb.evenement.iam;
 
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
-
-import ch.vd.registre.base.utils.Assert;
 import ch.vd.registre.base.validation.ValidationResults;
-import ch.vd.uniregctb.common.LengthConstants;
-import ch.vd.uniregctb.declaration.Declaration;
-import ch.vd.uniregctb.declaration.DeclarationImpotOrdinaire;
-import ch.vd.uniregctb.declaration.ModeleDocument;
-import ch.vd.uniregctb.declaration.ModeleDocumentDAO;
-import ch.vd.uniregctb.declaration.PeriodeFiscale;
-import ch.vd.uniregctb.declaration.PeriodeFiscaleDAO;
-import ch.vd.uniregctb.evenement.cedi.EvenementCedi;
-import ch.vd.uniregctb.evenement.cedi.EvenementCediException;
-import ch.vd.uniregctb.evenement.cedi.EvenementCediHandler;
-import ch.vd.uniregctb.evenement.cedi.EvenementCediService;
-import ch.vd.uniregctb.evenement.cedi.RetourDI;
-import ch.vd.uniregctb.iban.IbanHelper;
-import ch.vd.uniregctb.tiers.Contribuable;
 import ch.vd.uniregctb.tiers.DebiteurPrestationImposable;
 import ch.vd.uniregctb.tiers.TiersDAO;
-import ch.vd.uniregctb.type.ModeCommunication;
-import ch.vd.uniregctb.type.TypeDocument;
 import ch.vd.uniregctb.validation.ValidationService;
 
 public class EvenementIAMServiceImpl implements EvenementIAMService, EvenementIAMHandler {
@@ -48,24 +29,20 @@ public class EvenementIAMServiceImpl implements EvenementIAMService, EvenementIA
 			final long debiteurId = infoEmployeur.getNoEmployeur();
 			final DebiteurPrestationImposable debiteur = tiersDAO.getDebiteurPrestationImposableByNumero(debiteurId);
 			if (debiteur == null) {
-				throw new EvenementIAMException("l'employeur n°" + debiteurId + ".");
+				throw new EvenementIAMException("L'employeur n°" + debiteurId + " est inconnu.");
 			}
 
 			final ValidationResults results = validationService.validate(debiteur);
-
 			if (results.hasErrors()) {
 				throw new EvenementIAMException("L'employeur  n°" + debiteurId + " ne valide pas (" + results.toString() + ").");
 			}
 
-			updateInformationsDebiteur(infoEmployeur,debiteur);
-
+			updateInformationsDebiteur(infoEmployeur, debiteur);
 		}
-
-
 	}
 
-	/**Mets a jour le mode de communication et l'id du logicile pour le débiteur
-	 *
+	/**
+	 * Mets a jour le mode de communication et l'id du logicile pour le débiteur
 	 * @param infoEmployeur
 	 * @param debiteur
 	 */
