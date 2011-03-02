e04fe732356277782d93d2b6545a4e32b114bcfc
Manuel Siggen <manuel.siggen@vd.ch>
2011-03-02 11:40:42 +0000

    [code cleanup] Corrigé la hiérarchie de l'annulation de décès.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@55077 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/evenement/civil/interne/annulation/deces/AnnulationDeces.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/evenement/civil/interne/annulation/deces/AnnulationDeces.java
@@ -1,7 +1,13 @@
 package ch.vd.uniregctb.evenement.civil.interne.annulation.deces;
 
-import ch.vd.uniregctb.evenement.civil.interne.deces.Deces;
+import ch.vd.uniregctb.evenement.civil.interne.EvenementCivilInterne;
+import ch.vd.uniregctb.interfaces.model.Individu;
 
-public interface AnnulationDeces extends Deces {
+public interface AnnulationDeces extends EvenementCivilInterne {
 
+	/**
+	 * Renvoie le conjoint veuf s'il existe.
+	 * @return le conjoint veuf s'il existe
+	 */
+	public Individu getConjointSurvivant();
 }
