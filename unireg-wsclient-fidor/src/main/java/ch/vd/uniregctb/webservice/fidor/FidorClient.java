aecba71252ddae100ac53fef89516b7bde0d61ab
Manuel Siggen <manuel.siggen@vd.ch>
2011-03-22 14:14:26 +0000

    Version 2.1.0 : Intégré les nouvelles méthodes pour les fusions de communes (UNIREG-3379).
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@56159 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg-wsclient-fidor/src/main/java/ch/vd/uniregctb/webservice/fidor/FidorClient.java
+++ unireg-wsclient-fidor/src/main/java/ch/vd/uniregctb/webservice/fidor/FidorClient.java
@@ -1,49 +1,113 @@
 package ch.vd.uniregctb.webservice.fidor;
 
+import javax.xml.datatype.XMLGregorianCalendar;
 import java.util.Collection;
+import java.util.List;
 
 import ch.vd.fidor.ws.v2.Acces;
+import ch.vd.fidor.ws.v2.CommuneFiscale;
+import ch.vd.fidor.ws.v2.FidorBusinessException_Exception;
 import ch.vd.fidor.ws.v2.Logiciel;
 import ch.vd.fidor.ws.v2.ParameterMap;
 import ch.vd.fidor.ws.v2.Pays;
 
+@SuppressWarnings({"UnusedDeclaration"})
 public interface FidorClient {
-	/**Retourne les informations d'un logiciel
-	 *
-	 * @param logicielId l'identifiant du logiciel
-	 * @return  les details du logiciel
-	 */
-	public Logiciel getLogicielDetail(long logicielId);
 
-	/**Retourne les information d'un pays
+	/**
+	 * Retourne une commune à partir de son numéro Ofs et d'une date de référence.
 	 *
+	 * @param ofsId un numéro Ofs de commune
+	 * @param date  une date de référence
+	 * @return la commune avec le numéro Ofs demandé valide à la date spécifiée; ou <b>null</b> si la commune est inconnue.
+	 * @throws ch.vd.fidor.ws.v2.FidorBusinessException_Exception
+	 *          en cas d'erreur métier
+	 */
+	CommuneFiscale getCommuneParNoOFS(int ofsId, XMLGregorianCalendar date) throws FidorBusinessException_Exception;
+
+	/**
+	 * Retourne une commune à partir de son numéro technique.
+	 *
+	 * @param noTechnique le numéro technique (interne à l'ACV) de la commune
+	 * @return la commune avec le numéro technique demandé; ou <b>null</b> si la commune est inconnue.
+	 * @throws ch.vd.fidor.ws.v2.FidorBusinessException_Exception
+	 *          en cas d'erreur métier
+	 */
+	CommuneFiscale getCommuneParNoTechnique(int noTechnique) throws FidorBusinessException_Exception;
+
+	/**
+	 * Retourne l'historique complet (notamment en cas de fusion, changement de nom, ..) d'une commune à partir de son numéro Ofs. Les communes retournées peuvent donc avoir des numéros Ofs différents de
+	 * celui spécifié, dans la mesure où elles représentent d'anciennes communes fusionnées.
+	 *
+	 * @param ofsId un numéro Ofs de commune
+	 * @return l'historique de la commune avec le numéro Ofs spécifié.
+	 * @throws ch.vd.fidor.ws.v2.FidorBusinessException_Exception
+	 *          en cas d'erreur métier
+	 */
+	List<CommuneFiscale> getCommunesHistoParNoOFS(int ofsId) throws FidorBusinessException_Exception;
+
+	/**
+	 * Retourne l'historique complet (notamment en cas de fusion, changement de nom, ..) d'une commune à partir de son numéro technique.
+	 *
+	 * @param noTechnique un numéro Ofs de commune
+	 * @return l'historique de la commune avec le numéro Ofs spécifié.
+	 * @throws ch.vd.fidor.ws.v2.FidorBusinessException_Exception
+	 *          en cas d'erreur métier
+	 */
+	List<CommuneFiscale> getCommunesHistoParNoTechnique(int noTechnique) throws FidorBusinessException_Exception;
+
+	/**
+	 * @param date une date ou <b>null</b> pour obtenir l'historique complet.
+	 * @return toutes les communes valides à la date demandée, ou l'historique complet des communes si la date est nulle.
+	 * @throws ch.vd.fidor.ws.v2.FidorBusinessException_Exception
+	 *          en cas d'erreur métier
+	 */
+	List<CommuneFiscale> getCommunes(XMLGregorianCalendar date) throws FidorBusinessException_Exception;
+
+	/**
+	 * @param ofsCommune le numéro Ofs de la commune d'annonce qui a associé la bâtiment avec une adresse.
+	 * @param egid       le numéro de bâtiment
+	 * @param date       une date de référence
+	 * @return la commune sur laquelle est sis le bâtiment identifié par son numéro Ofs.
+	 */
+	CommuneFiscale getCommuneParBatiment(int ofsCommune, int egid, XMLGregorianCalendar date);
+
+	/**
 	 * @param ofsId identifiant d'un pays
-	 * @return
+	 * @return les information d'un pays
 	 */
-	public Pays getPaysDetail(long ofsId);
+	Pays getPaysDetail(long ofsId);
 
-
-	/** Retourne la liste de tous les logiciels
-	 *
-	 * @return collection contenant tous les logiciels
-	 */
-	public Collection<Logiciel> getTousLesLogiciels();
-
-
-	/**Retourne la liste de tous les pays
-	 *
+	/**
+	 * Retourne la liste de tous les pays
 	 *
 	 * @return collection contenant la liste des pays
 	 */
-	public Collection<Pays> getTousLesPays();
+	Collection<Pays> getTousLesPays();
 
-	/**retourne l'url d'accès pour une application et une cible passées en paramètre
+	/**
+	 * Retourne les informations d'un logiciel
 	 *
-	 * @param app
-	 * @param acces
-	 * @param targetType
-	 * @param map
+	 * @param logicielId l'identifiant du logiciel
+	 * @return les details du logiciel
+	 */
+	Logiciel getLogicielDetail(long logicielId);
+
+	/**
+	 * Retourne la liste de tous les logiciels
+	 *
+	 * @return collection contenant tous les logiciels
+	 */
+	Collection<Logiciel> getTousLesLogiciels();
+
+	/**
+	 * retourne l'url d'accès pour une application et une cible passées en paramètre
+	 *
+	 * @param app        le code de l'application
+	 * @param acces      le type d'accès
+	 * @param targetType le type de target
+	 * @param map        les paramètres à intégrer à l'url
 	 * @return l'url
 	 */
-	public String getUrl(String app, Acces acces, String targetType, ParameterMap map);
+	String getUrl(String app, Acces acces, String targetType, ParameterMap map);
 }
--- unireg-wsclient-fidor/src/main/java/ch/vd/uniregctb/webservice/fidor/FidorClientImpl.java
+++ unireg-wsclient-fidor/src/main/java/ch/vd/uniregctb/webservice/fidor/FidorClientImpl.java
@@ -1,24 +1,28 @@
 package ch.vd.uniregctb.webservice.fidor;
 
+import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.ws.BindingProvider;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.cxf.message.Message;
 import org.apache.log4j.Logger;
-import org.springframework.util.Assert;
 import org.springframework.util.ResourceUtils;
 
 import ch.vd.fidor.ws.v2.Acces;
+import ch.vd.fidor.ws.v2.CommuneFiscale;
+import ch.vd.fidor.ws.v2.FidorBusinessException_Exception;
 import ch.vd.fidor.ws.v2.FidorPortType;
 import ch.vd.fidor.ws.v2.FidorService;
 import ch.vd.fidor.ws.v2.Logiciel;
 import ch.vd.fidor.ws.v2.ParameterMap;
 import ch.vd.fidor.ws.v2.Pays;
 
+@SuppressWarnings({"UnusedDeclaration"})
 public class FidorClientImpl implements FidorClient {
 
 	private static final Logger LOGGER = Logger.getLogger(FidorClientImpl.class);
@@ -27,15 +31,7 @@ public class FidorClientImpl implements FidorClient {
 	private String username;
 	private String password;
 
-	private String patternTaoPP;
-	private String patternTaoBA;
-	private String patternTaoIS;
-	private String patternSipf;
-
 	private FidorPortType service;
-
-	private long lastTentative = 0;
-	private static final long fiveMinutes = 5L * 60L * 1000000000L; // en nanosecondes
 
 	@SuppressWarnings({"UnusedDeclaration"})
 	public void setServiceUrl(String serviceUrl) {
@@ -54,7 +50,7 @@ public class FidorClientImpl implements FidorClient {
 
 
 	public Logiciel getLogicielDetail(long logicielId) {
-		initWebService();
+		init();
 		final Logiciel logiciel = service.getLogicielDetail(logicielId);
 		if (logiciel == null) {
 			LOGGER.error(String.format("Il manque le logiciel dont l'id est %s dans FiDoR !", logicielId));
@@ -62,8 +58,38 @@ public class FidorClientImpl implements FidorClient {
 		return logiciel;
 	}
 
+	public CommuneFiscale getCommuneParNoOFS(int ofsId, XMLGregorianCalendar date) throws FidorBusinessException_Exception {
+		init();
+		return service.getCommuneParNoOFS(ofsId, date);
+	}
+
+	public CommuneFiscale getCommuneParNoTechnique(int noTechnique) throws FidorBusinessException_Exception {
+		init();
+		return service.getCommuneParNoTechnique(noTechnique);
+	}
+
+	public List<CommuneFiscale> getCommunesHistoParNoOFS(int ofsId) throws FidorBusinessException_Exception {
+		init();
+		return service.getCommunesHistoParNoOFS(ofsId);
+	}
+
+	public List<CommuneFiscale> getCommunesHistoParNoTechnique(int noTechnique) throws FidorBusinessException_Exception {
+		init();
+		return service.getCommunesHistoParNoTechnique(noTechnique);
+	}
+
+	public List<CommuneFiscale> getCommunes(XMLGregorianCalendar date) throws FidorBusinessException_Exception {
+		init();
+		return service.getCommunes(date);
+	}
+
+	public CommuneFiscale getCommuneParBatiment(int ofsCommune, int egid, XMLGregorianCalendar date) {
+		init();
+		return service.getCommuneParBatiment(ofsCommune, egid, date);
+	}
+
 	public Pays getPaysDetail(long ofsId) {
-		initWebService();
+		init();
 		final Pays pays = service.getPaysDetail(ofsId);
 		if (pays == null) {
 			LOGGER.error(String.format("Il manque le pays dont l'id est %s dans FiDoR !", ofsId));
@@ -72,27 +98,25 @@ public class FidorClientImpl implements FidorClient {
 	}
 
 	public Collection<Logiciel> getTousLesLogiciels() {
-		initWebService();
+		init();
 		final Collection<Logiciel> logiciels = service.getTousLesLogiciels();
 		if (logiciels == null || logiciels.isEmpty()) {
 			LOGGER.error("Il manque la liste des logiciels dans FiDoR !");
-
 		}
 		return logiciels;
 	}
 
 	public Collection<Pays> getTousLesPays() {
-		initWebService();
+		init();
 		final Collection<Pays> listePays = service.getTousLesPays();
 		if (listePays == null || listePays.isEmpty()) {
 			LOGGER.error("Il manque la liste des pays dans FiDoR !");
-
 		}
 		return listePays;
 	}
 
 	public String getUrl(String app, Acces acces, String targetType, ParameterMap map) {
-		initWebService();
+		init();
 		final String url = service.getUrl(app, acces, targetType, map);
 		if (url == null) {
 			LOGGER.error(String.format("Il manque l'url d'accès à %s (target %s) dans FiDoR !", app, targetType));
@@ -100,33 +124,38 @@ public class FidorClientImpl implements FidorClient {
 		return url;
 	}
 
-
-	private void initWebService() {
-
+	private void init() {
 		if (service == null) {
-			final URL wsdlUrl;
-			try {
-				wsdlUrl = ResourceUtils.getURL("classpath:fidor-v2.wsdl");
-			}
-			catch (FileNotFoundException e) {
-				throw new RuntimeException(e);
-			}
-
-			final FidorService ts = new FidorService(wsdlUrl);
-			service = ts.getFidorPort();
-			final Map<String, Object> context = ((BindingProvider) service).getRequestContext();
-			if (StringUtils.isNotBlank(username)) {
-				context.put(BindingProvider.USERNAME_PROPERTY, username);
-				context.put(BindingProvider.PASSWORD_PROPERTY, password);
-			}
-			context.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, serviceUrl);
-
-			// Désactive la validation du schéma (= ignore silencieusement les éléments inconnus), de manière à permettre l'évolution ascendante-compatible du WSDL.
-			context.put(Message.SCHEMA_VALIDATION_ENABLED, false);
-			context.put("set-jaxb-validation-event-handler", false);
-
+			service = initWebService();
 		}
 	}
 
+	private synchronized FidorPortType initWebService() {
+		if (service != null) {
+			return service;
+		}
 
+		final URL wsdlUrl;
+		try {
+			wsdlUrl = ResourceUtils.getURL("classpath:fidor-v2.wsdl");
+		}
+		catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+		final FidorService ts = new FidorService(wsdlUrl);
+		final FidorPortType port = ts.getFidorPort();
+		final Map<String, Object> context = ((BindingProvider) port).getRequestContext();
+		if (StringUtils.isNotBlank(username)) {
+			context.put(BindingProvider.USERNAME_PROPERTY, username);
+			context.put(BindingProvider.PASSWORD_PROPERTY, password);
+		}
+		context.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, serviceUrl);
+
+		// Désactive la validation du schéma (= ignore silencieusement les éléments inconnus), de manière à permettre l'évolution ascendante-compatible du WSDL.
+		context.put(Message.SCHEMA_VALIDATION_ENABLED, false);
+		context.put("set-jaxb-validation-event-handler", false);
+
+		return port;
+	}
 }
