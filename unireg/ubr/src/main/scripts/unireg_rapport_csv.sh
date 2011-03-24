d46c22450a7b966672de135e266fd1d9a6b96c8a
Jacob Deker <jacob.deker@vd.ch>
2011-03-24 07:05:23 +0000

    Ajouté l'utilisation de la variable d'environnement JAVA_HOME pour lancer la JVM (utile dans les environnements torturés qui mélangent allègrement les versions de JVM)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@56234 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/ubr/src/main/scripts/unireg_rapport_csv.sh
+++ unireg/ubr/src/main/scripts/unireg_rapport_csv.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -z "$JAVA_HOME" ]; then
+	echo "Environment variable JAVA_HOME must be set!" >&2
+	exit 1
+elif [ ! -e "$JAVA_HOME/bin/java" ]; then
+	echo "File $JAVA_HOME/bin/java cannot be found!" >&2
+fi
+
 dir=lib
 cp=ubr.jar
 for i in $(ls $dir); do
@@ -7,4 +14,4 @@ for i in $(ls $dir); do
 done
 
 #echo "CLasspath: $cp"
-java -cp $cp ch.vd.uniregctb.rapport.RapportCsvExtractorApp $@
+$JAVA_HOME/bin/java -cp $cp ch.vd.uniregctb.rapport.RapportCsvExtractorApp $@
