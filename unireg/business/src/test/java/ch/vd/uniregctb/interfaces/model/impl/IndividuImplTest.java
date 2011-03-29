f1b1be40cf232bde841d036d2e8edb8e17115688
Jacob Deker <jacob.deker@vd.ch>
2011-03-29 15:00:50 +0000

    [SIFISC-35] Les adresses civiles avec une date de début dans le futur doivent être ignorées, et les adresses civiles avec une date de fin dans le futur doivent être considérées comme n'ayant pas de date de fin
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@56529 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/test/java/ch/vd/uniregctb/interfaces/model/impl/IndividuImplTest.java
+++ unireg/business/src/test/java/ch/vd/uniregctb/interfaces/model/impl/IndividuImplTest.java
@@ -1,14 +1,17 @@
 package ch.vd.uniregctb.interfaces.model.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 
+import ch.vd.registre.base.date.DateHelper;
 import ch.vd.registre.base.date.RegDate;
 import ch.vd.registre.civil.model.EnumTypeEtatCivil;
 import ch.vd.registre.civil.model.impl.EtatCivilImpl;
 import ch.vd.uniregctb.common.WithoutSpringTest;
+import ch.vd.uniregctb.interfaces.model.Adresse;
 import ch.vd.uniregctb.interfaces.model.EtatCivil;
 import ch.vd.uniregctb.interfaces.model.TypeEtatCivil;
 
@@ -47,5 +50,44 @@ public class IndividuImplTest extends WithoutSpringTest {
 		assertEquals(0, etatCivil.getNoSequence());
 		assertNull(etatCivil.getDateDebutValidite());
 		assertEquals(TypeEtatCivil.DIVORCE, etatCivil.getTypeEtatCivil());
+	}
+
+	/**
+	 * [SIFISC-35] problématique des adresses civiles dans le futur
+	 */
+	@Test
+	public void testAdresseCivileDebutDansLeFutur() throws Exception {
+
+		final ch.vd.registre.civil.model.impl.IndividuImpl individu = new ch.vd.registre.civil.model.impl.IndividuImpl();
+		individu.setNoTechnique(536824);
+
+		final ch.vd.common.model.impl.AdresseImpl adresse = new ch.vd.common.model.impl.AdresseImpl();
+		adresse.setDateDebutValidite(DateHelper.getFirstDayOfNextMonth(DateHelper.getCurrentDate()));
+		individu.setAdresses(Arrays.asList(adresse));
+
+		final IndividuImpl wrapper = new IndividuImpl(individu);
+		assertEquals(0, wrapper.getAdresses().size());
+	}
+
+	/**
+	 * [SIFISC-35] problématique des adresses civiles dans le futur
+	 */
+	@Test
+	public void testAdresseCivileFinDansLeFutur() throws Exception {
+
+		final ch.vd.registre.civil.model.impl.IndividuImpl individu = new ch.vd.registre.civil.model.impl.IndividuImpl();
+		individu.setNoTechnique(536824);
+
+		final ch.vd.common.model.impl.AdresseImpl adresse = new ch.vd.common.model.impl.AdresseImpl();
+		adresse.setDateDebutValidite(DateHelper.getDate(2001, 9, 12));
+		adresse.setDateFinValidite(DateHelper.getFirstDayOfNextMonth(DateHelper.getCurrentDate()));
+		individu.setAdresses(Arrays.asList(adresse));
+
+		final IndividuImpl wrapper = new IndividuImpl(individu);
+		assertEquals(1, wrapper.getAdresses().size());
+
+		final Adresse adresseWrapper = wrapper.getAdresses().iterator().next();
+		assertEquals(RegDate.get(2001, 9, 12), adresseWrapper.getDateDebut());
+		assertNull(adresseWrapper.getDateFin());
 	}
 }
