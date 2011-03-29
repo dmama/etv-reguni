75d380992aeb0b8cd1556486c9f0a5da8c73983f
Jacob Deker <jacob.deker@vd.ch>
2011-03-29 07:37:09 +0000

    Corrigé le cas où aucun log ne fait plus de 200 Mo
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@56486 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/scripts/logs/compress-big-logs.sh
+++ unireg/scripts/logs/compress-big-logs.sh
@@ -1,3 +1,3 @@
 #! /bin/bash -
 # Compresse les logs qui font plus de 200M déjà en avance (= ne pas forcément attendre 10 jours)
-find $HOME/logs -name "*.log.????-??-??" -size +200M -print0 | xargs -0 lzma
+find $HOME/logs -name "*.log.????-??-??" -size +200M -exec lzma {} \;
