3cbf64dce4115a0839fbe2059f05f242c0cef6a1
Manuel Siggen <manuel.siggen@vd.ch>
2011-05-09 11:56:12 +0000

    Corrigé le test suite à une correction du service Acicom en intégration qui retourne un fichier zippé s'il y a plus d'une pièce attachée à un message.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/4.10@58987 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business-it/src/test/java/ch/vd/uniregctb/identification/contribuable/AciComServiceTest.java
+++ unireg/business-it/src/test/java/ch/vd/uniregctb/identification/contribuable/AciComServiceTest.java
@@ -23,8 +23,8 @@ public class AciComServiceTest extends BusinessItTest {
 		final String businessId = "3001-000101-2-BE-5-Test_d26bb202-ac04-47e8-a356-9177886";
 		final FichierOrigine file = service.getMessageFile(businessId);
 		assertNotNull(file);
-		assertEquals("pdf", file.getExtension());
-		assertEquals("application/pdf", file.getMimeType());
+		assertEquals("zip", file.getExtension());
+		assertEquals("application/zip", file.getMimeType());
 		// suite au reprise de données, le fichier PDF lui-même est perdu : assertNotNull(file.getContent());
 	}
 }
