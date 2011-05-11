94e567119f6137988d210d557013639d5c871dfb
Manuel Siggen <manuel.siggen@vd.ch>
2011-05-11 14:19:52 +0000

    [SIFISC-1189] Factorisé la méthode 'extractBoundaries' dans la classe 'TimelineHelper'.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@59191 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/java/ch/vd/uniregctb/tiers/timeline/AdresseTimelineController.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/tiers/timeline/AdresseTimelineController.java
@@ -61,61 +61,6 @@ public class AdresseTimelineController {
 	}
 
 	/**
-	 * A partir d'une liste de ranges (qui peuvent se recouvrir), détermine la liste triées par ordre croissant des dates de début et de fin des ranges. Chaque date n'apparaît qu'une fois dans la liste.
-	 * <p/>
-	 * <b>Note:</b> Si au moins un des ranges possède une date de début nulle, la première date de la liste est nulle. De même, si au moins un des ranges possède une date de fin nulle, la dernière date
-	 * de la liste est nulle.
-	 * <p/>
-	 * <pre>
-	 * Ranges d'entrées:
-	 * +----------------------+                           +----------------+
-	 * ¦x                    y¦                           ¦u              v¦
-	 * +----------------------+                           +----------------+
-	 *                      +----------------------+              +----------------+
-	 *                      ¦y                    z¦              ¦r              s¦
-	 *                      +----------------------+              +----------------+
-	 * .
-	 * Dates de sortie: x, y, y+1, z+1, u, r, v+1, s+1
-	 * </pre>
-	 */
-	public static List<RegDate> extractBoundaries(Collection<? extends DateRange> ranges) {
-
-		boolean dateDebutNull = false;
-		boolean dateFinNull = false;
-
-		// Détermination de l'ensemble des dates
-		final SortedSet<RegDate> dates = new TreeSet<RegDate>();
-		for (DateRange r : ranges) {
-			final RegDate dateDebut = r.getDateDebut();
-			if (dateDebut == null) {
-				dateDebutNull = true;
-			}
-			else {
-				dates.add(dateDebut);
-			}
-			RegDate dateFin = r.getDateFin();
-			if (dateFin == null) {
-				dateFinNull = true;
-			}
-			else {
-				dates.add(dateFin.getOneDayAfter());
-			}
-		}
-
-		// Transforme l'ensemble en liste + traitement des valeurs nulles
-		List<RegDate> list = new ArrayList<RegDate>(dates);
-		if (dateDebutNull) {
-			list.add(0, null);
-		}
-		if (dateFinNull) {
-			list.add(null);
-		}
-
-		return list;
-	}
-
-
-	/**
 	 * Remplit les structures de données nécessaire à l'affichage de l'historique du tiers
 	 */
 	private void fillTimeline(AdresseTimelineView bean) {
@@ -171,7 +116,7 @@ public class AdresseTimelineController {
 		columns.add(AdresseCouche.RESULTAT);
 
 		final List<DateRange> periodes = new ArrayList<DateRange>();
-		final List<RegDate> boundaries = extractBoundaries(ranges);
+		final List<RegDate> boundaries = TimelineHelper.extractBoundaries(ranges);
 		for (int i = 1; i < boundaries.size(); i++) {
 			RegDate previous = boundaries.get(i - 1);
 			RegDate current = boundaries.get(i);
--- unireg/web/src/main/java/ch/vd/uniregctb/tiers/timeline/TiersTimelineController.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/tiers/timeline/TiersTimelineController.java
@@ -1,10 +1,7 @@
 package ch.vd.uniregctb.tiers.timeline;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
@@ -86,61 +83,6 @@ public class TiersTimelineController {
 	}
 
 	/**
-	 * A partir d'une liste de ranges (qui peuvent se recouvrir), détermine la liste triées par ordre croissant des dates de début et de fin des ranges. Chaque date n'apparaît qu'une fois dans la liste.
-	 * <p/>
-	 * <b>Note:</b> Si au moins un des ranges possède une date de début nulle, la première date de la liste est nulle. De même, si au moins un des ranges possède une date de fin nulle, la dernière date
-	 * de la liste est nulle.
-	 * <p/>
-	 * <pre>
-	 * Ranges d'entrées:
-	 * +----------------------+                           +----------------+
-	 * ¦x                    y¦                           ¦u              v¦
-	 * +----------------------+                           +----------------+
-	 *                      +----------------------+              +----------------+
-	 *                      ¦y                    z¦              ¦r              s¦
-	 *                      +----------------------+              +----------------+
-	 * .
-	 * Dates de sortie: x, y, y+1, z+1, u, r, v+1, s+1
-	 * </pre>
-	 */
-	public static List<RegDate> extractBoundaries(Collection<? extends DateRange> ranges) {
-
-		boolean dateDebutNull = false;
-		boolean dateFinNull = false;
-
-		// Détermination de l'ensemble des dates
-		final SortedSet<RegDate> dates = new TreeSet<RegDate>();
-		for (DateRange r : ranges) {
-			final RegDate dateDebut = r.getDateDebut();
-			if (dateDebut == null) {
-				dateDebutNull = true;
-			}
-			else {
-				dates.add(dateDebut);
-			}
-			RegDate dateFin = r.getDateFin();
-			if (dateFin == null) {
-				dateFinNull = true;
-			}
-			else {
-				dates.add(dateFin.getOneDayAfter());
-			}
-		}
-
-		// Transforme l'ensemble en liste + traitement des valeurs nulles
-		List<RegDate> list = new ArrayList<RegDate>(dates);
-		if (dateDebutNull) {
-			list.add(0, null);
-		}
-		if (dateFinNull) {
-			list.add(null);
-		}
-
-		return list;
-	}
-
-
-	/**
 	 * Remplit les structures de données nécessaire à l'affichage de l'historique du tiers
 	 */
 	private void fillTimeline(TiersTimelineView bean) {
@@ -208,7 +150,7 @@ public class TiersTimelineController {
 		ranges.addAll(forsGestion);
 
 		final List<DateRange> periodes = new ArrayList<DateRange>();
-		final List<RegDate> boundaries = extractBoundaries(ranges);
+		final List<RegDate> boundaries = TimelineHelper.extractBoundaries(ranges);
 		RegDate previous = null;
 		for (RegDate current : boundaries) {
 			if (previous != null) {
