c0d40423eba057fd431fc32f8cc3bd787410a905
Manuel Siggen <manuel.siggen@vd.ch>
2011-05-24 08:55:35 +0000

    Version 1.0.0: ajouté la méthode 'equals' sur la classe Date.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@59839 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

---
+++ jaxb-unireg-ws-plugin/src/main/java/ch/vd/uniregctb/UniregCustomWsPlugin.java
@@ -0,0 +1,52 @@
+package ch.vd.uniregctb;
+
+import java.lang.reflect.Field;
+import java.util.Iterator;
+import java.util.logging.Logger;
+
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JMethod;
+import com.sun.tools.xjc.Options;
+import com.sun.tools.xjc.Plugin;
+import com.sun.tools.xjc.outline.ClassOutline;
+import com.sun.tools.xjc.outline.Outline;
+import org.xml.sax.ErrorHandler;
+
+public class UniregCustomWsPlugin extends Plugin {
+
+	private static final Logger LOGGER = Logger.getLogger(UniregCustomWsPlugin.class.getName());
+
+	private static final String EQUALS_DATE = "\n@Override\n" +
+			"public boolean equals(Object obj) {\n" +
+			"\tif (this == obj)\n" +
+			"\t\treturn true;\n" +
+			"\tif (obj == null)\n" +
+			"\t\treturn false;\n" +
+			"\tif (getClass() != obj.getClass())\n" +
+			"\t\treturn false;\n" +
+			"\tDate other = (Date) obj;\n" +
+			"\treturn day == other.day && month == other.month && year == other.year;\n" +
+			"}";
+
+	@Override
+	public String getOptionName() {
+		return "Xunireg-ws";
+	}
+
+	@Override
+	public String getUsage() {
+		return "  -Xunireg-ws        :  special customizations for Unireg";
+	}
+
+	@Override
+	public boolean run(final Outline outline, final Options options, final ErrorHandler errorHandler) {
+		// For each defined class
+
+		for (final ClassOutline classOutline : outline.getClasses()) {
+			if (classOutline.implClass.name().equals("Date")) {
+				classOutline.implClass.direct(EQUALS_DATE);
+			}
+		}
+		return true;
+	}
+}
