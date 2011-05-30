083d9e34ccadbfafaf813e318105e30032fdc591
Manuel Siggen <manuel.siggen@vd.ch>
2011-05-30 08:42:56 +0000

    Version 1.1.0: ajouté les méthodes 'hashCode' et 'compareTo' sur les dates.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@60181 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- jaxb-unireg-ws-plugin/src/main/java/ch/vd/uniregctb/UniregCustomWsPlugin.java
+++ jaxb-unireg-ws-plugin/src/main/java/ch/vd/uniregctb/UniregCustomWsPlugin.java
@@ -1,32 +1,53 @@
 package ch.vd.uniregctb;
 
-import java.lang.reflect.Field;
-import java.util.Iterator;
-import java.util.logging.Logger;
-
-import com.sun.codemodel.JDefinedClass;
-import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JClass;
 import com.sun.tools.xjc.Options;
 import com.sun.tools.xjc.Plugin;
 import com.sun.tools.xjc.outline.ClassOutline;
 import com.sun.tools.xjc.outline.Outline;
 import org.xml.sax.ErrorHandler;
 
+@SuppressWarnings({"UnusedDeclaration"})
 public class UniregCustomWsPlugin extends Plugin {
 
-	private static final Logger LOGGER = Logger.getLogger(UniregCustomWsPlugin.class.getName());
+	//private static final Logger LOGGER = Logger.getLogger(UniregCustomWsPlugin.class.getName());
 
-	private static final String EQUALS_DATE = "\n    @Override\n" +
+	private static final String DATE_EQUALS = "\n    @Override\n" +
 			"    public boolean equals(Object obj) {\n" +
 			"        if (this == obj)\n" +
 			"            return true;\n" +
 			"        if (obj == null)\n" +
-			"        return false;\n" +
-			"    if (getClass() != obj.getClass())\n" +
-			"        return false;\n" +
-			"    Date other = (Date) obj;\n" +
-			"    return day == other.day && month == other.month && year == other.year;\n" +
-			"}";
+			"            return false;\n" +
+			"        if (getClass() != obj.getClass())\n" +
+			"            return false;\n" +
+			"        Date other = (Date) obj;\n" +
+			"        return day == other.day && month == other.month && year == other.year;\n" +
+			"    }\n";
+	
+	private static final String DATE_HASHCODE = "\n    @Override\n" +
+			"    public int hashCode() {\n" +
+			"        final int prime = 31;\n" +
+			"        int result = 1;\n" +
+			"        result = prime * result + day;\n" +
+			"        result = prime * result + month;\n" +
+			"        result = prime * result + year;\n" +
+			"        return result;\n" +
+			"    }\n";
+	
+	private static final String DATE_COMPARETO = "\n    @Override\n" +
+			"    public int compareTo(Date o) {\n" +
+			"        if (this.year == o.year) {\n" +
+			"            if (this.month == o.month) {\n" +
+			"                return this.day - o.day;\n" +
+			"            }\n" +
+			"            else {\n" +
+			"                return this.month - o.month;\n" +
+			"            }\n" +
+			"        }\n" +
+			"        else {\n" +
+			"            return this.year - o.year;\n" +
+			"        }\n" +
+			"    }\n";
 
 	@Override
 	public String getOptionName() {
@@ -44,7 +65,11 @@ public class UniregCustomWsPlugin extends Plugin {
 
 		for (final ClassOutline classOutline : outline.getClasses()) {
 			if (classOutline.implClass.name().equals("Date")) {
-				classOutline.implClass.direct(EQUALS_DATE);
+				final JClass comparableClass = classOutline.implClass.owner().ref("java.lang.Comparable<Date>");
+				classOutline.implClass._implements(comparableClass);
+				classOutline.implClass.direct(DATE_HASHCODE);
+				classOutline.implClass.direct(DATE_EQUALS);
+				classOutline.implClass.direct(DATE_COMPARETO);
 			}
 		}
 		return true;
