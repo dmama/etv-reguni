4bdf628ab0245e81a4656d5d2b8b620fc82b23b7
Jacob Deker <jacob.deker@vd.ch>
2011-07-14 05:14:23 +0000

    Explicitement exclu le répertoire des logs d'intégration des répertoires ciblés pour la compression des gros logs
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@62333 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/scripts/logs/compress-big-logs.sh
+++ unireg/scripts/logs/compress-big-logs.sh
@@ -1,6 +1,7 @@
 #! /bin/bash -
 # Compresse les logs qui font plus de 200M déjà en avance (= ne pas forcément attendre 10 jours)
-find $HOME/logs -name "*.log.????-??-??" -size +200M | while read FILE; do
+# JDE, 14.07.2011 : on ne compresse pas les fichiers dans le répertoire IN (pour l'intégration) car ils sont obtenus par rsync
+find $HOME/logs -name "*.log.????-??-??" -size +200M | grep -v "/IN/" | while read FILE; do
 	BEFORE=$(stat --format "%s" "$FILE")
 	lzma "$FILE"
 	AFTER=$(stat --format "%s" "$FILE.lzma")
