2cfe25b643fd75d8c0a7a66f72623fde7c183d67
Manuel Siggen <manuel.siggen@vd.ch>
2011-07-20 08:05:56 +0000

    Corrigé le test suite aux modifications de namespace apportées dans le project 'unireg-jaxb2-beans'.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@62870 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business-it/src/test/java/ch/vd/uniregctb/evenement/di/EvenementDeclarationSenderTest.java
+++ unireg/business-it/src/test/java/ch/vd/uniregctb/evenement/di/EvenementDeclarationSenderTest.java
@@ -42,8 +42,8 @@ public class EvenementDeclarationSenderTest extends EvenementTest {
 		clearQueue(OUTPUT_QUEUE);
 
 		final ESBXMLValidator esbValidator = new ESBXMLValidator();
-		esbValidator.setSources(new Resource[]{new ClassPathResource("common/unireg-common-1.xsd"), new ClassPathResource("di/evenementDeclarationImpot-common-1.xsd"),
-				new ClassPathResource("di/evenementDeclarationImpot-output-1.xsd")});
+		esbValidator.setSources(new Resource[]{new ClassPathResource("unireg-common-1.xsd"), new ClassPathResource("event/di/evenementDeclarationImpot-common-1.xsd"),
+				new ClassPathResource("event/di/evenementDeclarationImpot-output-1.xsd")});
 
 		esbMessageFactory = new EsbMessageFactory();
 		esbMessageFactory.setValidator(esbValidator);
@@ -62,12 +62,13 @@ public class EvenementDeclarationSenderTest extends EvenementTest {
 		assertTextMessage(OUTPUT_QUEUE,
 				"<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
 						"<ns3:evenement " +
-						"xmlns:ns3=\"http://www.vd.ch/fiscalite/registre/evenementDeclarationImpot/output/1\" " +
-						"xmlns=\"http://www.vd.ch/fiscalite/registre/evenementDeclarationImpot/common/1\" " +
+						"xmlns:ns3=\"http://www.vd.ch/unireg/event/di/output/1\" " +
+						"xmlns=\"http://www.vd.ch/unireg/event/di/common/1\" " +
 						"xmlns:ns2=\"http://www.vd.ch/unireg/common/1\" " +
 						"xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
 						"xsi:type=\"ns3:evenementEmissionDeclarationImpotType\">" +
-						"<ns3:context><periodeFiscale>2000</periodeFiscale>" +
+						"<ns3:context>" +
+						"<periodeFiscale>2000</periodeFiscale>" +
 						"<numeroContribuable>12344556</numeroContribuable>" +
 						"<date><ns2:year>2000</ns2:year><ns2:month>1</ns2:month><ns2:day>1</ns2:day></date>" +
 						"</ns3:context>" +
@@ -83,8 +84,8 @@ public class EvenementDeclarationSenderTest extends EvenementTest {
 		assertTextMessage(OUTPUT_QUEUE,
 				"<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
 						"<ns3:evenement " +
-						"xmlns:ns3=\"http://www.vd.ch/fiscalite/registre/evenementDeclarationImpot/output/1\" " +
-						"xmlns=\"http://www.vd.ch/fiscalite/registre/evenementDeclarationImpot/common/1\" " +
+						"xmlns:ns3=\"http://www.vd.ch/unireg/event/di/output/1\" " +
+						"xmlns=\"http://www.vd.ch/unireg/event/di/common/1\" " +
 						"xmlns:ns2=\"http://www.vd.ch/unireg/common/1\" " +
 						"xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
 						"xsi:type=\"ns3:evenementAnnulationDeclarationImpotType\">" +
