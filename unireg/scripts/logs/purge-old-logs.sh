08bf9e5932d77a7442d02e392273e2dbab06422b
Jacob Deker <jacob.deker@vd.ch>
2011-09-13 05:32:24 +0000

    Ajouter un nouveau script pour purger les vieux logs en précisant une durée de conservation limite qui peut varier d'un environnement à l'autre
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@65707 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

---
+++ unireg/scripts/logs/purge-old-logs.sh
@@ -0,0 +1,18 @@
+#! /bin/bash -
+
+ENVIRONMENT="$1"
+LIMIT="$2"
+if [ -z "$ENVIRONMENT" ]; then
+        echo "Syntaxe : $(basename "$0") <env> <days> avec <env> l'un de PR, PO, VA, PP, FO et <days> l'âge maximal de conservation des logs dans cet environnement" >&2
+        exit 1
+elif [[ ! "$ENVIRONMENT" =~ ^(PR|PO|VA|PP|FO)$ ]]; then
+        echo "Pour l'environnement, seuls PR, PO, VA, PP et FO sont acceptés (trouvé : '$ENVIRONMENT')" >&2
+        exit 1
+elif [[ ! "$LIMIT" =~ ^0*[1-9][0-9]*$ ]]; then
+	echo "L'âge maximal de conservation des logs doit être un entier positif (trouvé : '$LIMIT')" >&2
+	exit 1
+fi
+
+find ~/logs/$ENVIRONMENT -name "*.log*" -type f -mtime +$LIMIT | while read FILE; do if [ -e "$FILE" ]; then rm -f "$FILE"; fi; done
+find ~/logs/$ENVIRONMENT -type d -empty | while read DIR; do if [ -d "$DIR" ]; then rmdir "$DIR"; fi; done
+find ~/logs/$ENVIRONMENT -mindepth 2 -type d -mtime +$(($LIMIT + 31)) ! -path "*/repository/*" | while read DIR; do if [ -d "$DIR" ]; then rm -rf "$DIR"; fi; done
