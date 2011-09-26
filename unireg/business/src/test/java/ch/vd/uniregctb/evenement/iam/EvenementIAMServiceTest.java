9f33fe7a241a322d9afe9962509c2b44b9a9d9a0
Baba Issa Ngom <baba-issa.ngom@vd.ch>
2011-09-26 08:51:59 +0000

    [SIFISC-2063] Renvoyer une exception lors ce que l'action du message IAM est incoherente avec le contenu du message: Action create sans informations employeurs.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@66382 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/test/java/ch/vd/uniregctb/evenement/iam/EvenementIAMServiceTest.java
+++ unireg/business/src/test/java/ch/vd/uniregctb/evenement/iam/EvenementIAMServiceTest.java
@@ -40,7 +40,7 @@ public class EvenementIAMServiceTest extends BusinessTest {
 			public Long execute(TransactionStatus status) throws Exception {
 
 				// Un tiers tout ce quil y a de plus ordinaire
-				final DebiteurPrestationImposable siggenAirlines = addDebiteur(CategorieImpotSource.REGULIERS, PeriodiciteDecompte.TRIMESTRIEL,date(2010,1,1));
+				final DebiteurPrestationImposable siggenAirlines = addDebiteur(CategorieImpotSource.REGULIERS, PeriodiciteDecompte.TRIMESTRIEL, date(2010, 1, 1));
 				return siggenAirlines.getNumero();
 			}
 		});
@@ -85,7 +85,7 @@ public class EvenementIAMServiceTest extends BusinessTest {
 			public Long execute(TransactionStatus status) throws Exception {
 
 				// Un tiers tout ce quil y a de plus ordinaire
-				final DebiteurPrestationImposable siggenAirlines = addDebiteur(CategorieImpotSource.REGULIERS, PeriodiciteDecompte.TRIMESTRIEL,date(2010,1,1));
+				final DebiteurPrestationImposable siggenAirlines = addDebiteur(CategorieImpotSource.REGULIERS, PeriodiciteDecompte.TRIMESTRIEL, date(2010, 1, 1));
 				return siggenAirlines.getNumero();
 			}
 		});
@@ -130,7 +130,7 @@ public class EvenementIAMServiceTest extends BusinessTest {
 			public Long execute(TransactionStatus status) throws Exception {
 
 				// Un tiers tout ce quil y a de plus ordinaire
-				final DebiteurPrestationImposable siggenAirlines = addDebiteur(CategorieImpotSource.REGULIERS, PeriodiciteDecompte.TRIMESTRIEL,date(2010,1,1));
+				final DebiteurPrestationImposable siggenAirlines = addDebiteur(CategorieImpotSource.REGULIERS, PeriodiciteDecompte.TRIMESTRIEL, date(2010, 1, 1));
 				siggenAirlines.setModeCommunication(ModeCommunication.SITE_WEB);
 				siggenAirlines.setLogicielId(1L);
 				return siggenAirlines.getNumero();
@@ -143,8 +143,8 @@ public class EvenementIAMServiceTest extends BusinessTest {
 			public Long execute(TransactionStatus status) throws Exception {
 
 				// Un tiers tout ce quil y a de plus ordinaire
-				final DebiteurPrestationImposable siggenHolding = addDebiteur(CategorieImpotSource.REGULIERS, PeriodiciteDecompte.ANNUEL,date(2010,1,1));
-				 siggenHolding.setModeCommunication(ModeCommunication.PAPIER);
+				final DebiteurPrestationImposable siggenHolding = addDebiteur(CategorieImpotSource.REGULIERS, PeriodiciteDecompte.ANNUEL, date(2010, 1, 1));
+				siggenHolding.setModeCommunication(ModeCommunication.PAPIER);
 				return siggenHolding.getNumero();
 			}
 		});
@@ -194,4 +194,39 @@ public class EvenementIAMServiceTest extends BusinessTest {
 			}
 		});
 	}
+
+	@Test
+	@Transactional(rollbackFor = Throwable.class)
+	public void testModifierInfoEmployeurIncoherenceAction() throws Exception {
+
+		// Création d'un débiteur
+		final Long id = doInNewTransaction(new TxCallback<Long>() {
+			@Override
+			public Long execute(TransactionStatus status) throws Exception {
+
+				// Un tiers tout ce quil y a de plus ordinaire
+				final DebiteurPrestationImposable siggenAirlines = addDebiteur(CategorieImpotSource.REGULIERS, PeriodiciteDecompte.TRIMESTRIEL, date(2010, 1, 1));
+				return siggenAirlines.getNumero();
+			}
+		});
+
+		// Simule la réception d'un enregistrement de debiteur
+
+		final InfoEmployeur infoEmployeur = new InfoEmployeur();
+		infoEmployeur.setNoEmployeur(id);
+		infoEmployeur.setModeCommunication(ModeCommunication.ELECTRONIQUE);
+		List<InfoEmployeur> listeEmp = new ArrayList<InfoEmployeur>();
+		listeEmp.add(infoEmployeur);
+		final EnregistrementEmployeur enregistrementEmployeur = new EnregistrementEmployeur();
+
+		try {
+			service.onEnregistrementEmployeur(enregistrementEmployeur);
+		}
+		catch (EvenementIAMException e) {
+			assertEquals("Informations employeurs absentes pour une action create ou update", e.getMessage());
+		}
+
+
+	}
+
 }
