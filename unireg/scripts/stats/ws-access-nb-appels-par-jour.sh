e934ca379b7b4e7985c4f280b5628698d9f28a25
Jacob Deker <jacob.deker@vd.ch>
2011-11-04 07:26:29 +0000

    Renommé le script + adapté aux logs du nouveau service Party 3
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@68537 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/scripts/stats/ws-access-nb-appels-par-jour.sh
+++ unireg/scripts/stats/ws-access-nb-appels-par-jour.sh
@@ -1,4 +1,6 @@
 #! /bin/bash -
+# Calcul du nombre d'appels moyen par jour et par contribuable (on se base exclusivement sur les appels unitaires - pas les GetBatch... - pour lesquels
+# le nom du paramètre du numéro du tiers est tiersNumber - v2 - ou partyNumber - v3)
 
 echo "DATE;NB_CTB_DIFFERENTS;NB_APPELS_TOTAL;MAX_APPELS_PAR_CTB;MOYENNE_APPELS_PAR_CTB"
 
@@ -6,7 +8,7 @@ echo "DATE;NB_CTB_DIFFERENTS;NB_APPELS_TOTAL;MAX_APPELS_PAR_CTB;MOYENNE_APPELS_P
 # 1. date
 # 2. nombre de contribuables différents appelés ce jour-là
 # 3. nombre total d'appels concernant ces contribuables
-# 4. nombre maximal d'appels qu'à reçu un contribuable individuel
+# 4. nombre maximal d'appels qu'a reçu un contribuable individuel
 function dump() {
 	MEAN=$(python -c "print $3.0/$2")
 	echo "$1;$2;$3;$4;$MEAN"
@@ -33,7 +35,7 @@ while read NB DATE CTB; do
 		MAX_CALLS_PER_CTB="$NB"
 	fi
 
-done < <(grep -h "tiersNumber=" "$@" | awk '{ print $3 "\t" $0; }' | sed -e 's/^.\([^\t]\+\)\t.*tiersNumber=\([0-9]\+\).*$/\1\t\2/' | sort | uniq -c)
+done < <(grep -h "\(tiersNumber\|partyNumber\)=" "$@" | awk '{ print $3 "\t" $0; }' | sed -e 's/^.\([^\t]\+\)\t.*\(tiersNumber\|partyNumber\)=\([0-9]\+\).*$/\1\t\3/' | sort | uniq -c)
 
 if [ "$NB_CALLS" -gt 0 ]; then
 	dump "$OLD_DATE" "$NB_CTB" "$NB_CALLS" "$MAX_CALLS_PER_CTB"
