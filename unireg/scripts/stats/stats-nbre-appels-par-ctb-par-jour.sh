e7349c0ac88ffc47e8557bf3a562676f9bf5ea93
Jacob Deker <jacob.deker@vd.ch>
2011-11-04 07:00:28 +0000

    Ajouté le calcul de la moyenne d'appels par contribuable + extrait la date du contenu du log et plus du nom de fichier
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@68533 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/scripts/stats/stats-nbre-appels-par-ctb-par-jour.sh
+++ unireg/scripts/stats/stats-nbre-appels-par-ctb-par-jour.sh
@@ -1,25 +1,40 @@
 #! /bin/bash -
 
-echo "DATE;NB_CTB;NB_CALLS"
+echo "DATE;NB_CTB_DIFFERENTS;NB_APPELS_TOTAL;MAX_APPELS_PAR_CTB;MOYENNE_APPELS_PAR_CTB"
 
-SUM_CALLS=0
+# paramètres :
+# 1. date
+# 2. nombre de contribuables différents appelés ce jour-là
+# 3. nombre total d'appels concernant ces contribuables
+# 4. nombre maximal d'appels qu'à reçu un contribuable individuel
+function dump() {
+	MEAN=$(python -c "print $3.0/$2")
+	echo "$1;$2;$3;$4;$MEAN"
+}
+
+NB_CALLS=0
 NB_CTB=0
+MAX_CALLS_PER_CTB=0
 OLD_DATE=""
 while read NB DATE CTB; do
 
 	if [ "$DATE" != "$OLD_DATE" ]; then
-		if [ "$SUM_CALLS" -gt 0 ]; then
-			echo "$OLD_DATE;$NB_CTB;$SUM_CALLS"
+		if [ "$NB_CALLS" -gt 0 ]; then
+			dump "$OLD_DATE" "$NB_CTB" "$NB_CALLS" "$MAX_CALLS_PER_CTB"
 		fi
 		OLD_DATE="$DATE"
 		NB_CTB=0
-		SUM_CALLS=0
+		NB_CALLS=0
+		MAX_CALLS_PER_CTB=0
 	fi
 	((++NB_CTB))
-	((SUM_CALLS+=$NB))
+	((NB_CALLS+=$NB))
+	if [ "$MAX_CALLS_PER_CTB" -lt "$NB" ]; then
+		MAX_CALLS_PER_CTB="$NB"
+	fi
 
-done < <(grep -H "tiersNumber=" "$@" | sed -e 's/:/\t/' -e 's/^.*\.\([0-9]\{4\}\)/\1/' | sed -e 's/^\([^\t]\+\).*tiersNumber=\([0-9]\+\).*$/\1\t\2/' | sort | uniq -c)
+done < <(grep -h "tiersNumber=" "$@" | awk '{ print $3 "\t" $0; }' | sed -e 's/^.\([^\t]\+\)\t.*tiersNumber=\([0-9]\+\).*$/\1\t\2/' | sort | uniq -c)
 
-if [ "$SUM_CALLS" -gt 0 ]; then
-	echo "$OLD_DATE;$NB_CTB;$SUM_CALLS"
+if [ "$NB_CALLS" -gt 0 ]; then
+	dump "$OLD_DATE" "$NB_CTB" "$NB_CALLS" "$MAX_CALLS_PER_CTB"
 fi
