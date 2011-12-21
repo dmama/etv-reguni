e10086e908bc38c38408d411a9a2cea423f27bce
Manuel Siggen <manuel.siggen@vd.ch>
2011-12-21 11:56:50 +0000

    Augmenté la taille du champ 'cron' à 30 caractères.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@71102 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- moscow/src/main/java/ch/vd/moscow/data/JobDefinition.java
+++ moscow/src/main/java/ch/vd/moscow/data/JobDefinition.java
@@ -1,8 +1,17 @@
 package ch.vd.moscow.data;
 
-import ch.vd.moscow.job.processor.JobProcessor;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
-import javax.persistence.*;
+import ch.vd.moscow.job.processor.JobProcessor;
 
 /**
  * @author Manuel Siggen <manuel.siggen@vd.ch>
@@ -39,7 +48,7 @@ public abstract class JobDefinition {
 		this.name = name;
 	}
 
-	@Column(name = "cron", length = 15)
+	@Column(name = "cron", length = 30)
 	public String getCronExpression() {
 		return cronExpression;
 	}
