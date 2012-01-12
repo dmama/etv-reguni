283e19a3e595e624ba678dd4e06de11d04b09a58
Jacob Deker <jacob.deker@vd.ch>
2012-01-12 06:02:52 +0000

    Ajouté le script de migration directe de la version 4.12.1 à la version 4.13.1
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@71840 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

---
+++ unireg/base/sql/4.13.1/unireg-schema_4.12.1_4.13.1_upgrade.sql
@@ -0,0 +1,16 @@
+-- Version
+INSERT INTO VERSION_DB (VERSION_NB, SCRIPT_ID) VALUES ('4.13.1', '4.12.1_4.13.1_upgrade');
+
+-- [code cleanup] Dé-duplification de l'utilisation de la colonne TYPE sur ADRESSE_TIERS
+ALTER TABLE ADRESSE_TIERS ADD (AUTRE_TYPE nvarchar2(14), TYPE_PM nvarchar2(11));
+UPDATE ADRESSE_TIERS SET AUTRE_TYPE = TYPE where ADR_TYPE = 'AdresseAutreTiers';
+UPDATE ADRESSE_TIERS SET TYPE_PM = TYPE where ADR_TYPE = 'AdressePM';
+ALTER TABLE ADRESSE_TIERS DROP COLUMN TYPE;
+
+--
+-- [SIFISC-2854] puis [SIFISC-3156] Nouveau format de la table immeuble
+--
+drop table IMMEUBLE cascade constraints;
+create table IMMEUBLE (id number(19,0) not null, ANNULATION_DATE timestamp, ANNULATION_USER nvarchar2(65), LOG_CDATE timestamp, LOG_CUSER nvarchar2(65), LOG_MDATE timestamp, LOG_MUSER nvarchar2(65), DATE_DEBUT number(10,0), DATE_DERNIERE_MUTATION number(10,0), DATE_FIN number(10,0), DATE_VALID_RF number(10,0), DERNIERE_MUTATION nvarchar2(28), ESTIMATION_FISCALE number(10,0), GENRE_PROPRIETE nvarchar2(12) not null, ID_RF nvarchar2(40) not null, LIEN_RF nvarchar2(500), NATURE_IMMEUBLE nvarchar2(255), NOM_COMMUNE nvarchar2(255) not null, NUMERO_IMMEUBLE nvarchar2(20) not null, PART_PROPRIETE_DENOMINATEUR number(10,0) not null, PART_PROPRIETE_NUMERATEUR number(10,0) not null, ID_PROPRIETAIRE_RF nvarchar2(40) not null, NUMERO_INDIVIDU_RF number(19,0) not null, REF_ESTIM_FISC nvarchar2(255), TYPE_IMMEUBLE nvarchar2(27) not null, CTB_ID number(19,0) not null, primary key (id));
+create index IDX_IMM_CTB_ID on IMMEUBLE (CTB_ID);
+alter table IMMEUBLE add constraint FK_IMM_CTB_ID foreign key (CTB_ID) references TIERS;
