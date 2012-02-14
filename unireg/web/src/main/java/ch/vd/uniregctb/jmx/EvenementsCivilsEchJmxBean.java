5282998878a52ba9cddd7336712df4617d23bc36
Jacob Deker <jacob.deker@vd.ch>
2012-02-14 15:07:42 +0000

    Découpé l'écoute des événements civil e-CH sur deux queues ESB (une pour les messages générés manuellement, l'autre pour les messages générés en masse)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@73904 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/java/ch/vd/uniregctb/jmx/EvenementsCivilsEchJmxBean.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/jmx/EvenementsCivilsEchJmxBean.java
@@ -2,7 +2,34 @@ package ch.vd.uniregctb.jmx;
 
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 
-public interface EvenementsCivilsEchJmxBean extends EvenementsCivilsJmxBean {
+public interface EvenementsCivilsEchJmxBean {
+
+	@ManagedAttribute(description = "Total number of events received from the JMS queue (manual events)")
+	int getNbManualEventsReceived();
+
+	@ManagedAttribute(description = "Total number of events received from the JMS queue (batch events)")
+	int getNbBatchEventsReceived();
+
+	@ManagedAttribute(description = "Total number of events sent to the error queue (manual events)")
+	int getNbManualEventsRejectedToErrorQueue();
+
+	@ManagedAttribute(description = "Total number of events sent to the error queue (batch events)")
+	int getNbBatchEventsRejectedToErrorQueue();
+
+	@ManagedAttribute(description = "Total number of rejected events (DLQ for manual events)")
+	int getNbManualEventsRejectedException();
+
+	@ManagedAttribute(description = "Total number of rejected events (DLQ for batch events)")
+	int getNbBatchEventsRejectedException();
+
+	@ManagedAttribute(description = "Total number of events posted for processing since application start")
+	int getNbMeaningfullEventsReceived();
+
+	@ManagedAttribute(description = "Number of concurrent consumers listening to the ESB queue (manual events)")
+	int getNbManualConsumers();
+
+	@ManagedAttribute(description = "Number of concurrent consumers listening to the ESB queue (batch events)")
+	int getNbBatchConsumers();
 
 	@ManagedAttribute(description = "Total number of individuals currently waiting to be processed")
 	int getNbIndividualsAwaitingTreatment();
--- unireg/web/src/main/java/ch/vd/uniregctb/jmx/EvenementsCivilsEchJmxBeanImpl.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/jmx/EvenementsCivilsEchJmxBeanImpl.java
@@ -1,18 +1,36 @@
 package ch.vd.uniregctb.jmx;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedResource;
 
 import ch.vd.uniregctb.evenement.civil.ech.EvenementCivilEchReceptionMonitor;
+import ch.vd.uniregctb.jms.ErrorMonitorableMessageListener;
+import ch.vd.uniregctb.jms.JmxAwareEsbMessageEndpointManager;
 
 @ManagedResource
-public class EvenementsCivilsEchJmxBeanImpl extends EvenementsCivilsJmxBeanImpl implements EvenementsCivilsEchJmxBean {
+public class EvenementsCivilsEchJmxBeanImpl implements EvenementsCivilsEchJmxBean, InitializingBean {
 
+	private JmxAwareEsbMessageEndpointManager endpointManagerMasse;
+	private JmxAwareEsbMessageEndpointManager endpointManagerIndividuel;
 	private EvenementCivilEchReceptionMonitor monitor;
+
+	private ErrorMonitorableMessageListener masseListener;
+	private ErrorMonitorableMessageListener individuelListener;
 
 	@SuppressWarnings("UnusedDeclaration")
 	public void setMonitor(EvenementCivilEchReceptionMonitor monitor) {
 		this.monitor = monitor;
+	}
+
+	@SuppressWarnings("UnusedDeclaration")
+	public void setEndpointManagerMasse(JmxAwareEsbMessageEndpointManager endpointManagerMasse) {
+		this.endpointManagerMasse = endpointManagerMasse;
+	}
+
+	@SuppressWarnings("UnusedDeclaration")
+	public void setEndpointManagerIndividuel(JmxAwareEsbMessageEndpointManager endpointManagerIndividuel) {
+		this.endpointManagerIndividuel = endpointManagerIndividuel;
 	}
 
 	@Override
@@ -25,5 +43,84 @@ public class EvenementsCivilsEchJmxBeanImpl extends EvenementsCivilsJmxBeanImpl 
 	@ManagedAttribute
 	public int getNbIndividualsAwaitingTreatment() {
 		return monitor.getNombreIndividusEnAttenteDeTraitement();
+	}
+
+	@Override
+	@ManagedAttribute
+	public int getNbManualEventsReceived() {
+		return individuelListener.getNombreMessagesRecus();
+	}
+
+	@Override
+	@ManagedAttribute
+	public int getNbBatchEventsReceived() {
+		return masseListener.getNombreMessagesRecus();
+	}
+
+	@Override
+	@ManagedAttribute
+	public int getNbManualEventsRejectedToErrorQueue() {
+		return individuelListener.getNombreMessagesRenvoyesEnErreur();
+	}
+
+	@Override
+	@ManagedAttribute
+	public int getNbBatchEventsRejectedToErrorQueue() {
+		return masseListener.getNombreMessagesRenvoyesEnErreur();
+	}
+
+	@Override
+	@ManagedAttribute
+	public int getNbManualEventsRejectedException() {
+		return individuelListener.getNombreMessagesRenvoyesEnException();
+	}
+
+	@Override
+	@ManagedAttribute
+	public int getNbBatchEventsRejectedException() {
+		return masseListener.getNombreMessagesRenvoyesEnException();
+	}
+
+	@Override
+	@ManagedAttribute
+	public int getNbManualConsumers() {
+		return endpointManagerIndividuel.getMaxConcurrentConsumers();
+	}
+
+	@Override
+	@ManagedAttribute
+	public int getNbBatchConsumers() {
+		return endpointManagerMasse.getMaxConcurrentConsumers();
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if (endpointManagerIndividuel == null) {
+			throw new IllegalArgumentException("La propriété endpointManagerIndividuel est nulle");
+		}
+		else {
+			final Object listener = endpointManagerIndividuel.getMessageListener();
+			if (listener == null || !(listener instanceof ErrorMonitorableMessageListener)) {
+				throw new IllegalArgumentException("Le listener d'événements civils doit implémenter l'interface " + ErrorMonitorableMessageListener.class.getName());
+			}
+
+			this.individuelListener = (ErrorMonitorableMessageListener) listener;
+		}
+
+		if (endpointManagerMasse == null) {
+			throw new IllegalArgumentException("La propriété endpointManagerMasse est nulle");
+		}
+		else {
+			final Object listener = endpointManagerMasse.getMessageListener();
+			if (listener == null || !(listener instanceof ErrorMonitorableMessageListener)) {
+				throw new IllegalArgumentException("Le listener d'événements civils doit implémenter l'interface " + ErrorMonitorableMessageListener.class.getName());
+			}
+
+			this.masseListener = (ErrorMonitorableMessageListener) listener;
+		}
+
+		if (this.masseListener == this.individuelListener) {
+			throw new IllegalArgumentException("Les deux listeners 'masse' et 'individuels' devraient être différents");
+		}
 	}
 }
