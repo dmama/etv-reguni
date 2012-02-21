99f046f2650cfe51f1c0f926e4761ddf211ee564
Jacob Deker <jacob.deker@vd.ch>
2012-02-21 07:19:15 +0000

    Affiné encore l'anonymisation des messages d'erreur dans le calcul des statistiques
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@74282 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/rapport/PdfStatistiquesEvenementsRapport.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/rapport/PdfStatistiquesEvenementsRapport.java
@@ -414,7 +414,7 @@ public class PdfStatistiquesEvenementsRapport extends PdfRapport {
 		public final TypeEvenementCivil type;
 
 		public EvtCivilRegPPMsgTypeKey(String msg, TypeEvenementCivil type) {
-			this.msg = msg.replaceAll("[0-9]+", "?");       // pour enlever toutes les différences sur des dates ou des numéros d'individu, de tiers...
+			this.msg = msg.replaceAll("[0-9]+(\\.[0-9]+)*", "?");       // pour enlever toutes les différences sur des dates ou des numéros d'individu, de tiers...
 			this.type = type;
 		}
 
