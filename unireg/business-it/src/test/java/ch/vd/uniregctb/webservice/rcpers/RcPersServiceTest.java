9f2360baac5d22b3fcc44131f0432071a45b4900
Manuel Siggen <manuel.siggen@vd.ch>
2012-03-12 11:49:07 +0000

    Corrigé l'url d'accès aux personnes RcPers en intégration, en attendant la correction du problème du côté RcPers.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@75441 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business-it/src/test/java/ch/vd/uniregctb/webservice/rcpers/RcPersServiceTest.java
+++ unireg/business-it/src/test/java/ch/vd/uniregctb/webservice/rcpers/RcPersServiceTest.java
@@ -1,18 +1,13 @@
 package ch.vd.uniregctb.webservice.rcpers;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.Test;
-
-import ch.vd.evd0001.v3.ListOfPersons;
-import ch.vd.evd0001.v3.ListOfRelations;
-import ch.vd.evd0001.v3.Person;
-import ch.vd.evd0001.v3.Relations;
-import ch.vd.evd0001.v3.Relationship;
+import ch.vd.evd0001.v3.*;
 import ch.vd.evd0006.v1.Event;
 import ch.vd.unireg.wsclient.rcpers.RcPersClientImpl;
 import ch.vd.uniregctb.interfaces.model.impl.IndividuRCPers;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -87,9 +82,9 @@ public class RcPersServiceTest {
 	private static RcPersClientImpl buildClient() throws Exception {
 		final RcPersClientImpl client = new RcPersClientImpl();
 		client.setBaseUrl("http://rp-ws-va.etat-de-vaud.ch/registres/int-rcpers/west/ws/v3");
-		client.setPeoplePath("persons/ct.vd.rcpers.unireg");
+		client.setPeoplePath("persons/ct.vd.rcpers");
 		client.setEventPath("event");
-		client.setRelationsPath("relations/ct.vd.rcpers.unireg");
+		client.setRelationsPath("relations/ct.vd.rcpers");
 		client.setUsername("gvd0unireg");
 		client.setPassword("welc0me_");
 		client.afterPropertiesSet();
