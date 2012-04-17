d6894d343b5680f2ed1b27e5b261df76cc737e62
Manuel Siggen <manuel.siggen@vd.ch>
2012-04-17 14:07:56 +0000

    [SIFISC-4815] Réimplémenté la boîte de dialogue d'affichage des logs des entités hibernate en utilisant une récupération par Ajax/JSON, plutôt que d'embedder une page html complète à l'intérieur de la page courante (et cela corrige un problème de double chargement de css qui provoquait des trucs bizarres avec IE6).
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@77536 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/java/ch/vd/uniregctb/common/ConsultLogController.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/common/ConsultLogController.java
@@ -1,49 +1,31 @@
 package ch.vd.uniregctb.common;
 
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import org.apache.log4j.Logger;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
-
-import ch.vd.uniregctb.adresse.AdresseTiers;
 import ch.vd.uniregctb.adresse.AdresseTiersDAO;
-import ch.vd.uniregctb.declaration.DeclarationImpotOrdinaire;
 import ch.vd.uniregctb.declaration.DeclarationImpotOrdinaireDAO;
-import ch.vd.uniregctb.declaration.DeclarationImpotSource;
-import ch.vd.uniregctb.declaration.EtatDeclaration;
 import ch.vd.uniregctb.declaration.EtatDeclarationDAO;
 import ch.vd.uniregctb.declaration.ListeRecapitulativeDAO;
-import ch.vd.uniregctb.declaration.Periodicite;
 import ch.vd.uniregctb.declaration.PeriodiciteDAO;
-import ch.vd.uniregctb.evenement.civil.ech.EvenementCivilEch;
 import ch.vd.uniregctb.evenement.civil.ech.EvenementCivilEchDAO;
-import ch.vd.uniregctb.evenement.civil.regpp.EvenementCivilRegPP;
 import ch.vd.uniregctb.evenement.civil.regpp.EvenementCivilRegPPDAO;
 import ch.vd.uniregctb.evenement.identification.contribuable.IdentCtbDAO;
-import ch.vd.uniregctb.evenement.identification.contribuable.IdentificationContribuable;
-import ch.vd.uniregctb.mouvement.MouvementDossier;
 import ch.vd.uniregctb.mouvement.MouvementDossierDAO;
-import ch.vd.uniregctb.rf.Immeuble;
 import ch.vd.uniregctb.rf.ImmeubleDAO;
 import ch.vd.uniregctb.security.DroitAccesDAO;
-import ch.vd.uniregctb.tiers.DroitAcces;
-import ch.vd.uniregctb.tiers.ForFiscal;
 import ch.vd.uniregctb.tiers.ForFiscalDAO;
-import ch.vd.uniregctb.tiers.RapportEntreTiers;
 import ch.vd.uniregctb.tiers.RapportEntreTiersDAO;
-import ch.vd.uniregctb.tiers.SituationFamille;
 import ch.vd.uniregctb.tiers.SituationFamilleDAO;
-import ch.vd.uniregctb.tiers.Tache;
 import ch.vd.uniregctb.tiers.TacheDAO;
-import ch.vd.uniregctb.tiers.Tiers;
 import ch.vd.uniregctb.tiers.TiersDAO;
 
-public class ConsultLogController extends AbstractSimpleFormController {
-
-	protected final Logger LOGGER = Logger.getLogger(ConsultLogController.class);
+@Controller
+public class ConsultLogController {
 
 	private ForFiscalDAO forFiscalDAO;
 	private SituationFamilleDAO situationFamilleDAO;
@@ -62,10 +44,6 @@ public class ConsultLogController extends AbstractSimpleFormController {
 	private EtatDeclarationDAO etatDeclarationDAO;
 	private ImmeubleDAO immeubleDAO;
 
-	private PlatformTransactionManager transactionManager;
-
-	public final static String ID_PARAMETER_NAME = "id";
-	public final static String NATURE_PARAMETER_NAME = "nature";
 	public final static String NATURE_FOR_PARAMETER_VALUE = "ForFiscal";
 	public final static String NATURE_SITUATION_PARAMETER_VALUE = "SituationFamille";
 	public final static String NATURE_RAPPORT_PARAMETER_VALUE = "RapportEntreTiers";
@@ -84,106 +62,63 @@ public class ConsultLogController extends AbstractSimpleFormController {
 	public final static String NATURE_PERIODICITE_PARAMETER_VALUE = "periodicite";
 	public final static String NATURE_IMMEUBLE = "Immeuble";
 
-	@SuppressWarnings({"UnnecessaryLocalVariable"})
-	@Override
-	protected Object formBackingObject(HttpServletRequest request) throws Exception {
+	@ResponseBody
+	@Transactional(readOnly = true, rollbackFor = Throwable.class)
+	@RequestMapping(value = "/common/consult-log.do", method = RequestMethod.GET)
+	public ConsultLogView consultLog(@RequestParam("id") final long id, @RequestParam("nature") final String nature) {
 
-		final String idParam = request.getParameter(ID_PARAMETER_NAME);
-		final String nature = request.getParameter(NATURE_PARAMETER_NAME);
+		HibernateEntity objet = null;
 
-		if (idParam == null || "".equals(idParam.trim())) {
-			return null;
+		if (nature.equals(NATURE_FOR_PARAMETER_VALUE)) {
+			objet = forFiscalDAO.get(id);
+		}
+		else if (nature.equals(NATURE_SITUATION_PARAMETER_VALUE)) {
+			objet = situationFamilleDAO.get(id);
+		}
+		else if (nature.equals(NATURE_ADRESSE_PARAMETER_VALUE)) {
+			objet = adresseTiersDAO.get(id);
+		}
+		else if (nature.equals(NATURE_RAPPORT_PARAMETER_VALUE)) {
+			objet = rapportEntreTiersDAO.get(id);
+		}
+		else if (nature.equals(NATURE_DI_PARAMETER_VALUE)) {
+			objet = diDAO.get(id);
+		}
+		else if (nature.equals(NATURE_LR_PARAMETER_VALUE)) {
+			objet = lrDAO.get(id);
+		}
+		else if (nature.equals(NATURE_TIERS_PARAMETER_VALUE)) {
+			objet = tiersDAO.get(id);
+		}
+		else if (nature.equals(NATURE_MOUVEMENT_PARAMETER_VALUE)) {
+			objet = mouvementDossierDAO.get(id);
+		}
+		else if (nature.equals(NATURE_TACHE_PARAMETER_VALUE)) {
+			objet = tacheDAO.get(id);
+		}
+		else if (nature.equals(NATURE_DROIT_ACCES_PARAMETER_VALUE)) {
+			objet = droitAccesDAO.get(id);
+		}
+		else if (nature.equals(NATURE_EVENEMENT_PARAMETER_VALUE)) {
+			objet = evenementCivilRegPPDAO.get(id);
+		}
+		else if (nature.equals(NATURE_EVENEMENT_ECH_PARAMETER_VALUE)) {
+			objet = evenementCivilEchDAO.get(id);
+		}
+		else if (nature.equals(NATURE_IDENTIFICATION_PARAMETER_VALUE)) {
+			objet = identCtbDAO.get(id);
+		}
+		else if (nature.equals(NATURE_PERIODICITE_PARAMETER_VALUE)) {
+			objet = periodiciteDAO.get(id);
+		}
+		else if (nature.equals(NATURE_ETAT_PARAMETER_VALUE)) {
+			objet = etatDeclarationDAO.get(id);
+		}
+		else if (nature.equals(NATURE_IMMEUBLE)) {
+			objet = immeubleDAO.get(id);
 		}
 
-		final Long id = Long.parseLong(idParam);
-
-		final TransactionTemplate template = new TransactionTemplate(transactionManager);
-		template.setReadOnly(true);
-
-		final ConsultLogView consultLogView = template.execute(new TransactionCallback<ConsultLogView>() {
-			@Override
-			public ConsultLogView doInTransaction(TransactionStatus status) {
-				if (nature.equals(NATURE_FOR_PARAMETER_VALUE)) {
-					ForFiscal forFiscal = forFiscalDAO.get(id);
-					return fillConsultLogView(forFiscal);
-				}
-				else if (nature.equals(NATURE_SITUATION_PARAMETER_VALUE)) {
-					SituationFamille situationFamille = situationFamilleDAO.get(id);
-					return fillConsultLogView(situationFamille);
-				}
-				else if (nature.equals(NATURE_ADRESSE_PARAMETER_VALUE)) {
-					AdresseTiers adresseTiers = adresseTiersDAO.get(id);
-					return fillConsultLogView(adresseTiers);
-				}
-				else if (nature.equals(NATURE_RAPPORT_PARAMETER_VALUE)) {
-					RapportEntreTiers rapportEntreTiers = rapportEntreTiersDAO.get(id);
-					return fillConsultLogView(rapportEntreTiers);
-				}
-				else if (nature.equals(NATURE_DI_PARAMETER_VALUE)) {
-					DeclarationImpotOrdinaire di = diDAO.get(id);
-					return fillConsultLogView(di);
-				}
-				else if (nature.equals(NATURE_LR_PARAMETER_VALUE)) {
-					DeclarationImpotSource lr = lrDAO.get(id);
-					return fillConsultLogView(lr);
-				}
-				else if (nature.equals(NATURE_TIERS_PARAMETER_VALUE)) {
-					Tiers tiers = tiersDAO.get(id);
-					return fillConsultLogView(tiers);
-				}
-				else if (nature.equals(NATURE_MOUVEMENT_PARAMETER_VALUE)) {
-					MouvementDossier mvt = mouvementDossierDAO.get(id);
-					return fillConsultLogView(mvt);
-				}
-				else if (nature.equals(NATURE_TACHE_PARAMETER_VALUE)) {
-					Tache tache = tacheDAO.get(id);
-					return fillConsultLogView(tache);
-				}
-				else if (nature.equals(NATURE_DROIT_ACCES_PARAMETER_VALUE)) {
-					DroitAcces droitAcces = droitAccesDAO.get(id);
-					return fillConsultLogView(droitAcces);
-				}
-				else if (nature.equals(NATURE_EVENEMENT_PARAMETER_VALUE)) {
-					EvenementCivilRegPP evenementCivilExterne = evenementCivilRegPPDAO.get(id);
-					return fillConsultLogView(evenementCivilExterne);
-				}
-				else if (nature.equals(NATURE_EVENEMENT_ECH_PARAMETER_VALUE)) {
-					EvenementCivilEch evenementCivilExterne = evenementCivilEchDAO.get(id);
-					return fillConsultLogView(evenementCivilExterne);
-				}
-				else if (nature.equals(NATURE_IDENTIFICATION_PARAMETER_VALUE)) {
-					IdentificationContribuable message = identCtbDAO.get(id);
-					return fillConsultLogView(message);
-				}
-				else if (nature.equals(NATURE_PERIODICITE_PARAMETER_VALUE)) {
-					Periodicite periodicite = periodiciteDAO.get(id);
-					return fillConsultLogView(periodicite);
-				}
-				else if (nature.equals(NATURE_ETAT_PARAMETER_VALUE)) {
-					EtatDeclaration etatDeclaration = etatDeclarationDAO.get(id);
-					return fillConsultLogView(etatDeclaration);
-				}
-				else if (nature.equals(NATURE_IMMEUBLE)) {
-					Immeuble i = immeubleDAO.get(id);
-					return fillConsultLogView(i);
-				}
-				return null;
-			}
-		});
-
-
-		return consultLogView;
-	}
-
-	private ConsultLogView fillConsultLogView(HibernateEntity objet) {
-		ConsultLogView consultLogView = new ConsultLogView();
-		consultLogView.setDateHeureCreation(objet.getLogCreationDate());
-		consultLogView.setUtilisateurCreation(objet.getLogCreationUser());
-		consultLogView.setDateHeureDerniereModif(objet.getLogModifDate());
-		consultLogView.setUtilisateurDerniereModif(objet.getLogModifUser());
-		consultLogView.setDateHeureAnnulation(objet.getAnnulationDate());
-		consultLogView.setUtilisateurAnnulation(objet.getAnnulationUser());
-		return consultLogView;
+		return objet == null ? null : new ConsultLogView(objet);
 	}
 
 	@SuppressWarnings({"UnusedDeclaration"})
@@ -233,11 +168,6 @@ public class ConsultLogController extends AbstractSimpleFormController {
 	@SuppressWarnings({"UnusedDeclaration"})
 	public void setDroitAccesDAO(DroitAccesDAO droitAccesDAO) {
 		this.droitAccesDAO = droitAccesDAO;
-	}
-
-	@SuppressWarnings({"UnusedDeclaration"})
-	public void setTransactionManager(PlatformTransactionManager transactionManager) {
-		this.transactionManager = transactionManager;
 	}
 
 	@SuppressWarnings({"UnusedDeclaration"})
--- unireg/web/src/main/java/ch/vd/uniregctb/common/ConsultLogView.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/common/ConsultLogView.java
@@ -4,23 +4,27 @@ import java.util.Date;
 
 /**
  * Vue pour la consultation des logs
- *
- * @author xcifde
- *
  */
 public class ConsultLogView {
 
 	private String utilisateurCreation;
-
 	private Date dateHeureCreation;
-
 	private String utilisateurDerniereModif;
-
 	private Date dateHeureDerniereModif;
-
 	private String utilisateurAnnulation;
-
 	private Date dateHeureAnnulation;
+
+	public ConsultLogView() {
+	}
+
+	public ConsultLogView(HibernateEntity objet) {
+		this.dateHeureCreation = objet.getLogCreationDate();
+		this.utilisateurCreation = objet.getLogCreationUser();
+		this.dateHeureDerniereModif = objet.getLogModifDate();
+		this.utilisateurDerniereModif = objet.getLogModifUser();
+		this.dateHeureAnnulation = objet.getAnnulationDate();
+		this.utilisateurAnnulation = objet.getAnnulationUser();
+	}
 
 	public String getUtilisateurCreation() {
 		return utilisateurCreation;
