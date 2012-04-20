88799b2fb5324ce590973e0de4d4276d70cf644c
Jacob Deker <jacob.deker@vd.ch>
2012-04-20 14:44:11 +0000

    [SIFISC-4727] Traitement de la naissance postérieure à l'arrivée
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@77814 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/test/java/ch/vd/uniregctb/evenement/civil/interne/naissance/NaissanceTest.java
+++ unireg/business/src/test/java/ch/vd/uniregctb/evenement/civil/interne/naissance/NaissanceTest.java
@@ -3,15 +3,19 @@ package ch.vd.uniregctb.evenement.civil.interne.naissance;
 import java.util.Arrays;
 import java.util.List;
 
+import junit.framework.Assert;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
 
+import ch.vd.registre.base.date.RegDate;
 import ch.vd.uniregctb.evenement.EvenementFiscal;
 import ch.vd.uniregctb.evenement.EvenementFiscalDAO;
 import ch.vd.uniregctb.evenement.EvenementFiscalNaissance;
 import ch.vd.uniregctb.evenement.EvenementFiscalSituationFamille;
+import ch.vd.uniregctb.evenement.civil.common.EvenementCivilException;
 import ch.vd.uniregctb.evenement.civil.interne.AbstractEvenementCivilInterneTest;
 import ch.vd.uniregctb.evenement.civil.interne.HandleStatus;
 import ch.vd.uniregctb.evenement.civil.interne.MessageCollector;
@@ -62,7 +66,7 @@ public class NaissanceTest extends AbstractEvenementCivilInterneTest {
 		}
 
 		Individu bebe = serviceCivil.getIndividu(NOUVEAU_NE, date(2007, 12, 31));
-		Naissance naissance = createValidNaissance(bebe);
+		Naissance naissance = createValidNaissance(bebe, true);
 
 		final MessageCollector collector = buildMessageCollector();
 		naissance.validate(collector, collector);
@@ -91,7 +95,7 @@ public class NaissanceTest extends AbstractEvenementCivilInterneTest {
 		}
 
 		Individu bebe = serviceCivil.getIndividu(NOUVEAU_NE_FIN_ANNEE, date(2007, 12, 31));
-		Naissance naissance = createValidNaissance(bebe);
+		Naissance naissance = createValidNaissance(bebe, true);
 
 		final MessageCollector collector = buildMessageCollector();
 		naissance.validate(collector, collector);
@@ -119,7 +123,7 @@ public class NaissanceTest extends AbstractEvenementCivilInterneTest {
 		LOGGER.debug("Test de traitement d'un événement de naissance.");
 
 		Individu bebe = serviceCivil.getIndividu(NOUVEAU_NE_MAJEUR, date(2007, 12, 31));
-		Naissance naissance = createValidNaissance(bebe);
+		Naissance naissance = createValidNaissance(bebe, true);
 
 		final MessageCollector collector = buildMessageCollector();
 		naissance.validate(collector, collector);
@@ -128,8 +132,119 @@ public class NaissanceTest extends AbstractEvenementCivilInterneTest {
 		assertTrue("Une erreur aurait du survenir puisque l'individu est majeur", collector.hasErreurs());
 	}
 
-	private Naissance createValidNaissance(Individu individu) {
-		return new Naissance(individu, null, individu.getDateNaissance(), 4848, null, context);
+	@Test
+	public void testNaissanceTiersExistantRegPP() throws Exception {
+
+		doInNewTransactionAndSession(new TransactionCallback<Object>() {
+			@Override
+			public Object doInTransaction(TransactionStatus status) {
+				addHabitant(NOUVEAU_NE);
+				return null;
+			}
+		});
+
+		final Individu bebe = serviceCivil.getIndividu(NOUVEAU_NE, date(2007, 12, 31));
+
+		doInNewTransactionAndSession(new TransactionCallback<Object>() {
+			@Override
+			public Object doInTransaction(TransactionStatus status) {
+				final Naissance naissance = createValidNaissance(bebe, true);
+				final MessageCollector collector = buildMessageCollector();
+				try {
+					naissance.validate(collector, collector);
+					naissance.handle(collector);
+					Assert.fail();
+				}
+				catch (EvenementCivilException e) {
+					Assert.assertEquals("Le tiers existe déjà avec cet individu " + NOUVEAU_NE + " alors que c'est une naissance", e.getMessage());
+				}
+				return null;
+			}
+		});
+	}
+
+
+	@Test
+	public void testNaissanceTiersExistantRCPers() throws Exception {
+
+		final long indPere = 392465236L;
+		final long indMere = 5423678234L;
+		final long indEnfant = 34678425L;
+		final RegDate dateNaissanceEnfant = date(2010, 2, 8);
+
+		// On crée la situation de départ : une mère et un fils mineur
+		serviceCivil.setUp(new MockServiceCivil() {
+			@Override
+			protected void init() {
+				MockIndividu pere = addIndividu(indPere, date(1980, 1, 1), "Cognac", "Raoul", true);
+				MockIndividu mere = addIndividu(indMere, date(1980, 1, 1), "Cognac", "Josette", false);
+				MockIndividu enfant = addIndividu(indEnfant, dateNaissanceEnfant, "Cognac", "Yvan", true);
+				enfant.setParentsFromIndividus(Arrays.<Individu>asList(pere, mere));
+			}
+		});
+
+		class Ids {
+			Long pere;
+			Long mere;
+			Long enfant;
+		}
+
+		// On crée le père et la mère
+		final Ids ids = doInNewTransactionAndSession(new TxCallback<Ids>() {
+			@Override
+			public Ids execute(TransactionStatus status) throws Exception {
+				final PersonnePhysique pere = addHabitant(indPere);
+				final PersonnePhysique mere = addHabitant(indMere);
+				final PersonnePhysique enfant = addHabitant(indEnfant);
+				final Ids ids = new Ids();
+				ids.pere = pere.getId();
+				ids.mere = mere.getId();
+				ids.enfant = enfant.getId();
+				return ids;
+			}
+		});
+
+		final Individu bebe = serviceCivil.getIndividu(indEnfant, dateNaissanceEnfant);
+
+		doInNewTransactionAndSession(new TransactionCallback<Object>() {
+			@Override
+			public Object doInTransaction(TransactionStatus status) {
+				final Naissance naissance = createValidNaissance(bebe, false);
+				final MessageCollector collector = buildMessageCollector();
+				try {
+					naissance.validate(collector, collector);
+					naissance.handle(collector);
+					Assert.assertFalse(collector.hasErreurs());
+					Assert.assertFalse(collector.hasWarnings());
+				}
+				catch (EvenementCivilException e) {
+					Assert.fail(e.getMessage());
+				}
+
+				// on vérifie les événements fiscaux (qui doivent quand-même avoir été envoyés)
+				// on vérifie que il y a eu :
+				// - un événement de changement de situation de famille
+				// - un événement de naissane
+				final List<EvenementFiscal> events = evenementFiscalDAO.getAll();
+				assertNotNull(events);
+				assertEquals(2, events.size());
+
+				final EvenementFiscalSituationFamille event0 = (EvenementFiscalSituationFamille) events.get(0);
+				assertNotNull(event0);
+
+				final EvenementFiscalNaissance event1 = (EvenementFiscalNaissance) events.get(1);
+				assertNotNull(event1);
+				assertEquals(ids.mere, event1.getTiers().getNumero());
+				assertEquals(ids.enfant, event1.getEnfant().getNumero());
+				assertEquals(dateNaissanceEnfant, event1.getDateEvenement());
+
+				return null;
+			}
+		});
+	}
+
+	private Naissance createValidNaissance(Individu individu, boolean regpp) {
+		return new Naissance(individu, null, individu.getDateNaissance(), 4848, null, context, regpp);
 	}
 
 	/**
@@ -142,7 +257,7 @@ public class NaissanceTest extends AbstractEvenementCivilInterneTest {
 		final long indMere = 2;
 		final long indFils = 3;
 
-		// On crée la situation de départ : une mère et un fils mineur qui possède un immeuble
+		// On crée la situation de départ : une mère et un fils mineur
 		serviceCivil.setUp(new MockServiceCivil() {
 			@Override
 			protected void init() {
@@ -177,7 +292,7 @@ public class NaissanceTest extends AbstractEvenementCivilInterneTest {
 			@Override
 			public Object execute(TransactionStatus status) throws Exception {
 				final Individu fils = serviceCivil.getIndividu(indFils, date(2010, 12, 31));
-				final Naissance naissance = createValidNaissance(fils);
+				final Naissance naissance = createValidNaissance(fils, true);
 
 				final MessageCollector collector = buildMessageCollector();
 				naissance.validate(collector, collector);
