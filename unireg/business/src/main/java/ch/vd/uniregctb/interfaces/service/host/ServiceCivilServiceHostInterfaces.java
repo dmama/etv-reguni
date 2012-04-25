5c76558cc00c0deb17ee88dab1a507932ae84f3f
Manuel Siggen <manuel.siggen@vd.ch>
2012-04-25 07:36:51 +0000

    Utilisé la manière correcte de forcer le log.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@78080 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/interfaces/service/host/ServiceCivilServiceHostInterfaces.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/interfaces/service/host/ServiceCivilServiceHostInterfaces.java
@@ -8,13 +8,12 @@ import java.util.List;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.mutable.MutableBoolean;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggingEvent;
 
 import ch.vd.registre.base.date.RegDate;
 import ch.vd.registre.civil.service.ServiceCivil;
 import ch.vd.registre.common.service.RegistreException;
+import ch.vd.uniregctb.common.ForceLogger;
 import ch.vd.uniregctb.common.JvmVersionHelper;
 import ch.vd.uniregctb.interfaces.IndividuDumper;
 import ch.vd.uniregctb.interfaces.model.AttributeIndividu;
@@ -63,7 +62,7 @@ public class ServiceCivilServiceHostInterfaces extends ServiceCivilServiceBase {
 				final String message = String.format("getIndividu(noIndividu=%d, date=%s, parties=%s) => %s", noIndividu, ServiceTracing.toString(date), ServiceTracing.toString(parties),
 						IndividuDumper.dump(ind, false, false, false));
 				// force le log en mode trace, même si le LOGGER n'est pas en mode trace
-				LOGGER.callAppenders(new LoggingEvent(Logger.class.getName(), LOGGER, Level.TRACE, message, null));
+				new ForceLogger(LOGGER).trace(message);
 			}
 
 			return ind;
@@ -99,7 +98,7 @@ public class ServiceCivilServiceHostInterfaces extends ServiceCivilServiceBase {
 				final String message = String.format("getIndividus(nosIndividus=%s, date=%s, parties=%s) => %s", ServiceTracing.toString(nosIndividus), ServiceTracing.toString(date),
 						ServiceTracing.toString(parties), IndividuDumper.dump(list, false, false));
 				// force le log en mode trace, même si le LOGGER n'est pas en mode trace
-				LOGGER.callAppenders(new LoggingEvent(Logger.class.getName(), LOGGER, Level.TRACE, message, null));
+				new ForceLogger(LOGGER).trace(message);
 			}
 
 			return list;
