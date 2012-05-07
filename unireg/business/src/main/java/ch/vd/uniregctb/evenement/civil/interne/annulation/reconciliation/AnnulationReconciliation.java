43a4ed515b95b4f03f08420f184d9fd750857cc5
Baba Issa Ngom <baba-issa.ngom@vd.ch>
2012-05-07 13:16:18 +0000

    [SIFISC-5095]Ajout de la detection de la redondance en cas d'annulation de réconciliation
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/5.0@79015 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/evenement/civil/interne/annulation/reconciliation/AnnulationReconciliation.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/evenement/civil/interne/annulation/reconciliation/AnnulationReconciliation.java
@@ -1,5 +1,7 @@
 package ch.vd.uniregctb.evenement.civil.interne.annulation.reconciliation;
 
+import java.util.Set;
+
 import org.jetbrains.annotations.NotNull;
 
 import ch.vd.registre.base.date.RegDate;
@@ -17,6 +19,8 @@ import ch.vd.uniregctb.metier.MetierServiceException;
 import ch.vd.uniregctb.tiers.EnsembleTiersCouple;
 import ch.vd.uniregctb.tiers.MenageCommun;
 import ch.vd.uniregctb.tiers.PersonnePhysique;
+import ch.vd.uniregctb.tiers.RapportEntreTiers;
+import ch.vd.uniregctb.type.TypeRapportEntreTiers;
 
 /**
  * Adapter pour l'annulation de réconciliation.
@@ -59,7 +63,7 @@ public class AnnulationReconciliation extends EvenementCivilInterne {
 			menage = menageComplet.getMenage();
 		}
 		// Si le tiers MenageCommun n'est pas trouvé, la base fiscale est inconsistente => mise en erreur de l'événement
-		if (menage == null) {
+		if (menage == null && !isAnnulationRedondante()) {
 			throw new EvenementCivilException("Le tiers ménage commun n'a pu être trouvé");
 		}
 
@@ -68,7 +72,7 @@ public class AnnulationReconciliation extends EvenementCivilInterne {
 			// Obtention du tiers correspondant au conjoint.
 			PersonnePhysique conjoint = context.getTiersService().getPersonnePhysiqueByNumeroIndividu(individuConjoint.getNoTechnique());
 			// Vérification de la cohérence
-			if (!menageComplet.estComposeDe(principal, conjoint)) {
+			if (menageComplet!=null && !menageComplet.estComposeDe(principal, conjoint)) {
 				throw new EvenementCivilException("Les tiers composant le tiers ménage trouvé ne correspondent pas avec les individus unis dans le civil");
 			}
 		}
@@ -77,6 +81,10 @@ public class AnnulationReconciliation extends EvenementCivilInterne {
 	@NotNull
 	@Override
 	public HandleStatus handle(EvenementCivilWarningCollector warnings) throws EvenementCivilException {
+
+		if (isAnnulationRedondante()) {
+			return HandleStatus.REDONDANT;
+		}
 		// Récupération du tiers principal.
 		PersonnePhysique principal = getPrincipalPP();
 		// Obtention du tiers correspondant au conjoint.
@@ -93,5 +101,26 @@ public class AnnulationReconciliation extends EvenementCivilInterne {
 			throw new EvenementCivilException(e.getMessage(), e);
 		}
 		return HandleStatus.TRAITE;
+	}
+
+	private boolean isAnnulationRedondante() {
+
+		PersonnePhysique personne = getPrincipalPP();
+		MenageCommun menageCommun = null;
+
+		final Set<RapportEntreTiers> rapportsSujet = personne.getRapportsSujet();
+		if (rapportsSujet != null) {
+			for (RapportEntreTiers rapportSujet : rapportsSujet) {
+				if (TypeRapportEntreTiers.APPARTENANCE_MENAGE == rapportSujet.getType() &&
+						rapportSujet.getDateDebut().equals(getDate()) && rapportSujet.isAnnule()) {
+					// le rapport annulée de l'apartenance a été trouvé, on est redondant
+					return true;
+
+				}
+			}
+
+		}
+
+		return false;
 	}
 }
