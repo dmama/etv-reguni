fa72ad9725550760201920f3405c3d9050b67d2d
Frédéric Noguier <frederic.noguier@vd.ch>
2012-06-05 16:38:03 +0000

    passage du controller en spring 3 (preparatif pour  SIFISC-4815)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@81102 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/java/ch/vd/uniregctb/tiers/MouvementVisuController.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/tiers/MouvementVisuController.java
@@ -1,74 +1,28 @@
 package ch.vd.uniregctb.tiers;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.log4j.Logger;
-import org.springframework.validation.BindException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import ch.vd.uniregctb.mouvement.view.MouvementDetailView;
 import ch.vd.uniregctb.tiers.manager.MouvementVisuManager;
 
 /**
  * Controller pour l'overlay de visualisation du détail du mouvement
  *
- * @author xcifde
- *
  */
-public class MouvementVisuController extends AbstractTiersController {
-
-	/**
-	 * Un LOGGER.
-	 */
-	protected final Logger LOGGER = Logger.getLogger(MouvementVisuController.class);
-
-	private static final String ID_MVT_PARAM = "idMvt";
+@Controller
+public class MouvementVisuController {
 
 	private MouvementVisuManager mouvementVisuManager;
-
-	public MouvementVisuManager getMouvementVisuManager() {
-		return mouvementVisuManager;
-	}
 
 	public void setMouvementVisuManager(MouvementVisuManager mouvementVisuManager) {
 		this.mouvementVisuManager = mouvementVisuManager;
 	}
 
-	/**
-	 * @see org.springframework.web.servlet.mvc.AbstractFormController#formBackingObject(javax.servlet.http.HttpServletRequest)
-	 */
-	@Override
-	protected Object formBackingObject(HttpServletRequest request) throws Exception {
-
-		MouvementDetailView mvtView = null;
-
-		final String idMvtParam = request.getParameter(ID_MVT_PARAM);
-		if (idMvtParam != null) {
-			final Long id = Long.parseLong(idMvtParam);
-			if (idMvtParam != null && !"".equals(idMvtParam)) {
-				mvtView = mouvementVisuManager.get(id);
-			}
-		}
-
-		return mvtView;
+	@RequestMapping("/tiers/mouvement.do")
+	public ModelAndView mouvement(@RequestParam("idMvt") Long idMvt) {
+		return new ModelAndView("tiers/visualisation/mouvement/mouvement", "command", mouvementVisuManager.get(idMvt));
 	}
-
-	/**
-	 * @see org.springframework.web.servlet.mvc.SimpleFormController#showForm(javax.servlet.http.HttpServletRequest,
-	 *      javax.servlet.http.HttpServletResponse, org.springframework.validation.BindException, java.util.Map)
-	 */
-	@SuppressWarnings("unchecked")
-	@Override
-	protected ModelAndView showForm(HttpServletRequest request, HttpServletResponse response, BindException errors, Map model)
-		throws Exception {
-		ModelAndView mav = super.showForm(request, response, errors, model);
-		return mav;
-	}
-
-
-
 
 }
