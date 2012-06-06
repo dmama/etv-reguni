9ea857ea523abfd87470a3b9e19f5840d1cb46bd
Frédéric Noguier <frederic.noguier@vd.ch>
2012-06-06 15:05:54 +0000

    [SIFISC-4815] Correction bug affichage sur l'onglet mouvement lors de l'affichage du detail. Refactoring par un appel JSON sur le modele de ce qu'a fait Manu pour les autres onglets
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/5.1.0@81210 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/java/ch/vd/uniregctb/tiers/MouvementVisuController.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/tiers/MouvementVisuController.java
@@ -1,74 +1,150 @@
 package ch.vd.uniregctb.tiers;
 
-import java.util.Map;
+import java.util.Date;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import org.apache.log4j.Logger;
-import org.springframework.validation.BindException;
-import org.springframework.web.servlet.ModelAndView;
-
+import ch.vd.registre.base.date.RegDate;
 import ch.vd.uniregctb.mouvement.view.MouvementDetailView;
 import ch.vd.uniregctb.tiers.manager.MouvementVisuManager;
+import ch.vd.uniregctb.utils.WebContextUtils;
 
 /**
  * Controller pour l'overlay de visualisation du détail du mouvement
  *
- * @author xcifde
- *
  */
-public class MouvementVisuController extends AbstractTiersController {
-
-	/**
-	 * Un LOGGER.
-	 */
-	protected final Logger LOGGER = Logger.getLogger(MouvementVisuController.class);
-
-	private static final String ID_MVT_PARAM = "idMvt";
+@Controller
+public class MouvementVisuController implements MessageSourceAware {
 
 	private MouvementVisuManager mouvementVisuManager;
-
-	public MouvementVisuManager getMouvementVisuManager() {
-		return mouvementVisuManager;
-	}
+	private MessageSource messageSource;
 
 	public void setMouvementVisuManager(MouvementVisuManager mouvementVisuManager) {
 		this.mouvementVisuManager = mouvementVisuManager;
 	}
 
-	/**
-	 * @see org.springframework.web.servlet.mvc.AbstractFormController#formBackingObject(javax.servlet.http.HttpServletRequest)
-	 */
+	//TODO Un check sur la securité peut-être ?
+	@RequestMapping("/tiers/mouvement.do")
+	@ResponseBody
+	public MouvementForJSON mouvement(@RequestParam("idMvt") Long idMvt) {
+		return new MouvementForJSON (mouvementVisuManager.get(idMvt)) {};
+	}
+
 	@Override
-	protected Object formBackingObject(HttpServletRequest request) throws Exception {
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
-		MouvementDetailView mvtView = null;
+	@SuppressWarnings("UnusedDeclaration")
+	public class MouvementForJSON {
 
-		final String idMvtParam = request.getParameter(ID_MVT_PARAM);
-		if (idMvtParam != null) {
-			final Long id = Long.parseLong(idMvtParam);
-			if (idMvtParam != null && !"".equals(idMvtParam)) {
-				mvtView = mouvementVisuManager.get(id);
-			}
+		MouvementDetailView view;
+
+		public MouvementForJSON (MouvementDetailView mouvementDetailView) {
+			view = mouvementDetailView;
 		}
 
-		return mvtView;
+		public String getId() {
+			return view.getId() == null ? "" : view.getId().toString();
+		}
+
+		public String getCollAdmDestinataireEnvoi() {
+			return view.getCollAdmDestinataireEnvoi() == null ? "" : view.getCollAdmDestinataireEnvoi();
+		}
+
+		public String getLocalisation() {
+
+			String loc = "";
+
+			if (view.getLocalisation() != null) {
+				loc = messageSource.getMessage(
+						"option.localisation." + view.getLocalisation().name(),  null,
+						WebContextUtils.getDefaultLocale());
+			}
+			return loc;
+		}
+
+		public RegDate getDateMouvement() {
+			return RegDate.get(view.getDateMouvement());
+		}
+
+		public String getTypeMouvement() {
+			String type = "";
+
+			if (view.getTypeMouvement() != null) {
+				type = messageSource.getMessage(
+						"option.type.mouvement." + view.getTypeMouvement().name(),  null,
+						WebContextUtils.getDefaultLocale());
+			}
+			return type;
+		}
+
+		public String getNoCollAdmDestinataireEnvoi() {
+			return view.getNoCollAdmDestinataireEnvoi() == null ? "" : view.getNoCollAdmDestinataireEnvoi().toString();
+		}
+
+		public String getIdTache() {
+			return view.getIdTache() == null ? "" : view.getIdTache().toString();
+		}
+
+		public String getNumeroUtilisateurEnvoi() {
+			return view.getNumeroUtilisateurEnvoi() == null ? "" : view.getNumeroUtilisateurEnvoi().toString();
+		}
+
+		public String getNumeroUtilisateurReception() {
+			return view.getNumeroUtilisateurReception() == null ? "" : view.getNumeroUtilisateurReception().toString();
+		}
+
+		public String getEtatMouvement() {
+			String etat = "";
+
+			if (view.getEtatMouvement() != null) {
+				etat = messageSource.getMessage(
+						"option.type.mouvement." + view.getEtatMouvement().name(),  null,
+						WebContextUtils.getDefaultLocale());
+			}
+			return etat;
+		}
+
+		public String getCollectiviteAdministrative() {
+			return view.getCollectiviteAdministrative() == null ? "" : view.getCollectiviteAdministrative();
+		}
+
+		public String getDestinationUtilisateur() {
+			return view.getDestinationUtilisateur() == null ? "" : view.getDestinationUtilisateur();
+		}
+
+		public String getNomPrenomUtilisateur() {
+			return view.getNomPrenomUtilisateur() == null ? "" : view.getNomPrenomUtilisateur();
+		}
+
+		public String getNumeroTelephoneUtilisateur() {
+			return view.getNumeroTelephoneUtilisateur() == null ? "" : view.getNumeroTelephoneUtilisateur();
+		}
+
+		public String getDestinationEnvoi() {
+			return view.getDestinationEnvoi() == null ? "" : view.getDestinationEnvoi();
+		}
+
+		public Date getDateExecution() {
+			return view.getDateExecution();
+		}
+
+		public String getExecutant() {
+			return view.getExecutant() == null ? "" : view.getExecutant();
+		}
+
+		public String getUtilisateurEnvoi() {
+			return view.getUtilisateurEnvoi() == null ? "" : view.getUtilisateurEnvoi();
+		}
+
+		public String getUtilisateurReception() {
+			return view.getUtilisateurReception() == null ? "" : view.getUtilisateurReception();
+		}
 	}
-
-	/**
-	 * @see org.springframework.web.servlet.mvc.SimpleFormController#showForm(javax.servlet.http.HttpServletRequest,
-	 *      javax.servlet.http.HttpServletResponse, org.springframework.validation.BindException, java.util.Map)
-	 */
-	@SuppressWarnings("unchecked")
-	@Override
-	protected ModelAndView showForm(HttpServletRequest request, HttpServletResponse response, BindException errors, Map model)
-		throws Exception {
-		ModelAndView mav = super.showForm(request, response, errors, model);
-		return mav;
-	}
-
-
-
-
 }
