2289e32f2f42547885e9caef8953631909ba4753
Jacob Deker <jacob.deker@vd.ch>
2012-06-12 05:32:55 +0000

    Ajouté la description javadoc des paramètres des méthodes du CopieConformeManager
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@81498 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/java/ch/vd/uniregctb/copieConforme/CopieConformeManager.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/copieConforme/CopieConformeManager.java
@@ -12,25 +12,24 @@ public interface CopieConformeManager {
 
 	/**
 	 * Renvoie un document PDF de la copie conforme de la sommation de déclaration dont l'état (de type 'SOMME') est indiqué en paramètre
-	 *
+	 * @param idEtatSomme identifiant de l'état "sommé" de la sommation (DI ou LR)
 	 * @return document PDF
 	 */
 	InputStream getPdfCopieConformeSommation(Long idEtatSomme) throws EditiqueException;
 
 	/**
 	 * Renvoie un document PDF de la copie conforme de la confirmation de délai pour une déclaration
-	 *
-	 * @param idDelai
+	 * @param idDelai identifiant du délai accordé pour le renvoi de la déclaration
 	 * @return document PDF
 	 */
 	InputStream getPdfCopieConformeDelai(Long idDelai) throws EditiqueException;
 
 	/**
 	 * Renvoie un document PDF identifié par sa clé d'archivage dans Folders
-	 *
 	 * @param noCtb numéro du contribuable pour lequel on va chercher un document
 	 * @param typeDoc le type de document (voir {@link ch.vd.uniregctb.editique.TypeDocumentEditique})
-	 * @param key la clé d'archivage  @return document PDF
+	 * @param key la clé d'archivage
+	 * @return document PDF
 	 */
 	InputStream getPdfCopieConforme(long noCtb, TypeDocumentEditique typeDoc, String key) throws EditiqueException;
 }
