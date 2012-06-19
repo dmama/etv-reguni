50c788d5652b425a1738323b572fc572e2f019d4
Jacob Deker <jacob.deker@vd.ch>
2012-06-19 05:03:36 +0000

    [e-Facture] Quelques corrections typographiques
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@82015 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/efacture/RetourEFacture.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/efacture/RetourEFacture.java
@@ -1,25 +1,29 @@
 package ch.vd.uniregctb.efacture;
 
+import org.jetbrains.annotations.Nullable;
+
 public enum RetourEFacture {
 
-
-	NUMERO_CTB_INCOHERENT( null,"Numéro de contribuable incohérent"),
-	NUMERO_AVS_CTB_INCOHERENT(null,"Numéro AVS incohérent avec le numéro de contribuable"),
-	ADRESSE_COURRIER_INEXISTANTE(null,"Aucune adresse courrier pour ce contribuable"),
-	EN_ATTENTE_CONTACT(1,"Mise en attente: L'assujettissement est incoherent avec l'e-facture"),
-	EN_ATTENTE_SIGNATURE(2,"En attente de confirmation d'inscritpion");
-
+	NUMERO_CTB_INCOHERENT(null, "Numéro de contribuable incohérent"),
+	NUMERO_AVS_CTB_INCOHERENT(null, "Numéro AVS incohérent avec le numéro de contribuable"),
+	ADRESSE_COURRIER_INEXISTANTE(null, "Aucune adresse courrier pour ce contribuable"),
+	EN_ATTENTE_CONTACT(1, "Mise en attente : l'assujettissement est incohérent avec la e-Facture"),
+	EN_ATTENTE_SIGNATURE(2, "En attente de confirmation d'inscription");
 
 	private final Integer code;
 	private final String description;
 
-
-	RetourEFacture(Integer code,String motif) {
+	private RetourEFacture(Integer code, String motif) {
 		this.code = code;
 		this.description = motif;
 	}
 
 	public String getDescription() {
 		return description;
+	}
+
+	@Nullable
+	public Integer getCode() {
+		return code;
 	}
 }
