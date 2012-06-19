b7778e00eb20eecbdd85b3f98553900739266053
Frédéric Noguier <frederic.noguier@vd.ch>
2012-06-19 17:04:10 +0000

    [e-Facture] verifications basiques pour la demande d'inscription
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@82103 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/efacture/EFactureEventHandlerImpl.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/efacture/EFactureEventHandlerImpl.java
@@ -1,14 +1,17 @@
 package ch.vd.uniregctb.efacture;
 
+import org.apache.commons.validator.EmailValidator;
 import org.apache.log4j.Logger;
 import org.springframework.core.io.ClassPathResource;
 
+import ch.vd.registre.base.avs.AvsHelper;
 import ch.vd.registre.base.date.RegDateHelper;
 import ch.vd.uniregctb.common.FormatNumeroHelper;
 
 public class EFactureEventHandlerImpl implements EFactureEventHandler {
 
 	private final Logger LOGGER = Logger.getLogger(EFactureEventHandlerImpl.class);
+	private EFactureMessageSender sender;
 
 	@Override
 	public void handle(EFactureEvent event) {
@@ -19,12 +22,69 @@ public class EFactureEventHandlerImpl implements EFactureEventHandler {
 			}
 			else {
 				// TODO e-facture à faire
-				LOGGER.info(String.format("Reçu demande d'inscription e-Facture du contribuable %s/%s au %s", FormatNumeroHelper.numeroCTBToDisplay(valid.getCtbId()), FormatNumeroHelper.formatNumAVS(valid.getNoAvs()), RegDateHelper.dateToDisplayString(valid.getDateDemande())));
+				final DemandeValidationInscription inscription = (DemandeValidationInscription) event;
+				LOGGER.info(String.format("Reçu demande d'inscription e-Facture du contribuable %s/%s au %s", FormatNumeroHelper.numeroCTBToDisplay(inscription.getCtbId()), FormatNumeroHelper.formatNumAVS(inscription.getNoAvs()), RegDateHelper.dateToDisplayString(inscription.getDateDemande())));
+				try {
+					if (!performBasicChecks(inscription)) {
+						return;
+					}
+
+					// 3.1.3. Vérifier demandes en cours
+
+					// 3.1.4. Identifier le contribuable
+
+					// 3.1.5. Stocker l’adresse de courrier électronique de la demande
+
+					// 3.1.6. Vérifier l’existence d’une adresse courrier
+
+					// 3.1.7. Vérifier l’historique e-facture
+
+					// 3.1.8. Vérifier de l’état du contribuable
+
+					// 3.1.9. Envoi des courriers
+
+					// 3.1.10. L’historique des états
+					   // ---> TODO Rien, si j'ai bien compris, l'historisation des états est géré par l'e-Facture
+
+					// 3.1.11. Archiver les documents de correspondance
+						// ---> On renvoie la clé d'archivage a e-facture qui la stocke
+
+				} catch (EvenementEfactureException e) {
+					LOGGER.error(e.getMessage(), e);
+				}
 			}
 		}
 		else {
 			throw new IllegalArgumentException("Type d'événement non supporté : " + event.getClass());
 		}
+	}
+
+	/**
+	 * @param inscription l'inscrition dont on doit verifier les données de base: n° avs, email, date de la demande
+	 *
+	 * @return true si les verifications de base sont ok; false sinon
+	 *
+	 * @throws EvenementEfactureException s'il y a un problème avec l'envoi d'un message d'erreur
+	 */
+	private boolean performBasicChecks(DemandeValidationInscription inscription) throws EvenementEfactureException {
+		//Check Numéro AVS à 13 chiffres
+		if (!AvsHelper.isValidNouveauNumAVS(inscription.getNoAvs())) {
+			sender.envoieRefusDemandeInscription(inscription.getIdDemande(), TypeRefusEFacture.NUMERO_AVS_INVALIDE);
+			return false;
+		}
+		//Check Adresse de courrier électronique
+		if (!EmailValidator.getInstance().isValid(inscription.getEmail())) {
+			sender.envoieRefusDemandeInscription(inscription.getIdDemande(), TypeRefusEFacture.EMAIL_INVALIDE);
+			return false;
+		}
+		//Check Date et heure de la demande
+		// TODO A DISCUTER: Les specs parlent d'heure de la demande or RegDate n'a pas d'heure
+		if (inscription.getDateDemande() == null) {
+			sender.envoieRefusDemandeInscription(inscription.getIdDemande(), TypeRefusEFacture.DATE_DEMANDE_ABSENTE);
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
--- unireg/business/src/main/java/ch/vd/uniregctb/efacture/TypeRefusEFacture.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/efacture/TypeRefusEFacture.java
@@ -2,6 +2,9 @@ package ch.vd.uniregctb.efacture;
 
 public enum TypeRefusEFacture {
 
+	NUMERO_AVS_INVALIDE("Numéro AVS invalide"),
+	EMAIL_INVALIDE("Adresse de courrier éléctronique invalide"),
+	DATE_DEMANDE_ABSENTE("Date de la demande non renseignée"),
 	NUMERO_CTB_INCOHERENT("Numéro de contribuable incohérent"),
 	NUMERO_AVS_CTB_INCOHERENT("Numéro AVS incohérent avec le numéro de contribuable"),
 	ADRESSE_COURRIER_INEXISTANTE("Aucune adresse courrier pour ce contribuable");
