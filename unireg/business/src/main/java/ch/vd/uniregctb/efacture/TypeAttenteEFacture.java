60d442d8efd648e88db560efb12cd94d5cc366f1
Baba Issa Ngom <baba-issa.ngom@vd.ch>
2012-06-19 11:27:50 +0000

    [E-Facture]Suppression des types de retour sans code
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@82048 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/efacture/TypeAttenteEFacture.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/efacture/TypeAttenteEFacture.java
@@ -2,18 +2,15 @@ package ch.vd.uniregctb.efacture;
 
 import org.jetbrains.annotations.Nullable;
 
-public enum RetourEFacture {
+public enum TypeAttenteEFacture {
 
-	NUMERO_CTB_INCOHERENT(null, "Numéro de contribuable incohérent"),
-	NUMERO_AVS_CTB_INCOHERENT(null, "Numéro AVS incohérent avec le numéro de contribuable"),
-	ADRESSE_COURRIER_INEXISTANTE(null, "Aucune adresse courrier pour ce contribuable"),
 	EN_ATTENTE_CONTACT(1, "Mise en attente : l'assujettissement est incohérent avec la e-Facture"),
 	EN_ATTENTE_SIGNATURE(2, "En attente de confirmation d'inscription");
 
 	private final Integer code;
 	private final String description;
 
-	private RetourEFacture(Integer code, String motif) {
+	private TypeAttenteEFacture(Integer code, String motif) {
 		this.code = code;
 		this.description = motif;
 	}
