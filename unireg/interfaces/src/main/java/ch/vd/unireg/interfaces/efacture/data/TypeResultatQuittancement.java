db843fb578ac268b5ca93d8743333dfaecf994cb
Jacob Deker <jacob.deker@vd.ch>
2012-07-24 10:43:14 +0000

    [e-Facture] Cleanup esthétique
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/5.1@84264 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/interfaces/src/main/java/ch/vd/unireg/interfaces/efacture/data/TypeResultatQuittancement.java
+++ unireg/interfaces/src/main/java/ch/vd/unireg/interfaces/efacture/data/TypeResultatQuittancement.java
@@ -1,18 +1,19 @@
 package ch.vd.unireg.interfaces.efacture.data;
 
-enum TypeResultatQuittancement {
+public enum TypeResultatQuittancement {
+
 	QUITTANCEMENT_EN_COURS(true, "Quittancement du contribuable (%s) en cours."),
 	QUITTANCEMENT_OK(true, "Quittancement du contribuable (%s) OK."),
-	DEJA_INSCRIT(true, "Quittancement du contribuable (%s) OK, Déjà inscrit."),
+	DEJA_INSCRIT(true, "Quittancement du contribuable (%s) OK, déjà inscrit."),
 	CONTRIBUABLE_INEXISTANT(false, "Le contribuable (%s) n’existe pas. Veuillez vérifier le numéro saisi."),
-	ETAT_EFACTURE_INCOHERENT(false, "Quittancement impossible. Le contribuable (%s) est dans un état e-facture incohérent"),
+	ETAT_EFACTURE_INCOHERENT(false, "Quittancement impossible. Le contribuable (%s) est dans un état e-facture incompatible avec un quittancement."),
 	ETAT_FISCAL_INCOHERENT (false, "Quittancement non-autorisé. L'asujettissement du contribuable (%s) est incompatible avec l'e-facture."),
-	AUCUNE_DEMANDE_EN_ATTENTE_SIGNATURE(false, "Aucune demande en cours de validation n'est en attente de signature pour le contribuable (%s)");
+	AUCUNE_DEMANDE_EN_ATTENTE_SIGNATURE(false, "Aucune demande en cours de validation n'est en attente de signature pour le contribuable (%s).");
 
-	private boolean ok;
-	private String description;
+	private final boolean ok;
+	private final String description;
 
-	TypeResultatQuittancement(boolean ok, String description) {
+	private TypeResultatQuittancement(boolean ok, String description) {
 		this.ok = ok;
 		this.description = description;
 	}
@@ -24,6 +25,4 @@ enum TypeResultatQuittancement {
 	public boolean isOk() {
 		return ok;
 	}
-
-
 }
