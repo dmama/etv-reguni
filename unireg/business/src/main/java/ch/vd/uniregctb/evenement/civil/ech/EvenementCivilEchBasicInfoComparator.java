e7106bf507367f3d51339beacedd35027cf5def4
Jacob Deker <jacob.deker@vd.ch>
2012-07-26 05:39:49 +0000

    A date, type et action égaux, les événements civils d'un individu sont triés par ordre croissant de leur identifiant.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@84342 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/evenement/civil/ech/EvenementCivilEchBasicInfoComparator.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/evenement/civil/ech/EvenementCivilEchBasicInfoComparator.java
@@ -7,10 +7,13 @@ import java.util.Comparator;
  *  1. les plus anciens en premier
  *  2. Suivant la priorité du type de l'événement pour les evenements ayant la meme date
  *  3. Suivant la priorité de l'action pour les evenements avec un type de même priorité
+ *  4. Puis par identifiant croissant
  */
-class EvenementCivilEchBasicInfoComparator implements Comparator<EvenementCivilEchBasicInfo> {
+public class EvenementCivilEchBasicInfoComparator implements Comparator<EvenementCivilEchBasicInfo> {
+
 	private static final TypeEvenementCivilEchComparator TYPE_PRIORITY_COMPARATOR = new TypeEvenementCivilEchComparator();
 	private static final ActionEvenementCivilEchComparator ACTION_PRIORITY_COMPARATOR = new ActionEvenementCivilEchComparator();
+
 	@Override
 	public int compare(EvenementCivilEchBasicInfo o1, EvenementCivilEchBasicInfo o2) {
 		int comp = o1.getDate().compareTo(o2.getDate());
@@ -19,6 +22,9 @@ class EvenementCivilEchBasicInfoComparator implements Comparator<EvenementCivilE
 		}
 		if (comp == 0) {
 			comp = ACTION_PRIORITY_COMPARATOR.compare(o1.getAction(), o2.getAction());
+		}
+		if (comp == 0) {
+			comp = (o1.getId() < o2.getId() ? -1 : (o1.getId() > o2.getId() ? 1 : 0));
 		}
 		return comp;
 	}
