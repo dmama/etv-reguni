c8d60a4e19e8aca21e2ce13bc16ce1da8be7ae3d
Jacob Deker <jacob.deker@vd.ch>
2012-08-06 13:33:37 +0000

    [SIFISC-5860][SIFISC-5862] Corrigé les messages affichés lors du quittancement "douchette" des confirmations d'inscription à la e-facture
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@84808 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/interfaces/src/main/java/ch/vd/unireg/interfaces/efacture/data/TypeResultatQuittancement.java
+++ unireg/interfaces/src/main/java/ch/vd/unireg/interfaces/efacture/data/TypeResultatQuittancement.java
@@ -2,13 +2,13 @@ package ch.vd.unireg.interfaces.efacture.data;
 
 public enum TypeResultatQuittancement {
 
-	QUITTANCEMENT_EN_COURS(true, "Quittancement du contribuable (%s) en cours."),
-	QUITTANCEMENT_OK(true, "Quittancement du contribuable (%s) OK."),
-	DEJA_INSCRIT(true, "Quittancement du contribuable (%s) OK, déjà inscrit."),
-	CONTRIBUABLE_INEXISTANT(false, "Le contribuable (%s) n’existe pas. Veuillez vérifier le numéro saisi."),
-	ETAT_EFACTURE_INCOHERENT(false, "Quittancement impossible. Le contribuable (%s) est dans un état e-facture incompatible avec un quittancement."),
-	ETAT_FISCAL_INCOHERENT (false, "Quittancement non-autorisé. L'asujettissement du contribuable (%s) est incompatible avec l'e-facture."),
-	AUCUNE_DEMANDE_EN_ATTENTE_SIGNATURE(false, "Aucune demande en cours de validation n'est en attente de signature pour le contribuable (%s).");
+	QUITTANCEMENT_EN_COURS(true, "Quittancement du contribuable %s en cours."),
+	QUITTANCEMENT_OK(true, "Le contribuable %s est maintenant inscrit à la e-facture."),
+	DEJA_INSCRIT(true, "Le contribuable %s est déjà inscrit à la e-facture."),
+	CONTRIBUABLE_INEXISTANT(false, "Le contribuable %s n’existe pas. Veuillez vérifier le numéro saisi."),
+	ETAT_EFACTURE_INCOHERENT(false, "Quittancement impossible. Le contribuable %s est dans un état e-facture incompatible avec un quittancement."),
+	ETAT_FISCAL_INCOHERENT (false, "Quittancement non-autorisé. L'assujettissement du contribuable %s est incompatible avec la e-facture."),
+	AUCUNE_DEMANDE_EN_ATTENTE_SIGNATURE(false, "Aucune demande en cours de validation n'est en attente de signature pour le contribuable %s.");
 
 	private final boolean ok;
 	private final String description;
