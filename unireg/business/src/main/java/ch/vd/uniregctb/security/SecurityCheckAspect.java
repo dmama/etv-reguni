cb99e8e0d367a05f5971ef26b0e720901ac7cffa
Jacob Deker <jacob.deker@vd.ch>
2012-08-09 06:46:55 +0000

    Petit cleanup
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@85026 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/security/SecurityCheckAspect.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/security/SecurityCheckAspect.java
@@ -6,15 +6,15 @@ import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class SecurityCheckAspect {
+
 	@Around("@annotation(securityCheck)")
 	public Object secutityCheck(ProceedingJoinPoint pjp, SecurityCheck securityCheck) throws Throwable {
-		for (Role role : securityCheck.rolesToCheck()) {
-			if (SecurityProvider.isGranted(role)) {
-				// L'utilisateur possède au moins un rôle, il a donc accés à la méthode sous-jacente
-				return pjp.proceed();
-			}
+		if (SecurityProvider.isAnyGranted(securityCheck.rolesToCheck())) {
+			// L'utilisateur possède au moins un des rôles demandés, il a donc accès à la méthode sous-jacente
+			return pjp.proceed();
 		}
-		// L'acces est refusé si au moins un des roles n'est pas accordé pour l'utilisateur
+
+		// L'acces est refusé si aucun des rôles n'est accordé pour l'utilisateur
 		throw new AccessDeniedException(securityCheck.accessDeniedMessage());
 	}
 }
