cfddc9ceaa639773bd1b58c21a94bc662d5fbf4d
Jacob Deker <jacob.deker@vd.ch>
2012-08-15 11:59:57 +0000

    [SIFISC-5587] Ré-implémenté la stratégie de comparaison des permis avant et après un événement civil de correction pour ne vraiment tenir compte que du permis C
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/5.1@85412 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/evenement/civil/engine/ech/PermisComparisonStrategy.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/evenement/civil/engine/ech/PermisComparisonStrategy.java
@@ -1,12 +1,13 @@
 package ch.vd.uniregctb.evenement.civil.engine.ech;
 
-import java.util.Comparator;
+import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import ch.vd.registre.base.date.NullDateBehavior;
 import ch.vd.unireg.interfaces.civil.data.IndividuApresEvenement;
 import ch.vd.unireg.interfaces.civil.data.Permis;
+import ch.vd.unireg.interfaces.civil.data.PermisList;
 import ch.vd.uniregctb.common.DataHolder;
 import ch.vd.uniregctb.type.TypePermis;
 
@@ -17,56 +18,54 @@ public class PermisComparisonStrategy implements IndividuComparisonStrategy {
 
 	private static final String ATTRIBUT = "permis";
 
-	private static final Comparator<Permis> PERMIS_COMPARATOR = new IndividuComparisonHelper.NullableComparator<Permis>(true) {
-		@Override
-		protected int compareNonNull(@NotNull Permis o1, @NotNull Permis o2) {
-			int comparison = NullDateBehavior.EARLIEST.compare(o1.getDateAnnulation(), o2.getDateAnnulation());
-			if (comparison == 0) {
-				comparison = IndividuComparisonHelper.RANGE_COMPARATOR.compare(o1, o2);
-				if (comparison == 0) {
-					final boolean c1 = isPermisC(o1);
-					final boolean c2 = isPermisC(o2);
-					if (!c1 && !c2) {
-						comparison = o1.getTypePermis().ordinal() - o2.getTypePermis().ordinal();
-					}
-					else if (c1 != c2) {
-						comparison = c1 ? -1 : 1;       // permis C passe devant
-					}
+	@Nullable
+	private static Permis getPermisC(List<Permis> list) {
+		for (Permis p : list) {
+			if (p.getTypePermis() == TypePermis.ETABLISSEMENT && p.getDateAnnulation() == null) {
+				return p;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @param list la liste des permis connus
+	 * @return <code>true</code> s'il existe au moins un permis non-annulé dans la liste
+	 */
+	private static boolean hasPermitAtAll(List<Permis> list) {
+		if (list != null && list.size() > 0) {
+			for (Permis p : list) {
+				if (p.getDateAnnulation() == null) {
+					return true;
 				}
 			}
-			return comparison;
 		}
-	};
-
-	private static final IndividuComparisonHelper.Equalator<Permis> PERMIS_EQUALATOR = new IndividuComparisonHelper.NullableEqualator<Permis>() {
-		@Override
-		protected boolean areNonNullEqual(@NotNull Permis o1, @NotNull Permis o2) {
-			if (o1.getDateAnnulation() != null && o2.getDateAnnulation() != null) {
-				return true;
-			}
-			else if (o1.getDateAnnulation() != null || o2.getDateAnnulation() != null) {
-				return false;
-			}
-
-			// peu importe si aucun des deux permis n'est un permis C -> le fiscal n'y accorde aucune importance (même sur les dates) ;
-			// de plus, si on passe d'un permis C à un permis non-C (ou vice-versa), c'est un problème
-			// et si on reste sur un permis C, les dates sont importantes
-			final boolean c1 = isPermisC(o1);
-			final boolean c2 = isPermisC(o2);
-			return (!c1 && !c2) || (c1 == c2 && IndividuComparisonHelper.RANGE_EQUALATOR.areEqual(o1, o2));
-		}
-	};
-
-	private static boolean isPermisC(@NotNull Permis permis) {
-		return permis.getTypePermis() == TypePermis.ETABLISSEMENT;
+		return false;
 	}
 
     @Override
 	public boolean isFiscalementNeutre(IndividuApresEvenement originel, IndividuApresEvenement corrige, @NotNull DataHolder<String> msg) {
-	    if (!IndividuComparisonHelper.areContentsEqual(originel.getIndividu().getPermis(), corrige.getIndividu().getPermis(), PERMIS_COMPARATOR, PERMIS_EQUALATOR)) {
-		    msg.set(ATTRIBUT);
-		    return false;
+
+	    final PermisList permis1 = originel.getIndividu().getPermis();
+	    final PermisList permis2 = corrige.getIndividu().getPermis();
+	    final boolean hasPermit1 = hasPermitAtAll(permis1);
+	    final boolean hasPermit2 = hasPermitAtAll(permis2);
+	    boolean neutre = true;
+	    if (hasPermit1 || hasPermit2) {
+		    if (hasPermit1 != hasPermit2) {
+			    // information présente d'un côté, mais pas de l'autre...
+			    neutre = false;
+		    }
+		    else {
+			    // ici, on a des permis des deux côtés... mais quels sont-ils ?
+			    final Permis p1 = getPermisC(permis1);
+			    final Permis p2 = getPermisC(permis2);
+			    neutre = (p1 == null && p2 == null) || IndividuComparisonHelper.RANGE_EQUALATOR.areEqual(p1, p2);
+		    }
 	    }
-	    return true;
+	    if (!neutre) {
+		    msg.set(ATTRIBUT);
+	    }
+	    return neutre;
     }
 }
