4b5076c75f8a0797d5b96632921d8aa44808ecb2
Jacob Deker <jacob.deker@vd.ch>
2012-08-15 05:19:50 +0000

    [SIFISC-5587] Adapté à la nouvelle règle concernant les différences sur les permis (seulement quand le type change de C à non-C ou de non-C à C)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/5.1@85354 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/evenement/civil/engine/ech/PermisComparisonStrategy.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/evenement/civil/engine/ech/PermisComparisonStrategy.java
@@ -8,6 +8,7 @@ import ch.vd.registre.base.date.NullDateBehavior;
 import ch.vd.unireg.interfaces.civil.data.IndividuApresEvenement;
 import ch.vd.unireg.interfaces.civil.data.Permis;
 import ch.vd.uniregctb.common.DataHolder;
+import ch.vd.uniregctb.type.TypePermis;
 
 /**
  * Comparateur d'individu basé sur les permis de l'individu
@@ -23,7 +24,14 @@ public class PermisComparisonStrategy implements IndividuComparisonStrategy {
 			if (comparison == 0) {
 				comparison = IndividuComparisonHelper.RANGE_COMPARATOR.compare(o1, o2);
 				if (comparison == 0) {
-					comparison = o1.getTypePermis().ordinal() - o2.getTypePermis().ordinal();
+					final boolean c1 = isPermisC(o1);
+					final boolean c2 = isPermisC(o2);
+					if (!c1 && !c2) {
+						comparison = o1.getTypePermis().ordinal() - o2.getTypePermis().ordinal();
+					}
+					else if (c1 != c2) {
+						comparison = c1 ? -1 : 1;       // permis C passe devant
+					}
 				}
 			}
 			return comparison;
@@ -40,9 +48,18 @@ public class PermisComparisonStrategy implements IndividuComparisonStrategy {
 				return false;
 			}
 
-			return IndividuComparisonHelper.RANGE_EQUALATOR.areEqual(o1, o2) && o1.getTypePermis() == o2.getTypePermis();
+			// peu importe si aucun des deux permis n'est un permis C -> le fiscal n'y accorde aucune importance (même sur les dates) ;
+			// de plus, si on passe d'un permis C à un permis non-C (ou vice-versa), c'est un problème
+			// et si on reste sur un permis C, les dates sont importantes
+			final boolean c1 = isPermisC(o1);
+			final boolean c2 = isPermisC(o2);
+			return (!c1 && !c2) || (c1 == c2 && IndividuComparisonHelper.RANGE_EQUALATOR.areEqual(o1, o2));
 		}
 	};
+
+	private static boolean isPermisC(@NotNull Permis permis) {
+		return permis.getTypePermis() == TypePermis.ETABLISSEMENT;
+	}
 
     @Override
 	public boolean isFiscalementNeutre(IndividuApresEvenement originel, IndividuApresEvenement corrige, @NotNull DataHolder<String> msg) {
