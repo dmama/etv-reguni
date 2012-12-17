eb0478e4e4a7aef0e06a6acf4eb65a18e3020692
Manuel Siggen <manuel.siggen@vd.ch>
2012-12-17 07:28:12 +0000

    Corrigé l'URL du web-service SIPF en intégration.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@93734 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- watchdog-sipf/src/test/java/ch.vd.watchdog/WatchDogBvrPlusIntegrationTest.java
+++ watchdog-sipf/src/test/java/ch.vd.watchdog/WatchDogBvrPlusIntegrationTest.java
@@ -1,11 +1,7 @@
 package ch.vd.watchdog;
 
-import java.math.BigInteger;
-
 import junit.framework.TestCase;
 
-import ch.vd.service.sipf.wsdl.sipfbvrplus_v1.BvrDemande;
-import ch.vd.service.sipf.wsdl.sipfbvrplus_v1.BvrReponse;
 import ch.vd.uniregctb.webservice.sipf.BVRPlusClientImpl;
 
 /**
@@ -16,7 +12,7 @@ public class WatchDogBvrPlusIntegrationTest extends TestCase {
 	public void testIntegration() throws Exception {
 
 		final BVRPlusClientImpl service = new BVRPlusClientImpl();
-		service.setServiceUrl("http://ssv0213v.etat-de-vaud.ch:50110/fiscalite/int-sipf/bvrplus/SipfBVRPlusImpl");
+		service.setServiceUrl("http://ssv0213v.etat-de-vaud.ch:50110/fiscalite/int-sipf/bvrplus/SipfBVRPlus");
 		service.ping();
 	}
 }
