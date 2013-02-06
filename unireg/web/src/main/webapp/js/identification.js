76ba2dc4d868669f8f54b879760caf5f3e255d42
Jacob Deker <jacob.deker@vd.ch>
2013-02-06 08:17:18 +0000

    [SIFISC-7961] Corrigé l'affichage des checkboxes de sélection des messages à soumettre/suspendre/bloquer/débloquer pour les bénéficiaires des rôles SUPERGRA et MW_IDENT_CTB_GEST_BO
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/5.3@96331 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/webapp/js/identification.js
+++ unireg/web/src/main/webapp/js/identification.js
@@ -13,12 +13,32 @@ var IdentificationCtb = {
         }
     },
 
-    /*
-    * Suspendre les messages
-    */
+	isAtLeastOneMessageSelected: function() {
+		var lignesMessage = document.getElementById('message').getElementsByTagName('tr');
+		var taille = lignesMessage.length;
+
+		for(var i=1; i < taille; i++) {
+			var sel = $('#tabIdsMessages_' + i);
+			if (sel && !sel.disabled && sel.attr('checked')) {
+				return true;
+			}
+		}
+		return false;
+	},
+
+	desactiveBoutonsActionsEnMasse: function() {
+		$('#actions-masse-messages').find(':button').attr('disabled', true);
+	},
+
+	/*
+	* Suspendre les messages
+	*/
     confirmeSuspensionMessage: function() {
-        if(confirm('Voulez-vous suspendre le(s) message(s) selectionné(s) ?')) {
-            $('#desynchro').show();
+	    if (!this.isAtLeastOneMessageSelected()) {
+		    alert("Veuillez sélectionner au moins un message à suspendre.");
+	    }
+        else if (confirm('Voulez-vous suspendre le(s) message(s) selectionné(s) ?')) {
+		    this.desactiveBoutonsActionsEnMasse();
             var form = $("#formRechercheMessage");
             form.attr('action', 'suspendre.do');
             form.submit();
@@ -29,8 +49,11 @@ var IdentificationCtb = {
     * Soumet les messages
     */
     confirmeSoumissionMessage: function() {
-        if(confirm('Voulez-vous soumettre à nouveau le(s) message(s) selectionné(s) ?')) {
-            $('#desynchro').show();
+	    if (!this.isAtLeastOneMessageSelected()) {
+		    alert("Veuillez sélectionner au moins un message à soumettre.");
+	    }
+	    else if (confirm('Voulez-vous soumettre à nouveau le(s) message(s) selectionné(s) ?')) {
+		    this.desactiveBoutonsActionsEnMasse();
             var form = $("#formRechercheMessage");
             form.attr('action', 'resoumettre.do');
             form.submit();
@@ -38,8 +61,11 @@ var IdentificationCtb = {
     },
 
     confirmeDeblocageMessage: function() {
-        if(confirm('Voulez-vous débloquer le(s) message(s) selectionné(s) ?')) {
-            $('#desynchro').show();
+	    if (!this.isAtLeastOneMessageSelected()) {
+		    alert("Veuillez sélectionner au moins un message à débloquer.");
+	    }
+	    else if (confirm('Voulez-vous débloquer le(s) message(s) selectionné(s) ?')) {
+		    this.desactiveBoutonsActionsEnMasse();
             var form = $("#formRechercheMessage");
             form.attr('action', 'unlock.do');
             form.submit();
@@ -47,8 +73,11 @@ var IdentificationCtb = {
     },
 
     confirmeBlocageMessage: function() {
-        if(confirm('Voulez-vous débloquer le(s) message(s) selectionné(s) ?')) {
-            $('#desynchro').show();
+	    if (!this.isAtLeastOneMessageSelected()) {
+		    alert("Veuillez sélectionner au moins un message à bloquer.");
+	    }
+	    else if (confirm('Voulez-vous bloquer le(s) message(s) selectionné(s) ?')) {
+		    this.desactiveBoutonsActionsEnMasse();
             var form = $("#formRechercheMessage");
             form.attr('action', 'lock.do');
             form.submit();
