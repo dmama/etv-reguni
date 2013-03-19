9e435312fd40477ee4aa111ec735db680edf78ec
Jacob Deker <jacob.deker@vd.ch>
2013-03-19 06:15:56 +0000

    Ajouté la surveillance de la charge d'appels sur les services civils et d'infrastructure à l'entrée dans le module Nexus
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@98644 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/nexus/src/main/java/ch/vd/unireg/interfaces/civil/ServiceCivilEndPoint.java
+++ unireg/nexus/src/main/java/ch/vd/unireg/interfaces/civil/ServiceCivilEndPoint.java
@@ -13,24 +13,31 @@ import ch.vd.unireg.interfaces.civil.data.AttributeIndividu;
 import ch.vd.unireg.interfaces.civil.data.Individu;
 import ch.vd.unireg.interfaces.civil.data.IndividuApresEvenement;
 import ch.vd.unireg.interfaces.civil.data.Nationalite;
+import ch.vd.uniregctb.load.DetailedLoadMeter;
+import ch.vd.uniregctb.load.DetailedLoadMonitorable;
+import ch.vd.uniregctb.load.LoadDetail;
+import ch.vd.uniregctb.load.MethodCallDescriptor;
 
 /**
  * Façade du service civil raw à utiliser pour exposer le service civil en spring remoting.
  * <p/>
  * Cette implémentation délègue tous les appels à une autre instance du service, en loggant et wrappant toutes les exceptions dans des {@link ServiceCivilException}.
  */
-public class ServiceCivilEndPoint implements ServiceCivilRaw {
+public class ServiceCivilEndPoint implements ServiceCivilRaw, ServiceCivilServiceWrapper, DetailedLoadMonitorable {
 
 	private static final Logger LOGGER = Logger.getLogger(ServiceCivilEndPoint.class);
 
 	private ServiceCivilRaw target;
+
+	private final DetailedLoadMeter<MethodCallDescriptor> loadMeter = new DetailedLoadMeter<>();
 
 	public void setTarget(ServiceCivilRaw target) {
 		this.target = target;
 	}
 
 	@Override
-	public Individu getIndividu(long noIndividu, AttributeIndividu... parties) throws ServiceCivilException {
+	public Individu getIndividu(long noIndividu, final AttributeIndividu... parties) throws ServiceCivilException {
+		loadMeter.start(new MethodCallDescriptor("getIndividu", "noIndividu", noIndividu, "parties", parties));
 		try {
 			return target.getIndividu(noIndividu, parties);
 		}
@@ -39,10 +46,14 @@ public class ServiceCivilEndPoint implements ServiceCivilRaw {
 			// on ne transmet que le message, pour éviter de transmettre des éléments non-sérializable
 			throw new ServiceCivilException(getMessage(e));
 		}
+		finally {
+			loadMeter.end();
+		}
 	}
 
 	@Override
 	public List<Individu> getIndividus(Collection<Long> nosIndividus, AttributeIndividu... parties) throws ServiceCivilException {
+		loadMeter.start(new MethodCallDescriptor("getIndividus", "nosIndividus", nosIndividus, "parties", parties));
 		try {
 			return target.getIndividus(nosIndividus, parties);
 		}
@@ -52,10 +63,14 @@ public class ServiceCivilEndPoint implements ServiceCivilRaw {
 			// on ne transmet que le message, pour éviter de transmettre des éléments non-sérializable
 			throw new ServiceCivilException(getMessage(e));
 		}
+		finally {
+			loadMeter.end();
+		}
 	}
 
 	@Override
 	public Individu getIndividuByEvent(long evtId, AttributeIndividu... parties) throws ServiceCivilException {
+		loadMeter.start(new MethodCallDescriptor("getIndividuByEvent", "eventId", evtId, "parties", parties));
 		try {
 			return target.getIndividuByEvent(evtId, parties);
 		}
@@ -65,10 +80,14 @@ public class ServiceCivilEndPoint implements ServiceCivilRaw {
 			// on ne transmet que le message, pour éviter de transmettre des éléments non-sérializable
 			throw new ServiceCivilException(getMessage(e));
 		}
+		finally {
+			loadMeter.end();
+		}
 	}
 
 	@Override
 	public IndividuApresEvenement getIndividuAfterEvent(long eventId) {
+		loadMeter.start(new MethodCallDescriptor("getIndividuAfterEvent", "eventId", eventId));
 		try {
 			return target.getIndividuAfterEvent(eventId);
 		}
@@ -77,10 +96,14 @@ public class ServiceCivilEndPoint implements ServiceCivilRaw {
 			// on ne transmet que le message, pour éviter de transmettre des éléments non-sérializable
 			throw new ServiceCivilException(getMessage(e));
 		}
+		finally {
+			loadMeter.end();
+		}
 	}
 
 	@Override
 	public Nationalite getNationaliteAt(long noIndividu, @Nullable RegDate date) {
+		loadMeter.start(new MethodCallDescriptor("getNationaliteAt", "noIndividu", noIndividu, "date", date));
 		try {
 			return target.getNationaliteAt(noIndividu, date);
 		}
@@ -89,10 +112,14 @@ public class ServiceCivilEndPoint implements ServiceCivilRaw {
 			// on ne transmet que le message, pour éviter de transmettre des éléments non-sérializable
 			throw new ServiceCivilException(getMessage(e));
 		}
+		finally {
+			loadMeter.end();
+		}
 	}
 
 	@Override
 	public void ping() throws ServiceCivilException {
+		loadMeter.start(new MethodCallDescriptor("ping"));
 		try {
 			target.ping();
 		}
@@ -101,10 +128,14 @@ public class ServiceCivilEndPoint implements ServiceCivilRaw {
 			// on ne transmet que le message, pour éviter de transmettre des éléments non-sérializable
 			throw new ServiceCivilException(getMessage(e));
 		}
+		finally {
+			loadMeter.end();
+		}
 	}
 
 	@Override
 	public boolean isWarmable() {
+		loadMeter.start(new MethodCallDescriptor("isWarmable"));
 		try {
 			return target.isWarmable();
 		}
@@ -112,6 +143,9 @@ public class ServiceCivilEndPoint implements ServiceCivilRaw {
 			LOGGER.error("Exception dans isWarmable() : " + getMessage(e), e);
 			// on ne transmet que le message, pour éviter de transmettre des éléments non-sérializable
 			throw new ServiceCivilException(getMessage(e));
+		}
+		finally {
+			loadMeter.end();
 		}
 	}
 
@@ -121,5 +155,30 @@ public class ServiceCivilEndPoint implements ServiceCivilRaw {
 			message = e.getClass().getSimpleName();
 		}
 		return message;
+	}
+
+	@Override
+	public List<LoadDetail> getLoadDetails() {
+		return loadMeter.getLoadDetails();
+	}
+
+	@Override
+	public int getLoad() {
+		return loadMeter.getLoad();
+	}
+
+	@Override
+	public ServiceCivilRaw getTarget() {
+		return target;
+	}
+
+	@Override
+	public ServiceCivilRaw getUltimateTarget() {
+		if (target instanceof ServiceCivilServiceWrapper) {
+			return ((ServiceCivilServiceWrapper) target).getUltimateTarget();
+		}
+		else {
+			return target;
+		}
 	}
 }
