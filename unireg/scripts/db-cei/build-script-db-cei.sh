6dec9092709d5e4b735dfb821e79aefae646bf92
Jacob Deker <jacob.deker@vd.ch>
2013-04-12 12:51:36 +0000

    Ajouté un peu plus de des commentaires dans le script de génération du ZIP de scripts SQL pour le CEI
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@100145 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/scripts/db-cei/build-script-db-cei.sh
+++ unireg/scripts/db-cei/build-script-db-cei.sh
@@ -5,11 +5,18 @@
 #	   UNIREG est le nom du schéma en base
 #	   SIFISC-8435-8715 est le nom à assigner au package (donc aussi au zip)
 #          *.sql sont les noms des scripts SQL à inclure dans le package (dans l'ordre indiqué)
+#
+# Le but est de construire un fichier ZIP avec l'arborescence suivante :
+# SIFISC-8435-8715/
+#	unireg-web/
+#		01_UNIREG_*.sql
+#		...
+#		99_UNIREG_*.sql
 
 NOM_APPLICATION="$1"
 NOM_SCHEMA="$2"
 NOM_PACKAGE="$3"
-shift 3		# comme ça, tous les paramètres restants sont des noms de scripts SQL"
+shift 3		# comme ça, tous les paramètres restants sont des noms de scripts SQL
 
 if [ -z "$NOM_APPLICATION" -o -z "$NOM_PACKAGE" -o -z "$NOM_SCHEMA" -o "$#" -lt 1 ]; then
 	echo "Syntaxe : $(basename "$0") nom-application nom-schéma nom-package script1.sql [script2.sql...]" >&2
@@ -22,6 +29,7 @@ if [[ "$NOM_PACKAGE" =~ \.sql$ ]]; then
 	exit 1
 fi
 
+# vérification des noms des fichiers de scripts (le standard CEI imposent que leur extension soit .sql)
 MAUVAIS_SCRIPTS=$(for SCRIPT in "$@"; do if [[ ! "$SCRIPT" =~ \.sql$ ]]; then echo "$SCRIPT"; fi; done)
 if [ -n "$MAUVAIS_SCRIPTS" ]; then
 	echo "Les noms des fichiers de script doivent se terminer en .sql:" >&2
@@ -29,6 +37,7 @@ if [ -n "$MAUVAIS_SCRIPTS" ]; then
 	exit 1
 fi
 
+# vérification que tous les fichiers de scripts sont bien accessibles en lecture
 SCRIPTS_ABSENTS=$(for SCRIPT in "$@"; do if [ ! -r "$SCRIPT" ]; then echo "$SCRIPT"; fi; done)
 if [ -n "$SCRIPTS_ABSENTS" ]; then
 	echo "Les noms des scripts doivent correspondre à des fichiers accessibles:" >&2
@@ -36,6 +45,7 @@ if [ -n "$SCRIPTS_ABSENTS" ]; then
 	exit 1
 fi
 
+# construction de la structure du zip dans un répertoire temporaire
 TMP_DIR=$(mktemp -d)
 mkdir -p "$TMP_DIR/$NOM_PACKAGE/$NOM_APPLICATION"
 
@@ -51,6 +61,7 @@ paste <(echo "$SEQ") <(echo "$SCRIPTS") | while read ID SCRIPT; do
 	cp "$SCRIPT" "$NEW_FILE"
 done
 
+# création du zip et cleanup
 (cd "$TMP_DIR" && zip -9r - "$NOM_PACKAGE") > "$NOM_PACKAGE.zip" 
 rm -rf "$TMP_DIR"
 echo "Fichier zip créé: $NOM_PACKAGE.zip"
