5dad55874cad883627ad2422c86382e7503520bd
Jacob Deker <jacob.deker@vd.ch>
2013-08-14 11:14:05 +0000

    [SIFISC-9341] Utilisation des nouveaux messages databaseEvents
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@106223 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/data/DataEventJmsHandler.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/data/DataEventJmsHandler.java
@@ -1,35 +1,173 @@
 package ch.vd.uniregctb.data;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.apache.xmlbeans.XmlError;
-import org.apache.xmlbeans.XmlObject;
-import org.apache.xmlbeans.XmlOptions;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.io.ClassPathResource;
+import org.xml.sax.SAXException;
 
-import ch.vd.fiscalite.registre.databaseEvent.DataChangeEventDocument;
-import ch.vd.fiscalite.registre.databaseEvent.DataChangeEventDocument.DataChangeEvent;
-import ch.vd.fiscalite.registre.databaseEvent.DataType;
-import ch.vd.fiscalite.registre.databaseEvent.DataType.Enum;
-import ch.vd.fiscalite.registre.databaseEvent.DatabaseLoadEventDocument;
-import ch.vd.fiscalite.registre.databaseEvent.DatabaseTruncateEventDocument;
 import ch.vd.technical.esb.EsbMessage;
+import ch.vd.unireg.xml.event.data.v1.DataEvent;
+import ch.vd.unireg.xml.event.data.v1.DatabaseLoadEvent;
+import ch.vd.unireg.xml.event.data.v1.DatabaseTruncateEvent;
+import ch.vd.unireg.xml.event.data.v1.DroitAccesChangeEvent;
+import ch.vd.unireg.xml.event.data.v1.IndividuChangeEvent;
+import ch.vd.unireg.xml.event.data.v1.ObjectFactory;
+import ch.vd.unireg.xml.event.data.v1.PmChangeEvent;
+import ch.vd.unireg.xml.event.data.v1.RelationChangeEvent;
+import ch.vd.unireg.xml.event.data.v1.TiersChangeEvent;
+import ch.vd.unireg.xml.tools.ClasspathCatalogResolver;
 import ch.vd.uniregctb.common.AuthenticationHelper;
-import ch.vd.uniregctb.jms.EsbBusinessCode;
-import ch.vd.uniregctb.jms.EsbBusinessException;
 import ch.vd.uniregctb.jms.EsbMessageHandler;
+import ch.vd.uniregctb.type.TypeRapportEntreTiers;
 
 /**
  * Bean qui traite les messages JMS de modification de la database pour propager l'information au database service
  *
  * @author Manuel Siggen <manuel.siggen@vd.ch>
  */
-public class DataEventJmsHandler implements EsbMessageHandler {
+public class DataEventJmsHandler implements EsbMessageHandler, InitializingBean {
 
 	private static final Logger LOGGER = Logger.getLogger(DataEventJmsHandler.class);
 
 	private DataEventService dataEventService;
+
+	private Schema schemaCache;
+
+	private Map<Class<? extends DataEvent>, Handler> handlers;
+
+	private static <T extends DataEvent> void addToMap(Map<Class<? extends DataEvent>, Handler> map, Class<T> clazz, Handler<T> handler) {
+		map.put(clazz, handler);
+	}
+
+	private Map<Class<? extends DataEvent>, Handler> buildHandlers() {
+		final Map<Class<? extends DataEvent>, Handler> map = new HashMap<>();
+		addToMap(map, DatabaseLoadEvent.class, new DatabaseLoadEventHandler());
+		addToMap(map, DatabaseTruncateEvent.class, new DatabaseTruncateEventHandler());
+		addToMap(map, DatabaseTruncateEvent.class, new DatabaseTruncateEventHandler());
+		addToMap(map, DroitAccesChangeEvent.class, new DroitAccesChangeEventHandler());
+		addToMap(map, IndividuChangeEvent.class, new IndividuChangeEventHandler());
+		addToMap(map, TiersChangeEvent.class, new TiersChangeEventHandler());
+		addToMap(map, PmChangeEvent.class, new PmChangeEventHandler());
+		addToMap(map, RelationChangeEvent.class, new RelationChangeEventHandler());
+		return map;
+	}
+
+	private static interface Handler<T extends DataEvent> {
+		void onEvent(T event);
+	}
+
+	private final class DatabaseLoadEventHandler implements Handler<DatabaseLoadEvent> {
+		@Override
+		public void onEvent(DatabaseLoadEvent event) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Réception d'un événement de chargement de la database");
+			}
+			dataEventService.onLoadDatabase();
+		}
+	}
+
+	private final class DatabaseTruncateEventHandler implements Handler<DatabaseTruncateEvent> {
+		@Override
+		public void onEvent(DatabaseTruncateEvent event) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Réception d'un événement de truncate de la database");
+			}
+			dataEventService.onTruncateDatabase();
+		}
+	}
+
+	private final class DroitAccesChangeEventHandler implements Handler<DroitAccesChangeEvent> {
+		@Override
+		public void onEvent(DroitAccesChangeEvent event) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Réception d'un événement db de changement sur les droits d'accès du tiers n°" + event.getId());
+			}
+			dataEventService.onDroitAccessChange(event.getId());
+		}
+	}
+
+	private final class IndividuChangeEventHandler implements Handler<IndividuChangeEvent> {
+		@Override
+		public void onEvent(IndividuChangeEvent event) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Réception d'un événement db de changement sur l'individu n°" + event.getId());
+			}
+			dataEventService.onIndividuChange(event.getId());
+		}
+	}
+
+	private final class TiersChangeEventHandler implements Handler<TiersChangeEvent> {
+		@Override
+		public void onEvent(TiersChangeEvent event) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Réception d'un événement db de changement sur le tiers n°" + event.getId());
+			}
+			dataEventService.onTiersChange(event.getId());
+		}
+	}
+
+	private final class PmChangeEventHandler implements Handler<PmChangeEvent> {
+		@Override
+		public void onEvent(PmChangeEvent event) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Réception d'un événement db de changement sur la PM n°" + event.getId());
+			}
+			dataEventService.onPersonneMoraleChange(event.getId());
+		}
+	}
+
+	private final class RelationChangeEventHandler implements Handler<RelationChangeEvent> {
+		@Override
+		public void onEvent(RelationChangeEvent event) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Réception d'un événement db de changement sur la relation de type " + event.getRelationType() + " entre les tiers sujet " + event.getSujetId() + " et objet " + event.getObjetId());
+			}
+			final TypeRapportEntreTiers type;
+			switch (event.getRelationType()) {
+				case ANNULE_ET_REMPLACE:
+					type = TypeRapportEntreTiers.ANNULE_ET_REMPLACE;
+					break;
+				case APPARTENANCE_MENAGE:
+					type = TypeRapportEntreTiers.APPARTENANCE_MENAGE;
+					break;
+				case CONSEIL_LEGAL:
+					type = TypeRapportEntreTiers.CONSEIL_LEGAL;
+					break;
+				case CONTACT_IMPOT_SOURCE:
+					type = TypeRapportEntreTiers.CONTACT_IMPOT_SOURCE;
+					break;
+				case CURATELLE:
+					type = TypeRapportEntreTiers.CURATELLE;
+					break;
+				case PARENTE:
+					type = TypeRapportEntreTiers.PARENTE;
+					break;
+				case PRESTATION_IMPOSABLE:
+					type = TypeRapportEntreTiers.PRESTATION_IMPOSABLE;
+					break;
+				case REPRESENTATION:
+					type = TypeRapportEntreTiers.REPRESENTATION;
+					break;
+				case TUTELLE:
+					type = TypeRapportEntreTiers.TUTELLE;
+					break;
+				default:
+					throw new IllegalArgumentException("Type de relation inconnu : " + event.getRelationType());
+			}
+			dataEventService.onRelationshipChange(type, event.getSujetId(), event.getObjetId());
+		}
+	}
 
 	@SuppressWarnings({"UnusedDeclaration"})
 	public void setDataEventService(DataEventService dataEventService) {
@@ -37,44 +175,30 @@ public class DataEventJmsHandler implements EsbMessageHandler {
 	}
 
 	@Override
+	public void afterPropertiesSet() throws Exception {
+		this.handlers = buildHandlers();
+	}
+
+	@Override
 	public void onEsbMessage(EsbMessage msg) throws Exception {
 
-		// Parse le message sous forme XML
-		final XmlObject doc = XmlObject.Factory.parse(msg.getBodyAsString());
+		final JAXBContext context = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName());
+		final Unmarshaller u = context.createUnmarshaller();
+		u.setSchema(getRequestSchema());
+		final JAXBElement element = (JAXBElement) u.unmarshal(msg.getBodyAsSource());
 
-		// Valide le bousin
-		final XmlOptions validateOptions = new XmlOptions();
-		final List<XmlError> errorList = new ArrayList<>();
-		validateOptions.setErrorListener(errorList);
-		if (!doc.validate(validateOptions)) {
-			final StringBuilder builder = new StringBuilder();
-			for (XmlError error : errorList) {
-				builder.append('\n');
-				builder.append("Message: ").append(error.getErrorCode()).append(' ').append(error.getMessage()).append('\n');
-				builder.append("Location of invalid XML: ").append(error.getCursorLocation().xmlText()).append('\n');
-			}
-
-			final String errorMsg = builder.toString();
-			LOGGER.error(errorMsg);
-			throw new EsbBusinessException(EsbBusinessCode.XML_INVALIDE, errorMsg, null);
-		}
-		else {
-
-			// Traite le message
+		final DataEvent evenement = element == null ? null : (DataEvent) element.getValue();
+		if (evenement != null) {
+			// traitement du message
 			AuthenticationHelper.pushPrincipal("JMS-DbEvent(" + msg.getMessageId() + ')');
 			try {
-
-				if (doc instanceof DataChangeEventDocument) {
-					onDataChange((DataChangeEventDocument) doc);
-				}
-				else if (doc instanceof DatabaseLoadEventDocument) {
-					onDatabaseLoad();
-				}
-				else if (doc instanceof DatabaseTruncateEventDocument) {
-					onDatabaseTruncate();
+				final Handler handler = handlers.get(evenement.getClass());
+				if (handler == null) {
+					LOGGER.error("Pas de handler enregistré pour le message " + msg.getMessageId() + " de classe " + evenement.getClass().getSimpleName());
 				}
 				else {
-					LOGGER.error("Type de message inconnu : " + doc.getClass().getName());
+					//noinspection unchecked
+					handler.onEvent(evenement);
 				}
 			}
 			catch (Exception e) {
@@ -86,49 +210,20 @@ public class DataEventJmsHandler implements EsbMessageHandler {
 		}
 	}
 
-	private void onDataChange(DataChangeEventDocument changeDoc) {
-
-		final DataChangeEvent event = changeDoc.getDataChangeEvent();
-		final long id = event.getId();
-		final Enum type = event.getType();
-
-		if (DataType.TIERS.equals(type)) {
-			if (LOGGER.isDebugEnabled()) {
-				LOGGER.debug("Réception d'un événement db de changement sur le tiers n°" + id);
-			}
-			dataEventService.onTiersChange(id);
+	private Schema getRequestSchema() throws SAXException, IOException {
+		if (schemaCache == null) {
+			buildRequestSchema();
 		}
-		if (DataType.INDIVIDU.equals(type)) {
-			if (LOGGER.isDebugEnabled()) {
-				LOGGER.debug("Réception d'un événement db de changement sur l'individu n°" + id);
-			}
-			dataEventService.onIndividuChange(id);
-		}
-		if (DataType.PM.equals(type)) {
-			if (LOGGER.isDebugEnabled()) {
-				LOGGER.debug("Réception d'un événement db de changement sur la PM n°" + id);
-			}
-			dataEventService.onPersonneMoraleChange(id);
-		}
-		else if (DataType.DROIT_ACCES.equals(type)) {
-			if (LOGGER.isDebugEnabled()) {
-				LOGGER.debug("Réception d'un événement db de changement sur les droits d'accès du tiers n°" + id);
-			}
-			dataEventService.onDroitAccessChange(id);
-		}
+		return schemaCache;
 	}
 
-	private void onDatabaseLoad() {
-		if (LOGGER.isDebugEnabled()) {
-			LOGGER.debug("Réception d'un événement de chargement de la database");
+	private synchronized void buildRequestSchema() throws SAXException, IOException {
+		if (schemaCache == null) {
+			final SchemaFactory sf = SchemaFactory.newInstance(javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			sf.setResourceResolver(new ClasspathCatalogResolver());
+			final ClassPathResource resource = new ClassPathResource("event/data/dataEvent-1.xsd");
+			Source source = new StreamSource(resource.getURL().toExternalForm());
+			schemaCache = sf.newSchema(source);
 		}
-		dataEventService.onLoadDatabase();
-	}
-
-	private void onDatabaseTruncate() {
-		if (LOGGER.isDebugEnabled()) {
-			LOGGER.debug("Réception d'un événement de truncate de la database");
-		}
-		dataEventService.onTruncateDatabase();
 	}
 }
--- unireg/business/src/main/java/ch/vd/uniregctb/data/DataEventJmsSender.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/data/DataEventJmsSender.java
@@ -1,17 +1,28 @@
 package ch.vd.uniregctb.data;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.transaction.annotation.Transactional;
+import org.w3c.dom.Document;
 
-import ch.vd.fiscalite.registre.databaseEvent.DataChangeEventDocument;
-import ch.vd.fiscalite.registre.databaseEvent.DataChangeEventDocument.DataChangeEvent;
-import ch.vd.fiscalite.registre.databaseEvent.DatabaseLoadEventDocument;
-import ch.vd.fiscalite.registre.databaseEvent.DatabaseTruncateEventDocument;
 import ch.vd.technical.esb.EsbMessage;
 import ch.vd.technical.esb.EsbMessageFactory;
 import ch.vd.technical.esb.jms.EsbJmsTemplate;
-import ch.vd.uniregctb.common.XmlUtils;
+import ch.vd.unireg.xml.event.data.v1.DataEvent;
+import ch.vd.unireg.xml.event.data.v1.DatabaseLoadEvent;
+import ch.vd.unireg.xml.event.data.v1.DatabaseTruncateEvent;
+import ch.vd.unireg.xml.event.data.v1.DroitAccesChangeEvent;
+import ch.vd.unireg.xml.event.data.v1.IndividuChangeEvent;
+import ch.vd.unireg.xml.event.data.v1.ObjectFactory;
+import ch.vd.unireg.xml.event.data.v1.PmChangeEvent;
+import ch.vd.unireg.xml.event.data.v1.RelationChangeEvent;
+import ch.vd.unireg.xml.event.data.v1.Relationship;
+import ch.vd.unireg.xml.event.data.v1.TiersChangeEvent;
 import ch.vd.uniregctb.type.TypeRapportEntreTiers;
 
 /**
@@ -28,6 +39,8 @@ public class DataEventJmsSender implements DataEventListener, InitializingBean {
 	private DataEventService dataEventService;
 	private String serviceDestination;
 	private String businessUser;
+
+	private final ObjectFactory objectFactory = new ObjectFactory();
 
 	/**
 	 * for testing purpose
@@ -69,7 +82,10 @@ public class DataEventJmsSender implements DataEventListener, InitializingBean {
 			if (LOGGER.isTraceEnabled()) {
 				LOGGER.trace("Emission d'un événement db de changement sur les droits d'accès du tiers n°" + tiersId);
 			}
-			sendChangeEvent(tiersId, DataType.DROIT_ACCES);
+
+			final DroitAccesChangeEvent event = objectFactory.createDroitAccesChangeEvent();
+			event.setId(tiersId);
+			sendDataEvent(String.valueOf(tiersId), event);
 		}
 		catch (Exception e) {
 			LOGGER.error("Impossible d'envoyer un message de changement de droit d'accès sur le tiers n°" + tiersId, e);
@@ -83,7 +99,10 @@ public class DataEventJmsSender implements DataEventListener, InitializingBean {
 			if (LOGGER.isTraceEnabled()) {
 				LOGGER.trace("Emission d'un événement db de changement sur le tiers n°" + id);
 			}
-			sendChangeEvent(id, DataType.TIERS);
+
+			final TiersChangeEvent event = objectFactory.createTiersChangeEvent();
+			event.setId(id);
+			sendDataEvent(String.valueOf(id), event);
 		}
 		catch (Exception e) {
 			LOGGER.error("Impossible d'envoyer un message de changement du tiers n°" + id, e);
@@ -97,7 +116,10 @@ public class DataEventJmsSender implements DataEventListener, InitializingBean {
 			if (LOGGER.isTraceEnabled()) {
 				LOGGER.trace("Emission d'un événement db de changement sur l'individu n°" + id);
 			}
-			sendChangeEvent(id, DataType.INDIVIDU);
+
+			final IndividuChangeEvent event = objectFactory.createIndividuChangeEvent();
+			event.setId(id);
+			sendDataEvent(String.valueOf(id), event);
 		}
 		catch (Exception e) {
 			LOGGER.error("Impossible d'envoyer un message de changement de l'individu n°" + id, e);
@@ -111,7 +133,10 @@ public class DataEventJmsSender implements DataEventListener, InitializingBean {
 			if (LOGGER.isTraceEnabled()) {
 				LOGGER.trace("Emission d'un événement db de changement sur la PM n°" + id);
 			}
-			sendChangeEvent(id, DataType.PM);
+
+			final PmChangeEvent event = objectFactory.createPmChangeEvent();
+			event.setId(id);
+			sendDataEvent(String.valueOf(id), event);
 		}
 		catch (Exception e) {
 			LOGGER.error("Impossible d'envoyer un message de changement de la PM n°" + id, e);
@@ -120,7 +145,53 @@ public class DataEventJmsSender implements DataEventListener, InitializingBean {
 
 	@Override
 	public void onRelationshipChange(TypeRapportEntreTiers type, long sujetId, long objetId) {
-		// TODO jde envoyer un événement...
+		try {
+			if (LOGGER.isTraceEnabled()) {
+				LOGGER.trace("Emission d'un événement db de changement de la relation de type " + type + " entre les tiers sujet " + sujetId + " et objet " + objetId);
+			}
+
+			final Relationship relationship;
+			switch (type) {
+				case ANNULE_ET_REMPLACE:
+					relationship = Relationship.ANNULE_ET_REMPLACE;
+					break;
+				case APPARTENANCE_MENAGE:
+					relationship = Relationship.APPARTENANCE_MENAGE;
+					break;
+				case CONSEIL_LEGAL:
+					relationship = Relationship.CONSEIL_LEGAL;
+					break;
+				case CONTACT_IMPOT_SOURCE:
+					relationship = Relationship.CONTACT_IMPOT_SOURCE;
+					break;
+				case CURATELLE:
+					relationship = Relationship.CURATELLE;
+					break;
+				case PARENTE:
+					relationship = Relationship.PARENTE;
+					break;
+				case PRESTATION_IMPOSABLE:
+					relationship = Relationship.PRESTATION_IMPOSABLE;
+					break;
+				case REPRESENTATION:
+					relationship = Relationship.REPRESENTATION;
+					break;
+				case TUTELLE:
+					relationship = Relationship.TUTELLE;
+					break;
+				default:
+					throw new IllegalArgumentException("Type de relation inconnu = [" + type + ']');
+			}
+
+			final RelationChangeEvent event = objectFactory.createRelationChangeEvent();
+			event.setRelationType(relationship);
+			event.setSujetId(sujetId);
+			event.setObjetId(objetId);
+			sendDataEvent(String.valueOf(event.hashCode()), event);
+		}
+		catch (Exception e) {
+			LOGGER.error("Impossible d'envoyer un message de load de la DB", e);
+		}
 	}
 
 	@Override
@@ -130,7 +201,9 @@ public class DataEventJmsSender implements DataEventListener, InitializingBean {
 			if (LOGGER.isTraceEnabled()) {
 				LOGGER.trace("Emission d'un événement de chargement de la database");
 			}
-			sendLoadEvent();
+
+			final DatabaseLoadEvent event = objectFactory.createDatabaseLoadEvent();
+			sendDataEvent(String.valueOf(event.hashCode()), event);
 		}
 		catch (Exception e) {
 			LOGGER.error("Impossible d'envoyer un message de load de la DB", e);
@@ -144,90 +217,44 @@ public class DataEventJmsSender implements DataEventListener, InitializingBean {
 			if (LOGGER.isTraceEnabled()) {
 				LOGGER.trace("Emission d'un événement de truncate de la database");
 			}
-			sendTruncateEvent();
+
+			final DatabaseTruncateEvent event = objectFactory.createDatabaseTruncateEvent();
+			sendDataEvent(String.valueOf(event.hashCode()), event);
 		}
 		catch (Exception e) {
 			LOGGER.error("Impossible d'envoyer un message de truncate de la DB", e);
 		}
 	}
 
-	private enum DataType {
-		TIERS,
-		INDIVIDU,
-		PM,
-		DROIT_ACCES
-	}
-
-	private void sendChangeEvent(long id, DataType type) throws Exception {
-
-		final DataChangeEventDocument doc = DataChangeEventDocument.Factory.newInstance();
-
-		final DataChangeEvent event = doc.addNewDataChangeEvent();
-		event.setId(id);
-		switch (type) {
-		case TIERS:
-			event.setType(ch.vd.fiscalite.registre.databaseEvent.DataType.TIERS);
-			break;
-		case INDIVIDU:
-			event.setType(ch.vd.fiscalite.registre.databaseEvent.DataType.INDIVIDU);
-			break;
-		case DROIT_ACCES:
-			event.setType(ch.vd.fiscalite.registre.databaseEvent.DataType.DROIT_ACCES);
-			break;
-		case PM:
-			event.setType(ch.vd.fiscalite.registre.databaseEvent.DataType.PM);
-			break;
-		default:
-			throw new IllegalArgumentException("Type de tiers inconnu = [" + type + ']');
-		}
+	private void sendDataEvent(String businessId, DataEvent event) throws Exception {
+		final JAXBContext context = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName());
+		final Marshaller marshaller = context.createMarshaller();
+		final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		final DocumentBuilder db = dbf.newDocumentBuilder();
+		final Document doc = db.newDocument();
+		marshaller.marshal(objectFactory.createEvent(event), doc);
 
 		final EsbMessage m = EsbMessageFactory.createMessage();
-		m.setBusinessId(String.valueOf(id));
+		m.setBusinessId(businessId);
 		m.setBusinessUser(businessUser);
 		m.setServiceDestination(serviceDestination);
 		m.setContext("databaseEvent");
-		m.setBody(XmlUtils.xmlbeans2string(doc));
+		m.setBody(doc);
 
 		if (outputQueue != null) {
 			m.setServiceDestination(outputQueue); // for testing only
 		}
 		send(m);
-	}
 
-	private void sendTruncateEvent() throws Exception {
-
-		final DatabaseTruncateEventDocument doc = DatabaseTruncateEventDocument.Factory.newInstance();
-		doc.addNewDatabaseTruncateEvent();
-
-		final EsbMessage m = EsbMessageFactory.createMessage();
-		m.setBusinessId(String.valueOf(m.hashCode()));
-		m.setBusinessUser(businessUser);
-		m.setServiceDestination(serviceDestination);
-		m.setContext("databaseEvent");
-		m.setBody(XmlUtils.xmlbeans2string(doc));
-
-		if (outputQueue != null) {
-			m.setServiceDestination(outputQueue); // for testing only
-		}
-		send(m);
-	}
-
-	private void sendLoadEvent() throws Exception {
-
-		final DatabaseLoadEventDocument doc = DatabaseLoadEventDocument.Factory.newInstance();
-		doc.addNewDatabaseLoadEvent();
-
-		final EsbMessage m = EsbMessageFactory.createMessage();
-		m.setBusinessId(String.valueOf(m.hashCode()));
-		m.setBusinessUser(businessUser);
-		m.setServiceDestination(serviceDestination);
-		m.setContext("databaseEvent");
-		m.setBody(XmlUtils.xmlbeans2string(doc));
-
-		if (outputQueue != null) {
-			m.setServiceDestination(outputQueue); // for testing only
-		}
-		send(m);
+		// Note : code pour unmarshaller un événement
+		//		JAXBContext context = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName());
+		//		Unmarshaller u = context.createUnmarshaller();
+		//		SchemaFactory sf = SchemaFactory.newInstance(javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		//		Schema schema = sf.newSchema(new File("mon_beau_xsd.xsd"));
+		//		u.setSchema(schema);
+		//		JAXBElement element = (JAXBElement) u.unmarshal(message);
+		//		evenement = element == null ? null : (EvenementDeclarationImpot) element.getValue();
 	}
 
 	private void send(EsbMessage m) throws Exception {
