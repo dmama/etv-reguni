8024416578c70ba5046f8165d4a8979c13e5502a
Jacob Deker <jacob.deker@vd.ch>
2013-09-05 11:38:16 +0000

    Corrigé le script de mise à jour du schéma DB (mauvais nom de colonne par rapport au code)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@107149 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/base/sql/5.6.0/schema_5.5.0_5.6.0_upgrade.sql
+++ unireg/base/sql/5.6.0/schema_5.5.0_5.6.0_upgrade.sql
@@ -2,8 +2,8 @@
 INSERT INTO VERSION_DB (VERSION_NB, SCRIPT_ID) VALUES ('5.6.0', '5.5.0_5.6.0_upgrade');
 
 -- SIFISC-9096 : flag d'obsolescence des relations de parenté (par exemple pour les enfants dont les parents ne sont pas connus fiscalement au moment de l'arrivée)
-ALTER TABLE TIERS ADD PP_DIRTY_PARENTE NUMBER(1,0);
-UPDATE TIERS SET PP_DIRTY_PARENTE = 0 WHERE TIERS_TYPE='PersonnePhysique';
+ALTER TABLE TIERS ADD PP_PARENTE_DIRTY NUMBER(1,0);
+UPDATE TIERS SET PP_PARENTE_DIRTY = 0 WHERE TIERS_TYPE='PersonnePhysique';
 
 -- SIFISC-4455 : numéro AVS fourni par l'UPI lors de l'identification automatique d'un contribuable dont la demande contenait un vieux NAVS13 annulé/remplacé depuis
 ALTER TABLE EVENEMENT_IDENTIFICATION_CTB ADD NAVS13_UPI nvarchar2(13);
