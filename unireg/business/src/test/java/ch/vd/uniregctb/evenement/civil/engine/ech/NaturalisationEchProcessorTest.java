46441815e08c77dc4bc61079f86b9fbaf88c8280
Jacob Deker <jacob.deker@vd.ch>
2014-02-05 09:31:50 +0000

    Corrigé le test unitaire des événements civils de naturalisation suisse en raison du changement de comportement depuis le 1.1.2014
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/5.7@113031 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/test/java/ch/vd/uniregctb/evenement/civil/engine/ech/NaturalisationEchProcessorTest.java
+++ unireg/business/src/test/java/ch/vd/uniregctb/evenement/civil/engine/ech/NaturalisationEchProcessorTest.java
@@ -27,11 +27,11 @@ import ch.vd.uniregctb.type.TypeEvenementCivilEch;
 public class NaturalisationEchProcessorTest extends AbstractEvenementCivilEchProcessorTest {
 
 	@Test(timeout = 10000L)
-	public void testNaturalisation() throws Exception {
+	public void testNaturalisationAvant2014() throws Exception {
 
 		final long noIndividu = 54678215611L;
 		final RegDate dateNaissance = date(1956, 12, 12);
-		final RegDate dateNaturalisation = RegDate.get().addMonths(-1);
+		final RegDate dateNaturalisation = date(2013, 11, 4);
 
 		serviceCivil.setUp(new DefaultMockServiceCivil() {
 			@Override
@@ -89,6 +89,76 @@ public class NaturalisationEchProcessorTest extends AbstractEvenementCivilEchPro
 				final ForFiscalPrincipal ffp = pp.getDernierForFiscalPrincipal();
 				Assert.assertNotNull(ffp);
 				Assert.assertEquals(dateNaturalisation.getOneDayAfter(), ffp.getDateDebut());
+				Assert.assertEquals(MotifFor.PERMIS_C_SUISSE, ffp.getMotifOuverture());
+				Assert.assertEquals(ModeImposition.ORDINAIRE, ffp.getModeImposition());
+				return null;
+			}
+		});
+	}
+
+	@Test(timeout = 10000L)
+	public void testNaturalisationApres2014() throws Exception {
+
+		final long noIndividu = 54678215611L;
+		final RegDate dateNaissance = date(1956, 12, 12);
+		final RegDate dateNaturalisation = date(2014, 1, 6);
+
+		serviceCivil.setUp(new DefaultMockServiceCivil() {
+			@Override
+			protected void init() {
+				final MockIndividu ind = addIndividu(noIndividu, dateNaissance, "Barbar", "Konan", true);
+				addNationalite(ind, MockPays.Liechtenstein, dateNaissance, null);
+			}
+		});
+
+		final long ppId = doInNewTransactionAndSession(new TransactionCallback<Long>() {
+			@Override
+			public Long doInTransaction(TransactionStatus status) {
+				final PersonnePhysique pp = addHabitant(noIndividu);
+				addForPrincipal(pp, date(2000, 1, 1), MotifFor.ARRIVEE_HS, null, null, MockCommune.Renens, MotifRattachement.DOMICILE, ModeImposition.SOURCE);
+				return pp.getNumero();
+			}
+		});
+
+		doModificationIndividu(noIndividu, new IndividuModification() {
+			@Override
+			public void modifyIndividu(MockIndividu individu) {
+				individu.setNationalites(Arrays.asList((Nationalite) new MockNationalite(dateNaturalisation, null, MockPays.Suisse)));
+			}
+		});
+
+		// événement civil (avec individu déjà renseigné pour ne pas devoir appeler RCPers...)
+		final long evtId = doInNewTransactionAndSession(new TransactionCallback<Long>() {
+			@Override
+			public Long doInTransaction(TransactionStatus status) {
+				final EvenementCivilEch evt = new EvenementCivilEch();
+				evt.setId(135566L);
+				evt.setAction(ActionEvenementCivilEch.PREMIERE_LIVRAISON);
+				evt.setDateEvenement(dateNaturalisation);
+				evt.setEtat(EtatEvenementCivil.A_TRAITER);
+				evt.setNumeroIndividu(noIndividu);
+				evt.setType(TypeEvenementCivilEch.NATURALISATION);
+				return hibernateTemplate.merge(evt).getId();
+			}
+		});
+
+		// traitement synchrone de l'événement
+		traiterEvenements(noIndividu);
+
+		doInNewTransactionAndSession(new TransactionCallback<Object>() {
+			@Override
+			public Object doInTransaction(TransactionStatus status) {
+				final EvenementCivilEch evt = evtCivilDAO.get(evtId);
+				Assert.assertNotNull(evt);
+				Assert.assertEquals(EtatEvenementCivil.TRAITE, evt.getEtat());
+
+				final PersonnePhysique pp = tiersService.getPersonnePhysiqueByNumeroIndividu(noIndividu);
+				Assert.assertNotNull(pp);
+				Assert.assertEquals((Long) ppId, pp.getNumero());
+
+				final ForFiscalPrincipal ffp = pp.getDernierForFiscalPrincipal();
+				Assert.assertNotNull(ffp);
+				Assert.assertEquals(dateNaturalisation, ffp.getDateDebut());
 				Assert.assertEquals(MotifFor.PERMIS_C_SUISSE, ffp.getMotifOuverture());
 				Assert.assertEquals(ModeImposition.ORDINAIRE, ffp.getModeImposition());
 				return null;
