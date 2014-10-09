22865aa18113b85c1cc6bc5e74fa21fc96f053c4
Jacob Deker <jacob.deker@vd.ch>
2014-10-09 08:43:24 +0000

    [SIFISC-13682] Corrigé l'erreur levée à la tentative de création d'une LR sur un débiteur dont tous les fors sont annulés
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@117067 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/java/ch/vd/uniregctb/lr/validator/ListeRecapEditDebiteurValidator.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/lr/validator/ListeRecapEditDebiteurValidator.java
@@ -1,10 +1,13 @@
 package ch.vd.uniregctb.lr.validator;
 
+import java.util.List;
+
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import ch.vd.uniregctb.lr.view.ListeRecapListView;
+import ch.vd.uniregctb.tiers.ForFiscal;
 import ch.vd.uniregctb.tiers.Tiers;
 import ch.vd.uniregctb.tiers.TiersDAO;
 
@@ -21,10 +24,10 @@ public class ListeRecapEditDebiteurValidator implements Validator {
 	@Override
 	@Transactional(readOnly = true)
 	public void validate(Object target, Errors errors) {
-		ListeRecapListView lrListView = (ListeRecapListView) target;
-
-		Tiers tiers = tiersDAO.get(lrListView.getDpi().getNumero());
-		if(tiers.getForsFiscaux() == null || tiers.getForsFiscaux().isEmpty()){
+		final ListeRecapListView lrListView = (ListeRecapListView) target;
+		final Tiers tiers = tiersDAO.get(lrListView.getDpi().getNumero());
+		final List<ForFiscal> forsFiscauxNonAnnules = tiers.getForsFiscauxNonAnnules(false);
+		if (forsFiscauxNonAnnules == null || forsFiscauxNonAnnules.isEmpty()) {
 			//la dpi ne possède pas de for => création LR interdite
 			errors.reject("error.lr.creation.interdit");
 		}
