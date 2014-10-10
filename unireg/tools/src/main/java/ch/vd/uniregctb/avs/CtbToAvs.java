14d28695c77f53f6f6d84c2e5fd2e552fb15a874
Jacob Deker <jacob.deker@vd.ch>
2014-10-10 14:03:00 +0000

    Revert du commit précédent car il inclus une dépendance de tools vers le composant uniregctb-ws (= war) qui n'est pas souhaitable
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@117085 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/tools/src/main/java/ch/vd/uniregctb/avs/CtbToAvs.java
+++ unireg/tools/src/main/java/ch/vd/uniregctb/avs/CtbToAvs.java
@@ -1,10 +1,12 @@
 package ch.vd.uniregctb.avs;
 
+import javax.xml.ws.BindingProvider;
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -12,28 +14,36 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.cxf.message.Message;
 import org.jetbrains.annotations.Nullable;
+import org.springframework.util.ResourceUtils;
 
-import ch.vd.unireg.ws.parties.v1.Entry;
-import ch.vd.unireg.ws.parties.v1.Parties;
-import ch.vd.unireg.xml.party.person.v3.NaturalPerson;
-import ch.vd.unireg.xml.party.v3.Party;
-import ch.vd.uniregctb.utils.WebServiceV5Helper;
+import ch.vd.unireg.webservices.party4.BatchParty;
+import ch.vd.unireg.webservices.party4.BatchPartyEntry;
+import ch.vd.unireg.webservices.party4.GetBatchPartyRequest;
+import ch.vd.unireg.webservices.party4.GetPartyRequest;
+import ch.vd.unireg.webservices.party4.PartyWebService;
+import ch.vd.unireg.webservices.party4.PartyWebServiceFactory;
+import ch.vd.unireg.webservices.party4.WebServiceException;
+import ch.vd.unireg.xml.common.v1.UserLogin;
+import ch.vd.unireg.xml.party.person.v2.NaturalPerson;
+import ch.vd.unireg.xml.party.v2.Party;
 
 public class CtbToAvs {
 
 	// INTEGRATION
-//	private static final String urlWebService = "http://unireg-in.etat-de-vaud.ch/fiscalite/int-unireg/ws/v5";
+//	private static final String urlWebService = "http://unireg-in.etat-de-vaud.ch/fiscalite/int-unireg/ws/party4";
 //	private static final String userWebService = "unireg";
 //	private static final String pwdWebService = "unireg_1014";
 
 	// PRE-PRODUCTION
-	private static final String urlWebService = "http://unireg-pp.etat-de-vaud.ch/fiscalite/unireg/ws/v5";
+	private static final String urlWebService = "http://unireg-pp.etat-de-vaud.ch/fiscalite/unireg/ws/party4";
 	private static final String userWebService = "web-it";
 	private static final String pwdWebService = "unireg_1014";
 
 	// PRODUCTION
-//	private static final String urlWebService = "http://unireg-pr.etat-de-vaud.ch/fiscalite/unireg/ws/v5";
+//	private static final String urlWebService = "http://unireg-pr.etat-de-vaud.ch/fiscalite/unireg/ws/party4";
 //	private static final String userWebService = "se renseigner...";
 //	private static final String pwdWebService = "se renseigner...";
 
@@ -45,6 +55,14 @@ public class CtbToAvs {
 	private static final String fichierDestination = "/tmp/tiers-avec-avs.csv";
 
 	public static void main(String[] args) throws Exception {
+
+		final PartyWebService service = initWebService(urlWebService, userWebService, pwdWebService);
+		final GetBatchPartyRequest batchPartyRequest = new GetBatchPartyRequest();
+		final UserLogin login = new UserLogin(userId, oid);
+		batchPartyRequest.setLogin(login);
+
+		final GetPartyRequest partyRequest = new GetPartyRequest();
+		partyRequest.setLogin(login);
 
 		// on lit le contenu du fichier
 		final Map<Integer, String> data = new LinkedHashMap<>();
@@ -94,22 +112,25 @@ public class CtbToAvs {
 		// et on boucle sur les lots
 		try {
 			for (List<Integer> lot : lots) {
-				try {
-					final Parties parties = WebServiceV5Helper.getParties(urlWebService, userWebService, pwdWebService, userId, oid, lot, null);
-					for (Entry entry : parties.getEntries()) {
-						final Party party = entry.getParty();
-						dumpTiers(party, entry.getPartyNo(), entry.getError(), data.get(party.getNumber()), ps);
-					}
+				batchPartyRequest.getPartyNumbers().clear();
+				batchPartyRequest.getPartyNumbers().addAll(lot);
 
+				try {
+					final BatchParty result = service.getBatchParty(batchPartyRequest);
+					for (BatchPartyEntry entry : result.getEntries()) {
+						final Party tiers = entry.getParty();
+						dumpTiers(tiers, entry.getNumber(), entry.getExceptionInfo(), data.get(entry.getNumber()), ps);
+					}
 				}
-				catch (Exception e) {
+				catch (WebServiceException e) {
 					// problème... on essaie un par un
 					for (Integer id : lot) {
+						partyRequest.setPartyNumber(id);
 						try {
-							final Party indivResult = WebServiceV5Helper.getParty(urlWebService, userWebService, pwdWebService, userId, oid, id, null);
+							final Party indivResult = service.getParty(partyRequest);
 							dumpTiers(indivResult, id, null, data.get(id), ps);
 						}
-						catch (Exception e1) {
+						catch (WebServiceException e1) {
 							dumpTiers(null, id, e1.getMessage(), data.get(id), ps);
 						}
 					}
@@ -129,7 +150,7 @@ public class CtbToAvs {
 			ps.println(String.format("%d%s;", tiersNumber, tail));
 		}
 		else if (party instanceof NaturalPerson) {
-			final Long avs = ((NaturalPerson) party).getVn();
+			final Long avs = ((NaturalPerson) party).getIdentification().getVn();
 			if (avs != null) {
 				ps.println(String.format("%d%s;%d", tiersNumber, tail, avs));
 			}
@@ -141,5 +162,23 @@ public class CtbToAvs {
 			System.err.println(String.format("%d n'est pas une personne physique (%s)", tiersNumber, party.getClass().getSimpleName()));
 			ps.println(String.format("%d%s;", tiersNumber, tail));
 		}
+	}
+
+	private static PartyWebService initWebService(String serviceUrl, String username, String password) throws Exception {
+		final URL wsdlUrl = ResourceUtils.getURL("classpath:PartyService4.wsdl");
+		final PartyWebServiceFactory ts = new PartyWebServiceFactory(wsdlUrl);
+		final PartyWebService service = ts.getService();
+		final Map<String, Object> context = ((BindingProvider) service).getRequestContext();
+		if (StringUtils.isNotBlank(username)) {
+			context.put(BindingProvider.USERNAME_PROPERTY, username);
+			context.put(BindingProvider.PASSWORD_PROPERTY, password);
+		}
+		context.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, serviceUrl);
+
+		// Désactive la validation du schéma (= ignore silencieusement les éléments inconnus), de manière à permettre l'évolution ascendante-compatible du WSDL.
+		context.put(Message.SCHEMA_VALIDATION_ENABLED, false);
+		context.put("set-jaxb-validation-event-handler", false);
+
+		return service;
 	}
 }
