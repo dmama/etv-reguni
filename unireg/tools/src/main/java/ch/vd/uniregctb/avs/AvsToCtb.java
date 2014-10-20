dacb0b87ec7caff646195a4e5f7c186a2bb61a61
Jacob Deker <jacob.deker@vd.ch>
2014-10-20 08:32:31 +0000

    Repassé les outils de mapping entre numéro de contribuable et numéro AVS en v5 (sans le souci de dépendance)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@117212 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/tools/src/main/java/ch/vd/uniregctb/avs/AvsToCtb.java
+++ unireg/tools/src/main/java/ch/vd/uniregctb/avs/AvsToCtb.java
@@ -1,44 +1,36 @@
 package ch.vd.uniregctb.avs;
 
-import javax.xml.ws.BindingProvider;
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.cxf.message.Message;
 import org.jetbrains.annotations.Nullable;
-import org.springframework.util.ResourceUtils;
 
-import ch.vd.unireg.webservices.party3.PartyWebService;
-import ch.vd.unireg.webservices.party3.PartyWebServiceFactory;
-import ch.vd.unireg.webservices.party3.SearchPartyRequest;
-import ch.vd.unireg.webservices.party3.SearchPartyResponse;
-import ch.vd.unireg.webservices.party3.WebServiceException;
-import ch.vd.unireg.xml.common.v1.UserLogin;
-import ch.vd.unireg.xml.party.v1.PartyInfo;
-import ch.vd.unireg.xml.party.v1.PartyType;
+import ch.vd.unireg.ws.search.party.v1.SearchResult;
+import ch.vd.unireg.xml.party.v3.PartyInfo;
+import ch.vd.uniregctb.utils.WebServiceV5Helper;
 
 public class AvsToCtb {
 
 	// INTEGRATION
-//	private static final String urlWebService = "http://unireg-in.etat-de-vaud.ch/fiscalite/int-unireg/ws/party3";
+//	private static final String urlWebService = "http://unireg-in.etat-de-vaud.ch/fiscalite/int-unireg/ws/v5";
 //	private static final String userWebService = "unireg";
 //	private static final String pwdWebService = "unireg_1014";
 
 	// PRE-PRODUCTION
-	private static final String urlWebService = "http://unireg-pp.etat-de-vaud.ch/fiscalite/unireg/ws/party3";
+	private static final String urlWebService = "http://unireg-pp.etat-de-vaud.ch/fiscalite/unireg/ws/v5";
 	private static final String userWebService = "web-it";
 	private static final String pwdWebService = "unireg_1014";
 
 	// PRODUCTION
-//	private static final String urlWebService = "http://unireg-pr.etat-de-vaud.ch/fiscalite/unireg/ws/party3";
+//	private static final String urlWebService = "http://unireg-pr.etat-de-vaud.ch/fiscalite/unireg/ws/v5";
 //	private static final String userWebService = "se renseigner...";
 //	private static final String pwdWebService = "se renseigner...";
 
@@ -49,12 +41,6 @@ public class AvsToCtb {
 	private static final String fichierDestination = "/tmp/tiers-from-avs.csv";
 
 	public static void main(String[] args) throws Exception {
-
-		final PartyWebService service = initWebService(urlWebService, userWebService, pwdWebService);
-		final SearchPartyRequest searchRequest = new SearchPartyRequest();
-		final UserLogin login = new UserLogin(userId, oid);
-		searchRequest.setLogin(login);
-		searchRequest.getPartyTypes().add(PartyType.NATURAL_PERSON);
 
 		// on lit le contenu du fichier
 		final List<String> avsList = new ArrayList<>();
@@ -83,14 +69,17 @@ public class AvsToCtb {
 
 		// et on boucle sur les lots
 		try {
+			final Set<String> partyTypes = new HashSet<>(Arrays.asList("NATURAL_PERSON"));
 			for (String avs : avsList) {
-				searchRequest.setSocialInsuranceNumber(avs);
 				try {
-					final SearchPartyResponse result = service.searchParty(searchRequest);
-					final List<PartyInfo> items = result.getItems();
+					final SearchResult result = WebServiceV5Helper.searchParty(urlWebService, userWebService, pwdWebService, userId, oid, null, null, avs, null, partyTypes);
+					if (result.getError() != null) {
+						throw new Exception(result.getError().getErrorMessage());
+					}
+					final List<PartyInfo> items = result.getParty();
 					dumpTiers(avs, items, null, ps);
 				}
-				catch (WebServiceException e) {
+				catch (Exception e) {
 					dumpTiers(avs, null, e, ps);
 				}
 			}
@@ -118,23 +107,4 @@ public class AvsToCtb {
 		}
 		ps.println(b.toString());
 	}
-
-	private static PartyWebService initWebService(String serviceUrl, String username, String password) throws Exception {
-		final URL wsdlUrl = ResourceUtils.getURL("classpath:PartyService3.wsdl");
-		final PartyWebServiceFactory ts = new PartyWebServiceFactory(wsdlUrl);
-		final PartyWebService service = ts.getService();
-		final Map<String, Object> context = ((BindingProvider) service).getRequestContext();
-		if (StringUtils.isNotBlank(username)) {
-			context.put(BindingProvider.USERNAME_PROPERTY, username);
-			context.put(BindingProvider.PASSWORD_PROPERTY, password);
-		}
-		context.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, serviceUrl);
-
-		// Désactive la validation du schéma (= ignore silencieusement les éléments inconnus), de manière à permettre l'évolution ascendante-compatible du WSDL.
-		context.put(Message.SCHEMA_VALIDATION_ENABLED, false);
-		context.put("set-jaxb-validation-event-handler", false);
-
-		return service;
-	}
-
 }
--- unireg/tools/src/main/java/ch/vd/uniregctb/utils/WebServiceV5Helper.java
+++ unireg/tools/src/main/java/ch/vd/uniregctb/utils/WebServiceV5Helper.java
@@ -3,9 +3,11 @@ package ch.vd.uniregctb.utils;
 import java.util.Collection;
 import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.cxf.jaxrs.client.WebClient;
 
 import ch.vd.unireg.ws.parties.v1.Parties;
+import ch.vd.unireg.ws.search.party.v1.SearchResult;
 import ch.vd.unireg.xml.party.v3.Party;
 import ch.vd.unireg.xml.party.v3.PartyPart;
 
@@ -31,5 +33,28 @@ public abstract class WebServiceV5Helper {
 			client.query("part", parts.toArray(new PartyPart[parts.size()]));
 		}
 		return client.get(Parties.class);
+	}
+
+	public static SearchResult searchParty(String urlWebService, String username, String password, String businessUser, int oid,
+	                                 String name, String nameSearchMode, String avs, String ide, Set<String> partyTypes) {
+		final WebClient client = WebClient.create(urlWebService, username, password, null);
+		client.path("searchParty");
+		client.query("user", String.format("%s/%d", businessUser, oid));
+		if (StringUtils.isNotBlank(name)) {
+			client.query("name", name);
+			if (StringUtils.isNotBlank(nameSearchMode)) {
+				client.query("nameSearchMode", nameSearchMode);
+			}
+		}
+		if (StringUtils.isNotBlank(avs)) {
+			client.query("vn", avs);
+		}
+		if (StringUtils.isNotBlank(ide)) {
+			client.query("uid", ide);
+		}
+		if (partyTypes != null && !partyTypes.isEmpty()) {
+			client.query("partyType", partyTypes.toArray(new String[partyTypes.size()]));
+		}
+		return client.get(SearchResult.class);
 	}
 }
