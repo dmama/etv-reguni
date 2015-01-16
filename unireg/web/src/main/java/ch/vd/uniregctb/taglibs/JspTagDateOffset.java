537bd82df71e706cd35bfff995ef041a169a0052
Jacob Deker <jacob.deker@vd.ch>
2015-01-16 06:15:15 +0000

    Amélioré le message (= heure incluse) affiché sur toutes les pages de l'application IHM en cas de décalage temporel
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@117960 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/java/ch/vd/uniregctb/taglibs/JspTagDateOffset.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/taglibs/JspTagDateOffset.java
@@ -3,10 +3,10 @@ package ch.vd.uniregctb.taglibs;
 import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.BodyTagSupport;
+import java.util.Date;
 
 import ch.vd.registre.base.date.DateConstants;
 import ch.vd.registre.base.date.DateHelper;
-import ch.vd.registre.base.date.RegDate;
 import ch.vd.registre.base.date.RegDateHelper;
 import ch.vd.registre.web.http.HttpUtilities;
 
@@ -21,8 +21,9 @@ public class JspTagDateOffset extends BodyTagSupport {
 			final JspWriter out = pageContext.getOut();
 
 			if (DateConstants.TIME_OFFSET != 0) {
+				final Date now = DateHelper.getCurrentDate();
 				final String message = "Attention ! Pour le testing, la date courante de l'application est décalée de " + DateHelper.getTimeOffsetAsString(DateConstants.TIME_OFFSET, false) +
-						". La date courante est donc le " + RegDateHelper.dateToDisplayString(RegDate.get()) + '.';
+						". La jour courant est donc le " + RegDateHelper.dateToDisplayString(RegDateHelper.get(now)) + ", et l'heure " + DateHelper.timeToString(now) + '.';
 				out.print("<div class=\"flash-error\">" + HttpUtilities.htmlEncode(message) + "</div>");
 			}
 
