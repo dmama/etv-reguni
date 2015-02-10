cb5a05c1d0771afa660b0ab0eb9f5f707a379e28
Jacob Deker <jacob.deker@vd.ch>
2015-02-10 06:26:28 +0000

    Corrigé le script de récupération des statistiques d'impression pour tenir compte des gros fichiers de log compressés avant l'heure
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@118181 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/scripts/logs/statistiques-impressions.sh
+++ unireg/scripts/logs/statistiques-impressions.sh
@@ -21,18 +21,28 @@ function encode_output() {
         fi
 }
 
+# extrait le contenu du fichier (décompressé au besoin) sur la sortie standard
+function dump_file() {
+	local FILE=$1
+	if [[ "$FILE" =~ \.xz$ ]]; then
+		xzcat "$FILE"
+	else
+		cat "$FILE"
+	fi
+}
+
 # fonction qui prend deux paramètres : le nom du fichier et l'intitulé de la date
 function compute_stats() {
-	LOG_FILE=$1
-	DATE_LABEL=$2
-	IMP=$(grep "\(docID:\|\[AUDIT\] Impression\)" "$LOG_FILE" -c)
-	LAST_IMP=$(grep "\(docID:\|\[AUDIT\] Impression\)" "$LOG_FILE" | sed -e '$! D' -e 's/[^0-9: \.-]//g' | awk '{ print $2; }')
-	FAIL=$(grep "EditiqueCommunicationException" "$LOG_FILE" -c)
-	LAST_FAIL=$(grep -B 1 "EditiqueCommunicationException" "$LOG_FILE" | sed -e '$ D' | sed -e '$! D' -e 's/[^0-9: \.-]//g' | awk '{ print $2; }')
-	FIRST_FAIL=$(grep -B 1 "EditiqueCommunicationException" "$LOG_FILE" | sed -e '2,$ D' -e 's/[^0-9: \.-]//g' | awk '{ print $2; }')
+	local LOG_FILE=$1
+	local DATE_LABEL=$2
+	local IMP=$(dump_file "$LOG_FILE" | grep "\(docID:\|\[AUDIT\] Impression\)" -c)
+	local LAST_IMP=$(dump_file "$LOG_FILE" | grep "\(docID:\|\[AUDIT\] Impression\)" | sed -e '$! D' -e 's/[^0-9: \.-]//g' | awk '{ print $2; }')
+	local FAIL=$(dump_file "$LOG_FILE" | grep "EditiqueCommunicationException" -c)
+	local LAST_FAIL=$(dump_file "$LOG_FILE" | grep -B 1 "EditiqueCommunicationException" | sed -e '$ D' | sed -e '$! D' -e 's/[^0-9: \.-]//g' | awk '{ print $2; }')
+	local FIRST_FAIL=$(dump_file "$LOG_FILE" | grep -B 1 "EditiqueCommunicationException" | sed -e '2,$ D' -e 's/[^0-9: \.-]//g' | awk '{ print $2; }')
 	echo -n "$DATE_LABEL : $FAIL échec(s) sur $IMP demande(s) d'impression"
 	if [ "$IMP" -ne 0 ]; then
-		PERCENT_FAILURE=$(($FAIL * 100 / $IMP))
+		local PERCENT_FAILURE=$(($FAIL * 100 / $IMP))
 		echo -n " ($PERCENT_FAILURE% d'échec)"
 		if [ "$FAIL" -ne 0 ]; then
 			echo -n ", échec(s) entre $FIRST_FAIL et $LAST_FAIL"
@@ -46,7 +56,7 @@ function compute_stats() {
 # les fichiers sont téléchargés dans un sous-répertoire spécifique à l'environnement voulu
 cd "$(dirname "$0")/$ENVIRONMENT/unireg-web"
 
-ls -1 unireg-web-cat_unireg${ENVIRONMENT}01.log.* | grep "[0-9]\+$" | while read FILE; do
+ls -1 unireg-web-cat_unireg${ENVIRONMENT}01.log.* | grep "[0-9]\+\(\.xz\)\?$" | while read FILE; do
 	DATE=$(echo "$FILE" | sed -e 's/^.*log//' -e 's/[^0-9]//g')
 	compute_stats "$FILE" "$DATE"
 done | encode_output
