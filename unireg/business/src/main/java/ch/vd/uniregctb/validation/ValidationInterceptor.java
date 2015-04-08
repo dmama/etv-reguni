974b8f79860a761063acd38d486cc45cac24d3f5
Jacob Deker <jacob.deker@vd.ch>
2015-04-08 13:34:47 +0000

    [Optimisation] Rollback de l'optimisation précédente autour des validations (apparemment, le postFlush n'est pas toujours appelé au bon moment...)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@118510 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/validation/ValidationInterceptor.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/validation/ValidationInterceptor.java
@@ -1,7 +1,7 @@
 package ch.vd.uniregctb.validation;
 
 import java.io.Serializable;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -24,43 +24,74 @@ public class ValidationInterceptor implements ModificationSubInterceptor, Initia
 
 	private final ThreadSwitch enabled = new ThreadSwitch(true);
 
-	private final ThreadLocal<Set<Object>> toValidate = new ThreadLocal<Set<Object>>() {
-		@Override
-		protected Set<Object> initialValue() {
-			return new LinkedHashSet<>();
-		}
-	};
-
 	private ModificationInterceptor parent;
 	private HibernateTemplate hibernateTemplate;
 	private ValidationService validationService;
 
 	@Override
-	public boolean onChange(HibernateEntity entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types, boolean isAnnulation) throws  CallbackException {
+	public boolean onChange(HibernateEntity entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types, boolean isAnnulation) throws CallbackException {
 		if (!isEnabled()) {
 			return false;
 		}
 
-		// on mémorise les entités à valider (see #postFlush())
-		final Set<Object> toValidate = new LinkedHashSet<>();
-		fillInstancesToValidate(entity, isAnnulation, toValidate);
-		this.toValidate.get().addAll(toValidate);
-
+		// old-TO-DO (msi) mémoriser l'entité et déplacer la validation dans le preTransactionCommit.
+		// Non, en fait il n'est pas possible de déplacer la validation juste avant le commit de la transaction.
+		// Le problème est que les durées de vie de la transaction Spring et de la session Hibernate ne sont pas absolument pareilles. Dans certains cas, le preTransactionCommit sera appelé alors que
+		// la session Spring est encore valide (pour les transactions ouvertes par les @EmbeddedInTransaction, car le session sera fermée par un listener sur le même preTransactionCommit), mais dans d'autres
+		// cas (par exemple les batches qui ouvrent une transaction puis une session nestée à travers le BatchTransactionTemplate) la session est déjà invalide lors de la fermeture de la transaction
+		// et les collections Hibernate non-initialisées font sauter une exception lors de la validation.
+		validate(entity, isAnnulation);
 		return false;
 	}
 
-	private void fillInstancesToValidate(Object source, boolean isAnnulation, Set<Object> toValidate) {
-		// déjà là -> on ne va pas plus loin (pas besoin)
-		if (toValidate.contains(source)) {
+	@Override
+	public void postFlush() throws CallbackException {
+		// rien à faire ici
+	}
+
+	@Override
+	public void preTransactionCommit() {
+		// rien à faire ici
+	}
+
+	@Override
+	public void postTransactionCommit() {
+		// rien à faire ici
+	}
+
+	@Override
+	public void postTransactionRollback() {
+		// rien à faire ici
+	}
+
+	/**
+	 * Valide l'objet spécifié et tous les objets liés qui peuvent l'être.
+	 *
+	 * @param object       l'objet à valider
+	 * @param isAnnulation vrai si l'objet à valider vient d'être annulé.
+	 * @throws ValidationException en cas d'erreur de validation
+	 */
+	private void validate(Object object, boolean isAnnulation) throws ValidationException {
+		final Set<Object> visited = new HashSet<>();
+		validate(object, isAnnulation, visited);
+	}
+
+	private void validate(Object object, boolean isAnnulation, Set<Object> visited) {
+
+		if (visited.contains(object)) {
 			return;
 		}
 
-		// on le rajoute, à la fois pour remplir l'ensemble et pour bloquer les récursions infinies
-		toValidate.add(source);
+		final ValidationResults results = validationService.validate(object);
+		if (results.hasErrors()) {
+			throw new ValidationException(object, results.getErrors(), results.getWarnings());
+		}
 
-		// si l'objet pointe vers d'autres objets validables, on les validera aussi
-		if (source instanceof LinkedEntity) {
-			final LinkedEntity entity = (LinkedEntity) source;
+		visited.add(object);
+
+		// si l'objet pointe vers d'autres objets validables, on les valide aussi
+		if (object instanceof LinkedEntity) {
+			final LinkedEntity entity =(LinkedEntity) object;
 			final List<?> linked = entity.getLinkedEntities(isAnnulation);
 			if (linked != null) {
 				for (Object o : linked) {
@@ -68,70 +99,10 @@ public class ValidationInterceptor implements ModificationSubInterceptor, Initia
 						final EntityKey k = (EntityKey) o;
 						o = hibernateTemplate.get(k.getClazz(), (Serializable) k.getId());
 					}
-					fillInstancesToValidate(o, false, toValidate);
+					validate(o, false, visited);
 				}
 			}
 		}
-	}
-
-	@Override
-	public void postFlush() throws CallbackException {
-		try {
-			if (!isEnabled()) {
-				return;
-			}
-
-			// boucle sur tous les objets recueillis, et validation de chacun d'eux
-			for (Object object : this.toValidate.get()) {
-				final ValidationResults results = validationService.validate(object);
-				if (results.hasErrors()) {
-					throw new ValidationException(object, results.getErrors(), results.getWarnings());
-				}
-			}
-		}
-		finally {
-			// on enlève tout pour la prochaine fois
-			this.toValidate.get().clear();
-		}
-	}
-
-	@Override
-	public void preTransactionCommit() {
-
-		// old-TO-DO (msi) mémoriser l'entité et déplacer la validation du onChange vers le preTransactionCommit.
-		// Non, en fait il n'est pas possible de déplacer la validation juste avant le commit de la transaction.
-		// Le problème est que les durées de vie de la transaction Spring et de la session Hibernate ne sont pas absolument pareilles. Dans certains cas, le preTransactionCommit sera appelé alors que
-		// la session Spring est encore valide (pour les transactions ouvertes par les @Transactional, car la session sera fermée par un listener sur le même preTransactionCommit), mais dans d'autres
-		// cas (par exemple les batches qui ouvrent une transaction puis une session nestée à travers le BatchTransactionTemplate) la session est déjà invalide lors de la fermeture de la transaction
-		// et les collections Hibernate non-initialisées font sauter une exception lors de la validation.
-
-		// JDE : en fait, on peut essayer de concillier les deux
-		// -> dans les cas levés par le @Transactional il n'y aura pas eu de flush avant (il vient apparemment après), donc on peut lancer le #postFlush ici
-		// -> dans les cas des batchs (= utilisation du TransactionTemplate en général), le flush a déjà été fait (et donc le #postFlush aussi) et l'appeler
-		//    ici ne fait pas de mal (= il n'y a plus rien dedans...)
-
-		postFlush();
-	}
-
-	@Override
-	public void postTransactionCommit() {
-		// on est censé être passé par un flush...
-		final Set<?> toValidate = this.toValidate.get();
-		try {
-			if (!toValidate.isEmpty()) {
-				throw new IllegalStateException("Il y a encore " + toValidate.size() + " entité(s) à valider au moment du post-commit (pas de flush ??)...");
-			}
-		}
-		finally {
-			// on enlève tout pour la prochaine fois
-			toValidate.clear();
-		}
-	}
-
-	@Override
-	public void postTransactionRollback() {
-		// on enlève tout pour la prochaine fois
-		this.toValidate.get().clear();
 	}
 
 	@Override
