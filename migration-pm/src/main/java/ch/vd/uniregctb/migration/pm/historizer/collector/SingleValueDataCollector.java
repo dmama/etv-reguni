7cae74618552c10e5acf589dee859fbb8b77edb6
Raphaël Marmier <raphael.marmier@vd.ch>
2015-06-03 12:33:16 +0000

    [SIPM][RCEnt client] Usage de DateRangeHelper.overlap pour controler la séquentialité... Suppression du reste.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@118760 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- migration-pm/src/main/java/ch/vd/uniregctb/migration/pm/historizer/collector/SingleValueDataCollector.java
+++ migration-pm/src/main/java/ch/vd/uniregctb/migration/pm/historizer/collector/SingleValueDataCollector.java
@@ -8,9 +8,9 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import ch.vd.registre.base.date.DateRangeHelper;
 import ch.vd.registre.base.date.RegDate;
 import ch.vd.uniregctb.migration.pm.historizer.container.DateRanged;
-import ch.vd.uniregctb.migration.pm.historizer.container.SequentialDateRangesChecker;
 import ch.vd.uniregctb.migration.pm.historizer.equalator.Equalator;
 
 /**
@@ -48,7 +48,7 @@ public class SingleValueDataCollector<S, D> extends ListDataCollector<S, D> {
 	@Override
 	public final List<DateRanged<D>> getCollectedData(Supplier<List<DateRanged<D>>> listFactory) {
 		List<DateRanged<D>> collectedData = getCollectedDataStream().collect(Collectors.toCollection(listFactory));
-		SequentialDateRangesChecker.ensureSequential(collectedData);
+		DateRangeHelper.overlaps(collectedData);
 		return collectedData;
 	}
 
--- migration-pm/src/main/java/ch/vd/uniregctb/migration/pm/historizer/collector/SingleValueIndexedDataCollector.java
+++ migration-pm/src/main/java/ch/vd/uniregctb/migration/pm/historizer/collector/SingleValueIndexedDataCollector.java
@@ -6,10 +6,10 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
+import ch.vd.registre.base.date.DateRangeHelper;
 import ch.vd.registre.base.date.RegDate;
 import ch.vd.uniregctb.migration.pm.historizer.container.DateRanged;
 import ch.vd.uniregctb.migration.pm.historizer.container.Keyed;
-import ch.vd.uniregctb.migration.pm.historizer.container.SequentialDateRangesChecker;
 import ch.vd.uniregctb.migration.pm.historizer.equalator.Equalator;
 
 /**
@@ -54,7 +54,7 @@ public class SingleValueIndexedDataCollector<S, D, K> extends IndexedDataCollect
 	@Override
 	public final Map<K, List<DateRanged<D>>> getCollectedData(Supplier<Map<K, List<DateRanged<D>>>> mapFactory, Supplier<List<DateRanged<D>>> listFactory) {
 		Map<K, List<DateRanged<D>>> collectedData = delegateCollector.getCollectedData(mapFactory, listFactory);
-		collectedData.values().forEach(SequentialDateRangesChecker::ensureSequential);
+		collectedData.values().forEach(DateRangeHelper::overlaps);
 		return collectedData;
 	}
 
