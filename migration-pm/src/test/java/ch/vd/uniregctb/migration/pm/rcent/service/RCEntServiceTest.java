07fe122edff2c09de219e08942ce6a6f0e421b1f
Raphaël Marmier <raphael.marmier@vd.ch>
2015-06-09 11:34:41 +0000

    [SIPM][RCEnt client] Fix test.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@118804 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- migration-pm/src/test/java/ch/vd/uniregctb/migration/pm/rcent/service/RCEntServiceTest.java
+++ migration-pm/src/test/java/ch/vd/uniregctb/migration/pm/rcent/service/RCEntServiceTest.java
@@ -115,30 +115,80 @@ public class RCEntServiceTest {
 		assertThat(organisation.getCantonalId(), equalTo(101202262L));
 
 		// Adresse
-		Address postalAddress_period1 = organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(1).getPayload();
-																												   // TODO: Voir probleme de l'ordre chronologique. Trier ou non à la création?
+		final DateRanged<Address> postOfficeBoxAddressRange1 = organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(0);
+		Address postalAddress_period1 = postOfficeBoxAddressRange1.getPayload();
+		assertThat(postOfficeBoxAddressRange1.getDateDebut(), equalTo(RegDate.get(2015, 4, 29)));
+		assertThat(postOfficeBoxAddressRange1.getDateFin(), equalTo(RegDate.get(2015, 4, 30)));
+		assertThat(postalAddress_period1.getAddressLine1(), nullValue());
+		assertThat(postalAddress_period1.getAddressLine2(), nullValue());
+		assertThat(postalAddress_period1.getStreet(), equalTo("Chemin du Mont"));
+		assertThat(postalAddress_period1.getHouseNumber(), equalTo("15"));
+		assertThat(postalAddress_period1.getTown(), equalTo("Triffouilli les oies"));
+		assertThat(postalAddress_period1.getSwissZipCode(), equalTo(1101L));
+		assertThat(postalAddress_period1.getCountry().getCountryName(), equalTo("CH"));
+
+		// Adresse changée
+		final DateRanged<Address> postOfficeBoxAddressRange2 = organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(1);
+		Address postalAddress_period2 = postOfficeBoxAddressRange2.getPayload();
+		assertThat(postOfficeBoxAddressRange2.getDateDebut(), equalTo(RegDate.get(2015, 5, 1)));
+		assertThat(postOfficeBoxAddressRange2.getDateFin(), nullValue());
+		assertThat(postalAddress_period2.getAddressLine1(), nullValue());
+		assertThat(postalAddress_period2.getAddressLine2(), nullValue());
+		assertThat(postalAddress_period2.getStreet(), equalTo("Chemin du Mont"));
+		assertThat(postalAddress_period2.getHouseNumber(), equalTo("15"));
+		assertThat(postalAddress_period2.getTown(), equalTo("Grandson"));
+		assertThat(postalAddress_period2.getSwissZipCode(), equalTo(1422L));
+		assertThat(postalAddress_period2.getCountry().getCountryName(), equalTo("CH"));
+	}
+
+	@Test
+	public void testGetOrganisationHistory4Snapshots() throws Exception {
+		File xml = new File("src/test/resources/samples/organisationData/CHE101992624-4snaps.xml");
+		JAXBElement<OrganisationData> data = (JAXBElement<OrganisationData>) unmarshaller.unmarshal(xml);
+
+		when(client.getOrganisation(101202262, null, true)).thenReturn(data.getValue());
+
+		Organisation organisation = service.getOrganisationHistory(101202262);
+		assertThat(organisation.getCantonalId(), equalTo(101202262L));
+
+		// Adresse postale
+		assertThat(organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().size(), equalTo(3));
+
+		Address postalAddress_period1 = organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(0).getPayload();
 		assertThat(organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(0).getDateDebut(), equalTo(RegDate.get(2015, 4, 29)));
 		assertThat(organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(0).getDateFin(), equalTo(RegDate.get(2015, 4, 30)));
 		assertThat(postalAddress_period1.getAddressLine1(), nullValue());
 		assertThat(postalAddress_period1.getAddressLine2(), nullValue());
 		assertThat(postalAddress_period1.getStreet(), equalTo("Chemin du Mont"));
 		assertThat(postalAddress_period1.getHouseNumber(), equalTo("15"));
-		assertThat(postalAddress_period1.getSwissZipCode(), equalTo(1422L));
+		assertThat(postalAddress_period1.getTown(), equalTo("Triffouilli les oies"));
+		assertThat(postalAddress_period1.getSwissZipCode(), equalTo(1101L));
 		assertThat(postalAddress_period1.getCountry().getCountryName(), equalTo("CH"));
 		assertThat(postalAddress_period1.getStreet(), equalTo("Chemin du Mont"));
 
 		// Adresse changée
-		Address postalAddress_period2 = organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(0).getPayload();
-																												// TODO: Voir probleme de l'ordre chronologique. Trier ou non à la création?
+		Address postalAddress_period2 = organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(1).getPayload();
 		assertThat(organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(1).getDateDebut(), equalTo(RegDate.get(2015, 5, 1)));
-		assertThat(organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(1).getDateFin(), nullValue());
+		assertThat(organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(1).getDateFin(), equalTo(RegDate.get(2015, 5, 23)));
 		assertThat(postalAddress_period2.getAddressLine1(), nullValue());
 		assertThat(postalAddress_period2.getAddressLine2(), nullValue());
 		assertThat(postalAddress_period2.getStreet(), equalTo("Chemin du Mont"));
 		assertThat(postalAddress_period2.getHouseNumber(), equalTo("15"));
-		assertThat(postalAddress_period2.getTown(), equalTo("Triffouilli les oies"));
-		assertThat(postalAddress_period2.getSwissZipCode(), equalTo(1101L));
+		assertThat(postalAddress_period2.getTown(), equalTo("Grandson"));
+		assertThat(postalAddress_period2.getSwissZipCode(), equalTo(1422L));
 		assertThat(postalAddress_period2.getCountry().getCountryName(), equalTo("CH"));
+
+		// Adresse enlevée puis nouvelle plus tard
+		Address postalAddress_period3 = organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(2).getPayload();
+		assertThat(organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(2).getDateDebut(), equalTo(RegDate.get(2015, 6, 6)));
+		assertThat(organisation.getLocationData().get(0).getUid().getPostOfficeBoxAddress().get(2).getDateFin(), nullValue());
+		assertThat(postalAddress_period3.getAddressLine1(), nullValue());
+		assertThat(postalAddress_period3.getAddressLine2(), nullValue());
+		assertThat(postalAddress_period3.getStreet(), equalTo("Chemin du lac"));
+		assertThat(postalAddress_period3.getHouseNumber(), equalTo("18"));
+		assertThat(postalAddress_period3.getTown(), equalTo("Hiverdon"));
+		assertThat(postalAddress_period3.getSwissZipCode(), equalTo(1919L));
+		assertThat(postalAddress_period3.getCountry().getCountryName(), equalTo("CH"));
 	}
 
 	@Test
