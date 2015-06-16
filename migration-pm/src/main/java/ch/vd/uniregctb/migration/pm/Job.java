a3724dde5fe3563778f9eb8f8bb4e09c884a60db
Jacob Deker <jacob.deker@vd.ch>
2015-06-16 08:39:09 +0000

    [SIPM][Migration] Divisé les fichiers de configuration en deux parties : migration.properties et credentials.properties
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@118860 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- migration-pm/src/main/java/ch/vd/uniregctb/migration/pm/Job.java
+++ migration-pm/src/main/java/ch/vd/uniregctb/migration/pm/Job.java
@@ -1,11 +1,13 @@
 package ch.vd.uniregctb.migration.pm;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Properties;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
-import org.apache.commons.configuration.PropertiesConfiguration;
-import org.apache.commons.configuration.reloading.InvariantReloadingStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -20,18 +22,22 @@ public class Job {
 	/**
 	 * Méthode d'entrée du batch
 	 *
-	 * @param args les paramètres de la ligne de commande
+	 * @param args les paramètres de la ligne de commandes
 	 */
 	public static void main(String[] args) throws Exception {
 
-		// le seul paramètre sur la ligne de commande est le chemin vers un fichier de propriétés
-		if (args == null || args.length != 1) {
+		// les paramètres sur la ligne de commandes sont les chemins vers les fichiers de propriétés
+		if (args == null || args.length < 1) {
 			dumpSyntax();
 			return;
 		}
 
-		// on le met dans l'environnement afin que le PreferencesPlaceholderConfigurer le prenne en compte...
-		System.setProperty("ch.vd.unireg.migration.pm.conf", args[0]);
+		// on met les chemins des fichiers de configuration dans l'environnement afin que cela puisse être pris en compte
+		// dans le PreferencesPlaceholderConfigurer
+		System.setProperty("ch.vd.unireg.migration.pm.conf.nb", Integer.toString(args.length));
+		for (int i = 0 ; i < args.length ; ++ i) {
+			System.setProperty("ch.vd.unireg.migration.pm.conf.file." + i, args[i]);
+		}
 
 		// chargement du contexte spring
 		final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/properties.xml",
@@ -44,13 +50,12 @@ public class Job {
 		context.registerShutdownHook();
 
 		// rechargement des propriétés pour dump dans les logs
-		final PropertiesConfiguration properties = new PropertiesConfiguration();
-		properties.setReloadingStrategy(new InvariantReloadingStrategy());
-		properties.setEncoding("UTF-8");
-		properties.setListDelimiter((char) 0); // Disabled
-		// Fais le chargement a la fin quand on a disabled le delimiter
-		properties.setFileName(args[0]);
-		properties.load();
+		final Properties properties = new Properties();
+		for (String arg : args) {
+			try (InputStream is = new FileInputStream(arg); Reader r = new InputStreamReader(is, "UTF-8")) {
+				properties.load(r);
+			}
+		}
 
 		// Dump des properties
 		dumpProperties(properties);
@@ -59,16 +64,15 @@ public class Job {
 	}
 
 	private static void dumpSyntax() {
-		System.err.println("Le job prend un paramètre qui correspond au chemin d'accès au fichier de configuration.");
+		System.err.println("Le job prend des paramètres qui correspondent aux chemins d'accès aux fichiers de configuration.");
 	}
 
-	private static void dumpProperties(PropertiesConfiguration props) {
-		final Pattern pwdPattern = Pattern.compile("\\bpassword\\b", Pattern.CASE_INSENSITIVE);
+	private static void dumpProperties(Properties props) {
+		final Pattern usrpwdPattern = Pattern.compile("\\b(user|password)\\b", Pattern.CASE_INSENSITIVE);
 		final StringBuilder b = new StringBuilder("Dump des propriétés du traitement :").append(System.lineSeparator());
-		final Iterable<String> iterableNames = props::getKeys;
-		b.append(StreamSupport.stream(iterableNames.spliterator(), false)
+		b.append(props.stringPropertyNames().stream()
 				         .sorted()
-				         .map(key -> String.format("\t%s -> %s", key, pwdPattern.matcher(key).find() ? "*******" : enquote(props.getString(key))))
+				         .map(key -> String.format("\t%s -> %s", key, usrpwdPattern.matcher(key).find() ? "*******" : enquote(props.getProperty(key))))
 				         .collect(Collectors.joining(System.lineSeparator())));
 		LOGGER.info(b.toString());
 	}
