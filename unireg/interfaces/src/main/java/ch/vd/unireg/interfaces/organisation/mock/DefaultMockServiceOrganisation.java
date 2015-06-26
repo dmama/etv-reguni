57b54a48d8840f302172fe6f31efb94ce979765a
Raphaël Marmier <raphael.marmier@vd.ch>
2015-06-26 08:13:05 +0000

    [SIPM] Mock service interface RCEnt initiaux.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@118908 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

---
+++ unireg/interfaces/src/main/java/ch/vd/unireg/interfaces/organisation/mock/DefaultMockServiceOrganisation.java
@@ -0,0 +1,8 @@
+package ch.vd.unireg.interfaces.organisation.mock;
+
+public class DefaultMockServiceOrganisation extends MockServiceOrganisation {
+	@Override
+	protected void init() {
+
+	}
+}
---
+++ unireg/interfaces/src/main/java/ch/vd/unireg/interfaces/organisation/mock/MockServiceOrganisation.java
@@ -0,0 +1,53 @@
+package ch.vd.unireg.interfaces.organisation.mock;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import ch.vd.registre.base.date.RegDate;
+import ch.vd.unireg.interfaces.organisation.ServiceOrganisationException;
+import ch.vd.unireg.interfaces.organisation.ServiceOrganisationRaw;
+import ch.vd.unireg.interfaces.organisation.data.Organisation;
+import ch.vd.unireg.interfaces.organisation.data.SiteOrganisation;
+
+public abstract class MockServiceOrganisation implements ServiceOrganisationRaw {
+
+	/**
+	 * Map des organisations par numéro
+	 */
+	private final Map<Long, Organisation> organisationMap = new HashMap<>();
+
+	/**
+	 * Cette méthode initialise le mock en fonction des données voulues.
+	 */
+	protected abstract void init();
+
+	@Override
+	public Organisation getOrganisation(long cantonalId) throws ServiceOrganisationException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Organisation getOrganisation(long cantonalId, RegDate date) throws ServiceOrganisationException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Organisation getOrganisationHistory(long cantonalId) throws ServiceOrganisationException {
+		return organisationMap.get(cantonalId);
+	}
+
+	@Override
+	public SiteOrganisation getLocation(Long cantonalId) throws ServiceOrganisationException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public SiteOrganisation getLocation(Long cantonalId, RegDate date) throws ServiceOrganisationException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public SiteOrganisation getLocationHistory(Long cantonalId) throws ServiceOrganisationException {
+		throw new UnsupportedOperationException();
+	}
+}
