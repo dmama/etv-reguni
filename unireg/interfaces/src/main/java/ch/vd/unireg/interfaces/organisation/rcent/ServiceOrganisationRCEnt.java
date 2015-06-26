81cb7a6b2fcaf57063f92a1bac2b257ee40c303c
Raphaël Marmier <raphael.marmier@vd.ch>
2015-06-26 08:13:02 +0000

    [SIPM] Interfacage vers RCEnt Adapter. Corrections.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@118905 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/interfaces/src/main/java/ch/vd/unireg/interfaces/organisation/rcent/ServiceOrganisationRCEnt.java
+++ unireg/interfaces/src/main/java/ch/vd/unireg/interfaces/organisation/rcent/ServiceOrganisationRCEnt.java
@@ -8,14 +8,16 @@ import ch.vd.unireg.interfaces.organisation.data.SiteOrganisation;
 import ch.vd.uniregctb.adapter.rcent.model.OrganisationLocation;
 import ch.vd.uniregctb.adapter.rcent.service.RCEntAdapter;
 
-public class serviceOrganisationRCEnt implements ServiceOrganisationRaw {
+public class ServiceOrganisationRCEnt implements ServiceOrganisationRaw {
 
 	private RCEntAdapter adapter;
 
 	@Override
 	public Organisation getOrganisation(long cantonalId) throws ServiceOrganisationException {
 		ch.vd.uniregctb.adapter.rcent.model.Organisation received = adapter.getOrganisation(cantonalId);
-		if (received == null) return null;
+		if (received == null) {
+			return null;
+		}
 		sanityCheck(cantonalId, received.getCantonalId());
 		return RCEntOrganisationHelper.get(received);
 	}
@@ -23,7 +25,9 @@ public class serviceOrganisationRCEnt implements ServiceOrganisationRaw {
 	@Override
 	public Organisation getOrganisation(long cantonalId, RegDate date) throws ServiceOrganisationException {
 		ch.vd.uniregctb.adapter.rcent.model.Organisation received = adapter.getOrganisation(cantonalId, date);
-		if (received == null) return null;
+		if (received == null) {
+			return null;
+		}
 		sanityCheck(cantonalId, received.getCantonalId());
 		return RCEntOrganisationHelper.get(received);
 	}
@@ -31,7 +35,9 @@ public class serviceOrganisationRCEnt implements ServiceOrganisationRaw {
 	@Override
 	public Organisation getOrganisationHistory(long cantonalId) throws ServiceOrganisationException {
 		ch.vd.uniregctb.adapter.rcent.model.Organisation received = adapter.getOrganisationHistory(cantonalId);
-		if (received == null) return null;
+		if (received == null) {
+			return null;
+		}
 		sanityCheck(cantonalId, received.getCantonalId());
 		return RCEntOrganisationHelper.get(received);
 	}
@@ -39,7 +45,9 @@ public class serviceOrganisationRCEnt implements ServiceOrganisationRaw {
 	@Override
 	public SiteOrganisation getLocation(Long cantonalId) throws ServiceOrganisationException {
 		OrganisationLocation received = adapter.getLocation(cantonalId);
-		if (received == null) return null;
+		if (received == null) {
+			return null;
+		}
 		sanityCheck(cantonalId, received.getCantonalId());
 		return RCEntSiteOrganisationHelper.get(received);
 	}
@@ -47,7 +55,9 @@ public class serviceOrganisationRCEnt implements ServiceOrganisationRaw {
 	@Override
 	public SiteOrganisation getLocation(Long cantonalId, RegDate date) throws ServiceOrganisationException {
 		OrganisationLocation received = adapter.getLocation(cantonalId, date);
-		if (received == null) return null;
+		if (received == null) {
+			return null;
+		}
 		sanityCheck(cantonalId, received.getCantonalId());
 		return RCEntSiteOrganisationHelper.get(received);
 	}
@@ -55,7 +65,9 @@ public class serviceOrganisationRCEnt implements ServiceOrganisationRaw {
 	@Override
 	public SiteOrganisation getLocationHistory(Long cantonalId) throws ServiceOrganisationException {
 		OrganisationLocation received = adapter.getLocationHistory(cantonalId);
-		if (received == null) return null;
+		if (received == null) {
+			return null;
+		}
 		sanityCheck(cantonalId, received.getCantonalId());
 		return RCEntSiteOrganisationHelper.get(received);
 	}
@@ -63,7 +75,7 @@ public class serviceOrganisationRCEnt implements ServiceOrganisationRaw {
 	private void sanityCheck(long cantonalId, long receivedId) {
 		if (receivedId != cantonalId) {
 			throw new IllegalStateException(
-					String.format("Incohérence des données retournées détectées: organisation demandée = %d, organisation retournée = %d.",
+					String.format("Incohérence des données retournées détectée: organisation demandée = %d, organisation retournée = %d.",
 					              cantonalId,
 					              receivedId));
 		}
