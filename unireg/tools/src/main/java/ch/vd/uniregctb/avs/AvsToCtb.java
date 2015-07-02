42489c11e1a0de47edd887018cb37b76397efce8
Jacob Deker <jacob.deker@vd.ch>
2015-07-02 09:15:28 +0000

    Adapté l'outils d'extraction de numéros de contribuables en fonction d'un numéro AVS pour être capable de compléter un fichier source plutôt que d'en produire un complètement nouveau
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@118956 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/tools/src/main/java/ch/vd/uniregctb/avs/AvsToCtb.java
+++ unireg/tools/src/main/java/ch/vd/uniregctb/avs/AvsToCtb.java
@@ -5,16 +5,21 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.Nullable;
 
 import ch.vd.unireg.ws.search.party.v1.SearchResult;
 import ch.vd.unireg.xml.party.v3.PartyInfo;
+import ch.vd.uniregctb.common.FormatNumeroHelper;
 import ch.vd.uniregctb.utils.WebServiceV5Helper;
 
 public class AvsToCtb {
@@ -42,15 +47,23 @@ public class AvsToCtb {
 
 	public static void main(String[] args) throws Exception {
 
-		// on lit le contenu du fichier
-		final List<String> avsList = new ArrayList<>();
+		// on lit le contenu du fichier, ligne par ligne
+		final List<Pair<String, String>> data = new LinkedList<>();
 		try (InputStream in = AvsToCtb.class.getResourceAsStream(nomFichier);
 		     InputStreamReader isr = new InputStreamReader(in);
 			 BufferedReader reader = new BufferedReader(isr)) {
 
+			final Pattern pattern = Pattern.compile("((?:[0-9.])*)(?:[^0-9.].*)?");
 			String line = reader.readLine();
 			while (line != null) {
-				avsList.add(line);
+				final Matcher m = pattern.matcher(line);
+				if (!m.matches()) {
+					System.err.println("Line ignorée : '" + line + "'");
+				}
+				else {
+					final String noAvs = FormatNumeroHelper.removeSpaceAndDash(m.group(1));
+					data.add(Pair.of(StringUtils.trimToEmpty(noAvs), line));
+				}
 				line = reader.readLine();
 			}
 		}
@@ -69,18 +82,24 @@ public class AvsToCtb {
 
 		// et on boucle sur les lots
 		try {
-			final Set<String> partyTypes = new HashSet<>(Arrays.asList("NATURAL_PERSON"));
-			for (String avs : avsList) {
+			final Set<String> partyTypes = new HashSet<>(Collections.singletonList("NATURAL_PERSON"));
+			for (Pair<String, String> line : data) {
 				try {
-					final SearchResult result = WebServiceV5Helper.searchParty(urlWebService, userWebService, pwdWebService, userId, oid, null, null, avs, null, partyTypes);
-					if (result.getError() != null) {
-						throw new Exception(result.getError().getErrorMessage());
+					if (StringUtils.isNotBlank(line.getLeft())) {
+						final SearchResult result = WebServiceV5Helper.searchParty(urlWebService, userWebService, pwdWebService, userId, oid, null, null, line.getLeft(), null, partyTypes);
+						if (result.getError() != null) {
+							throw new Exception(result.getError().getErrorMessage());
+						}
+						final List<PartyInfo> items = result.getParty();
+						dumpTiers(line, items, null, ps);
 					}
-					final List<PartyInfo> items = result.getParty();
-					dumpTiers(avs, items, null, ps);
+					else {
+						// rien trouvé car pas de numéro AVS en entrée
+						dumpTiers(line, null, null, ps);
+					}
 				}
 				catch (Exception e) {
-					dumpTiers(avs, null, e, ps);
+					dumpTiers(line, null, e, ps);
 				}
 			}
 		}
@@ -91,18 +110,27 @@ public class AvsToCtb {
 		}
 	}
 
-	private static void dumpTiers(String avs, @Nullable List<PartyInfo> items, @Nullable Exception e, PrintStream ps) {
-		final StringBuilder b = new StringBuilder(avs);
-		b.append(';');
+	private static void dumpTiers(Pair<String, String> line, @Nullable List<PartyInfo> items, @Nullable Exception e, PrintStream ps) {
+		final StringBuilder b = new StringBuilder(line.getRight());
+		b.append(";");
 		if (e != null) {
 			b.append(e.getClass().getName());
 			b.append(';');
 			b.append(e.getMessage());
 		}
 		else if (items != null && !items.isEmpty()) {
-			for (PartyInfo item : items) {
-				b.append(item.getNumber());
-				b.append(';');
+			if (items.size() == 1) {
+				b.append(items.get(0).getNumber());
+			}
+			else {
+				boolean first = true;
+				for (PartyInfo item : items) {
+					if (!first) {
+						b.append("/");
+					}
+					b.append(item.getNumber());
+					first = false;
+				}
 			}
 		}
 		ps.println(b.toString());
