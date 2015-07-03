3f453f83471294770d48bc5383f34c4c3e47d690
Jacob Deker <jacob.deker@vd.ch>
2015-07-03 06:06:44 +0000

    [SIPM][Migration] Petit cleanup dans la classe LockHelper
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@118965 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- migration-pm/src/main/java/ch/vd/uniregctb/migration/pm/utils/LockHelper.java
+++ migration-pm/src/main/java/ch/vd/uniregctb/migration/pm/utils/LockHelper.java
@@ -4,6 +4,8 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Supplier;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Classe d'accès à des sections critiques partagées (read) ou exclusives (write)
  */
@@ -33,7 +35,7 @@ public class LockHelper {
 	 * @param callback callback à appeler
 	 */
 	public void doInWriteLock(Runnable callback) {
-		doInWriteLock(() -> { callback.run(); return null; });
+		doInWriteLock(nullSupplierFromRunnable(callback));
 	}
 
 	/**
@@ -51,9 +53,30 @@ public class LockHelper {
 	 * @param callback callback à appeler
 	 */
 	public void doInReadLock(Runnable callback) {
-		doInReadLock(() -> { callback.run(); return null; });
+		doInReadLock(nullSupplierFromRunnable(callback));
 	}
 
+	/**
+	 * Converti une instance de {@link Runnable} en une instance de {@link Supplier} qui renvoie <code>null</code>.
+	 * @param runnable le {@link Runnable} à convertir
+	 * @param <T> un type arbitraire (<code>null</code> est de tout type)
+	 * @return le {@link Supplier} adéquat
+	 */
+	@NotNull
+	private static <T> Supplier<T> nullSupplierFromRunnable(Runnable runnable) {
+		return () -> {
+			runnable.run();
+			return null;
+		};
+	}
+
+	/**
+	 * Execute le callback fourni dans le contexte du verrou fourni
+	 * @param lock verrou à activer pendant l'exécution du callback
+	 * @param callback callback à exécuter
+	 * @param <T> type de la valeur retournée par le callback
+	 * @return valeur retournée par le callback
+	 */
 	private static <T> T doInLock(Lock lock, Supplier<T> callback) {
 		lock.lock();
 		try {
