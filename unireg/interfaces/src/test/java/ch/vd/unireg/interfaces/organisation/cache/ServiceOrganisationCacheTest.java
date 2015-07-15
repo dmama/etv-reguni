07d47f9cda123419c77aab35081822c9f3617efd
Raphaël Marmier <raphael.marmier@vd.ch>
2015-07-15 17:47:58 +0000

    [SIPM][Service Organisation] Fix le test de la cache du service. Ehcache fournit un singleton qu'il faut reinitialiser.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@119040 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/interfaces/src/test/java/ch/vd/unireg/interfaces/organisation/cache/ServiceOrganisationCacheTest.java
+++ unireg/interfaces/src/test/java/ch/vd/unireg/interfaces/organisation/cache/ServiceOrganisationCacheTest.java
@@ -12,6 +12,7 @@ import ch.vd.unireg.interfaces.organisation.ServiceOrganisationException;
 import ch.vd.unireg.interfaces.organisation.data.Organisation;
 import ch.vd.unireg.interfaces.organisation.mock.DefaultMockServiceOrganisation;
 import ch.vd.uniregctb.cache.UniregCacheManagerImpl;
+import ch.vd.uniregctb.common.WithoutSpringTest;
 import ch.vd.uniregctb.data.DataEventListener;
 import ch.vd.uniregctb.data.DataEventService;
 import ch.vd.uniregctb.stats.MockStatsService;
@@ -19,12 +20,59 @@ import ch.vd.uniregctb.type.TypeRapportEntreTiers;
 
 import static org.junit.Assert.assertEquals;
 
-public class ServiceOrganisationCacheTest {
+public class ServiceOrganisationCacheTest extends WithoutSpringTest {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(ServiceOrganisationCacheTest.class);
 
 	private CacheMockServiceOrganisation target;
 	private ServiceOrganisationCache cache;
+
+	private final DataEventService dataEventService = new DataEventService() {
+		@Override
+		public void register(DataEventListener listener) {
+
+		}
+
+		@Override
+		public void onTiersChange(long id) {
+
+		}
+
+		@Override
+		public void onIndividuChange(long id) {
+
+		}
+
+		@Override
+		public void onOrganisationChange(long id) {
+
+		}
+
+		@Override
+		public void onPersonneMoraleChange(long id) {
+
+		}
+
+		@Override
+		public void onDroitAccessChange(long ppId) {
+
+		}
+
+		@Override
+		public void onRelationshipChange(TypeRapportEntreTiers type, long sujetId, long objetId) {
+
+		}
+
+		@Override
+		public void onLoadDatabase() {
+
+		}
+
+		@Override
+		public void onTruncateDatabase() {
+
+		}
+	};
 
 	public static class CacheMockServiceOrganisation extends DefaultMockServiceOrganisation {
 		private int historyCounter = 0;
@@ -48,57 +96,13 @@ public class ServiceOrganisationCacheTest {
 
 		cache = new ServiceOrganisationCache();
 		final CacheManager manager = CacheManager.create(ResourceUtils.getFile("classpath:ut/ehcache.xml").getPath());
+		manager.clearAll(); // Manager is a singleton, and may exist already
 		cache.setCacheManager(manager);
 		cache.setCacheName("serviceOrganisation");
 		cache.setUniregCacheManager(new UniregCacheManagerImpl());
 		cache.setStatsService(new MockStatsService());
 		cache.setTarget(target);
-		cache.setDataEventService(new DataEventService() {
-			@Override
-			public void register(DataEventListener listener) {
-
-			}
-
-			@Override
-			public void onTiersChange(long id) {
-
-			}
-
-			@Override
-			public void onIndividuChange(long id) {
-
-			}
-
-			@Override
-			public void onOrganisationChange(long id) {
-
-			}
-
-			@Override
-			public void onPersonneMoraleChange(long id) {
-
-			}
-
-			@Override
-			public void onDroitAccessChange(long ppId) {
-
-			}
-
-			@Override
-			public void onRelationshipChange(TypeRapportEntreTiers type, long sujetId, long objetId) {
-
-			}
-
-			@Override
-			public void onLoadDatabase() {
-
-			}
-
-			@Override
-			public void onTruncateDatabase() {
-
-			}
-		});
+		cache.setDataEventService(dataEventService);
 		cache.afterPropertiesSet();
 	}
 
@@ -106,19 +110,20 @@ public class ServiceOrganisationCacheTest {
 	public void testCallTwiceHitServiceOnce() {
 		long id = 101202100L;
 		Organisation organisation = cache.getOrganisationHistory(id);
-		LOGGER.info(String.format("Target called once, return organisation %d.", id));
+		LOGGER.info(String.format("Target called once, return organisation %d.", organisation.getNo()));
 		organisation = cache.getOrganisationHistory(id);
-		LOGGER.info(String.format("Target called a second time, return organisation %d.", id));
+		LOGGER.info(String.format("Target called a second time, return organisation %d.", organisation.getNo()));
 		assertEquals(1, target.getHistoryCounter());
 	}
+
 	@Test
 	public void testCallTwiceOrganisationChangesInBetween() {
 		long id = 101202100L;
 		Organisation organisation = cache.getOrganisationHistory(id);
-		LOGGER.info(String.format("Target called once, return organisation %d.", id));
+		LOGGER.info(String.format("Target called once, return organisation %d.", organisation.getNo()));
 		cache.onOrganisationChange(id);
 		organisation = cache.getOrganisationHistory(id);
-		LOGGER.info(String.format("Target called a second time, return organisation %d.", id));
+		LOGGER.info(String.format("Target called a second time, return organisation %d.", organisation.getNo()));
 		assertEquals(2, target.getHistoryCounter());
 	}
 
