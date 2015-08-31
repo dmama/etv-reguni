86a49e38b50831a4332e02d81031a97d8d0c1719
Raphaël Marmier <raphael.marmier@vd.ch>
2015-08-31 14:09:16 +0000

    [SIPM][Web] Corriger une NPE, enlever du code mort.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@119291 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/web/src/main/java/ch/vd/uniregctb/entreprise/EntrepriseView.java
+++ unireg/web/src/main/java/ch/vd/uniregctb/entreprise/EntrepriseView.java
@@ -6,6 +6,20 @@ import ch.vd.uniregctb.tiers.view.EtatPMView;
 
 public class EntrepriseView {
 
+	enum SourceCivile {
+		RCENT("label.source.donnees.civiles.rcent"), UNIREG("application.name");
+
+		private String key;
+
+		SourceCivile(String key) {
+			this.key = key;
+		}
+
+		public String getKey() {
+			return key;
+		}
+	}
+
 	private String raisonSociale;
 	private List<String> autresRaisonsSociales;
 
@@ -14,6 +28,8 @@ public class EntrepriseView {
 	private List<CapitalView> capitaux;
 	private List<EtatPMView> etats;
 	private List<String> numerosIDE;
+
+	private SourceCivile source;
 
 	public String getRaisonSociale() {
 		return raisonSociale;
@@ -69,5 +85,17 @@ public class EntrepriseView {
 
 	public void setNumerosIDE(List<String> numerosIDE) {
 		this.numerosIDE = numerosIDE;
+	}
+
+	public String getSourceKey() {
+		return source.getKey();
+	}
+
+	public SourceCivile getSource() {
+		return source;
+	}
+
+	public void setSource(SourceCivile source) {
+		this.source = source;
 	}
 }
