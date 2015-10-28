fa8e3a1583c6dfec3e8bbadbca3ef537e0cdc8bf
Jacob Deker <jacob.deker@vd.ch>
2015-10-28 07:55:31 +0000

    Corrigé Watchdog suite au passage sous IAM de l'environnement SIPM
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@119666 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- watchdog/src/test/java/ch/vd/watchdog/WatchDogUniregSipmTest.java
+++ watchdog/src/test/java/ch/vd/watchdog/WatchDogUniregSipmTest.java
@@ -22,10 +22,8 @@ public class WatchDogUniregSipmTest extends WatchDogTest {
 		LOGGER.info("Vérification de Unireg sur l'environnement SIPM...");
 
 		// pour le moment, on n'a pas encore IAM
-		final WebClient webClient = new WebClient();
-		final HtmlPage page = getPage(webClient, new URL("http://slv2984v.etat-de-vaud.ch:50600/fiscalite/test-unireg/web/"));
-//		final WebClient webClient = loginIamValidation();
-//		HtmlPage page = getPage(webClient, new URL("https://validation.portail.etat-de-vaud.ch/fiscalite/test-unireg/web/"));
+		final WebClient webClient = loginIamValidation();
+		HtmlPage page = getPage(webClient, new URL("https://validation.portail.etat-de-vaud.ch/fiscalite/sipm-unireg/web/"));
 		assertNotNull(page);
 		String titre = page.getTitleText();
 		assertTrue(titre, titre.equalsIgnoreCase("Recherche des tiers") || titre.equalsIgnoreCase("Sélection de l'OID de travail"));
