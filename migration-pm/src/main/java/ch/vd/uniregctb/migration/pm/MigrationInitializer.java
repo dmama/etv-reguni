56fa02c2014446c4d74884cddd4b26a4f4171c0b
Jacob Deker <jacob.deker@vd.ch>
2015-11-30 11:31:23 +0000

    [SIPM][Migration] Passé en multi-thread la phase d'indexation des non-habitants inconnus de RCPers
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@119900 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- migration-pm/src/main/java/ch/vd/uniregctb/migration/pm/MigrationInitializer.java
+++ migration-pm/src/main/java/ch/vd/uniregctb/migration/pm/MigrationInitializer.java
@@ -5,6 +5,14 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import org.hibernate.Query;
@@ -14,10 +22,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
 
 import ch.vd.registre.base.date.RegDate;
 import ch.vd.uniregctb.common.AuthenticationHelper;
+import ch.vd.uniregctb.common.BatchIterator;
+import ch.vd.uniregctb.common.StandardBatchIterator;
 import ch.vd.uniregctb.declaration.PeriodeFiscale;
+import ch.vd.uniregctb.migration.pm.engine.probe.ProgressMeasurementProbe;
 import ch.vd.uniregctb.migration.pm.indexeur.NonHabitantIndex;
 import ch.vd.uniregctb.tiers.PersonnePhysique;
 import ch.vd.uniregctb.transaction.TransactionTemplate;
@@ -34,6 +47,8 @@ public class MigrationInitializer implements InitializingBean {
 	private PlatformTransactionManager uniregTransactionManager;
 	private NonHabitantIndex nonHabitantIndex;
 	private boolean nonHabitantIndexToBeCleared = false;
+	private int nbThreadsIndexation = 1;
+	private ProgressMeasurementProbe indexationProgressMonitor;
 
 	public void setRegistrar(MigrationInitializationRegistrar registrar) {
 		this.registrar = registrar;
@@ -55,6 +70,14 @@ public class MigrationInitializer implements InitializingBean {
 		this.nonHabitantIndexToBeCleared = nonHabitantIndexToBeCleared;
 	}
 
+	public void setNbThreadsIndexation(int nbThreadsIndexation) {
+		this.nbThreadsIndexation = nbThreadsIndexation;
+	}
+
+	public void setIndexationProgressMonitor(ProgressMeasurementProbe indexationProgressMonitor) {
+		this.indexationProgressMonitor = indexationProgressMonitor;
+	}
+
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		// génération des PF éventuellement manquantes
@@ -72,27 +95,68 @@ public class MigrationInitializer implements InitializingBean {
 			// nettoyage préalable
 			nonHabitantIndex.overwriteIndex();
 
-			final TransactionTemplate template = new TransactionTemplate(uniregTransactionManager);
-			template.setReadOnly(true);
-			template.execute(status -> {
+			// récupération des identifiants à traiter
+			//noinspection unchecked
+			final List<Long> ids = doInUniregTransaction(true, status -> {
 				final Session currentSession = uniregSessionFactory.getCurrentSession();
-				final Query query = currentSession.createQuery("select p from PersonnePhysique p where numeroIndividu is null");
+				final Query query = currentSession.createQuery("select p.numero from PersonnePhysique p where numeroIndividu is null");
+				return query.list();
+			});
 
-				//noinspection unchecked
-				final Iterator<PersonnePhysique> iterator = query.iterate();
-				int cursor = 0;
-				while (iterator.hasNext()) {
-					final PersonnePhysique pp = iterator.next();
-					nonHabitantIndex.index(pp);
-					currentSession.evict(pp);       // ne pas bouffer toute la RAM...
+			LOGGER.info(String.format("%d non-habitants inconnus de RCPers trouvés.", ids.size()));
 
-					if (++ cursor % 10000 == 0) {
-						LOGGER.info(String.format("Indexation des inconnus civils : %d.", cursor));
+			// commande des traitements d'indexation
+			final ExecutorService executor = Executors.newFixedThreadPool(nbThreadsIndexation);
+			try {
+				final CompletionService<Integer> completionService = new ExecutorCompletionService<>(executor);
+				final BatchIterator<Long> idsIterator = new StandardBatchIterator<>(ids, 100);
+				int nbBatches = 0;
+				while (idsIterator.hasNext()) {
+					final List<Long> localIds = idsIterator.next();
+					completionService.submit(() -> doInUniregTransaction(true, status -> {
+						final Session currentSession = uniregSessionFactory.getCurrentSession();
+						final Query query = currentSession.createQuery("select p from PersonnePhysique p where p.numero in (:ids)");
+						query.setParameterList("ids", localIds);
+
+						// boucle sur les personnes physiques
+						//noinspection unchecked
+						final Iterator<PersonnePhysique> iterator = query.iterate();
+						while (iterator.hasNext()) {
+							final PersonnePhysique pp = iterator.next();
+							nonHabitantIndex.index(pp);
+						}
+					}), localIds.size());
+
+					++nbBatches;
+				}
+				executor.shutdown();
+
+				// réception des résultats
+				int treated = 0;
+				while (nbBatches > 0) {
+					final Future<Integer> tailleBatch = completionService.poll(1, TimeUnit.SECONDS);
+					if (tailleBatch != null) {
+						--nbBatches;
+
+						treated += tailleBatch.get();
+						indexationProgressMonitor.setPercentProgress(treated * 100 / ids.size());
 					}
 				}
-				LOGGER.info(String.format("%d inconnus civils indexés.", cursor));
-				return null;
-			});
+			}
+			catch (ExecutionException e) {
+				LOGGER.error("Exception lancée lors de l'indexation des non-habitants...", e.getCause());
+				throw new RuntimeException(e.getCause());
+			}
+			finally {
+				executor.shutdownNow();
+				while (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
+					// on attend...
+				}
+			}
+		}
+		catch (InterruptedException e) {
+			LOGGER.error("Thread interrompu!", e);
+			throw new RuntimeException();
 		}
 		finally {
 			LOGGER.info("Génération des données d'indexation des non-habitants terminée.");
@@ -103,9 +167,7 @@ public class MigrationInitializer implements InitializingBean {
 		LOGGER.info("Ajout des PF manquantes dans Unireg (depuis " + PREMIERE_PF + ").");
 		AuthenticationHelper.pushPrincipal(MigrationConstants.VISA_MIGRATION);
 		try {
-			final TransactionTemplate template = new TransactionTemplate(uniregTransactionManager);
-			template.setReadOnly(false);
-			template.execute(status -> {
+			doInUniregTransaction(false, status -> {
 				final Session currentSession = uniregSessionFactory.getCurrentSession();
 				final Query query = currentSession.createQuery("select pf from PeriodeFiscale pf");
 				//noinspection unchecked
@@ -114,20 +176,30 @@ public class MigrationInitializer implements InitializingBean {
 				for (int annee = PREMIERE_PF ; annee <= RegDate.get().year() ; ++ annee) {
 					if (!anneesCouvertes.contains(annee)) {
 						LOGGER.info("Création de la PF manquante " + annee + ".");
-
-						// TODO vérifier s'il n'y a pas d'autres valeurs de paramètres à ajouter...
 						final PeriodeFiscale pf = new PeriodeFiscale();
 						pf.setAnnee(annee);
 						pf.setDefaultPeriodeFiscaleParametres();
 						currentSession.save(pf);
 					}
 				}
-				return null;
 			});
 		}
 		finally {
 			AuthenticationHelper.popPrincipal();
 			LOGGER.info("Ajout des PF manquantes terminé.");
 		}
+	}
+
+	private <T> T doInUniregTransaction(boolean readonly, TransactionCallback<T> callback) {
+		final TransactionTemplate template = new TransactionTemplate(uniregTransactionManager);
+		template.setReadOnly(readonly);
+		return template.execute(callback);
+	}
+
+	private void doInUniregTransaction(boolean readonly, Consumer<TransactionStatus> callback) {
+		doInUniregTransaction(readonly, status -> {
+			callback.accept(status);
+			return null;
+		});
 	}
 }
