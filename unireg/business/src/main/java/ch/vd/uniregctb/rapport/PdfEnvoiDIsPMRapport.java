5f8735871145a3af965d5cd31ffa142afc7a7355
Jacob Deker <jacob.deker@vd.ch>
2015-12-18 07:06:42 +0000

    [SIPM][SIFISC-17376] Ajouté la date limite de bouclement (paramètre du job) au rapport d'exécution du job d'envoi des déclarations des personnes morales
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@120059 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/rapport/PdfEnvoiDIsPMRapport.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/rapport/PdfEnvoiDIsPMRapport.java
@@ -43,6 +43,7 @@ public class PdfEnvoiDIsPMRapport extends PdfRapport {
                 @Override
                 public void fillTable(PdfTableSimple table) throws DocumentException {
                     table.addLigne("Période fiscale considérée :", String.valueOf(results.getPeriodeFiscale()));
+                    table.addLigne("Date limite de bouclement :", RegDateHelper.dateToDisplayString(results.getDateLimiteBouclements()));
                     table.addLigne("Type de document :", results.getType().name());
                     table.addLigne("Nombre maximum d'envois :", results.getNbMaxEnvois() == null ? "-" : String.valueOf(results.getNbMaxEnvois()));
 	                table.addLigne("Nombre de threads :", String.valueOf(results.getNbThreads()));
