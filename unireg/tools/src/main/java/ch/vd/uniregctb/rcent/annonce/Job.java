04c92790aa9a05c4810061581e5b34034958d676
Jacob Deker <jacob.deker@vd.ch>
2016-02-16 11:30:32 +0000

    Ajouté le numéro de contribuable à la liste générée pour les annonces RCEnt
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@120526 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/tools/src/main/java/ch/vd/uniregctb/rcent/annonce/Job.java
+++ unireg/tools/src/main/java/ch/vd/uniregctb/rcent/annonce/Job.java
@@ -47,7 +47,7 @@ public class Job {
 		     BufferedWriter bw = new BufferedWriter(osw)) {
 
 			// la ligne décrivant les colonnes
-			bw.write("RAISON_SOCIALE;CHEZ;RUE;NPA;LIEU;CANTON_SIEGE;PAYS_SIEGE;FORME_JURIDIQUE;CODE_FORME_JURIDIQUE;LANGUE;SIEGE");
+			bw.write("NO_CTB;RAISON_SOCIALE;CHEZ;RUE;NPA;LIEU;CANTON_SIEGE;PAYS_SIEGE;FORME_JURIDIQUE;CODE_FORME_JURIDIQUE;LANGUE;SIEGE");
 			bw.newLine();
 
 			// on boucle sur les sociétés qui doivent être exportées
@@ -67,7 +67,8 @@ public class Job {
 					// export des données
 					if (adresse != null && donneesEntreprise != null) {
 						final Pair<String, String> typeEntite = getTypeEntite(donneesEntreprise.getFormeJuridique());
-						bw.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;Français;%s",
+						bw.write(String.format("%d;%s;%s;%s;%s;%s;%s;%s;%s;%s;Français;%s",
+						                       entry.getKey(),
 						                       StringUtils.trimToEmpty(donneesEntreprise.getRaisonSociale()),
 						                       StringUtils.trimToEmpty(adresse.getChez()),
 						                       StringUtils.trimToEmpty(adresse.getRue()),
