d3a047292352fa38311a810ad748e6ef5edf601d
Jacob Deker <jacob.deker@vd.ch>
2016-03-07 13:34:24 +0000

    [SIFISC-18115] Pris en compte l'historique complet des sièges d'une entreprise dans les données exposées par les WS (et pas seulement les données fiscales)
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/6.0.8@120729 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/xml/party/v3/strategy/CorporationStrategy.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/xml/party/v3/strategy/CorporationStrategy.java
@@ -10,7 +10,6 @@ import java.util.Set;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import ch.vd.registre.base.date.DateRange;
 import ch.vd.registre.base.date.DateRangeHelper;
 import ch.vd.registre.base.date.RegDate;
 import ch.vd.unireg.interfaces.organisation.data.DateRanged;
@@ -26,9 +25,8 @@ import ch.vd.unireg.xml.party.v3.UidNumberList;
 import ch.vd.uniregctb.common.CollectionsUtils;
 import ch.vd.uniregctb.metier.bouclement.ExerciceCommercial;
 import ch.vd.uniregctb.tiers.CapitalHisto;
-import ch.vd.uniregctb.tiers.DomicileEtablissement;
+import ch.vd.uniregctb.tiers.DomicileHisto;
 import ch.vd.uniregctb.tiers.Entreprise;
-import ch.vd.uniregctb.tiers.Etablissement;
 import ch.vd.uniregctb.tiers.EtatEntreprise;
 import ch.vd.uniregctb.tiers.FormeLegaleHisto;
 import ch.vd.uniregctb.tiers.IdentificationEntreprise;
@@ -176,19 +174,15 @@ public class CorporationStrategy extends TaxPayerStrategy<Corporation> {
 	@NotNull
 	private List<LegalSeat> extractSieges(Entreprise entreprise, Context context) {
 		final List<LegalSeat> liste = new ArrayList<>();
-		final List<DateRanged<Etablissement>> etablissements = context.tiersService.getEtablissementsPrincipauxEntreprise(entreprise);
-		for (DateRanged<Etablissement> etb : etablissements) {
-			final List<DomicileEtablissement> domiciles = etb.getPayload().getSortedDomiciles(false);
-			for (DomicileEtablissement domicile : domiciles) {
-				final DateRange intersection = DateRangeHelper.intersection(domicile, etb);
-				if (intersection != null) {
-					final LegalSeat seat = new LegalSeat();
-					seat.setDateFrom(DataHelper.coreToXMLv2(intersection.getDateDebut()));
-					seat.setDateTo(DataHelper.coreToXMLv2(intersection.getDateFin()));
-					seat.setFsoId(domicile.getNumeroOfsAutoriteFiscale());
-					seat.setType(EnumHelper.coreToXMLLegalSeatv3(domicile.getTypeAutoriteFiscale()));
-					liste.add(seat);
-				}
+		final List<DomicileHisto> sieges = context.tiersService.getSieges(entreprise);
+		for (DomicileHisto siege : sieges) {
+			if (!siege.isAnnule()) {
+				final LegalSeat seat = new LegalSeat();
+				seat.setDateFrom(DataHelper.coreToXMLv2(siege.getDateDebut()));
+				seat.setDateTo(DataHelper.coreToXMLv2(siege.getDateFin()));
+				seat.setFsoId(siege.getNoOfs());
+				seat.setType(EnumHelper.coreToXMLLegalSeatv3(siege.getTypeAutoriteFiscale()));
+				liste.add(seat);
 			}
 		}
 		return liste;
