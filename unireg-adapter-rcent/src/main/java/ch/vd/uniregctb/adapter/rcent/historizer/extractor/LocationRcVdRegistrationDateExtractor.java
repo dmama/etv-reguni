160a9467da4e274fcd22f07e8887ac1eb46f2d46
Raphaël Marmier <raphael.marmier@vd.ch>
2016-04-13 17:29:11 +0000

    [SIPM][RCEnt Adapter] Correction d'un bug dans la lecture de la date d'inscription RC VD.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@121015 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg-adapter-rcent/src/main/java/ch/vd/uniregctb/adapter/rcent/historizer/extractor/LocationRcVdRegistrationDateExtractor.java
+++ unireg-adapter-rcent/src/main/java/ch/vd/uniregctb/adapter/rcent/historizer/extractor/LocationRcVdRegistrationDateExtractor.java
@@ -12,7 +12,7 @@ public class LocationRcVdRegistrationDateExtractor implements Function<Organisat
 	@Override
 	public Stream<Keyed<BigInteger, RegDate>> apply(Organisation org) {
 		return org.getOrganisationLocation().stream()
-				.filter(ol -> ol.getCommercialRegisterData() != null && ol.getCommercialRegisterData().getVdDeregistrationDate() != null)
-				.map(ol -> new Keyed<>(ol.getCantonalId(), ol.getCommercialRegisterData().getVdDeregistrationDate()));
+				.filter(ol -> ol.getCommercialRegisterData() != null && ol.getCommercialRegisterData().getVdRegistrationDate() != null)
+				.map(ol -> new Keyed<>(ol.getCantonalId(), ol.getCommercialRegisterData().getVdRegistrationDate()));
 	}
 }
