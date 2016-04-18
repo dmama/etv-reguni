71bf88391827b3108178aed135f6ea9b93236eb9
Jacob Deker <jacob.deker@vd.ch>
2016-04-18 14:26:32 +0000

    [SIPM] Cleanup autour des dépendances du MetierServicePM
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/6.0@121095 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/metier/MetierServicePMImpl.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/metier/MetierServicePMImpl.java
@@ -12,7 +12,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import ch.vd.registre.base.date.DateRange;
 import ch.vd.registre.base.date.DateRangeHelper;
@@ -35,13 +34,8 @@ import ch.vd.uniregctb.adresse.TypeAdresseFiscale;
 import ch.vd.uniregctb.common.CollectionsUtils;
 import ch.vd.uniregctb.common.FormatNumeroHelper;
 import ch.vd.uniregctb.common.LengthConstants;
-import ch.vd.uniregctb.efacture.EFactureService;
 import ch.vd.uniregctb.evenement.fiscal.EvenementFiscalInformationComplementaire;
 import ch.vd.uniregctb.evenement.fiscal.EvenementFiscalService;
-import ch.vd.uniregctb.hibernate.HibernateTemplate;
-import ch.vd.uniregctb.interfaces.service.ServiceInfrastructureService;
-import ch.vd.uniregctb.interfaces.service.ServiceOrganisationService;
-import ch.vd.uniregctb.parametrage.ParametreAppService;
 import ch.vd.uniregctb.tache.TacheService;
 import ch.vd.uniregctb.tiers.ActiviteEconomique;
 import ch.vd.uniregctb.tiers.CapitalFiscalEntreprise;
@@ -62,7 +56,6 @@ import ch.vd.uniregctb.tiers.RaisonSocialeFiscaleEntreprise;
 import ch.vd.uniregctb.tiers.RapportEntreTiers;
 import ch.vd.uniregctb.tiers.Remarque;
 import ch.vd.uniregctb.tiers.Tiers;
-import ch.vd.uniregctb.tiers.TiersDAO;
 import ch.vd.uniregctb.tiers.TiersService;
 import ch.vd.uniregctb.tiers.dao.RemarqueDAO;
 import ch.vd.uniregctb.type.MotifFor;
@@ -72,87 +65,28 @@ import ch.vd.uniregctb.type.TypeAutoriteFiscale;
 import ch.vd.uniregctb.type.TypeEtatEntreprise;
 import ch.vd.uniregctb.type.TypeGenerationEtatEntreprise;
 import ch.vd.uniregctb.type.TypeRapportEntreTiers;
-import ch.vd.uniregctb.validation.ValidationInterceptor;
-import ch.vd.uniregctb.validation.ValidationService;
 
 public class MetierServicePMImpl implements MetierServicePM {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(MetierServicePMImpl.class);
 
-	private PlatformTransactionManager transactionManager;
-	private HibernateTemplate hibernateTemplate;
-	private ServiceInfrastructureService serviceInfra;
-	private ServiceOrganisationService serviceOrganisationService;
-	private TiersDAO tiersDAO;
 	private TiersService tiersService;
 	private AdresseService adresseService;
 	private RemarqueDAO remarqueDAO;
-	private ValidationService validationService;
-	private ValidationInterceptor validationInterceptor;
-	private EFactureService eFactureService;
-	private ParametreAppService parametreAppService;
 	private EvenementFiscalService evenementFiscalService;
 	private TacheService tacheService;
-
-	public void setTransactionManager(PlatformTransactionManager transactionManager) {
-		this.transactionManager = transactionManager;
-	}
-
-	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
-		this.hibernateTemplate = hibernateTemplate;
-	}
-
-	public void setInfrastructureService(ServiceInfrastructureService serviceInfra) {
-		this.serviceInfra = serviceInfra;
-	}
-
-	public void setServiceOrganisationService(ServiceOrganisationService serviceOrganisationService) {
-		this.serviceOrganisationService = serviceOrganisationService;
-	}
-
-	public void setParametreAppService(ParametreAppService parametreAppService) {
-		this.parametreAppService = parametreAppService;
-	}
-
-	public TiersDAO getTiersDAO() {
-		return tiersDAO;
-	}
-
-	public void setTiersDAO(TiersDAO tiersDAO) {
-		this.tiersDAO = tiersDAO;
-	}
 
 	public void setRemarqueDAO(RemarqueDAO remarqueDAO) {
 		this.remarqueDAO = remarqueDAO;
 	}
 
-	/**
-	 * @param adresseService the adresseService to set
-	 */
 	public void setAdresseService(AdresseService adresseService) {
 		this.adresseService = adresseService;
-	}
-
-	public TiersService getTiersService() {
-		return tiersService;
 	}
 
 	public void setTiersService(TiersService tiersService) {
 		this.tiersService = tiersService;
 	}
-
-	public void setValidationService(ValidationService validationService) {
-		this.validationService = validationService;
-	}
-
-	public void setValidationInterceptor(ValidationInterceptor validationInterceptor) {
-		this.validationInterceptor = validationInterceptor;
-	}
-
-	public void seteFactureService(EFactureService eFactureService) {
-		this.eFactureService = eFactureService;
-	}
-
 
 	public void setEvenementFiscalService(EvenementFiscalService evenementFiscalService) {
 		this.evenementFiscalService = evenementFiscalService;
