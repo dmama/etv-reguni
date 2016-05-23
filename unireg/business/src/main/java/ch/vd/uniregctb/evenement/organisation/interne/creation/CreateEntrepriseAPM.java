a26a8f0293ac12686baeb908898dbf726ef9a590
Raphaël Marmier <raphael.marmier@vd.ch>
2016-05-23 15:28:41 +0000

    [SIPM] [Evenements Organisation] SIFISC-19335 - Mettre les créations d'APM à vérifier.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/6.0@121403 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/evenement/organisation/interne/creation/CreateEntrepriseAPM.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/evenement/organisation/interne/creation/CreateEntrepriseAPM.java
@@ -5,6 +5,7 @@ import ch.vd.registre.base.date.RegDateHelper;
 import ch.vd.registre.base.utils.Assert;
 import ch.vd.unireg.interfaces.organisation.data.FormeLegale;
 import ch.vd.unireg.interfaces.organisation.data.Organisation;
+import ch.vd.uniregctb.common.FormatNumeroHelper;
 import ch.vd.uniregctb.evenement.organisation.EvenementOrganisation;
 import ch.vd.uniregctb.evenement.organisation.EvenementOrganisationContext;
 import ch.vd.uniregctb.evenement.organisation.EvenementOrganisationException;
@@ -42,19 +43,22 @@ public class CreateEntrepriseAPM extends CreateEntreprise {
 	public void doHandle(EvenementOrganisationWarningCollector warnings, EvenementOrganisationSuiviCollector suivis) throws EvenementOrganisationException {
 		super.doHandle(warnings, suivis);
 
-			MotifFor motifOuverture = determineMotifOuvertureFor(isCreation());
+		MotifFor motifOuverture = determineMotifOuvertureFor(isCreation());
 
-			openForFiscalPrincipal(getDateDeCreation(),
-			                       getAutoriteFiscalePrincipale(),
-			                       MotifRattachement.DOMICILE,
-			                       motifOuverture,
-			                       GenreImpot.BENEFICE_CAPITAL,
-			                       warnings, suivis);
+		openForFiscalPrincipal(getDateDeCreation(),
+		                       getAutoriteFiscalePrincipale(),
+		                       MotifRattachement.DOMICILE,
+		                       motifOuverture,
+		                       GenreImpot.BENEFICE_CAPITAL,
+		                       warnings, suivis);
 
-			// Création du bouclement
-			createAddBouclement(getDateDeCreation(), isCreation(), suivis);
-			// Ajoute les for secondaires
-			adapteForsSecondairesPourEtablissementsVD(getEntreprise(), getDateDeCreation(), warnings, suivis);
+		// Création du bouclement
+		createAddBouclement(getDateDeCreation(), isCreation(), suivis);
+		// Ajoute les for secondaires
+		adapteForsSecondairesPourEtablissementsVD(getEntreprise(), getDateDeCreation(), warnings, suivis);
+
+		// SIFISC-19335 - mettre à l'état "a vérifier" les annoces de créations d'APM
+		warnings.addWarning(String.format("Vérification requise après la création de l'APM n°%s.", FormatNumeroHelper.numeroCTBToDisplay(getEntreprise().getNumero())));
 	}
 
 	@Override
