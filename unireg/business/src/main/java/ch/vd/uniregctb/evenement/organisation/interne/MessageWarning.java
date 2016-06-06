eeb6c2289e929c5bf484a43790520ebaec36c46c
Raphaël Marmier <raphael.marmier@vd.ch>
2016-06-06 16:37:05 +0000

    [SIPM] [Evenements Organisation] SIFISC-19483 - Gérer correctement le cas où on utilise les données Unireg pour le siège de départ. Y compris quand le tiers a en fait été créé à la main avec le bouton créer tiers associé.
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@121498 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

---
+++ unireg/business/src/main/java/ch/vd/uniregctb/evenement/organisation/interne/MessageWarning.java
@@ -0,0 +1,55 @@
+package ch.vd.uniregctb.evenement.organisation.interne;
+
+import org.apache.commons.lang3.StringUtils;
+
+import ch.vd.unireg.interfaces.organisation.data.Organisation;
+import ch.vd.uniregctb.evenement.organisation.EvenementOrganisation;
+import ch.vd.uniregctb.evenement.organisation.EvenementOrganisationContext;
+import ch.vd.uniregctb.evenement.organisation.EvenementOrganisationException;
+import ch.vd.uniregctb.evenement.organisation.EvenementOrganisationOptions;
+import ch.vd.uniregctb.evenement.organisation.audit.EvenementOrganisationErreurCollector;
+import ch.vd.uniregctb.evenement.organisation.audit.EvenementOrganisationSuiviCollector;
+import ch.vd.uniregctb.evenement.organisation.audit.EvenementOrganisationWarningCollector;
+import ch.vd.uniregctb.tiers.Entreprise;
+
+/**
+ * <p>
+ *     Evénement interne sans effet servant publier un message en tant que warning <strong>pendant</strong> l'exécution. Cet événement interne a pour conséquence
+ *     l'ajout d'une ligne dans les messages affichés à l'utilisateur et de provoquer un état "à vérifier"
+ * </p>
+ * <p>
+ *     Cette classe d'événement est utile pour communiquer avec l'utilisateur des informations importantes en lien avec
+ *     l'exécution des événements internes. En particulier pour communiquer des informations supplémentaires qui
+ *     one correspondent pas à des actions systèmes.
+ * </p>
+ * <p>
+ *     Les messages sont ajoutés au cours de l'étape d'exécution [handle()]. De cette manière, les messages sont affichés à leur place dans la séquence
+ *     des traitements.
+ * </p>
+ */
+public class MessageWarning extends EvenementOrganisationInterneDeTraitement {
+
+	private String warning = null;
+
+	public MessageWarning(EvenementOrganisation evenement, Organisation organisation, Entreprise entreprise, EvenementOrganisationContext context,
+	                      EvenementOrganisationOptions options, String warning) throws EvenementOrganisationException {
+		super(evenement, organisation, entreprise, context, options);
+		this.warning = warning;
+	}
+
+	@Override
+	public String describe() {
+		return null; // On ne veut pas de message descriptif sur cet événement qui n'en est pas un.
+	}
+
+	@Override
+	public void doHandle(EvenementOrganisationWarningCollector warnings, EvenementOrganisationSuiviCollector suivis) throws EvenementOrganisationException {
+		if (StringUtils.isNotBlank(warning)) {
+			warnings.addWarning(warning);
+		}
+	}
+
+	@Override
+	protected void validateSpecific(EvenementOrganisationErreurCollector erreurs, EvenementOrganisationWarningCollector warnings, EvenementOrganisationSuiviCollector suivis) throws EvenementOrganisationException {
+	}
+}
