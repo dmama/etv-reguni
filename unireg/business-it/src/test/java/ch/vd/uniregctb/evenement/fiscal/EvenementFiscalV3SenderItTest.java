8d016166e11e87bcda03b8fcbe4fc636122d83ba
Jacob Deker <jacob.deker@vd.ch>
2016-07-07 12:44:07 +0000

    Nouveau service /fiscalEvents sur le WS v7 (correction du build .... )
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@121690 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business-it/src/test/java/ch/vd/uniregctb/evenement/fiscal/EvenementFiscalV3SenderItTest.java
+++ unireg/business-it/src/test/java/ch/vd/uniregctb/evenement/fiscal/EvenementFiscalV3SenderItTest.java
@@ -11,9 +11,6 @@ import ch.vd.registre.base.date.RegDate;
 import ch.vd.technical.esb.jms.EsbJmsTemplate;
 import ch.vd.technical.esb.store.raft.RaftEsbStore;
 import ch.vd.unireg.interfaces.infra.mock.MockCommune;
-import ch.vd.unireg.xml.event.fiscal.v3.TypeEvenementFiscalDeclarationRappelable;
-import ch.vd.unireg.xml.event.fiscal.v3.TypeEvenementFiscalDeclarationSommable;
-import ch.vd.unireg.xml.event.fiscal.v3.TypeInformationComplementaire;
 import ch.vd.uniregctb.common.AuthenticationHelper;
 import ch.vd.uniregctb.evenement.EvenementTest;
 import ch.vd.uniregctb.tiers.ContribuableImpositionPersonnesPhysiques;
@@ -123,69 +120,6 @@ public class EvenementFiscalV3SenderItTest extends EvenementTest {
 		}
 		finally {
 			AuthenticationHelper.popPrincipal();
-		}
-	}
-
-	@Test
-	public void testInstanciateAllegementFiscal() throws Exception {
-		// on doit vérifier que types d'événement d'allègement fiscal sont acceptés par l'XSD des événements fiscaux v3
-		for (EvenementFiscalAllegementFiscal.TypeEvenementFiscalAllegement type : EvenementFiscalAllegementFiscal.TypeEvenementFiscalAllegement.values()) {
-			final ch.vd.unireg.xml.event.fiscal.v3.EvenementFiscalAllegementFiscal instance = EvenementFiscalV3SenderImpl.instanciate(type);
-			Assert.assertNotNull("type " + type + " inconnu dans la XSD des événements fiscaux v3", instance);
-		}
-	}
-
-	@Test
-	public void testTypeActionEvenementDeclarationSommable() throws Exception {
-		// on doit vérifier que types d'événement autour des déclarations sont acceptés par l'XSD des événements fiscaux v3
-		for (EvenementFiscalDeclarationSommable.TypeAction type : EvenementFiscalDeclarationSommable.TypeAction.values()) {
-			final TypeEvenementFiscalDeclarationSommable mapped = EvenementFiscalV3SenderImpl.mapType(type);
-			Assert.assertNotNull("type " + type + " inconnu dans la XSD des événements fiscaux v3", mapped);
-		}
-	}
-
-	@Test
-	public void testTypeActionEvenementDeclarationRappelable() throws Exception {
-		// on doit vérifier que types d'événement autour des déclarations sont acceptés par l'XSD des événements fiscaux v3
-		for (EvenementFiscalDeclarationRappelable.TypeAction type : EvenementFiscalDeclarationRappelable.TypeAction.values()) {
-			final TypeEvenementFiscalDeclarationRappelable mapped = EvenementFiscalV3SenderImpl.mapType(type);
-			Assert.assertNotNull("type " + type + " inconnu dans la XSD des événements fiscaux v3", mapped);
-		}
-	}
-
-	@Test
-	public void testInstanciateFor() throws Exception {
-		// on doit vérifier que types d'événement de fors sont acceptés par l'XSD des événements fiscaux v3
-		for (EvenementFiscalFor.TypeEvenementFiscalFor type : EvenementFiscalFor.TypeEvenementFiscalFor.values()) {
-			final ch.vd.unireg.xml.event.fiscal.v3.EvenementFiscalFor instance = EvenementFiscalV3SenderImpl.instanciate(type);
-			Assert.assertNotNull("type " + type + " inconnu dans la XSD des événements fiscaux v3", instance);
-		}
-	}
-
-	@Test
-	public void testTypeInformationComplementaire() throws Exception {
-		// on doit vérifier que types d'événement autour des informations complémentaires sont acceptés par l'XSD des événements fiscaux v3
-		for (EvenementFiscalInformationComplementaire.TypeInformationComplementaire type : EvenementFiscalInformationComplementaire.TypeInformationComplementaire.values()) {
-			final TypeInformationComplementaire mapped = EvenementFiscalV3SenderImpl.mapType(type);
-			Assert.assertNotNull("type " + type + " inconnu dans la XSD des événements fiscaux v3", mapped);
-		}
-	}
-
-	@Test
-	public void testInstanciateParente() throws Exception {
-		// on doit vérifier que types d'événement de parenté sont acceptés par l'XSD des événements fiscaux v3
-		for (EvenementFiscalParente.TypeEvenementFiscalParente type : EvenementFiscalParente.TypeEvenementFiscalParente.values()) {
-			final ch.vd.unireg.xml.event.fiscal.v3.EvenementFiscalParente instance = EvenementFiscalV3SenderImpl.instanciate(type);
-			Assert.assertNotNull("type " + type + " inconnu dans la XSD des événements fiscaux v3", instance);
-		}
-	}
-
-	@Test
-	public void testInstanciateRegimeFiscal() throws Exception {
-		// on doit vérifier que types d'événement autour des régimes fiscaux sont acceptés par l'XSD des événements fiscaux v3
-		for (EvenementFiscalRegimeFiscal.TypeEvenementFiscalRegime type : EvenementFiscalRegimeFiscal.TypeEvenementFiscalRegime.values()) {
-			final ch.vd.unireg.xml.event.fiscal.v3.EvenementFiscalRegimeFiscal instance = EvenementFiscalV3SenderImpl.instanciate(type);
-			Assert.assertNotNull("type " + type + " inconnu dans la XSD des événements fiscaux v3", instance);
 		}
 	}
 }
