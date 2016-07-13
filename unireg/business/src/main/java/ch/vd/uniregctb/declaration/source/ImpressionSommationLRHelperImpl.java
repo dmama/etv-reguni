41608ee4100e41f08a47a5c4ad60eeab772c6101
Jacob Deker <jacob.deker@vd.ch>
2016-07-13 09:25:05 +0000

    [SIFISC-17799] "Administration des impôts" devient "Administration cantonale des impôts"
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/branches/6.1@121761 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

--- unireg/business/src/main/java/ch/vd/uniregctb/declaration/source/ImpressionSommationLRHelperImpl.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/declaration/source/ImpressionSommationLRHelperImpl.java
@@ -387,7 +387,7 @@ public class ImpressionSommationLRHelperImpl extends EditiqueAbstractLegacyHelpe
 		bvrstd.setPrefixe(prefixeBVRST);
 		bvrstd.setLibImp(IMPOT_A_LA_SOURCE_MIN + ' ' + lr.getPeriode().getAnnee().toString());
 		bvrstd.setVersPourLigne1("Département des finances");
-		bvrstd.setVersPourLigne2("Administration des impôts");
+		bvrstd.setVersPourLigne2("Administration cantonale des impôts");
 		bvrstd.setVersPourLigne3("");
 		bvrstd.setVersPourNpaLoc("1014 Lausanne");
 		bvrstd.setLigneReference(noReference);
