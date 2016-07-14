fbaf103b982568b29e0094b910f821a429d6a214
Jacob Deker <jacob.deker@vd.ch>
2016-07-14 14:05:45 +0000

    [SIFISC-19841] Nouvel outil de génération de script SQL pour le rattrapage des mutations sur les liens mandataires en production (= dans SIMPA) depuis la MeP
    git-svn-id: https://svn.etat-de-vaud.ch/SVN_SOCLE/registre/unireg/trunk@121765 cb3efa0a-b8b0-9f43-a4c8-343d8b7e4362

---
+++ unireg/tools/src/main/java/ch/vd/uniregctb/rattrapage/simpa/mandataires/TypeTiers.java
@@ -0,0 +1,22 @@
+package ch.vd.uniregctb.rattrapage.simpa.mandataires;
+
+import java.text.ParseException;
+
+public enum TypeTiers {
+	INDIVIDU,
+	ENTREPRISE,
+	ETABLISSEMENT;
+
+	public static TypeTiers fromTypeMandataire(String str) throws ParseException {
+		switch (str) {
+		case "IND":
+			return TypeTiers.INDIVIDU;
+		case "ENT":
+			return TypeTiers.ENTREPRISE;
+		case "ETA":
+			return TypeTiers.ETABLISSEMENT;
+		default:
+			throw new ParseException("Valeur non-supportée pour un type de mandataire : '" + str + "'", 0);
+		}
+	}
+}
