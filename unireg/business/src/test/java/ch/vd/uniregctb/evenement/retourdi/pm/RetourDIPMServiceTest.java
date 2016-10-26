8d365d7735c35d41911c427354298e2301a4df8e
Jacob Deker <jacob.deker@vd.ch>
2016-10-26 10:14:00 +0200

    [SIFISC-21738] Ajouté l'effacement des numéros de téléphone de contact de l'entreprise à la réception de nouvelles données de contact depuis e-DIPM

--- unireg/business/src/test/java/ch/vd/uniregctb/evenement/retourdi/pm/RetourDIPMServiceTest.java
+++ unireg/business/src/test/java/ch/vd/uniregctb/evenement/retourdi/pm/RetourDIPMServiceTest.java
@@ -6447,4 +6447,312 @@ public class RetourDIPMServiceTest extends BusinessTest {
 			}
 		});
 	}
+
+	/**
+	 * [SIFISC-21738] Effacement du numéro de téléphone utilisé pour le contact en cas de changement dans les données du contact
+	 */
+	@Test
+	public void testEffacementNumeroTelephoneContact() throws Exception {
+
+		final int annee = 2015;
+		final RegDate dateDebutEntreprise = date(2009, 8, 1);
+		final RegDate dateQuittance = date(annee + 1, 5, 18);
+
+		final long id = doInNewTransactionAndSession(new TransactionCallback<Long>() {
+			@Override
+			public Long doInTransaction(TransactionStatus status) {
+				final Entreprise entreprise = addEntrepriseInconnueAuCivil();
+				addRaisonSociale(entreprise, dateDebutEntreprise, null, "Ma petite entreprise SARL");
+				addFormeJuridique(entreprise, dateDebutEntreprise, null, FormeJuridiqueEntreprise.SARL);
+				addRegimeFiscalVD(entreprise, dateDebutEntreprise, null, MockTypeRegimeFiscal.ORDINAIRE_PM);
+				addRegimeFiscalCH(entreprise, dateDebutEntreprise, null, MockTypeRegimeFiscal.ORDINAIRE_PM);
+				addBouclement(entreprise, dateDebutEntreprise, DayMonth.get(6, 30), 12);
+				addForPrincipal(entreprise, dateDebutEntreprise, MotifFor.DEBUT_EXPLOITATION, MockCommune.Echallens);
+
+				entreprise.setPersonneContact("Le Chef");
+				entreprise.setNumeroTelephonePortable("0789999999");
+				entreprise.setNumeroTelephonePrive("0219999999");
+				entreprise.setNumeroTelephoneProfessionnel("0213169999");
+				entreprise.setNumeroTelecopie("0213160000");
+
+				final PeriodeFiscale pf = addPeriodeFiscale(annee);
+				final ModeleDocument md = addModeleDocument(TypeDocument.DECLARATION_IMPOT_PM_BATCH, pf);
+				final CollectiviteAdministrative oipm = tiersService.getCollectiviteAdministrative(MockOfficeImpot.OID_PM.getNoColAdm());
+				final DeclarationImpotOrdinairePM di = addDeclarationImpot(entreprise, pf, date(annee - 1, 7, 1), date(annee, 6, 30), oipm, TypeContribuable.VAUDOIS_ORDINAIRE, md);
+				addEtatDeclarationEmise(di, date(annee, 7, 5));
+				addEtatDeclarationRetournee(di, dateQuittance);
+
+				return entreprise.getNumero();
+			}
+		});
+
+		// réception des données de retour
+		final AdresseRaisonSociale adresse = new AdresseRaisonSociale.StructureeSuisse(new DestinataireAdresse.Organisation(null, null, null, null, "M. Alfred Proutprout"), null, null, null, null, null, null, null, null, null, null);
+		final InformationsEntreprise infoEntreprise = new InformationsEntreprise(null, adresse, null, null, null, null);
+		final RetourDI retour = new RetourDI(id, annee, 1, infoEntreprise, null);
+
+		// traitement de ces données
+		doInNewTransactionAndSession(new TxCallbackWithoutResult() {
+			@Override
+			public void execute(TransactionStatus transactionStatus) throws Exception {
+				service.traiterRetour(retour, Collections.<String, String>emptyMap());
+			}
+		});
+
+		// vérification du résultat
+		doInNewTransactionAndSession(new TransactionCallbackWithoutResult() {
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				final Entreprise entreprise = (Entreprise) tiersDAO.get(id);
+				Assert.assertNotNull(entreprise);
+
+				Assert.assertEquals("M. Alfred Proutprout", entreprise.getPersonneContact());
+				Assert.assertNull(entreprise.getNumeroTelephonePortable());
+				Assert.assertNull(entreprise.getNumeroTelephonePrive());
+				Assert.assertNull(entreprise.getNumeroTelephoneProfessionnel());
+				Assert.assertNull(entreprise.getNumeroTelecopie());
+
+				final Set<Remarque> remarques = entreprise.getRemarques();
+				Assert.assertNotNull(remarques);
+				Assert.assertEquals(0, remarques.size());
+
+				final TacheCriteria tacheCriteria = new TacheCriteria();
+				tacheCriteria.setTypeTache(TypeTache.TacheControleDossier);
+				final List<Tache> tachesControle = tacheDAO.find(tacheCriteria);
+				Assert.assertNotNull(tachesControle);
+				Assert.assertEquals(0, tachesControle.size());
+			}
+		});
+	}
+
+	/**
+	 * [SIFISC-21738] Effacement du numéro de téléphone utilisé pour le contact en cas de changement dans les données du contact
+	 */
+	@Test
+	public void testEffacementNumeroTelephoneContactAjoutTitre() throws Exception {
+
+		final int annee = 2015;
+		final RegDate dateDebutEntreprise = date(2009, 8, 1);
+		final RegDate dateQuittance = date(annee + 1, 5, 18);
+
+		final long id = doInNewTransactionAndSession(new TransactionCallback<Long>() {
+			@Override
+			public Long doInTransaction(TransactionStatus status) {
+				final Entreprise entreprise = addEntrepriseInconnueAuCivil();
+				addRaisonSociale(entreprise, dateDebutEntreprise, null, "Ma petite entreprise SARL");
+				addFormeJuridique(entreprise, dateDebutEntreprise, null, FormeJuridiqueEntreprise.SARL);
+				addRegimeFiscalVD(entreprise, dateDebutEntreprise, null, MockTypeRegimeFiscal.ORDINAIRE_PM);
+				addRegimeFiscalCH(entreprise, dateDebutEntreprise, null, MockTypeRegimeFiscal.ORDINAIRE_PM);
+				addBouclement(entreprise, dateDebutEntreprise, DayMonth.get(6, 30), 12);
+				addForPrincipal(entreprise, dateDebutEntreprise, MotifFor.DEBUT_EXPLOITATION, MockCommune.Echallens);
+
+				entreprise.setPersonneContact("Le Chef");
+				entreprise.setNumeroTelephonePortable("0789999999");
+				entreprise.setNumeroTelephonePrive("0219999999");
+				entreprise.setNumeroTelephoneProfessionnel("0213169999");
+				entreprise.setNumeroTelecopie("0213160000");
+
+				final PeriodeFiscale pf = addPeriodeFiscale(annee);
+				final ModeleDocument md = addModeleDocument(TypeDocument.DECLARATION_IMPOT_PM_BATCH, pf);
+				final CollectiviteAdministrative oipm = tiersService.getCollectiviteAdministrative(MockOfficeImpot.OID_PM.getNoColAdm());
+				final DeclarationImpotOrdinairePM di = addDeclarationImpot(entreprise, pf, date(annee - 1, 7, 1), date(annee, 6, 30), oipm, TypeContribuable.VAUDOIS_ORDINAIRE, md);
+				addEtatDeclarationEmise(di, date(annee, 7, 5));
+				addEtatDeclarationRetournee(di, dateQuittance);
+
+				return entreprise.getNumero();
+			}
+		});
+
+		// réception des données de retour (juste ajout d'un titre sur le nom déjà connu)
+		final AdresseRaisonSociale adresse = new AdresseRaisonSociale.StructureeSuisse(new DestinataireAdresse.Organisation(null, null, null, null, "M. Le Chef"), null, null, null, null, null, null, null, null, null, null);
+		final InformationsEntreprise infoEntreprise = new InformationsEntreprise(null, adresse, null, null, null, null);
+		final RetourDI retour = new RetourDI(id, annee, 1, infoEntreprise, null);
+
+		// traitement de ces données
+		doInNewTransactionAndSession(new TxCallbackWithoutResult() {
+			@Override
+			public void execute(TransactionStatus transactionStatus) throws Exception {
+				service.traiterRetour(retour, Collections.<String, String>emptyMap());
+			}
+		});
+
+		// vérification du résultat
+		doInNewTransactionAndSession(new TransactionCallbackWithoutResult() {
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				final Entreprise entreprise = (Entreprise) tiersDAO.get(id);
+				Assert.assertNotNull(entreprise);
+
+				Assert.assertEquals("M. Le Chef", entreprise.getPersonneContact());
+				Assert.assertNull(entreprise.getNumeroTelephonePortable());
+				Assert.assertNull(entreprise.getNumeroTelephonePrive());
+				Assert.assertNull(entreprise.getNumeroTelephoneProfessionnel());
+				Assert.assertNull(entreprise.getNumeroTelecopie());
+
+				final Set<Remarque> remarques = entreprise.getRemarques();
+				Assert.assertNotNull(remarques);
+				Assert.assertEquals(0, remarques.size());
+
+				final TacheCriteria tacheCriteria = new TacheCriteria();
+				tacheCriteria.setTypeTache(TypeTache.TacheControleDossier);
+				final List<Tache> tachesControle = tacheDAO.find(tacheCriteria);
+				Assert.assertNotNull(tachesControle);
+				Assert.assertEquals(0, tachesControle.size());
+			}
+		});
+	}
+
+	/**
+	 * [SIFISC-21738] Non-effacement du numéro de téléphone utilisé pour le contact en cas de non-changement dans les données du contact
+	 */
+	@Test
+	public void testNonEffacementNumeroTelephoneContactSiPasChangementContact() throws Exception {
+
+		final int annee = 2015;
+		final RegDate dateDebutEntreprise = date(2009, 8, 1);
+		final RegDate dateQuittance = date(annee + 1, 5, 18);
+
+		final long id = doInNewTransactionAndSession(new TransactionCallback<Long>() {
+			@Override
+			public Long doInTransaction(TransactionStatus status) {
+				final Entreprise entreprise = addEntrepriseInconnueAuCivil();
+				addRaisonSociale(entreprise, dateDebutEntreprise, null, "Ma petite entreprise SARL");
+				addFormeJuridique(entreprise, dateDebutEntreprise, null, FormeJuridiqueEntreprise.SARL);
+				addRegimeFiscalVD(entreprise, dateDebutEntreprise, null, MockTypeRegimeFiscal.ORDINAIRE_PM);
+				addRegimeFiscalCH(entreprise, dateDebutEntreprise, null, MockTypeRegimeFiscal.ORDINAIRE_PM);
+				addBouclement(entreprise, dateDebutEntreprise, DayMonth.get(6, 30), 12);
+				addForPrincipal(entreprise, dateDebutEntreprise, MotifFor.DEBUT_EXPLOITATION, MockCommune.Echallens);
+
+				entreprise.setPersonneContact("Le Chef");
+				entreprise.setNumeroTelephonePortable("0789999999");
+				entreprise.setNumeroTelephonePrive("0219999999");
+				entreprise.setNumeroTelephoneProfessionnel("0213169999");
+				entreprise.setNumeroTelecopie("0213160000");
+
+				final PeriodeFiscale pf = addPeriodeFiscale(annee);
+				final ModeleDocument md = addModeleDocument(TypeDocument.DECLARATION_IMPOT_PM_BATCH, pf);
+				final CollectiviteAdministrative oipm = tiersService.getCollectiviteAdministrative(MockOfficeImpot.OID_PM.getNoColAdm());
+				final DeclarationImpotOrdinairePM di = addDeclarationImpot(entreprise, pf, date(annee - 1, 7, 1), date(annee, 6, 30), oipm, TypeContribuable.VAUDOIS_ORDINAIRE, md);
+				addEtatDeclarationEmise(di, date(annee, 7, 5));
+				addEtatDeclarationRetournee(di, dateQuittance);
+
+				return entreprise.getNumero();
+			}
+		});
+
+		// réception des données de retour
+		final AdresseRaisonSociale adresse = new AdresseRaisonSociale.StructureeSuisse(new DestinataireAdresse.Organisation(null, null, null, null, "Le Chef"), null, null, null, null, null, null, null, null, null, null);
+		final InformationsEntreprise infoEntreprise = new InformationsEntreprise(null, adresse, null, null, null, null);
+		final RetourDI retour = new RetourDI(id, annee, 1, infoEntreprise, null);
+
+		// traitement de ces données
+		doInNewTransactionAndSession(new TxCallbackWithoutResult() {
+			@Override
+			public void execute(TransactionStatus transactionStatus) throws Exception {
+				service.traiterRetour(retour, Collections.<String, String>emptyMap());
+			}
+		});
+
+		// vérification du résultat
+		doInNewTransactionAndSession(new TransactionCallbackWithoutResult() {
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				final Entreprise entreprise = (Entreprise) tiersDAO.get(id);
+				Assert.assertNotNull(entreprise);
+
+				Assert.assertEquals("Le Chef", entreprise.getPersonneContact());
+				Assert.assertEquals("0789999999", entreprise.getNumeroTelephonePortable());
+				Assert.assertEquals("0219999999", entreprise.getNumeroTelephonePrive());
+				Assert.assertEquals("0213169999", entreprise.getNumeroTelephoneProfessionnel());
+				Assert.assertEquals("0213160000", entreprise.getNumeroTelecopie());
+
+				final Set<Remarque> remarques = entreprise.getRemarques();
+				Assert.assertNotNull(remarques);
+				Assert.assertEquals(0, remarques.size());
+
+				final TacheCriteria tacheCriteria = new TacheCriteria();
+				tacheCriteria.setTypeTache(TypeTache.TacheControleDossier);
+				final List<Tache> tachesControle = tacheDAO.find(tacheCriteria);
+				Assert.assertNotNull(tachesControle);
+				Assert.assertEquals(0, tachesControle.size());
+			}
+		});
+	}
+
+	/**
+	 * [SIFISC-21738] Non-effacement du numéro de téléphone utilisé pour le contact en d'absence de données de contact
+	 */
+	@Test
+	public void testNonEffacementNumeroTelephoneContactAbsenceDonneesContact() throws Exception {
+
+		final int annee = 2015;
+		final RegDate dateDebutEntreprise = date(2009, 8, 1);
+		final RegDate dateQuittance = date(annee + 1, 5, 18);
+
+		final long id = doInNewTransactionAndSession(new TransactionCallback<Long>() {
+			@Override
+			public Long doInTransaction(TransactionStatus status) {
+				final Entreprise entreprise = addEntrepriseInconnueAuCivil();
+				addRaisonSociale(entreprise, dateDebutEntreprise, null, "Ma petite entreprise SARL");
+				addFormeJuridique(entreprise, dateDebutEntreprise, null, FormeJuridiqueEntreprise.SARL);
+				addRegimeFiscalVD(entreprise, dateDebutEntreprise, null, MockTypeRegimeFiscal.ORDINAIRE_PM);
+				addRegimeFiscalCH(entreprise, dateDebutEntreprise, null, MockTypeRegimeFiscal.ORDINAIRE_PM);
+				addBouclement(entreprise, dateDebutEntreprise, DayMonth.get(6, 30), 12);
+				addForPrincipal(entreprise, dateDebutEntreprise, MotifFor.DEBUT_EXPLOITATION, MockCommune.Echallens);
+
+				entreprise.setPersonneContact("Le Chef");
+				entreprise.setNumeroTelephonePortable("0789999999");
+				entreprise.setNumeroTelephonePrive("0219999999");
+				entreprise.setNumeroTelephoneProfessionnel("0213169999");
+				entreprise.setNumeroTelecopie("0213160000");
+
+				final PeriodeFiscale pf = addPeriodeFiscale(annee);
+				final ModeleDocument md = addModeleDocument(TypeDocument.DECLARATION_IMPOT_PM_BATCH, pf);
+				final CollectiviteAdministrative oipm = tiersService.getCollectiviteAdministrative(MockOfficeImpot.OID_PM.getNoColAdm());
+				final DeclarationImpotOrdinairePM di = addDeclarationImpot(entreprise, pf, date(annee - 1, 7, 1), date(annee, 6, 30), oipm, TypeContribuable.VAUDOIS_ORDINAIRE, md);
+				addEtatDeclarationEmise(di, date(annee, 7, 5));
+				addEtatDeclarationRetournee(di, dateQuittance);
+
+				return entreprise.getNumero();
+			}
+		});
+
+		// réception des données de retour
+		final AdresseRaisonSociale adresse = new AdresseRaisonSociale.StructureeSuisse(new DestinataireAdresse.Organisation(null, null, null, null, null), null, null, null, null, null, null, null, null, null, null);
+		final InformationsEntreprise infoEntreprise = new InformationsEntreprise(null, adresse, null, null, null, null);
+		final RetourDI retour = new RetourDI(id, annee, 1, infoEntreprise, null);
+
+		// traitement de ces données
+		doInNewTransactionAndSession(new TxCallbackWithoutResult() {
+			@Override
+			public void execute(TransactionStatus transactionStatus) throws Exception {
+				service.traiterRetour(retour, Collections.<String, String>emptyMap());
+			}
+		});
+
+		// vérification du résultat
+		doInNewTransactionAndSession(new TransactionCallbackWithoutResult() {
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				final Entreprise entreprise = (Entreprise) tiersDAO.get(id);
+				Assert.assertNotNull(entreprise);
+
+				Assert.assertEquals("Le Chef", entreprise.getPersonneContact());
+				Assert.assertEquals("0789999999", entreprise.getNumeroTelephonePortable());
+				Assert.assertEquals("0219999999", entreprise.getNumeroTelephonePrive());
+				Assert.assertEquals("0213169999", entreprise.getNumeroTelephoneProfessionnel());
+				Assert.assertEquals("0213160000", entreprise.getNumeroTelecopie());
+
+				final Set<Remarque> remarques = entreprise.getRemarques();
+				Assert.assertNotNull(remarques);
+				Assert.assertEquals(0, remarques.size());
+
+				final TacheCriteria tacheCriteria = new TacheCriteria();
+				tacheCriteria.setTypeTache(TypeTache.TacheControleDossier);
+				final List<Tache> tachesControle = tacheDAO.find(tacheCriteria);
+				Assert.assertNotNull(tachesControle);
+				Assert.assertEquals(0, tachesControle.size());
+			}
+		});
+	}
 }
