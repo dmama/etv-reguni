82c4f2ac325ae1fc5079add4475ec64bf2865059
Manuel Siggen <manuel.siggen@vd.ch>
2016-10-28 11:06:38 +0200

    Déplacé les scripts SQL des anciennes versions (< 6) dans le répertoire 'old', pour faciliter la lecture du répertoire.

--- unireg/base/sql/3.1/unireg-schema_2.7.5_3.1_upgrade.sql
+++
@@ -1,2 +0,0 @@
-alter table TIERS add (MAJORITE_TRAITEE decimal(1));
-alter table DECLARATION add (SANS_RAPPEL decimal(1));
---
+++ unireg/base/sql/old/3.1/unireg-schema_2.7.5_3.1_upgrade.sql
@@ -0,0 +1,2 @@
+alter table TIERS add (MAJORITE_TRAITEE decimal(1));
+alter table DECLARATION add (SANS_RAPPEL decimal(1));
