225c8314296f93eeaeaa9f909e0009959d030d78
Jacob Deker <jacob.deker@vd.ch>
2016-11-10 11:46:49 +0100

    [SIFISC-22006] Enlevé l'utilisation du numéro IDE mandataire fourni dans la DI PM en arrivée

--- unireg/business/src/test/java/ch/vd/uniregctb/evenement/retourdi/pm/V1HandlerTest.java
+++ unireg/business/src/test/java/ch/vd/uniregctb/evenement/retourdi/pm/V1HandlerTest.java
@@ -1,18 +1,29 @@
 package ch.vd.uniregctb.evenement.retourdi.pm;
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Assert;
 import org.junit.Test;
 
+import ch.vd.registre.base.date.RegDate;
+import ch.vd.unireg.interfaces.common.Adresse;
 import ch.vd.unireg.interfaces.infra.mock.MockLocalite;
+import ch.vd.unireg.interfaces.infra.mock.MockRue;
 import ch.vd.unireg.xml.event.taxation.ibc.v1.AddressInformation;
+import ch.vd.unireg.xml.event.taxation.ibc.v1.InformationMandataire;
 import ch.vd.unireg.xml.event.taxation.ibc.v1.InformationPersonneMoraleModifiee;
 import ch.vd.unireg.xml.event.taxation.ibc.v1.MailAddress;
 import ch.vd.unireg.xml.event.taxation.ibc.v1.OrganisationMailAddressInfo;
+import ch.vd.unireg.xml.event.taxation.ibc.v1.TypAdresse;
+import ch.vd.unireg.xml.event.taxation.ibc.v1.TypNumeroIdeAttr;
+import ch.vd.unireg.xml.event.taxation.ibc.v1.TypTxtMax40Attr;
 import ch.vd.uniregctb.common.BusinessTest;
+import ch.vd.uniregctb.common.NumeroIDEHelper;
 import ch.vd.uniregctb.interfaces.service.ServiceInfrastructureService;
 import ch.vd.uniregctb.jms.EsbBusinessException;
 
@@ -57,5 +68,53 @@ public class V1HandlerTest extends BusinessTest {
 		Assert.assertEquals("Mme Albertine Martin", infoEntreprise.getAdresseCourrier().getContact());
 		Assert.assertNotNull(infoEntreprise.getAdresseCourrier().getDestinataire());
 		Assert.assertEquals("Mme Albertine Martin", infoEntreprise.getAdresseCourrier().getDestinataire().getContact());
+	}
+
+	@Test
+	public void testNonPriseEnCompteNumeroIDEMandataireAdresseStructuree() throws Exception {
+		final String ide = "CHE-116.311.185";
+		Assert.assertTrue(NumeroIDEHelper.isValid(ide));
+
+		final InformationMandataire info = new InformationMandataire();
+		final OrganisationMailAddressInfo organisation = new OrganisationMailAddressInfo(new JAXBElement<>(new QName("IDE-BIDON"), String.class, ide), "Chez Bernard", null, null, null, null, null);
+		final AddressInformation addressInformation = new AddressInformation(null, null, null, "Avenue de la Gare", "12", null, null, "Lausanne", null, 1003L, null, null, null, null);
+		final MailAddress address = new MailAddress(organisation, null, addressInformation, Boolean.TRUE);
+		info.setAdresseCourrierStructuree(address);
+		final InformationsMandataire infoExtraite = handler.extractInformationsMandataire(info);
+		Assert.assertNotNull(infoExtraite);
+		Assert.assertNull(infoExtraite.getIdeMandataire());             // l'IDE n'est pas extrait, même valide
+		Assert.assertNotNull(infoExtraite.getAdresse());
+
+		// mais l'adresse est bien prise en compte
+		final Pair<String, Adresse> split = infoExtraite.getAdresse().split(serviceInfra, tiersService, RegDate.get());
+		Assert.assertNotNull(split);
+		Assert.assertEquals("Chez Bernard", split.getLeft());
+		Assert.assertEquals("Avenue de la Gare", split.getRight().getRue());
+	}
+
+	@Test
+	public void testNonPriseEnCompteNumeroIDEMandataireAdresseNonStructuree() throws Exception {
+		final String ide = "CHE-116.311.185";
+		Assert.assertTrue(NumeroIDEHelper.isValid(ide));
+
+    	final InformationMandataire info = new InformationMandataire();
+		final JAXBElement<TypNumeroIdeAttr> ideMandataire = new JAXBElement<>(new QName("IDE_BIDON"), TypNumeroIdeAttr.class, new TypNumeroIdeAttr(ide, true, Boolean.FALSE));
+		final InformationMandataire.AdresseCourrier adresseCourrier = new InformationMandataire.AdresseCourrier(ideMandataire, new TypAdresse(toTxtMax40("Chez Bernard"), toTxtMax40("Avenue de la Gare 12"), null, null, null, toTxtMax40("1003"), toTxtMax40("Lausanne")));
+		info.setAdresseCourrier(adresseCourrier);
+
+		final InformationsMandataire infoExtraite = handler.extractInformationsMandataire(info);
+		Assert.assertNotNull(infoExtraite);
+		Assert.assertNull(infoExtraite.getIdeMandataire());             // l'IDE n'est pas extrait, même valide
+		Assert.assertNotNull(infoExtraite.getAdresse());
+
+		// mais l'adresse est bien prise en compte
+		final Pair<String, Adresse> split = infoExtraite.getAdresse().split(serviceInfra, tiersService, RegDate.get());
+		Assert.assertNotNull(split);
+		Assert.assertEquals("Chez Bernard", split.getLeft());
+		Assert.assertEquals(MockRue.Lausanne.AvenueDeLaGare.getNoRue(), split.getRight().getNumeroRue());
+	}
+
+	private static JAXBElement<TypTxtMax40Attr> toTxtMax40(String string) {
+		return new JAXBElement<>(new QName("STR_BIDON"), TypTxtMax40Attr.class, null, new TypTxtMax40Attr(string, true, Boolean.FALSE));
 	}
 }
