540ab7ddb4cbed8bd70c453822b302c5930c009d
Raphaël Marmier <raphael.marmier@vd.ch>
2016-11-16 13:07:18 +0100

    [Annonces IDE] SIFISC-22086 - Sauver le numéro IDE provisoire dans le tiers à la réception du rapport d'annonce IDE.

--- unireg/business/src/test/java/ch/vd/uniregctb/evenement/ide/ReponseIDEProcessorTest.java
+++ unireg/business/src/test/java/ch/vd/uniregctb/evenement/ide/ReponseIDEProcessorTest.java
@@ -49,6 +49,101 @@ public class ReponseIDEProcessorTest extends BusinessTest {
 	}
 
 	@Test
+	public void testNouvelleEntrepriseTransmisIDE() throws Exception {
+		/*
+			Une nouvelle entité vient d'être créée dans Unireg. Une annonce de création est partie vers le registre IDE, et on reçoit la quittance de transmission avec
+			un identifiant IDE provisoire.
+		 */
+
+		// Création de l'entreprise
+		final Long noEntreprise = doInNewTransactionAndSession(new TransactionCallback<Long>() {
+			@Override
+			public Long doInTransaction(TransactionStatus transactionStatus) {
+				Entreprise entreprise = addEntrepriseInconnueAuCivil();
+
+				addRaisonSocialeFiscaleEntreprise(entreprise, date(2016, 9, 5), null, "Syntruc Asso");
+				addFormeJuridique(entreprise, date(2016, 9, 5), null, FormeJuridiqueEntreprise.ASSOCIATION);
+
+				entreprise.changeSecteurActivite("Fabrication d'objets synthétiques");
+
+				final AdresseSuisse adresseSuisse = addAdresseSuisse(entreprise, TypeAdresseTiers.DOMICILE, date(2016, 9, 5), null, MockRue.Renens.QuatorzeAvril);
+				adresseSuisse.setNumeroMaison("1");
+
+				return entreprise.getNumero();
+			}
+		});
+
+		// Création de l'établissement
+		final Long noEtablissement = doInNewTransactionAndSession(new TransactionCallback<Long>() {
+			@Override
+			public Long doInTransaction(TransactionStatus transactionStatus) {
+
+				Entreprise entreprise = (Entreprise) tiersDAO.get(noEntreprise);
+
+				Etablissement etablissement = addEtablissement();
+				addDomicileEtablissement(etablissement, date(2016, 9, 5), null, MockCommune.Renens);
+				addActiviteEconomique(entreprise, etablissement, date(2016, 9, 5), null, true);
+
+				return etablissement.getNumero();
+			}
+		});
+
+		// Ajout de la référence d'annonce
+		final Long idReferenceAnnonce = doInNewTransactionAndSession(new TransactionCallback<Long>() {
+			@Override
+			public Long doInTransaction(TransactionStatus transactionStatus) {
+
+				Etablissement etablissement = (Etablissement) tiersDAO.get(noEtablissement);
+
+				final ReferenceAnnonceIDE refAnnonce = addReferenceAnnonceIDE("test_business_id", etablissement);
+
+				return refAnnonce.getId();
+			}
+		});
+
+		// Validation
+		final AnnonceIDE annonce =
+				RCEntAnnonceIDEHelper.createAnnonceIDE(idReferenceAnnonce, TypeAnnonce.CREATION, DateHelper.getDateTime(2016, 9, 5, 11, 0, 0), null, null, TypeDeSite.ETABLISSEMENT_PRINCIPAL, null, null,
+				                                       new NumeroIDE("CHE111111114"), null, null, null, null, null,
+				                                       "Syntruc Asso", null, FormeLegale.N_0109_ASSOCIATION, "Fabrication d'objets synthétiques",
+				                                       RCEntAnnonceIDEHelper
+						                                       .createAdresseAnnonceIDERCEnt(MockRue.Renens.QuatorzeAvril.getDesignationCourrier(), "1", null, MockLocalite.Renens.getNPA(), MockLocalite.Renens.getNoOrdre(), MockLocalite.Renens.getNom(), MockPays.Suisse.getNoOfsEtatSouverain(),
+						                                                                     MockPays.Suisse.getCodeIso2(), MockPays.Suisse.getNomCourt(), null,
+						                                                                     null, null),
+				                                       new AnnonceIDEData.StatutImpl(StatutAnnonce.TRANSMIS, DateHelper.getDateTime(2016, 9, 5, 12, 0, 0), null), RCEntAnnonceIDEHelper.SERVICE_IDE_UNIREG);
+
+		// Traiter la quittance en réponse
+		doInNewTransactionAndSession(new TransactionCallbackWithoutResult() {
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+				try {
+					reponseIDEProcessor.traiterReponseAnnonceIDE(annonce);
+				}
+				catch (ReponseIDEProcessorException e) {
+					fail(String.format("Le traitement de l'annonce a rencontré un problème inattendu: %s", e.getMessage()));
+				}
+			}
+
+		});
+
+		// Vérification du résultat
+		doInNewTransactionAndSession(new TransactionCallbackWithoutResult() {
+			@Override
+			public void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+				final Entreprise entreprise = (Entreprise) tiersDAO.get(noEntreprise);
+				final Set<IdentificationEntreprise> listIdentEntreprise = entreprise.getIdentificationsEntreprise();
+				assertNotNull(listIdentEntreprise);
+				assertEquals("CHE111111114", listIdentEntreprise.iterator().next().getNumeroIde());
+
+				final Etablissement etablissementPrincipal = tiersService.getEtablissementPrincipal(entreprise, date(2016, 9, 5));
+				final Set<IdentificationEntreprise> listIdentEtablissementPrincipal = etablissementPrincipal.getIdentificationsEntreprise();
+				assertNotNull(listIdentEtablissementPrincipal);
+				assertEquals("CHE111111114", listIdentEtablissementPrincipal.iterator().next().getNumeroIde());
+			}
+		});
+	}
+
+	@Test
 	public void testNouvelleEntrepriseAccepteeIDE() throws Exception {
 		/*
 			Une nouvelle entité vient d'être créée dans Unireg. Une annonce de création est partie vers le registre IDE, et on reçoit la quittance, positive.
