cecc272f0109a757dcf1953069785043ebb8423e
Manuel Siggen <manuel.siggen@vd.ch>
2016-11-30 08:30:40 +0100

    [SIFISC-20372] Ajouté le paramètre CONTINUE_WITH_IDENTIFICATION_JOB qui manquait pour redémarrer le job de traitement des mutations du registre foncier.

--- unireg/business/src/main/java/ch/vd/uniregctb/registrefoncier/RegistreFoncierServiceImpl.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/registrefoncier/RegistreFoncierServiceImpl.java
@@ -97,6 +97,7 @@ public class RegistreFoncierServiceImpl implements RegistreFoncierService {
 		final HashMap<String, Object> params = new HashMap<>();
 		params.put(TraiterMutationsRFJob.ID, importId);
 		params.put(TraiterMutationsRFJob.NB_THREADS, 8);
+		params.put(TraiterMutationsRFJob.CONTINUE_WITH_IDENTIFICATION_JOB, true);
 		batchScheduler.startJob(TraiterMutationsRFJob.NAME, params);
 	}
 
