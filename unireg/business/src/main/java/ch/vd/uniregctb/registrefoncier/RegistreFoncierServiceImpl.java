994ecd5782cbcaaa2374b92dd1a931e584ad6653
Manuel Siggen <manuel.siggen@vd.ch>
2017-01-24 14:46:45 +0100

    [SIFISC-20373] Implémenté la nouvelle règle de détermination des droits en fonction des périodes de validité des rapprochements entre tiers Unireg et tiers RF.

--- unireg/business/src/main/java/ch/vd/uniregctb/registrefoncier/RegistreFoncierServiceImpl.java
+++ unireg/business/src/main/java/ch/vd/uniregctb/registrefoncier/RegistreFoncierServiceImpl.java
@@ -3,13 +3,15 @@ package ch.vd.uniregctb.registrefoncier;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import ch.vd.registre.base.date.DateRangeComparator;
+import ch.vd.registre.base.date.DateRangeHelper;
 import ch.vd.unireg.interfaces.infra.data.ApplicationFiscale;
+import ch.vd.uniregctb.common.Annulable;
 import ch.vd.uniregctb.common.ObjectNotFoundException;
 import ch.vd.uniregctb.interfaces.service.ServiceInfrastructureService;
 import ch.vd.uniregctb.registrefoncier.dao.AyantDroitRFDAO;
@@ -44,11 +46,46 @@ public class RegistreFoncierServiceImpl implements RegistreFoncierService {
 	@Override
 	public List<DroitRF> getDroitsForCtb(@NotNull Contribuable ctb) {
 		return ctb.getRapprochementsRF().stream()
-				.filter(r -> r.isValidAt(null))         // on ne prend que les rapprochements valides
-				.map(RapprochementRF::getTiersRF)       // on général, il n'y a qu'un tiers RF, mais le modèle permet d'en avoir plusieurs
-				.flatMap(r -> r.getDroits().stream())   // si on a plusieurs tiers rapprochés, on prend l'ensemble des droits
-				.sorted(new DateRangeComparator<>(DateRangeComparator.CompareOrder.ASCENDING))
+				.filter(Annulable::isNotAnnule)                         // on ignore les rapprochements annulés
+				.flatMap(RegistreFoncierServiceImpl::getDroitsValides)  // on demande les droits valides pour le rapprochement
+				.sorted(DroitRF::compareTo)                             // on trie les droits pour garder un ordre constant entre chaque appel
 				.collect(Collectors.toList());
+	}
+
+	/**
+	 * Cette méthode calcule et retourne les droits valides pour un rapprochement déterminé.
+	 * <p/>
+	 * Selon la spécification SCU-Unireg-RFI-Mettre_a_disposition_immeuble.doc, version 0.4, chapitre 3.1.2,
+	 * les droits valides pour un rapprochement donné sont les droits du tiers RF correspondant qui
+	 * <b>intersectent</b> la période de validité du rapprochement:
+	 * <p>
+	 * <pre>
+	 *  Contribuable              |---------------------------------------|
+	 *  Rapprochement tiers RF 1  |-------------------|
+	 *  Rapprochement tiers RF 2                      |-------------------|
+	 *  Droits tiers RF 1            |--------A----------|
+	 *                                                       |---B---|
+	 *                                  |----C---|
+	 *  Droits tiers RF 2                               |--------D-------|
+	 *                            |--------E-------|
+	 *                                  |-------F--------|
+	 *
+	 *  ----------------------------------------------------------------------
+	 *
+	 *  Droits contribuables
+	 *                               |--------A----------|
+	 *                                  |----C---|
+	 *                                                  |--------D-------|
+	 *                                  |-------F--------|
+	 * </pre>
+	 *
+	 * @param rapprochement un rapprochement entre un contribuable et un tiers RF
+	 * @return les liste des droits valides pour le contribuable
+	 */
+	private static Stream<DroitRF> getDroitsValides(RapprochementRF rapprochement) {
+		return rapprochement.getTiersRF().getDroits().stream()
+				.filter(Annulable::isNotAnnule)
+				.filter(d -> DateRangeHelper.intersect(d, rapprochement));
 	}
 
 	@Nullable
--- unireg/business/src/test/java/ch/vd/uniregctb/registrefoncier/RegistreFoncierServiceTest.java
+++ unireg/business/src/test/java/ch/vd/uniregctb/registrefoncier/RegistreFoncierServiceTest.java
@@ -130,6 +130,172 @@ public class RegistreFoncierServiceTest extends BusinessTest {
 	}
 
 	/**
+	 * Ce test vérifie que la méthode getDroitsForCtb fonctionne dans le cas où un contribuable a été rapproché successivement à deux tiers RF.
+	 */
+	@Test
+	public void testGetDroitsForCtbMultipleRapprochements() throws Exception {
+
+		final long noIndividu = 481548L;
+		final RegDate dateNaissance = date(1970, 7, 2);
+
+		// mise en place civile
+		serviceCivil.setUp(new MockServiceCivil() {
+			@Override
+			protected void init() {
+				final MockIndividu individu = addIndividu(noIndividu, date(1970, 7, 2), "Charles", "Widmer", Sexe.MASCULIN);
+				addAdresse(individu, TypeAdresseCivil.PRINCIPALE, MockRue.CossonayVille.AvenueDuFuniculaire, null, dateNaissance, null);
+			}
+		});
+
+		// mise en place fiscale
+		final long pp = doInNewTransaction(status -> {
+			final PersonnePhysique ctb = tiersService.createNonHabitantFromIndividu(noIndividu);
+			return ctb.getNumero();
+		});
+
+		// mise en place foncière
+		doInNewTransaction(status -> {
+
+			// un tiers RF avec deux immeubles
+			final CommuneRF laSarraz = addCommuneRF(61, "La Sarraz", 5498);
+			final CommuneRF gland = addCommuneRF(242, "Gland", 5721);
+			final BienFondRF immeuble0 = addBienFondRF("01faeee", "some egrid", laSarraz, 579);
+			final BienFondRF immeuble1 = addBienFondRF("02faeee", "some egrid", gland, 4298);
+			final BienFondRF immeuble2 = addBienFondRF("03faeee", "some egrid", laSarraz, 580);
+			final BienFondRF immeuble3 = addBienFondRF("04faeee", "some egrid", gland, 4299);
+			final BienFondRF immeuble4 = addBienFondRF("05faeee", "some egrid", laSarraz, 581);
+			final BienFondRF immeuble5 = addBienFondRF("06faeee", "some egrid", gland, 4300);
+
+			final PersonnePhysique ctb = (PersonnePhysique) tiersDAO.get(pp);
+
+
+			final PersonnePhysiqueRF tiersRF1 = addPersonnePhysiqueRF("78838e838ca92", "Charles-Jean", "Widmer", date(1970, 1, 2));
+			addRapprochementRF(ctb, tiersRF1, RegDate.get(1997, 10, 7), RegDate.get(2005, 12, 31), TypeRapprochementRF.MANUEL);
+
+			addDroitPropriete(tiersRF1, immeuble0, null,
+			                  GenrePropriete.INDIVIDUELLE, new Fraction(1, 1),
+			                  RegDate.get(2004, 5, 21), RegDate.get(2004, 5, 21), null, "Achat", null,
+			                  new IdentifiantAffaireRF(123, 2004, 202, 3), "48390a0e044");
+
+			addDroitPropriete(tiersRF1, immeuble1, null,
+			                  GenrePropriete.INDIVIDUELLE, new Fraction(1, 1),
+			                  RegDate.get(2008, 3, 1), RegDate.get(2008, 3, 1), null, "Achat", null,
+			                  new IdentifiantAffaireRF(123, 2008, 2, 23), "4782389c8e");
+
+			addDroitPropriete(tiersRF1, immeuble2, null,
+			                  GenrePropriete.COPROPRIETE, new Fraction(1, 3),
+			                  RegDate.get(1997, 10, 7), RegDate.get(1997, 7, 2), RegDate.get(2002, 2, 23), "Achat", "Vente",
+			                  new IdentifiantAffaireRF(23, 1997, 13, 0), "47e7d7e773");
+
+			final PersonnePhysiqueRF tiersRF2 = addPersonnePhysiqueRF("38383830ae3ff", "Charles", "Widmer", date(1970, 7, 2));
+			addRapprochementRF(ctb, tiersRF2, RegDate.get(2006, 1, 1), null, TypeRapprochementRF.MANUEL);
+
+			addDroitPropriete(tiersRF2, immeuble3, null,
+			                  GenrePropriete.INDIVIDUELLE, new Fraction(1, 1),
+			                  RegDate.get(1960, 2, 7), RegDate.get(1960, 2, 7), RegDate.get(1994, 4, 5), "Achat", "Expropriation",
+			                  new IdentifiantAffaireRF(123, 1960, 4, 3), "48392345234234");
+
+			addDroitPropriete(tiersRF2, immeuble4, null,
+			                  GenrePropriete.INDIVIDUELLE, new Fraction(1, 1),
+			                  RegDate.get(2003, 5, 1), RegDate.get(2003, 5, 1), null, "Achat", "Vente",
+			                  new IdentifiantAffaireRF(123, 2003, 22, 7), "834838c");
+
+			addDroitPropriete(tiersRF2, immeuble5, null,
+			                  GenrePropriete.COPROPRIETE, new Fraction(1, 3),
+			                  RegDate.get(2010, 6, 6), RegDate.get(2010, 6, 6), null, "Achat", null,
+			                  new IdentifiantAffaireRF(23, 2010, 33, 30), "c83839e");
+
+			return null;
+		});
+
+		// appel du service
+		doInNewTransactionAndSession(new TxCallbackWithoutResult() {
+			@Override
+			public void execute(TransactionStatus transactionStatus) throws Exception {
+				final PersonnePhysique ctb = (PersonnePhysique) tiersDAO.get(pp);
+				assertNotNull(ctb);
+
+				// Tiers RF 1 :
+				// -------------
+				// Rapprochement         1997-10-07 |=============================| 2005-12-31
+				// Droit A                                         2004-05-21 |------------------------------->
+				// Droit B                                                      2008-03-01 |------------------>
+				// Droit C               1997-10-07 |----------| 2002-02-23
+				//
+				// Tiers RF 2 :
+				// -------------
+				// Rapprochement                                       2006-01-01 |===========================>
+				// Droit D          1960-02-07 |-----| 1994-04-05
+				// Droit E                                    2003-05-01 |------------------------------------>
+				// Droit F                                                                  2010-06-06 |------>
+				//
+				// Droits du contribuable
+				// ---------------------
+				// Droit A                                         2004-05-21 |------------------------------->
+				// Droit C               1997-10-07 |----------| 2002-02-23
+				// Droit E                                    2003-05-01 |------------------------------------>
+				// Droit F                                                                  2010-06-06 |------>
+				//
+
+				final List<DroitRF> droits = serviceRF.getDroitsForCtb(ctb);
+				assertEquals(4, droits.size());
+
+				final DroitProprietePersonnePhysiqueRF droit0 = (DroitProprietePersonnePhysiqueRF) droits.get(0);
+				assertNull(droit0.getCommunaute());
+				assertEquals(GenrePropriete.COPROPRIETE, droit0.getRegime());
+				assertEquals(new Fraction(1, 3), droit0.getPart());
+				assertEquals(RegDate.get(1997, 10, 7), droit0.getDateDebut());
+				assertEquals(RegDate.get(1997, 7, 2), droit0.getDateDebutOfficielle());
+				assertEquals(RegDate.get(2002, 2, 23), droit0.getDateFin());
+				assertEquals("Achat", droit0.getMotifDebut());
+				assertEquals("Vente", droit0.getMotifFin());
+				assertEquals(new IdentifiantAffaireRF(23, 1997, 13, 0), droit0.getNumeroAffaire());
+				assertEquals("47e7d7e773", droit0.getMasterIdRF());
+				assertEquals("03faeee", droit0.getImmeuble().getIdRF());
+
+				final DroitProprietePersonnePhysiqueRF droit1 = (DroitProprietePersonnePhysiqueRF) droits.get(1);
+				assertNull(droit1.getCommunaute());
+				assertEquals(GenrePropriete.INDIVIDUELLE, droit1.getRegime());
+				assertEquals(new Fraction(1, 1), droit1.getPart());
+				assertEquals(RegDate.get(2003, 5, 1), droit1.getDateDebut());
+				assertEquals(RegDate.get(2003, 5, 1), droit1.getDateDebutOfficielle());
+				assertNull(droit1.getDateFin());
+				assertEquals("Achat", droit1.getMotifDebut());
+				assertEquals("Vente", droit1.getMotifFin());
+				assertEquals(new IdentifiantAffaireRF(123, 2003, 22, 7), droit1.getNumeroAffaire());
+				assertEquals("834838c", droit1.getMasterIdRF());
+				assertEquals("05faeee", droit1.getImmeuble().getIdRF());
+
+				final DroitProprietePersonnePhysiqueRF droit2 = (DroitProprietePersonnePhysiqueRF) droits.get(2);
+				assertNull(droit2.getCommunaute());
+				assertEquals(GenrePropriete.INDIVIDUELLE, droit2.getRegime());
+				assertEquals(new Fraction(1, 1), droit2.getPart());
+				assertEquals(RegDate.get(2004, 5, 21), droit2.getDateDebut());
+				assertEquals(RegDate.get(2004, 5, 21), droit2.getDateDebutOfficielle());
+				assertNull(droit2.getDateFin());
+				assertEquals("Achat", droit2.getMotifDebut());
+				assertNull(droit2.getMotifFin());
+				assertEquals(new IdentifiantAffaireRF(123, 2004, 202, 3), droit2.getNumeroAffaire());
+				assertEquals("48390a0e044", droit2.getMasterIdRF());
+				assertEquals("01faeee", droit2.getImmeuble().getIdRF());
+
+				final DroitProprietePersonnePhysiqueRF droit3 = (DroitProprietePersonnePhysiqueRF) droits.get(3);
+				assertNull(droit3.getCommunaute());
+				assertEquals(GenrePropriete.COPROPRIETE, droit3.getRegime());
+				assertEquals(new Fraction(1, 3), droit3.getPart());
+				assertEquals(RegDate.get(2010, 6, 6), droit3.getDateDebut());
+				assertEquals(RegDate.get(2010, 6, 6), droit3.getDateDebutOfficielle());
+				assertNull(droit3.getDateFin());
+				assertEquals("Achat", droit3.getMotifDebut());
+				assertNull(droit3.getMotifFin());
+				assertEquals(new IdentifiantAffaireRF(23, 2010, 33, 30), droit3.getNumeroAffaire());
+				assertEquals("c83839e", droit3.getMasterIdRF());
+				assertEquals("06faeee", droit3.getImmeuble().getIdRF());
+			}
+		});
+	}
+
+	/**
 	 * [SIFISC-20373] Ce test vérifie que la résolution des URLs vers Capitastra fonctionne correctement.
 	 */
 	@Test
