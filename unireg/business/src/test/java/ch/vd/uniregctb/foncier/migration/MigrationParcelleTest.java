e3d0ed7d0f62eed9af53605d0c5403fd8bbe4675
Jacob Deker <jacob.deker@vd.ch>
2017-02-07 08:32:19 +0100

    [SIFISC-23189] Retour arrière sur le traitement du slash

--- unireg/business/src/test/java/ch/vd/uniregctb/foncier/migration/MigrationParcelleTest.java
+++ unireg/business/src/test/java/ch/vd/uniregctb/foncier/migration/MigrationParcelleTest.java
@@ -1,16 +1,15 @@
 package ch.vd.uniregctb.foncier.migration;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class MigrationParcelleTest {
 
 	/**
-	 * Ce test vérifie que le numéro de base de la parcelle est bien utilisé si le numéro de parcelle manque. Version "Tiret"
+	 * Ce test vérifie que le numéro de base de la parcelle est bien utilisé si le numéro de parcelle manque.
 	 */
 	@Test
-	public void testConstructeurBaseParcelleTirets() throws Exception {
+	public void testConstructeurBaseParcelle() throws Exception {
 		assertParcelle(123, null, null, null, new MigrationParcelle("123", null, null));
 		assertParcelle(123, 34, null, null, new MigrationParcelle("123-34", null, null));
 		assertParcelle(123, null, null, null, new MigrationParcelle("123", null, "34"));
@@ -19,22 +18,10 @@ public class MigrationParcelleTest {
 	}
 
 	/**
-	 * Ce test vérifie que le numéro de base de la parcelle est bien utilisé si le numéro de parcelle manque. Version "Slash"
+	 * Ce test vérifie que le numéro de base de la parcelle est ignoré si le numéro de parcelle est spécifié.
 	 */
 	@Test
-	public void testConstructeurBaseParcelleSlashes() throws Exception {
-		assertParcelle(123, null, null, null, new MigrationParcelle("123", null, null));
-		assertParcelle(123, 34, null, null, new MigrationParcelle("123/34", null, null));
-		assertParcelle(123, null, null, null, new MigrationParcelle("123", null, "34"));
-		assertParcelle(123, 34, 4, null, new MigrationParcelle("123/34/4", null, null));
-		assertParcelle(123, 34, 4, 1, new MigrationParcelle("123/34/4/1", null, null));
-	}
-
-	/**
-	 * Ce test vérifie que le numéro de base de la parcelle est ignoré si le numéro de parcelle est spécifié. Version "Tiret"
-	 */
-	@Test
-	public void testConstructeurBaseNoParcelleEtNoParcelleTirets() throws Exception {
+	public void testConstructeurBaseNoParcelleEtNoParcelle() throws Exception {
 		assertParcelle(123, null, null, null, new MigrationParcelle("100", "123", null));
 		assertParcelle(123, 34, null, null, new MigrationParcelle("100", "123-34", null));
 		assertParcelle(123, null, null, null, new MigrationParcelle("100", "123", "34"));
@@ -42,24 +29,12 @@ public class MigrationParcelleTest {
 		assertParcelle(123, 34, 4, 1, new MigrationParcelle("100", "123-34-4-1", null));
 	}
 
-	/**
-	 * Ce test vérifie que le numéro de base de la parcelle est ignoré si le numéro de parcelle est spécifié. Version "Slash"
-	 */
-	@Test
-	public void testConstructeurBaseNoParcelleEtNoParcelleSlashes() throws Exception {
-		assertParcelle(123, null, null, null, new MigrationParcelle("100", "123", null));
-		assertParcelle(123, 34, null, null, new MigrationParcelle("100", "123/34", null));
-		assertParcelle(123, null, null, null, new MigrationParcelle("100", "123", "34"));
-		assertParcelle(123, 34, 4, null, new MigrationParcelle("100", "123/34/4", null));
-		assertParcelle(123, 34, 4, 1, new MigrationParcelle("100", "123/34/4/1", null));
-	}
-
 	@Test
 	public void testToString() throws Exception {
-		assertEquals("123/null/null/null", new MigrationParcelle("123", null, null).toString());
-		assertEquals("123/34/null/null", new MigrationParcelle("123-34", null, null).toString());
-		assertEquals("123/34/4/null", new MigrationParcelle("123-34-4", null, null).toString());
-		assertEquals("123/34/4/1", new MigrationParcelle("123-34-4-1", null, null).toString());
+		Assert.assertEquals("123/null/null/null", new MigrationParcelle("123", null, null).toString());
+		Assert.assertEquals("123/34/null/null", new MigrationParcelle("123-34", null, null).toString());
+		Assert.assertEquals("123/34/4/null", new MigrationParcelle("123-34-4", null, null).toString());
+		Assert.assertEquals("123/34/4/1", new MigrationParcelle("123-34-4-1", null, null).toString());
 	}
 
 	@Test
@@ -71,16 +46,27 @@ public class MigrationParcelleTest {
 	}
 
 	@Test
-	public void testNumeroParcelleRessembleADate() {
+	public void testNumeroParcelleRessembleADate() throws Exception {
 		assertParcelle(4871, 4, null, null, new MigrationParcelle("4871-4", "11.22.3333", null));
 		assertParcelle(4871, 3, null, null, new MigrationParcelle("4871-3", "11.mai", null));
 		assertParcelle(48743, 2, null, null, new MigrationParcelle("48743-2", "févr.15", null));
 	}
 
+	@Test
+	public void testSlashNonInterprete() throws Exception {
+		try {
+			new MigrationParcelle("513/1", null, null);
+			Assert.fail();
+		}
+		catch (NumberFormatException e) {
+			Assert.assertEquals("For input string: \"513/1\"", e.getMessage());
+		}
+	}
+
 	private void assertParcelle(int noParcelle, Integer index1, Integer index2, Integer index3, MigrationParcelle parcelle) {
-		assertEquals(noParcelle, parcelle.getNoParcelle());
-		assertEquals(index1, parcelle.getIndex1());
-		assertEquals(index2, parcelle.getIndex2());
-		assertEquals(index3, parcelle.getIndex3());
+		Assert.assertEquals(noParcelle, parcelle.getNoParcelle());
+		Assert.assertEquals(index1, parcelle.getIndex1());
+		Assert.assertEquals(index2, parcelle.getIndex2());
+		Assert.assertEquals(index3, parcelle.getIndex3());
 	}
 }
