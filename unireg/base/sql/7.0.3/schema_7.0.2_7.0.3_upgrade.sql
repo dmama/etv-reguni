ab2ce130704f25d5375b051cdb3d1303054e006d
Manuel Siggen <manuel.siggen@vd.ch>
2017-02-13 07:06:53 +0100

    Supprimé les scripts SQL de passage à la version 7.0.3 car la version 7.0.2 n'a pas encore été releasée.

--- unireg/base/sql/7.0.3/schema_7.0.2_7.0.3_upgrade.sql
+++
@@ -1,7 +0,0 @@
--- Version
-INSERT INTO VERSION_DB (VERSION_NB, SCRIPT_ID) VALUES ('7.0.3', '7.0.2_7.0.3_upgrade');
-
---
--- [SIFISC-22288] Prise en compte des numéros d'affaire en text libre
---
-ALTER TABLE RF_DROIT MODIFY NO_AFFAIRE NVARCHAR2(40) DEFAULT NULL;
