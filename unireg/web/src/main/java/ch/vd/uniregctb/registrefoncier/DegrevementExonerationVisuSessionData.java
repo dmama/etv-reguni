055303d20dcdadd4a46f0cd86eddb2f21cb771fa
Jacob Deker <jacob.deker@vd.ch>
2017-03-24 13:16:48 +0100

    [SIFISC-21763] Ajouté la gestion du retour depuis l'édition vers la visualisation des immeubles d'un contribuable

---
+++ unireg/web/src/main/java/ch/vd/uniregctb/registrefoncier/DegrevementExonerationVisuSessionData.java
@@ -0,0 +1,48 @@
+package ch.vd.uniregctb.registrefoncier;
+
+import java.io.Serializable;
+
+public class DegrevementExonerationVisuSessionData implements Serializable {
+
+	private static final long serialVersionUID = -5184869724787189904L;
+	
+	private final long idContribuable;
+	private final int ofsCommune;
+	private final Integer noParcelle;
+	private final Integer index1;
+	private final Integer index2;
+	private final Integer index3;
+
+	public DegrevementExonerationVisuSessionData(long idContribuable, int ofsCommune, Integer noParcelle, Integer index1, Integer index2, Integer index3) {
+		this.idContribuable = idContribuable;
+		this.ofsCommune = ofsCommune;
+		this.noParcelle = noParcelle;
+		this.index1 = index1;
+		this.index2 = index2;
+		this.index3 = index3;
+	}
+
+	public long getIdContribuable() {
+		return idContribuable;
+	}
+
+	public int getOfsCommune() {
+		return ofsCommune;
+	}
+
+	public Integer getNoParcelle() {
+		return noParcelle;
+	}
+
+	public Integer getIndex1() {
+		return index1;
+	}
+
+	public Integer getIndex2() {
+		return index2;
+	}
+
+	public Integer getIndex3() {
+		return index3;
+	}
+}
