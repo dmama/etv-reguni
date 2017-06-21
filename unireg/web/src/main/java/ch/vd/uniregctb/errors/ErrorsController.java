400fa8b4f9493e31443610662f62d6c960c98a6b
Manuel Siggen <manuel.siggen@vd.ch>
2017-06-21 08:34:51 +0200

    [code cleanup] Passé le contrôleur d'erreurs au format Spring MVC 3.

---
+++ unireg/web/src/main/java/ch/vd/uniregctb/errors/ErrorsController.java
@@ -0,0 +1,25 @@
+package ch.vd.uniregctb.errors;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorsController {
+
+	@RequestMapping(value = "/error.do")
+	public String error() throws IOException {
+		return "errors/error";
+	}
+
+	@RequestMapping(value = "/403.do")
+	public String forbidden() throws IOException {
+		return "errors/403";
+	}
+
+	@RequestMapping(value = "/404.do")
+	public String notFound() throws IOException {
+		return "errors/404";
+	}
+}
