5cc5be03e8a6c0bd908a4af7065c97877f93633b
Jacob Deker <jacob.deker@vd.ch>
2017-08-02 07:47:38 +0200

    Corrigé le script d'extraction des dépendances SNAPSHOT existantes quand elles doivent être récupérées de NEXUS (= ne sont pas encore téléchargées dans le répertoire .m2 local)

--- unireg/check-snapshot-dependencies.sh
+++ unireg/check-snapshot-dependencies.sh
@@ -7,10 +7,20 @@ cd "$(dirname "$0")"
 CURRENT_VERSION=$(grep "^long=" base/version.txt | sed -e 's/^long=//')
 if [ -z "$CURRENT_VERSION" ]; then
 	echo "Impossible de retrouver le numéro de version courant (depuis le fichier $(pwd)/base/version.txt)" >&2
-	exit 1;
+	exit 1
 fi
 
-# lançons un calcul des dépendances SNAPSHOT
+# lançons un calcul des dépendances SNAPSHOT (après une petite compile silencieuse histoire de tout avoir en local)
+(cd base && mvn install -Pall,not) >/dev/null 2>&1
+ERR_COMPILATION=$?
+if [ $ERR_COMPILATION -ne 0 ]; then
+	if [ $ERR_COMPILATION -gt 128 ]; then
+		echo "Abandon (signal $((ERR_COMPILATION - 128)) reçu)..." >&2
+	else
+		echo "Il semble y avoir un souci de compilation dans le projet..." >&2
+	fi
+	exit $ERR_COMPILATION
+fi
 (cd base && mvn dependency:list) | grep SNAPSHOT | grep -v "\bBuilding Unireg - " | grep -v "uniregctb.*:${CURRENT_VERSION}:" | awk '{ print $2; }' | sort -u | while IFS=: read GROUP_ID ARTIFACT_ID FORMAT VERSION SCOPE; do
 	echo "Dépendance SNAPSHOT trouvée : ${GROUP_ID}/${ARTIFACT_ID}, version ${VERSION} (scope ${SCOPE})"
 done
