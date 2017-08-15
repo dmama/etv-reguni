2c137b29b3b14b8cced872045474e462d6a136f7
Manuel Siggen <manuel.siggen@vd.ch>
2017-08-15 14:10:07 +0200

    Implémenté la possibilité de spécifier plusieurs ordres de tri par défaut dans les paramètres de pagination.

---
+++ unireg/core/src/test/java/ch/vd/uniregctb/common/pagination/ParamPaginationTest.java
@@ -0,0 +1,41 @@
+package ch.vd.uniregctb.common.pagination;
+
+import org.junit.Test;
+
+import ch.vd.uniregctb.dbutils.QueryFragment;
+
+import static org.junit.Assert.assertEquals;
+
+public class ParamPaginationTest {
+
+	@Test
+	public void testBuildOrderClausePasDeSortingParDefaut() throws Exception {
+
+		final ParamPagination pagination = new ParamPagination(1, 20, null, true);
+		final QueryFragment orderClause = pagination.buildOrderClause("test", null);
+
+		assertEquals("order by test.id asc , test.id asc", orderClause.getQuery());
+	}
+
+	@Test
+	public void testBuildOrderClauseunSortingParDefaut() throws Exception {
+
+		final ParamPagination pagination = new ParamPagination(1, 20, null, true);
+		final QueryFragment orderClause = pagination.buildOrderClause("test", null,
+		                                                              new ParamSorting("canton", true));
+
+		assertEquals("order by test.canton asc , test.id asc", orderClause.getQuery());
+	}
+
+	@Test
+	public void testBuildOrderClausePlusieursSortingsParDefaut() throws Exception {
+
+		final ParamPagination pagination = new ParamPagination(1, 20, null, true);
+		final QueryFragment orderClause = pagination.buildOrderClause("test", null,
+		                                                              new ParamSorting("rue", true),
+		                                                              new ParamSorting("commune", false),
+		                                                              new ParamSorting("canton", true));
+
+		assertEquals("order by test.rue asc, test.commune desc, test.canton asc , test.id asc", orderClause.getQuery());
+	}
+}
