5bada167eca9a31268b109bfca908b85c6c71acf
Jacob Deker <jacob.deker@vd.ch>
2017-08-24 06:46:05 +0200

    Corrigé scripts 7.2.1 vers 7.2.3 depuis la branche 7.2

--- unireg/base/sql/7.2.3/schema_7.2.1_7.2.3_upgrade.sql
+++ unireg/base/sql/7.2.3/schema_7.2.1_7.2.3_upgrade.sql
@@ -1,3 +1,5 @@
 -- Version
 INSERT INTO VERSION_DB (VERSION_NB, SCRIPT_ID) VALUES ('7.2.3', '7.2.1_7.2.3_upgrade');
 
+-- SIFISC-24367 : ajouté les surcharges fiscales de communes sur les situations RF.
+ALTER TABLE RF_SITUATION ADD NO_OFS_COMMUNE_SURCHARGE NUMBER(10);
