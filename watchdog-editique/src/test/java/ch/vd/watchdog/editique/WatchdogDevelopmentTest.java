58d70caaa9737d5492afbbae2d22b4d392e61d6f
Manuel Siggen <manuel.siggen@vd.ch>
2017-09-13 11:41:36 +0200

    Supprimé le projet 'watchdog-editique' qui n'est plus utilisé depuis 5 ans.

--- watchdog-editique/src/test/java/ch/vd/watchdog/editique/WatchdogDevelopmentTest.java
+++
@@ -1,7 +0,0 @@
-package ch.vd.watchdog.editique;
-
-import org.springframework.test.context.ContextConfiguration;
-
-@ContextConfiguration(locations = "classpath:development.xml")
-public class WatchdogDevelopmentTest extends WatchdogTest {
-}
--- watchdog-editique/src/test/java/ch/vd/watchdog/editique/WatchdogIntegrationTest.java
+++
@@ -1,7 +0,0 @@
-package ch.vd.watchdog.editique;
-
-import org.springframework.test.context.ContextConfiguration;
-
-@ContextConfiguration(locations = "classpath:integration.xml")
-public class WatchdogIntegrationTest extends WatchdogTest {
-}
