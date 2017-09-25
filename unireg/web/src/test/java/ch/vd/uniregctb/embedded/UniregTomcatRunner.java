ab44490343397d694eb080c3b8793e75666323ff
Manuel Siggen <manuel.siggen@vd.ch>
2017-09-25 15:32:59 +0200

    Passé en version 7.3.1-SNAPSHOT pour la 17R4.B et suivantes

--- unireg/web/src/test/java/ch/vd/uniregctb/embedded/UniregTomcatRunner.java
+++ unireg/web/src/test/java/ch/vd/uniregctb/embedded/UniregTomcatRunner.java
@@ -44,7 +44,7 @@ public class UniregTomcatRunner {
 		// Propriétés normallement configurées par Maven et qu'on spécifie à la main parce qu'on pointe vers le répertoire sources (et non pas target).
 
 		// BEGIN_LVERSION
-		System.setProperty("project.version", "7.3.0-SNAPSHOT");
+		System.setProperty("project.version", "7.3.1-SNAPSHOT");
 		// END_LVERSION
 
 		System.setProperty("project.name", "UniregCTB - Web");
