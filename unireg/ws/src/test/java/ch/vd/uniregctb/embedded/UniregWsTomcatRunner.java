82cc4522995c40ae4f89da42a19de8f93f37e16f
Baba NGOM <baba-issa.ngom@vd.ch>
2017-10-26 15:42:02 +0200

    Passage en version 7.3.1 (17R4.B)

--- unireg/ws/src/test/java/ch/vd/uniregctb/embedded/UniregWsTomcatRunner.java
+++ unireg/ws/src/test/java/ch/vd/uniregctb/embedded/UniregWsTomcatRunner.java
@@ -44,7 +44,7 @@ public class UniregWsTomcatRunner {
 		// Propriétés normallement configurées par Maven et qu'on spécifie à la main parce qu'on pointe vers le répertoire sources (et non pas target).
 
 		// BEGIN_LVERSION
-		System.setProperty("project.version", "7.3.1-SNAPSHOT");
+		System.setProperty("project.version", "7.3.1");
 		// END_LVERSION
 
 		System.setProperty("project.name", "UniregCTB - Webservices");
