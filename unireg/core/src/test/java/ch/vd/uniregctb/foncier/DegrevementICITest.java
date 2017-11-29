47f58964885b4aea3e01bc644a8290f5fe559afd
Manuel Siggen <manuel.siggen@vd.ch>
2017-11-29 15:03:55 +0100

    [SIFISC-27250][code cleanup] Utilisé des valeurs BigDecimal avec deux décimales de précision (scale=2) dans le calcul du pourcentage de dégrèvement.

--- unireg/core/src/test/java/ch/vd/uniregctb/foncier/DegrevementICITest.java
+++ unireg/core/src/test/java/ch/vd/uniregctb/foncier/DegrevementICITest.java
@@ -9,23 +9,29 @@ import ch.vd.uniregctb.common.WithoutSpringTest;
 
 public class DegrevementICITest extends WithoutSpringTest {
 
+	// big décimals avec précision de 2 décimales
+	private static final BigDecimal ZERO = BigDecimal.valueOf(0, 2);
+	private static final BigDecimal CENT = BigDecimal.valueOf(10000, 2);
+	private static final BigDecimal SOIXANTE = BigDecimal.valueOf(6000, 2);
+	private static final BigDecimal HUITANTE = BigDecimal.valueOf(8000, 2);
+
 	/**
 	 * [SIFISC-26123] si aucune valeur n'est arrêtée, le dégrèvement final doit être 0% (et non pas une valeur nulle)
 	 */
 	@Test
 	public void testGetPourcentageDegrevementAucuneValeurArretee() throws Exception {
 		final DegrevementICI deg = new DegrevementICI();
-		Assert.assertEquals(BigDecimal.valueOf(0), deg.getPourcentageDegrevement());
+		Assert.assertEquals(ZERO, deg.getPourcentageDegrevement());
 
 		deg.setLoiLogement(new DonneesLoiLogement());
 		deg.setPropreUsage(new DonneesUtilisation());
 		deg.setLocation(new DonneesUtilisation());
-		Assert.assertEquals(BigDecimal.valueOf(0), deg.getPourcentageDegrevement());
+		Assert.assertEquals(ZERO, deg.getPourcentageDegrevement());
 
 		deg.setLoiLogement(new DonneesLoiLogement(Boolean.TRUE, null, null, BigDecimal.TEN));
 		deg.setPropreUsage(new DonneesUtilisation(100L, 100L, 100L, BigDecimal.valueOf(50L), null));
 		deg.setLocation(new DonneesUtilisation(100L, 100L, 100L, BigDecimal.valueOf(50L), null));
-		Assert.assertEquals(BigDecimal.valueOf(0), deg.getPourcentageDegrevement());
+		Assert.assertEquals(ZERO, deg.getPourcentageDegrevement());
 	}
 
 	@Test
@@ -34,7 +40,7 @@ public class DegrevementICITest extends WithoutSpringTest {
 		deg.setPropreUsage(new DonneesUtilisation(100L, 100L, 100L, BigDecimal.valueOf(80L), BigDecimal.valueOf(60L)));
 		deg.setLocation(new DonneesUtilisation(167L, 167L, 167L, BigDecimal.valueOf(20L), BigDecimal.valueOf(40L)));
 
-		Assert.assertEquals(BigDecimal.valueOf(60L).stripTrailingZeros(), deg.getPourcentageDegrevement());
+		Assert.assertEquals(SOIXANTE, deg.getPourcentageDegrevement());
 	}
 
 	@Test
@@ -45,7 +51,7 @@ public class DegrevementICITest extends WithoutSpringTest {
 		deg.setLoiLogement(new DonneesLoiLogement(Boolean.TRUE, date(2009, 1, 1), null, BigDecimal.valueOf(50L)));
 
 		// 60% + (50% * 40%) = 80%
-		Assert.assertEquals(BigDecimal.valueOf(80L).stripTrailingZeros(), deg.getPourcentageDegrevement());
+		Assert.assertEquals(HUITANTE, deg.getPourcentageDegrevement());
 	}
 
 	@Test
@@ -55,7 +61,7 @@ public class DegrevementICITest extends WithoutSpringTest {
 		deg.setLocation(new DonneesUtilisation(167L, 167L, 167L, BigDecimal.valueOf(20L), BigDecimal.valueOf(40L)));
 		deg.setLoiLogement(new DonneesLoiLogement(Boolean.FALSE, date(2009, 1, 1), null, BigDecimal.valueOf(50L)));
 
-		Assert.assertEquals(BigDecimal.valueOf(60L).stripTrailingZeros(), deg.getPourcentageDegrevement());
+		Assert.assertEquals(SOIXANTE, deg.getPourcentageDegrevement());
 	}
 
 	@Test
@@ -65,7 +71,7 @@ public class DegrevementICITest extends WithoutSpringTest {
 		deg.setLocation(new DonneesUtilisation(167L, 167L, 167L, BigDecimal.valueOf(20L), null));
 		deg.setLoiLogement(new DonneesLoiLogement(Boolean.FALSE, null, null, null));
 
-		Assert.assertEquals(BigDecimal.valueOf(60L).stripTrailingZeros(), deg.getPourcentageDegrevement());
+		Assert.assertEquals(SOIXANTE, deg.getPourcentageDegrevement());
 	}
 
 	@Test
@@ -75,7 +81,7 @@ public class DegrevementICITest extends WithoutSpringTest {
 		deg.setLocation(new DonneesUtilisation(167L, 167L, 167L, BigDecimal.valueOf(20L), BigDecimal.valueOf(40L)));
 		deg.setLoiLogement(new DonneesLoiLogement(Boolean.FALSE, null, null, null));
 
-		Assert.assertEquals(BigDecimal.valueOf(60L).stripTrailingZeros(), deg.getPourcentageDegrevement());
+		Assert.assertEquals(SOIXANTE, deg.getPourcentageDegrevement());
 	}
 
 	@Test
@@ -86,7 +92,7 @@ public class DegrevementICITest extends WithoutSpringTest {
 		deg.setLoiLogement(new DonneesLoiLogement(Boolean.TRUE, date(2009, 1, 1), null, BigDecimal.valueOf(70L)));
 
 		// 60% + (70% * 80%) = 116% cappé à 100%
-		Assert.assertEquals(BigDecimal.valueOf(100L).stripTrailingZeros(), deg.getPourcentageDegrevement());
+		Assert.assertEquals(CENT, deg.getPourcentageDegrevement());
 	}
 
 }
