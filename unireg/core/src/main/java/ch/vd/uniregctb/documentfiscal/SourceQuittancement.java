ea0c511aed93d635baf8e673b5b8526535247651
Raphaël Marmier <raphael.marmier@vd.ch>
2017-12-08 18:42:08 +0100

    [SIFISC-21866] Cycle de vie LB et DDICI - Source de quittancement de la lettre de bienvenue.

---
+++ unireg/core/src/main/java/ch/vd/uniregctb/documentfiscal/SourceQuittancement.java
@@ -0,0 +1,34 @@
+package ch.vd.uniregctb.documentfiscal;
+
+/**
+ * [SIFISC-1782] La source du quittancement de la déclaration (CEDI, ADDI ou manuel).
+ *
+ * @author Raphaël Marmier, 2017-12-08, <raphael.marmier@vd.ch>
+ */
+public interface SourceQuittancement {
+
+	/**
+	 * Nom de la source en cas de quittance à travers l'interface web d'Unireg.
+	 */
+	public static final String SOURCE_WEB = "WEB";
+
+	/**
+	 * Nom de la source en cas de quittance automatique des DIs des contribuables indigents.
+	 */
+	public static final String SOURCE_INDIGENT = "INDIGENT";
+
+	/**
+	 * Nom de la source en cas de quittance par le web-service lorsque la source n'est pas explicitement spécifiée (= anciens clients).
+	 */
+	public static final String SOURCE_CEDI = "CEDI";
+
+	/**
+	 * @return la source de quittancement
+	 */
+	String getSource();
+
+	/**
+	 * @param source la source de quittancement
+	 */
+	void setSource(String source);
+}
