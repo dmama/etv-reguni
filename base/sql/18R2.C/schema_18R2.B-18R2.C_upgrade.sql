7708c048f2857ebafef850dd38dcc937cda633e3
Manuel Siggen <manuel.siggen@vd.ch>
2018-05-16 08:55:55 +0200

    [SIFISC-28193] Corrigé le script de rattrapage SQL complémentaire sur l'état des documents pour supprimer le décalage de 3 jours qui ne tient pas compte des jours ouvrés (on préfère avoir les deux dates identiques dans ce cas).

--- base/sql/18R2.C/schema_18R2.B-18R2.C_upgrade.sql
+++ base/sql/18R2.C/schema_18R2.B-18R2.C_upgrade.sql
@@ -1,5 +1,5 @@
 -- Version
 INSERT INTO VERSION_DB (VERSION_NB, SCRIPT_ID) VALUES ('18R2.C', '18R2.B_18R2.C_upgrade');
 
--- [SIFISC-28193] rattrapage des 442 documents dont la date date d'envoi du courrier est antérieur à la date d'obtention calculée par le script de migration de la 18R2.B
-update ETAT_DOCUMENT_FISCAL set DATE_OBTENTION = TO_NUMBER(TO_CHAR(TO_DATE(DATE_ENVOI_COURRIER, 'YYYYMMDD') - 3, 'YYYYMMDD')) where DATE_ENVOI_COURRIER < DATE_OBTENTION;
+-- [SIFISC-28193] rattrapage des 442 documents dont la date d'envoi du courrier est antérieur à la date d'obtention calculée par le script de migration de la 18R2.B
+update ETAT_DOCUMENT_FISCAL set DATE_OBTENTION = DATE_ENVOI_COURRIER, LOG_MDATE = CURRENT_DATE, LOG_MUSER = 'SQL-SIFISC-28193' where DATE_ENVOI_COURRIER < DATE_OBTENTION;
