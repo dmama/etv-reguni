f09174d46b5a1a4ddb81263676ff496139e2ebcb
Manuel Siggen <manuel.siggen@vd.ch>
2018-05-29 15:09:36 +0200

    Version 18R2.C.0

--- web/src/test/java/ch/vd/unireg/embedded/UniregTomcatRunner.java
+++ web/src/test/java/ch/vd/unireg/embedded/UniregTomcatRunner.java
@@ -44,7 +44,7 @@ public class UniregTomcatRunner {
 		// Propriétés normallement configurées par Maven et qu'on spécifie à la main parce qu'on pointe vers le répertoire sources (et non pas target).
 
 		// BEGIN_LVERSION
-		System.setProperty("project.version", "18R2.C.0-SNAPSHOT");
+		System.setProperty("project.version", "18R2.C.0");
 		// END_LVERSION
 
 		System.setProperty("project.name", "Unireg - Web");
