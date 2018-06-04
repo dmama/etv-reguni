427a52c50560480f576e99032460beadac007f50
Baba NGOM <baba-issa.ngom@vd.ch>
2018-06-04 11:42:00 +0200

    [FISCPROJ-23] QSNC Changer le numéro de version XSD supportéon en est à la 16.12

--- business/src/main/java/ch/vd/unireg/editique/EditiqueAbstractHelperImpl.java
+++ business/src/main/java/ch/vd/unireg/editique/EditiqueAbstractHelperImpl.java
@@ -66,7 +66,7 @@ public abstract class EditiqueAbstractHelperImpl implements EditiqueAbstractHelp
 	public static final String IMPOT_COMPLEMENTAIRE_IMMEUBLES = "IMPÔT COMPLÉMENTAIRE SUR LES IMMEUBLES";
 	public static final String CODE_PORTE_ADRESSE_MANDATAIRE = "M";
 
-	public static final String VERSION_XSD = "16.11";
+	public static final String VERSION_XSD = "16.12";
 
 	public static final String TYPE_DOCUMENT_CO = "CO";     // pour "courrier", apparemment
 	public static final String TYPE_DOCUMENT_DI = "DI";
