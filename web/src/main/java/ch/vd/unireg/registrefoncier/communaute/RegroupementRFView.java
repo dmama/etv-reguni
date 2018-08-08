1ffa0d857b15aaa80228a41a85c1bc268ca7d86c
Manuel Siggen <manuel.siggen@vd.ch>
2018-08-08 11:03:56 +0200

    [IMM-1271] Corrigé un crash lorsqu'on essayait d'afficher une communauté avec plusieurs droits (situation valide pour autant que tous les droits pointent vers le même immeuble).

--- web/src/main/java/ch/vd/unireg/registrefoncier/communaute/RegroupementRFView.java
+++ web/src/main/java/ch/vd/unireg/registrefoncier/communaute/RegroupementRFView.java
@@ -1,11 +1,15 @@
 package ch.vd.unireg.registrefoncier.communaute;
 
-import java.util.Set;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.NotNull;
 
 import ch.vd.registre.base.date.RegDate;
+import ch.vd.unireg.common.AnnulableHelper;
+import ch.vd.unireg.registrefoncier.CommunauteRF;
 import ch.vd.unireg.registrefoncier.DroitProprieteRF;
+import ch.vd.unireg.registrefoncier.ImmeubleRF;
 import ch.vd.unireg.registrefoncier.RegistreFoncierService;
 import ch.vd.unireg.registrefoncier.RegroupementCommunauteRF;
 
@@ -19,14 +23,29 @@ public class RegroupementRFView {
 	public RegroupementRFView(@NotNull RegroupementCommunauteRF r, @NotNull RegistreFoncierService registreFoncierService) {
 		this.dateDebut = r.getDateDebut();
 		this.dateFin = r.getDateFin();
-		this.communauteId = r.getCommunaute().getId();
-		final Set<DroitProprieteRF> droits = r.getCommunaute().getDroitsPropriete();
-		if (droits.size() > 1) {
-			// par définition, une communauté ne possède qu'un seul droit
-			throw new IllegalArgumentException("La communauté n°" + r.getCommunaute().getId() + " possède plusieurs droits");
+		final CommunauteRF communaute = r.getCommunaute();
+		this.communauteId = communaute.getId();
+		final ImmeubleRF immeuble = getImmeuble(communaute);
+		this.immeuble = new ImmeubleRFView(immeuble, registreFoncierService);
+	}
+
+	/**
+	 * @param communaute une communauté
+	 * @return l'immeuble possédé par la communauté
+	 */
+	@NotNull
+	private static ImmeubleRF getImmeuble(@NotNull CommunauteRF communaute) {
+		// [IMM-1271] une communauté peut posséder plusieurs droits, mais ils doivent tous pointer vers le même immeuble
+		final List<ImmeubleRF> immeubles = communaute.getDroitsPropriete().stream()
+				.filter(AnnulableHelper::nonAnnule)
+				.map(DroitProprieteRF::getImmeuble)
+				.distinct()
+				.collect(Collectors.toList());
+		if (immeubles.size() > 1) {
+			// par définition, une communauté ne possède qu'un seul immeuble
+			throw new IllegalArgumentException("La communauté n°" + communaute.getId() + " possède plusieurs immeubles (incohérence des données)");
 		}
-		final DroitProprieteRF droit = droits.iterator().next();
-		this.immeuble = new ImmeubleRFView(droit.getImmeuble(), registreFoncierService);
+		return immeubles.iterator().next();
 	}
 
 	public RegDate getDateDebut() {
