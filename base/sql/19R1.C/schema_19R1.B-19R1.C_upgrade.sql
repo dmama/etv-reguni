6c4dd2f9309dd7c34e4a7e292b5f3f1b8d7a4f8b
Manuel Siggen <manuel.siggen@vd.ch>
2019-01-30 13:32:55 +0100

    [FISCPROJ-983] Ajouté le script de rattrapage du type de délai sur les premiers délais créés par des zaixxx.

--- base/sql/19R1.C/schema_19R1.B-19R1.C_upgrade.sql
+++ base/sql/19R1.C/schema_19R1.B-19R1.C_upgrade.sql
@@ -1,2 +1,16 @@
 -- Version
 INSERT INTO VERSION_DB (VERSION_NB, SCRIPT_ID) VALUES ('19R1.C', '19R1.B_19R1.C_upgrade');
+
+-- FISCPROJ-983 : rattrapage du type de délai sur les premiers délais créés par des zaixxx
+update DELAI_DOCUMENT_FISCAL
+set TYPE_DELAI = 'IMPLICITE'
+where ID in (select d.ID
+             from DELAI_DOCUMENT_FISCAL d
+                      left join (select min(id) as MIN_ID, DOCUMENT_FISCAL_ID
+                                 from DELAI_DOCUMENT_FISCAL
+                                 where DISCRIMINATOR = 'DELAI_DECLARATION'
+                                   and ANNULATION_DATE is null
+                                 group by DOCUMENT_FISCAL_ID)
+                                on d.id = MIN_ID
+             where TYPE_DELAI = 'EXPLICITE'
+               and (LOG_CUSER like 'ZAI%' or LOG_CUSER like 'zai%'));
