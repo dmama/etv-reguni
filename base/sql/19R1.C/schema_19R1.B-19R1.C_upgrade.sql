9b0d8e770cec64674538fa2a16612c72828db412
Manuel Siggen <manuel.siggen@vd.ch>
2019-02-05 10:31:01 +0100

    [FISCPROJ-1062] Modifié le script de rattrapage du type de délai pour :  - passer le premier délai de chaque déclaration à 'implicite' (sauf s'il est déjà modifié par e-Délai) ;  - passer tous les autres délais à 'explicite'.

--- base/sql/19R1.C/schema_19R1.B-19R1.C_upgrade.sql
+++ base/sql/19R1.C/schema_19R1.B-19R1.C_upgrade.sql
@@ -1,16 +1,27 @@
 -- Version
 INSERT INTO VERSION_DB (VERSION_NB, SCRIPT_ID) VALUES ('19R1.C', '19R1.B_19R1.C_upgrade');
 
--- FISCPROJ-983 : rattrapage du type de délai sur les premiers délais créés par des zaixxx
+--
+-- FISCPROJ-983/FISCPROJ-1062 : rattrape des types de délai sur tous les délais
+--
+
+-- reset de tous les délais à 'explicite'
 update DELAI_DOCUMENT_FISCAL
-set TYPE_DELAI = 'IMPLICITE'
-where ID in (select d.ID
-             from DELAI_DOCUMENT_FISCAL d
-                      left join (select min(id) as MIN_ID, DOCUMENT_FISCAL_ID
-                                 from DELAI_DOCUMENT_FISCAL
-                                 where DISCRIMINATOR = 'DELAI_DECLARATION'
-                                   and ANNULATION_DATE is null
-                                 group by DOCUMENT_FISCAL_ID)
-                                on d.id = MIN_ID
-             where TYPE_DELAI = 'EXPLICITE'
-               and (LOG_CUSER like 'ZAI%' or LOG_CUSER like 'zai%'));
+set TYPE_DELAI = 'EXPLICITE'
+where DISCRIMINATOR = 'DELAI_DECLARATION';
+
+-- forçage de tous les premiers délais à 'implicite' (sauf ceux déjà modifiés par e-Délai qui doivent rester à 'explicite')
+update DELAI_DOCUMENT_FISCAL d
+set d.TYPE_DELAI = 'IMPLICITE'
+where d.ID in (
+    -- retourne l'id du premier délai de la déclaration (sauf si modifié par e-Délai)
+    select v.ID
+    from (
+             -- retourne la liste des délais par ordre croissant de traitement et numérotés
+             select s.ID, s.LOG_MUSER, s.DATE_TRAITEMENT, ROW_NUMBER() over (order by s.DATE_TRAITEMENT asc) as ROW_INDEX
+             from DELAI_DOCUMENT_FISCAL s
+             where s.DISCRIMINATOR = 'DELAI_DECLARATION'
+               and s.ANNULATION_DATE is null
+               and s.DOCUMENT_FISCAL_ID = d.DOCUMENT_FISCAL_ID) v
+    where v.ROW_INDEX = 1
+      and v.LOG_MUSER != 'JMS-EvtDelaisDeclaration');
