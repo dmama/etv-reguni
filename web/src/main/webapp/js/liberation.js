03694ef4721806a9b0717a6f435bbe02ff299a62
Denis Mama <denis.mama@vd.ch>
2019-03-19 14:48:22 +0100

    [SIFISC-30906] fix IE PopUP de liberation de document fiscal, IE n'interprete pas les variables dynamique des oject JS.

--- web/src/main/webapp/js/liberation.js
+++ web/src/main/webapp/js/liberation.js
@@ -1,47 +1,50 @@
 'use strict';
-var liberation = {'liberation': {}};
+let liberation = {'liberation': {}};
 
 (function (liberation) {
 	liberation.creerModalLiberation = creerModalLiberation;
 
 	function creerModalLiberation(idDI, idModal, action, buttonValidationVariableLabel, titleModal) {
 
-		var dialog = Dialog.create_dialog_div(idModal);
+		let dialog = Dialog.create_dialog_div(idModal);
 
 		// charge le contenu de la boîte de dialogue
 		dialog.load(App.curl(action) + '?id=' + idDI + '&' + new Date().getTime());
+
+		let modalButtons = {};
+		modalButtons[buttonValidationVariableLabel] = validateButtonFn(buttonValidationVariableLabel, action, dialog);
+		modalButtons["Annuler"] = function () {
+			dialog.dialog("close");
+		};
 
 		dialog.dialog({
 			              title: titleModal,
 			              height: 220,
 			              width: 500,
 			              modal: true,
-			              buttons: {
-				              [buttonValidationVariableLabel]: function (event) {
-					              // les boutons ne font pas partie de la boîte de dialogue (au niveau du DOM), on peut donc utiliser le sélecteur jQuery normal
-
-					              var form = dialog.find('#formLiberation');
-					              var motif = form.find('textarea[id=motifValue]').val();
-					              if (motif === null || motif === "") {
-						              alert('Veuillez préciser le motif de liberation de la DI');
-					              }
-					              else {
-						              var buttons = $('.ui-button');
-						              buttons.each(function () {
-							              if ($(this).text() === buttonValidationVariableLabel) {
-								              $(this).addClass('ui-state-disabled');
-								              $(this).attr('disabled', true);
-							              }
-						              });
-						              form.attr('action', App.curl(action));
-						              form.submit();
-					              }
-
-				              },
-				              "Annuler": function () {
-					              dialog.dialog("close");
-				              }
-			              }
+			              buttons: modalButtons
 		              });
+	}
+
+	function validateButtonFn(buttonLabel, action, popUpDialog) {
+		// les boutons ne font pas partie de la boîte de dialogue (au niveau du DOM), on peut donc utiliser le sélecteur jQuery normal
+
+		let form = popUpDialog.find('#formLiberation');
+		let motif = form.find('textarea[id=motifValue]').val();
+		if (motif === null || motif === "") {
+			alert('Veuillez préciser le motif de liberation de la DI');
+		}
+		else {
+			var buttons = $('.ui-button');
+			buttons.each(function () {
+				if ($(this).text() === buttonLabel) {
+					$(this).addClass('ui-state-disabled');
+					$(this).attr('disabled', true);
+				}
+			});
+			form.attr('action', App.curl(action));
+			form.submit();
+		}
+
 	}
 })(liberation);
