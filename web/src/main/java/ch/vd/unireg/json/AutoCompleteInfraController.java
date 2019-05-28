fd601278ec0770e9f497dec5e14e41134b5af3db
Denis Mama <denis.mama@vd.ch>
2019-05-28 10:11:47 +0200

    [SIFISC-31302] la liste des OID n'est plus triée dans l'ordre alphabétique

--- web/src/main/java/ch/vd/unireg/json/AutoCompleteInfraController.java
+++ web/src/main/java/ch/vd/unireg/json/AutoCompleteInfraController.java
@@ -3,6 +3,7 @@ package ch.vd.unireg.json;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
@@ -168,6 +169,7 @@ public class AutoCompleteInfraController {
 						list.add(new AutoCompleteItem(c.getNomCourt(), c.getNomCourt(), String.valueOf(c.getNoColAdm())));
 					}
 				}
+				list.sort(Comparator.comparing(AutoCompleteItem::getDesc));
 			}
 		}
 
